19390a121b9d5471344abcee8e8a0afe98ecbc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:22 +0000

    Add package nanorequests/0.3/nanorequests-0.3.tar.gz

---
+++ packages/nanorequests/nanorequests-0.3.tar.gz/nanorequests-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nanorequests',
+    version='0.3',
+    license='MIT',
+    author="Viral Parmar",
+    description='The simplest REST requests package for quick API integration',
+    author_email='viralparmarme@gmail.com',
+    url='https://github.com/viralparmarme/nanorequests',
+    keywords='nanorequests viralparmarme viralparmar',
+    py_modules=['nanorequests'],
+    install_requires=[
+        'requests'
+    ],
+)
