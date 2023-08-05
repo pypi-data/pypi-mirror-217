e35a0f00924cce2f67c8f49172fd89aba5d83112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:10 +0000

    Add package scriptl/0.2/scriptl-0.2.tar.gz

---
+++ packages/scriptl/scriptl-0.2.tar.gz/scriptl-0.2/scriptl/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+if "SCRIPTL_HIDE_MSG" not in os.environ:
+	print("\u001b[0mThanks for using Scriptl in your project! Find out more at https://pypi.org/project/scriptl.")
+
+from scriptl import console
