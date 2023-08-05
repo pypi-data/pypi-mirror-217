778e280a5be73ea2b4877a2e5cb6390cec3e5f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:28 +0000

    Add package fogLedger/2.0.0/fogLedger-2.0.0.tar.gz

---
+++ packages/fogLedger/fogLedger-2.0.0.tar.gz/fogLedger-2.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fogLedger",
+    version="2.0.0",
+    description='Plugin to build DLTs in Fogbed.',
+    long_description='Plugin to build DLT in Fogbed. Suport Hyperledger Indy',
+    keywords=['networking', 'emulator', 'protocol', 'Internet', 'dlt', 'indy', 'fog'],
+    url='https://github.com/larsid/FogLedger/tree/v2.0.0',
+    author='Matheus Nascimento',
+    author_email='matheusnascimentoti99@gmail.com',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'numpy>=1.24.2',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
