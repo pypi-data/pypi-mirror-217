d6b2599feb894286d76fd8b70c1df6d0fd967c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:15 +0000

    Add package chat-box-streamlit/0.0.9/chat-box-streamlit-0.0.9.tar.gz

---
+++ packages/chat-box-streamlit/chat-box-streamlit-0.0.9.tar.gz/chat-box-streamlit-0.0.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from pathlib import Path
+
+
+setuptools.setup(
+    name="chat-box-streamlit",
+    version="0.0.9",
+    author="SSK-14",
+    author_email="sanjaykumar1481999@gmail.com",
+    description="Seamlessly visualize engaging conversations in a sleek ChatBox.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SSK-14/Streamlit-Chatbox",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+)
