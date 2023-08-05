8f52bb92577de98a892e9434a2f97464b6ab6fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:38 +0000

    Add package gdsfactory/6.112.0/gdsfactory-6.112.0.tar.gz

---
+++ packages/gdsfactory/gdsfactory-6.112.0.tar.gz/gdsfactory-6.112.0/gdsfactory/samples/21_add_fiber_array.py
@@ -0,0 +1,19 @@
+"""You can route all component optical ports to a fiber array."""
+
+from __future__ import annotations
+
+import gdsfactory as gf
+from gdsfactory.samples.big_device import big_device
+
+
+def test_big_device() -> None:
+    component = big_device(nports=10)
+    radius = 5.0
+    assert gf.routing.add_fiber_array(
+        component=component, radius=radius, fanout_length=50.0
+    )
+
+
+if __name__ == "__main__":
+    c = test_big_device()
+    c.show(show_ports=True)
---
+++ packages/gdsfactory/gdsfactory-6.112.0.tar.gz/gdsfactory-6.112.0/gdsfactory/samples/22_add_fiber_single.py
@@ -0,0 +1,17 @@
+"""You can also connect a component with single fiber INPUT and OUTPUTS (no fiber array)."""
+
+from __future__ import annotations
+
+import gdsfactory as gf
+from gdsfactory.samples.big_device import big_device
+
+
+def test_fiber_single() -> None:
+    w = h = 18 * 50
+    c = big_device(spacing=50.0, size=(w, h))
+    assert gf.routing.add_fiber_single(component=c)
+
+
+if __name__ == "__main__":
+    c = test_fiber_single()
+    c.show(show_ports=True)
