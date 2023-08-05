4c85f34236f348045ba9a8d79c2939570a6c90b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:13 +0000

    Add package pythresh/0.3.2/pythresh-0.3.2.tar.gz

---
+++ packages/pythresh/pythresh-0.3.2.tar.gz/pythresh-0.3.2/pythresh/thresholds/base.py
@@ -0,0 +1,38 @@
+import abc
+
+
+class BaseThresholder(metaclass=abc.ABCMeta):
+    """Abstract class for all outlier detection thresholding algorithms.
+
+       Parameters
+       ----------
+
+       Attributes
+       ----------
+
+       thresh_ : threshold value that separates inliers from outliers
+    """
+
+    @abc.abstractmethod
+    def __init__(self):
+
+        self.thresh_ = None
+        self.confidence_interval_ = None
+
+    @abc.abstractmethod
+    def eval(self, decision):
+        """Outlier/inlier evaluation process for decision scores.
+
+        Parameters
+        ----------
+        decision : np.array or list of shape (n_samples)
+                   which are the decision scores from a
+                   outlier detection.
+
+        Returns
+        -------
+        outlier_labels : numpy array of shape (n_samples,)
+            For each observation, tells whether or not
+            it should be considered as an outlier according to the
+            fitted model. 0 stands for inliers and 1 for outliers.
+        """
---
+++ packages/pythresh/pythresh-0.3.2.tar.gz/pythresh-0.3.2/pythresh/thresholds/thresh_utility.py
@@ -0,0 +1,37 @@
+import numpy as np
+import scipy.stats as stats
+from scipy.special import ndtr
+
+
+def normalize(data):
+
+    return ((data - data.min()) / (data.max() - data.min()))
+
+
+def cut(decision, limit):
+
+    labels = np.zeros(len(decision), dtype=int)
+
+    labels[decision >= limit] = 1
+
+    return labels
+
+
+def gen_kde(data, lower, upper, size):
+
+    # Create a KDE of the data
+    kde = stats.gaussian_kde(data)
+    dat_range = np.linspace(lower, upper, size)
+
+    return kde(dat_range), dat_range
+
+
+def gen_cdf(data, lower, upper, size):
+
+    # Create a KDE & CDF of the data
+    kde = stats.gaussian_kde(data)
+    dat_range = np.linspace(lower, upper, size)
+    cdf = np.array(tuple(ndtr(np.ravel(item - kde.dataset) / kde.factor).mean()
+                         for item in dat_range))
+
+    return cdf, dat_range
