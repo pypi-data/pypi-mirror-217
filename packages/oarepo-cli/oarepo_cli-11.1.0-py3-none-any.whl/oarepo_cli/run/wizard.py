b40e792a0ce19b4f664fb31f6162920ef3b9c2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:43 +0000

    Add package oarepo-cli/11.1.0/oarepo_cli-11.1.0-py3-none-any.whl

---
+++ packages/oarepo-cli/oarepo_cli-11.1.0-py3-none-any.whl/oarepo_cli/run/wizard.py
@@ -0,0 +1,14 @@
+from oarepo_cli.wizard import Wizard
+from oarepo_cli.wizard.docker import DockerRunner
+
+from .steps.run_server import RunServerStep
+
+
+class RunSiteWizard(Wizard):
+    def __init__(self, runner: DockerRunner, *, site_support):
+        self.site_support = site_support
+        super().__init__(
+            *runner.wrap_docker_steps(
+                RunServerStep(),
+            ),
+        )
