e3a38af7668aa3413ed4201aca440e89b4c97c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:22 +0000

    Add package interaction-devkit/0.1.2/interaction-devkit-0.1.2.tar.gz

---
+++ packages/interaction-devkit/interaction-devkit-0.1.2.tar.gz/interaction-devkit-0.1.2/interaction/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2023, Juanwu Lu <juanwu@purdue.edu>.
+# Released under the BSD-3-Clause license.
+# See https://opensource.org/license/bsd-3-clause/ for licensing details.
+from __future__ import annotations
+
+from . import _version
+
+__version__ = _version.version
+__doc__ = """
+interaction - INTERACTION dataset development toolkit.
+=============================================================
+
+This package provides a set of tools for working with the INTERACTION dataset.
+"""
