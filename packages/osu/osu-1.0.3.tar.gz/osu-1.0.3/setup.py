93c42b8233d493c7a4c3431858ba8283b8c66e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:13 +0000

    Add package osu/1.0.3/osu-1.0.3.tar.gz

---
+++ packages/osu/osu-1.0.3.tar.gz/osu-1.0.3/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="osu",
+    version="1.0.3",
+    author="Lekuru",
+    author_email="contact@lekuru.xyz",
+    description="A python library that emulates the osu! stable client",
+    long_description="osu.py is a python library that emulates part of the online functionality of the osu! stable client.",
+    packages=["osu"],
+    install_requires=["requests", "psutil"],
+    keywords=["osu", "osugame", "python", "bancho"],
+)
