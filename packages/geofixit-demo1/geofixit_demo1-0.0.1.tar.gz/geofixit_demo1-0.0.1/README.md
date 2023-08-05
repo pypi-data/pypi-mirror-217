39f757912719da8714bacad12e7e946a836cb35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:13 +0000

    Add package geofixit-demo1/0.0.1/geofixit_demo1-0.0.1.tar.gz

---
+++ packages/geofixit-demo1/geofixit_demo1-0.0.1.tar.gz/geofixit_demo1-0.0.1/README.md
@@ -0,0 +1,17 @@
+# geofixit_demo1
+
+
+[![image](https://img.shields.io/pypi/v/geofixit_demo1.svg)](https://pypi.python.org/pypi/geofixit_demo1)
+[![image](https://img.shields.io/conda/vn/conda-forge/geofixit_demo1.svg)](https://anaconda.org/conda-forge/geofixit_demo1)
+
+
+**An interactive mapping solution demo.**
+
+
+-   Free software: MIT license
+-   Documentation: https://geofixit.github.io/geofixit_demo1
+    
+
+## Features
+
+-   TODO
---
+++ packages/geofixit-demo1/geofixit_demo1-0.0.1.tar.gz/geofixit_demo1-0.0.1/tests/test_geofixit_demo1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `geofixit_demo1` package."""
+
+
+import unittest
+
+from geofixit_demo1 import geofixit_demo1
+
+
+class TestGeofixit_demo1(unittest.TestCase):
+    """Tests for `geofixit_demo1` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
