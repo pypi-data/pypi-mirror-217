de1af392f4f4fe9d4d00a53ea7b96b0b3a37d92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:21 +0000

    Add package chat-box-streamlit/0.0.92/chat_box_streamlit-0.0.92-py3-none-any.whl

---
+++ packages/chat-box-streamlit/chat_box_streamlit-0.0.92-py3-none-any.whl/chat_box_streamlit/__init__.py
@@ -0,0 +1,37 @@
+import os
+import streamlit as st
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+    _chat_box_streamlit = components.declare_component(
+        "chat_box_streamlit",
+        url="http://localhost:5173",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/dist")
+    _chat_box_streamlit = components.declare_component("chat_box_streamlit", path=build_dir)
+
+
+def st_message(isLeft=True, message="", loading=False, loadingText="Loading...", placeholder="Enter your input", height=500, key=None):
+    component_value =  _chat_box_streamlit(component="message", isLeft=isLeft, message=message, placeholder=placeholder, loading=loading, loadingText=loadingText, height=height, key=key)
+    return component_value
+
+def st_input(placeholder="Enter your input", loading=False, loadingText="Loading...", rows=1, key=None):
+    component_value = _chat_box_streamlit(component="input", placeholder=placeholder, rows=rows, loading=loading, loadingText=loadingText, key=key)
+    return component_value
+
+def st_display_chat(leftMessages = [], rightMessages=[], height=500, rows=1, key=None):
+    component_value = _chat_box_streamlit(component="", leftMessages=leftMessages, rightMessages=rightMessages, rows=rows, height=height, key=key)
+    return component_value
+
+if not _RELEASE:
+    st.title("Chat Box Streamlit")
+    st.write("This is a demo of the chat box component")
+    st.write("### Input:")
+    keyword = st_input(placeholder="Enter your input", loading=False, loadingText="Loading...", rows=1, key=None)
+    st.write("### Messages:")
+    st_display_chat(leftMessages = ["Hello", "How are you?"], rightMessages=["Hi", "I am fine"], height=500, rows=1, key=None)
+    
