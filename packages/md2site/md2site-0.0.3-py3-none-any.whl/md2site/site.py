582c16c81b208605d1f3faadbb700fd5820f8628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:24 +0000

    Add package md2site/0.0.3/md2site-0.0.3-py3-none-any.whl

---
+++ packages/md2site/md2site-0.0.3-py3-none-any.whl/md2site/site.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+
+
+@dataclass
+class PostMetaData:
+    name: str
+    title: str
+    created_at: datetime
+
+
+@dataclass
+class Site:
+    """
+    Site configuration loaded from config.toml.
+    """
+
+    base_url: str
+    name: str
+    link_map: dict[str, str] = field(default_factory=dict)
+    recent_posts: list[PostMetaData] = field(default_factory=list)
