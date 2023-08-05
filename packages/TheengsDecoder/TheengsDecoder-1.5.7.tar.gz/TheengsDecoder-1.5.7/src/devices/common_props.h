3e0c56c6c0586260200439f479391e2c2cd03c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:31 +0000

    Add package TheengsDecoder/1.5.7/TheengsDecoder-1.5.7.tar.gz

---
+++ packages/TheengsDecoder/TheengsDecoder-1.5.7.tar.gz/TheengsDecoder-1.5.7/src/devices/common_props.h
@@ -0,0 +1,70 @@
+#ifndef _DECODER_COMMON_PROPS
+#define _DECODER_COMMON_PROPS
+
+const char* _common_TH_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"hum\":{\"unit\":\"%\",\"name\":\"humidity\"}}}";
+/*R""""(
+{
+   "properties":{
+      "tempc":{
+         "unit":"°C",
+         "name":"temperature"
+      },
+      "hum":{
+         "unit":"%",
+         "name":"humidity"
+      }
+   }
+})"""";*/
+
+const char* _common_BTH_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"hum\":{\"unit\":\"%\",\"name\":\"humidity\"},\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"mac\":{\"unit\":\"string\",\"name\":\"MAC address\"}}}";
+/*
+R""""(
+{
+   "properties":{
+      "tempc":{
+         "unit":"°C",
+         "name":"temperature"
+      },
+      "hum":{
+         "unit":"%",
+         "name":"humidity"
+      },
+      "batt":{
+         "unit":"%",
+         "name":"battery"
+      },
+      "mac":{
+         "unit":"string",
+         "name":"MAC address"
+      }
+   }
+})"""";*/
+
+const char* _common_BVTH_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"hum\":{\"unit\":\"%\",\"name\":\"humidity\"},\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"volt\":{\"unit\":\"V\",\"name\":\"voltage\"},\"mac\":{\"unit\":\"string\",\"name\":\"MAC address\"}}}";
+/*R""""(
+{
+   "properties":{
+      "tempc":{
+         "unit":"°C",
+         "name":"temperature"
+      },
+      "hum":{
+         "unit":"%",
+         "name":"humidity"
+      },
+      "batt":{
+         "unit":"%",
+         "name":"battery"
+      },
+      "volt":{
+         "unit":"V",
+         "name":"voltage"
+      },
+      "mac":{
+         "unit":"string",
+         "name":"MAC address"
+      }
+   }
+})"""";*/
+
+#endif
