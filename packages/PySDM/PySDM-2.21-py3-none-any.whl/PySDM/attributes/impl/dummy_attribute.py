32ca90daddce5948196bf49d8462d7066e6c0715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:57 +0000

    Add package PySDM/2.21/PySDM-2.21-py3-none-any.whl

---
+++ packages/PySDM/PySDM-2.21-py3-none-any.whl/PySDM/attributes/impl/dummy_attribute.py
@@ -0,0 +1,24 @@
+""" logic around `PySDM.attributes.impl.dummy_attribute.DummyAttribute` - parent class
+for do-nothing attributes """
+import numpy as np
+
+from .attribute import Attribute
+
+
+class DummyAttribute(Attribute):
+    def __init__(self, builder, name):
+        super().__init__(builder, name)
+
+    def allocate(self, idx):
+        super().allocate(idx)
+        self.data[:] = np.nan
+
+    def get(self):
+        return self.data
+
+
+def make_dummy_attribute_factory(name):
+    def _factory(builder):
+        return DummyAttribute(builder, name=name)
+
+    return _factory
