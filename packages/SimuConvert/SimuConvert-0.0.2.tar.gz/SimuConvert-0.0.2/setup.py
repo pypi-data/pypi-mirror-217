f2337bb4cbbafdf74bf6af9f3b5dfd60bbfed2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:05 +0000

    Add package SimuConvert/0.0.2/SimuConvert-0.0.2.tar.gz

---
+++ packages/SimuConvert/SimuConvert-0.0.2.tar.gz/SimuConvert-0.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="SimuConvert",
+    version="0.0.2",
+    author="Yanzhong Huang",
+    author_email="yanzhong.huang@outlook.com",
+    description="A package for converting private fund equity data ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yanzhong-Hub/SimuConvert",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    
+    extras_require={
+        "dev": [
+            "pytest>=7.0",
+            "twine>=4.0.2",]
+    },
+    install_requires=[
+        "pandas>=1.5.0",
+        "openpyxl>=3.0.0",
+        ], 
+
+)
