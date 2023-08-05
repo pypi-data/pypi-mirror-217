038b0d42b346bb5b67a2b8227407bdb19052e89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:48 +0000

    Add package aggregate/0.16.1/aggregate-0.16.1-py3-none-any.whl

---
+++ packages/aggregate/aggregate-0.16.1-py3-none-any.whl/aggregate/constants.py
@@ -0,0 +1,20 @@
+# constants
+
+__all__ = ['FIG_W', 'FIG_H', 'WL', 'FONT_SIZE', 'LEGEND_FONT',
+           'PLOT_FACE_COLOR', 'FIGURE_BG_COLOR', 'VALIDATION_EPS']
+
+FIG_W = 3.5
+FIG_H = 2.45
+
+# level used for logging that replaces warning in cases where a warning is not appropriate
+# but was used for debugging purposes. (WL = Warning Level)
+WL = 25
+
+
+FONT_SIZE = 9
+LEGEND_FONT = 'x-small'
+# see https://matplotlib.org/stable/gallery/color/named_colors.html
+PLOT_FACE_COLOR = 'lightsteelblue'
+FIGURE_BG_COLOR = 'aliceblue'
+VALIDATION_EPS = 1e-4
+
---
+++ packages/aggregate/aggregate-0.16.1-py3-none-any.whl/aggregate/extensions/__init__.py
@@ -0,0 +1,23 @@
+# coding: utf-8 -*-
+
+
+# imports
+from . basic import *
+from . case_studies import *
+from . samples import *
+from . test_suite import *
+from . pir_figures import *
+from . figures import *
+from . risk_progression import *
+from . bodoff import *
+from . ft import *
+from . pentagon import Pentagon, mapper, make_possible_pentagons
+
+
+# module level doc-string
+__doc__ = """
+Extensions contains optional, nice to have code that extends basic functionality.
+For example, all CaseStudy material is included here. It is not required for core
+functionality. It is not included in the default import. It is included in the
+default build.
+"""
