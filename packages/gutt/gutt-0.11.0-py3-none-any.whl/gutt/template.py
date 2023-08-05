c549eac48bc8be93296b5232c6dc302c729a2e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:53 +0000

    Add package gutt/0.11.0/gutt-0.11.0-py3-none-any.whl

---
+++ packages/gutt/gutt-0.11.0-py3-none-any.whl/gutt/template.py
@@ -0,0 +1,198 @@
+from typing import List as LIST
+
+import attr
+from asttrs import (
+    AST,
+    Assert,
+    ClassDef,
+    Constant,
+    FunctionDef,
+    ImportFrom,
+    Load,
+    Name,
+    Pass,
+    alias,
+    arg,
+    arguments,
+    stmt,
+)
+
+from gutt.model import Code
+
+from .utils import load_module_by_name
+
+
+class Layout:
+
+    prefix = "Test"
+    ref = AST
+
+    def __init__(self, src: Code):
+
+        self._src = src
+
+    def build(self) -> AST:
+
+        fields = {f.name for f in attr.fields(self.ref)}
+
+        params = {fd: getattr(self, fd) for fd in fields if hasattr(self, fd)}
+
+        return self.ref(**params)
+
+
+class FunctionLayout(Layout):
+
+    prefix = "test_"
+    ref = FunctionDef
+
+    @property
+    def name(self):
+        return f"{self.prefix}{self._src.ast.name}"
+
+    @property
+    def args(self):
+        return arguments()
+
+    @property
+    def body(self):
+        return [
+            ImportFrom(
+                module=self._src.module.name,
+                names=[alias(name=self._src.ast.name)],
+                level=0,
+            ),
+            Pass(),
+        ]
+
+
+class MethodLayout(FunctionLayout):
+    @property
+    def body(self):
+        return [Pass()]
+
+    @property
+    def args(self):
+        return arguments(args=[arg(arg="self")])
+
+
+class ClassLayout(Layout):
+
+    prefix = "Test"
+    ref = ClassDef
+    method_layout = MethodLayout
+
+    @property
+    def setups_teardowns(self):
+        cls_args = arguments(args=[arg(arg="cls")])
+        cls_deco = Name(id="classmethod", ctx=Load())
+
+        setup_class = FunctionDef(
+            name="setup_class",
+            body=[
+                ImportFrom(
+                    module=self._src.module.name,
+                    names=[alias(name=self._src.ast.name)],
+                    level=0,
+                ),
+                Assert(test=Name(id=self._src.ast.name, ctx=Load())),
+            ],
+            args=cls_args,
+            decorator_list=[cls_deco],
+        )
+        teardown_class = FunctionDef(
+            name="teardown_class",
+            args=cls_args,
+            body=[Pass()],
+            decorator_list=[cls_deco],
+        )
+
+        self_args = arguments(args=[arg(arg="self"), arg(arg="method")])
+
+        setup_method = FunctionDef(name="setup_method", args=self_args, body=[Pass()])
+        teardown_method = FunctionDef(
+            name="teardown_method", args=self_args, body=[Pass()]
+        )
+
+        return [setup_class, teardown_class, setup_method, teardown_method]
+
+    @property
+    def methods(self):
+        methods = []
+        for m in self._src.ast.body:
+            if (isinstance(m, FunctionDef)) and (not m.name.startswith("__")):
+                methods.append(self.method_layout(self._src.evolve(ast=m)).build())
+
+        return methods
+
+    @property
+    def name(self) -> str:
+        return f"{self.prefix}{self._src.ast.name}"
+
+    @property
+    def body(self) -> LIST[stmt]:
+        return self.setups_teardowns + self.methods
+
+
+class Template:
+
+    class_layout = ClassLayout
+    function_layout = FunctionLayout
+
+    @classmethod
+    def load(cls, name: str):
+
+        modname, qname = name.rsplit(".", 1)
+
+        mod = load_module_by_name(modname)
+
+        return getattr(mod, qname)
+
+
+class AssertFalseFunctionLayout(FunctionLayout):
+    @property
+    def body(self):
+        return [
+            ImportFrom(
+                module=self._src.module.name,
+                names=[alias(name=self._src.ast.name)],
+                level=0,
+            ),
+            Assert(test=Constant(value=False)),
+        ]
+
+
+class AssertSelfFunctionLayout(FunctionLayout):
+    @property
+    def body(self):
+        return [
+            ImportFrom(
+                module=self._src.module.name,
+                names=[alias(name=self._src.ast.name)],
+                level=0,
+            ),
+            Assert(test=Name(id=self._src.ast.name, ctx=Load())),
+        ]
+
+
+class AssertFalseMethodLayout(MethodLayout):
+    @property
+    def body(self):
+        return [Assert(test=Constant(value=False))]
+
+
+class AssertFalseClassLayout(ClassLayout):
+
+    method_layout = AssertFalseMethodLayout
+
+
+class AssertFalseTemplate(Template):
+
+    function_layout = AssertFalseFunctionLayout
+    class_layout = AssertFalseClassLayout
+
+
+class AssertSelfTemplate(Template):
+    function_layout = AssertSelfFunctionLayout
+
+
+PassTemplate = Template
---
+++ packages/gutt/gutt-0.11.0-py3-none-any.whl/gutt/utils.py
@@ -0,0 +1,129 @@
+import os
+import pathlib
+import pkgutil
+import subprocess as sp
+import sys
+import tempfile
+from contextlib import contextmanager
+from importlib._bootstrap_external import SourceFileLoader
+from types import ModuleType
+from typing import Union
+
+
+def load_module_by_name(modname: str) -> ModuleType:
+    loader: SourceFileLoader = pkgutil.get_loader(modname)
+    return loader.load_module(modname)
+
+
+def blacking(source_code: str):
+
+    with tempfile.NamedTemporaryFile("w", delete=False) as f:
+        f.write(source_code)
+        fname = f.name
+
+    p = sp.Popen(f"cat {fname}".split(), stdout=sp.PIPE)
+
+    out = sp.check_output(f"{sys.executable} -m black -q -".split(), stdin=p.stdout)
+
+    p.wait()
+
+    try:
+        pathlib.Path(fname).unlink()
+    except FileNotFoundError:
+        pass
+
+    return out.decode()
+
+
+def isorting(source_code: str):
+
+    with tempfile.NamedTemporaryFile("w", delete=False) as f:
+        f.write(source_code)
+        fname = f.name
+
+    p = sp.Popen(f"cat {fname}".split(), stdout=sp.PIPE)
+
+    out = sp.check_output(f"{sys.executable} -m isort -q -".split(), stdin=p.stdout)
+
+    p.wait()
+
+    try:
+        pathlib.Path(fname).unlink()
+    except FileNotFoundError:
+        pass
+
+    return out.decode()
+
+
+@contextmanager
+def expand_sys_path(*paths: str):
+
+    num = len(paths)
+
+    for p in paths[::-1]:
+        if p and isinstance(p, str):
+
+            sys.path.insert(0, p)
+
+    yield
+
+    for _ in range(num):
+        sys.path.pop(0)
+
+
+def makefile(fullpath: str, content: Union[str, bytes] = "", overwrite: bool = False):
+
+    if not os.path.isfile(fullpath):
+
+        dirpath = os.path.dirname(fullpath)
+
+        if not os.path.isdir(dirpath):
+            os.makedirs(dirpath)
+
+        _writefile(fullpath, content)
+
+    elif overwrite:
+
+        _writefile(fullpath, content)
+
+
+def _writefile(fullpath: str, content: Union[str, bytes] = ""):
+
+    if not isinstance(content, (str, bytes)):
+        raise TypeError(f"content must be str or bytes, got: {type(content)}")
+
+    with open(fullpath, "wb" if isinstance(content, bytes) else "w") as f:
+        f.write(content)
+
+
+@contextmanager
+def catch_module_from_sys(modname: str):
+
+    m = sys.modules.pop(modname, None)
+
+    yield
+
+    if m is not None:
+        sys.modules[modname] = m
+
+
+def populate_init(folder: str):
+
+    root = pathlib.Path(folder)
+    paths = {root}
+
+    for path in root.glob("**/*.py"):
+        if path.name == "__init__.py":
+            continue
+
+        parent = path.parent
+
+        while parent > root:
+
+            if not parent.joinpath("__init__.py").exists():
+                paths.add(parent)
+
+            parent = parent.parent
+
+    for p in paths:
+        makefile(os.path.join(str(p), "__init__.py"))
