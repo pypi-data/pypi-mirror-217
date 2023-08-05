0410787c995fb690329eba0b43164e4bc2d3d218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:17 +0000

    Add package pyhafas/0.4.0/pyhafas-0.4.0-py3-none-any.whl

---
+++ packages/pyhafas/pyhafas-0.4.0-py3-none-any.whl/pyhafas/profile/__init__.py
@@ -0,0 +1,6 @@
+from .interfaces import ProfileInterface  # isort:skip
+from .base import BaseProfile
+from .db import DBProfile
+from .vsn import VSNProfile
+from .rkrp import RKRPProfile
+from .kvb import KVBProfile
