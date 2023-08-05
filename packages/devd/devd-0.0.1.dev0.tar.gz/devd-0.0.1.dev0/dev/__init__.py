ddda74dd4ad9d55856cb657483e06302784dda4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:33 +0000

    Add package devd/0.0.1.dev0/devd-0.0.1.dev0.tar.gz

---
+++ packages/devd/devd-0.0.1.dev0.tar.gz/devd-0.0.1.dev0/dev/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+cli for devs
+
+.. currentmodule:: dev
+.. moduleauthor:: Ashish Patel <ashishpatel0720@gmail.com>
+"""
+
+from .version import __version__, __release__  # noqa
