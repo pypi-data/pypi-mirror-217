37bf0aa367ea3d11e0d496e72c81ff5ee9716c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:32 +0000

    Add package dotstree/0.1.5/dotstree-0.1.5.tar.gz

---
+++ packages/dotstree/dotstree-0.1.5.tar.gz/dotstree-0.1.5/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from pathlib import Path
+
+from setuptools import find_packages
+
+setup(
+    name="dotstree",
+    version="0.1.5",
+    description="Flexible dotfile manager based on directory trees.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://www.github.com/metov/dotstree",
+    author="Azat Akhmetov",
+    author_email="azatinfo@yandex.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "coloredlogs",
+        "docopt",
+        "metovlogs",
+        "PyYAML",
+        "questionary",
+        "tabulate",
+        "tqdm",
+    ],
+    entry_points={
+        "console_scripts": [
+            "dots=dotstree.main:main",
+        ],
+    },
+)
