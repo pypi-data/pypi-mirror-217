d3a62257634749f7badb92bbe8d09e7d799c9b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:48 +0000

    Add package pyzjr/0.0.2/pyzjr-0.0.2.tar.gz

---
+++ packages/pyzjr/pyzjr-0.0.2.tar.gz/pyzjr-0.0.2/pyzjr/setup.py
@@ -0,0 +1,36 @@
+"""
+- author:Auorui(夏天是冰红茶)
+- creation time:2022.10
+- pyzjr is the Codebase accumulated by my python programming.
+  At present, it is only for my personal use. If you want to
+  use it, please contact me. Here are my email and WeChat.
+- WeChat: z15583909992
+- Email: zjricetea@gmail.com
+- Note: Currently still being updated, please refer to the latest version for any changes that may occur
+"""
+from setuptools import setup
+
+setup(name='pyzjr',
+      version='0.0.1',
+      description="Aurui's Visual Processing Library",
+      url='https://github.com/Auorui/pyzjr/tree/main',
+      author='Auorui',
+      author_email='zjricetea@gmail.com',
+      license='MIT',
+      packages=['pyzjr'],
+      zip_safe=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
