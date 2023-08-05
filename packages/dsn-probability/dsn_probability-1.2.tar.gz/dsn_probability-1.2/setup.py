2a86672db7c898bdcd1236dfec465dacabd6adb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:48 +0000

    Add package dsn-probability/1.2/dsn_probability-1.2.tar.gz

---
+++ packages/dsn-probability/dsn_probability-1.2.tar.gz/dsn_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsn_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsn_probability'],
+      author = 'Maryam Sikander',
+      author_email = 'maryamsikander@gmail.com',
+    zip_safe=False)
