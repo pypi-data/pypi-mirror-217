851a321d2c5d34ff32376601dbe4e941d2150357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:41 +0000

    Add package gthnk/0.8.1/gthnk-0.8.1-py3-none-any.whl

---
+++ packages/gthnk/gthnk-0.8.1-py3-none-any.whl/gthnk/__meta__.py
@@ -0,0 +1,7 @@
+# gthnk (c) Ian Dennis Miller
+
+__version__ = '0.8.1'
+__author__ = 'Ian Dennis Miller'
+__email__ = 'ian@iandennismiller.com'
+__url__ = 'http://www.gthnk.com'
+__copyright__ = '(c) Ian Dennis Miller'
