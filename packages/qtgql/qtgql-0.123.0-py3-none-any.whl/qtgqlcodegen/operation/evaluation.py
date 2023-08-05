a08a402d81304fd0a5257907df28f404aab04aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:35 +0000

    Add package qtgql/0.123.0/qtgql-0.123.0-py3-none-any.whl

---
+++ packages/qtgql/qtgql-0.123.0-py3-none-any.whl/qtgqlcodegen/operation/evaluation.py
@@ -0,0 +1,395 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from typing import TYPE_CHECKING
+
+import graphql
+from graphql import OperationDefinitionNode, OperationType, language as gql_lang
+from graphql.language import visitor
+
+from qtgqlcodegen.core.graphql_ref import (
+    has_id_selection,
+    has_typename_selection,
+    inject_id_selection,
+    inject_typename_selection,
+    is_field_node,
+    is_inline_fragment,
+    is_named_type_node,
+    is_nonnull_node,
+    is_operation_def_node,
+)
+from qtgqlcodegen.operation.definitions import (
+    OperationTypeInfo,
+    QtGqlOperationDefinition,
+    QtGqlQueriedField,
+    QtGqlVariableUse,
+)
+from qtgqlcodegen.schema.definitions import (
+    QtGqlFieldDefinition,
+    QtGqlVariableDefinition,
+    SchemaTypeInfo,
+)
+from qtgqlcodegen.schema.evaluation import evaluate_graphql_type
+from qtgqlcodegen.types import (
+    QtGqlInterface,
+    QtGqlList,
+    QtGqlOptional,
+    QtGqlQueriedInterface,
+    QtGqlQueriedObjectType,
+    QtGqlQueriedUnion,
+    QtGqlUnion,
+)
+from qtgqlcodegen.utils import require
+
+if TYPE_CHECKING:
+    from qtgqlcodegen.types import QtGqlObjectType, QtGqlTypeABC
+
+
+def is_type_name_selection(field_node: gql_lang.FieldNode):
+    # typename is not a 'real' selection and is handled with special care.
+    if field_node.name.value == "__typename":
+        return True
+    return False
+
+
+def _evaluate_variable_uses(
+    type_info: OperationTypeInfo,
+    field: QtGqlFieldDefinition,
+    arguments: tuple[gql_lang.ArgumentNode, ...],
+) -> list[QtGqlVariableUse]:
+    ret: list[QtGqlVariableUse] = []
+    for arg in arguments:
+        index = field.index_for_argument(arg.name.value)
+        var_name = arg.value.name.value  # type: ignore[attr-defined]
+        for variable in type_info.variables:
+            if var_name == variable.name:
+                ret.append(
+                    QtGqlVariableUse(argument=(index, field.arguments[index]), variable=variable),
+                )
+    assert len(ret) == len(arguments), "could not find all variable uses"
+    return sorted(ret, key=lambda v: v.argument[0])
+
+
+def _evaluate_variable_node_type(
+    type_info: SchemaTypeInfo,
+    node: graphql.TypeNode,
+) -> QtGqlTypeABC:
+    if nonnull := is_nonnull_node(node):
+        return evaluate_graphql_type(
+            type_info,
+            graphql.type.GraphQLNonNull(
+                type_info.schema_definition.get_type(nonnull.type.name.value),  # type: ignore
+            ),
+        )
+
+    if named_type := is_named_type_node(node):
+        gql_concrete = type_info.schema_definition.get_type(named_type.name.value)
+        assert gql_concrete
+        return evaluate_graphql_type(type_info, gql_concrete)
+    raise NotImplementedError(node, "Type is not supported as a variable ATM")
+
+
+def _evaluate_variable(
+    type_info: SchemaTypeInfo,
+    var: gql_lang.VariableDefinitionNode,
+) -> QtGqlVariableDefinition:
+    return QtGqlVariableDefinition(
+        name=var.variable.name.value,
+        type=_evaluate_variable_node_type(type_info, var.type),
+    )
+
+
+def _evaluate_selection_set_type(
+    type_info: OperationTypeInfo,
+    concrete_type: QtGqlTypeABC,
+    selection_set_node: gql_lang.SelectionSetNode | None,
+    path: str,
+) -> QtGqlTypeABC:
+    ret: QtGqlTypeABC | None = None
+    if not selection_set_node:
+        # these types have no selections
+        assert (
+            concrete_type.is_builtin_scalar
+            or concrete_type.is_custom_scalar
+            or concrete_type.is_enum
+        )
+        ret = concrete_type  # currently there is no need for a "proxied" type.
+
+    elif obj_type := concrete_type.is_object_type:
+        ret = _evaluate_object_type(
+            type_info=type_info,
+            concrete=obj_type,
+            selection_set=selection_set_node,
+            path=path,
+        )
+    elif lst := concrete_type.is_model:
+        ret = _evaluate_list(
+            type_info=type_info,
+            concrete=lst,
+            selection_set=selection_set_node,
+            path=path,
+        )
+    elif interface := concrete_type.is_interface:
+        ret = _evaluate_interface(
+            type_info=type_info,
+            concrete=interface,
+            selection_set=selection_set_node,
+            path=path,
+        )
+    elif is_union := concrete_type.is_union:
+        ret = _evaluate_union(
+            type_info=type_info,
+            concrete=is_union,
+            selection_set=selection_set_node,
+            path=path,
+        )
+    if not ret:  # pragma: no cover
+        raise NotImplementedError(f"type {concrete_type} not supported yet")
+
+    if concrete_type.is_optional:
+        return QtGqlOptional(of_type=ret)
+    return ret
+
+
+def _evaluate_field(
+    type_info: OperationTypeInfo,
+    concrete_field: QtGqlFieldDefinition,
+    field_node: gql_lang.FieldNode,
+    path: str,
+    origin: QtGqlObjectType,
+) -> QtGqlQueriedField:
+    path += concrete_field.name
+    return QtGqlQueriedField(
+        type=_evaluate_selection_set_type(
+            type_info,
+            concrete_field.type,
+            field_node.selection_set,
+            path,
+        ),
+        concrete=concrete_field,
+        variable_uses=_evaluate_variable_uses(type_info, concrete_field, field_node.arguments),
+        origin=origin,
+        type_info=type_info,
+    )
+
+
+def _evaluate_list(
+    type_info: OperationTypeInfo,
+    concrete: QtGqlList,
+    selection_set: gql_lang.SelectionSetNode,
+    path: str,
+) -> QtGqlList:
+    return QtGqlList(
+        of_type=_evaluate_selection_set_type(
+            type_info,
+            concrete_type=concrete.of_type,
+            selection_set_node=selection_set,
+            path=path,
+        ),
+    )
+
+
+def _evaluate_union(
+    type_info: OperationTypeInfo,
+    concrete: QtGqlUnion,
+    selection_set: gql_lang.SelectionSetNode,
+    path: str,
+) -> QtGqlQueriedUnion:
+    choices: defaultdict[str, dict[str, QtGqlQueriedField]] = defaultdict(dict)
+    for selection in selection_set.selections:
+        fragment = is_inline_fragment(selection)
+        assert fragment
+        type_name = fragment.type_condition.name.value
+        # unions support only object types http://spec.graphql.org/October2021/#sec-Unions
+        resolved_type = require(concrete.get_by_name(type_name))
+        if not has_typename_selection(fragment.selection_set):
+            inject_typename_selection(fragment.selection_set)
+        if not has_id_selection(fragment.selection_set) and resolved_type.implements_node:
+            inject_id_selection(fragment.selection_set)
+
+        for selection_node in fragment.selection_set.selections:
+            inner_field_node = require(is_field_node(selection_node))
+            if not is_type_name_selection(inner_field_node):
+                concrete_field = resolved_type.fields_dict[inner_field_node.name.value]
+                __f = _evaluate_field(
+                    type_info=type_info,
+                    concrete_field=concrete_field,
+                    field_node=inner_field_node,
+                    path=path,
+                    origin=resolved_type,
+                )
+                choices[type_name][concrete_field.name] = __f
+
+    return QtGqlQueriedUnion(
+        concrete=concrete,
+        choices=choices,
+    )
+
+
+def _evaluate_interface(
+    type_info: OperationTypeInfo,
+    concrete: QtGqlInterface,
+    selection_set: gql_lang.SelectionSetNode,
+    path: str,  # current path in the query tree.
+) -> QtGqlQueriedInterface:
+    choices: list[QtGqlQueriedObjectType] = []
+
+    # first get all linear selections, these are selection that can be applied to
+    # any of the interface implementors
+    inline_fragments: list[gql_lang.InlineFragmentNode] = []
+    linear_fields: dict[str, QtGqlQueriedField] = {}
+    for field_or_frag in selection_set.selections:
+        if inline_frag := is_inline_fragment(field_or_frag):
+            inline_fragments.append(inline_frag)
+        else:
+            field_node = is_field_node(field_or_frag)
+            assert field_node
+            __f = _evaluate_field(
+                type_info=type_info,
+                concrete_field=concrete.fields_dict[field_node.name.value],
+                field_node=field_node,
+                path=path,
+                origin=concrete,
+            )
+            linear_fields[__f.name] = __f
+    # evaluate type conditions
+    for inline_frag in inline_fragments:
+        type_name = inline_frag.type_condition.name.value
+        # no need to validate inner types are implementation, graphql-core does this.
+        concrete_choice = type_info.schema_type_info.get_object_type(
+            type_name,
+        ) or type_info.schema_type_info.get_interface(type_name)
+        assert concrete_choice
+        choices.append(
+            _evaluate_object_type(
+                type_info=type_info,
+                concrete=concrete_choice,
+                selection_set=inline_frag.selection_set,
+                path=path,
+            ),
+        )
+
+        # inject __type_name selection, we'll use this to deserialize correctly.
+        if not has_typename_selection(inline_frag.selection_set):
+            inject_typename_selection(inline_frag.selection_set)
+
+    name = f"{concrete.name}__{path}"
+    ret = QtGqlQueriedInterface(
+        name=name,
+        concrete=concrete,
+        choices=choices,
+        fields_dict=linear_fields,
+    )
+    for choice in choices:
+        choice.base_interface = ret
+        choice.fields_dict.update(linear_fields)
+    type_info.narrowed_interfaces_map[name] = ret
+    return ret
+
+
+def _evaluate_object_type(
+    type_info: OperationTypeInfo,
+    concrete: QtGqlObjectType,
+    selection_set: gql_lang.SelectionSetNode,
+    path: str,  # current path in the query tree.
+) -> QtGqlQueriedObjectType:
+    # inject id selection for node implementors, it is required for caching purposes.
+    if concrete.implements_node and not has_id_selection(selection_set):
+        inject_id_selection(selection_set)
+
+    fields: dict[str, QtGqlQueriedField] = {}
+    for selection in selection_set.selections:
+        if f_node := is_field_node(selection):
+            if is_type_name_selection(f_node):
+                continue  # __type_name selection is handled with special care.
+            concrete_field = concrete.fields_dict[f_node.name.value]
+            fields[concrete_field.name] = _evaluate_field(
+                type_info=type_info,
+                concrete_field=concrete_field,
+                field_node=f_node,
+                path=path,
+                origin=concrete,
+            )
+
+    name = f"{concrete.name}__{path}"
+    if ret := type_info.narrowed_types_map.get(name, None):
+        return ret
+
+    ret = QtGqlQueriedObjectType(
+        name=name,
+        concrete=concrete,
+        fields_dict=fields,
+    )
+    type_info.narrowed_types_map[name] = ret
+    return ret
+
+
+def _evaluate_operation(
+    operation: OperationDefinitionNode,
+    schema_type_info: SchemaTypeInfo,
+) -> QtGqlOperationDefinition:
+    """Each operation generates a whole new "proxy" schema. That schema will
+    contain only the fields that are currently queried. The way we do that is
+    creating a so-called "proxy objects".
+
+    - Each proxy object mirrors a concrete object at schema level.
+    - Each proxy object contains only the fields that was queried for this field in the tree.
+
+    And because of that, one object type (at the concrete schema) might have many proxy objects.
+    """
+    type_info = OperationTypeInfo(schema_type_info)
+
+    # input variables
+    if variables_def := operation.variable_definitions:
+        for var in variables_def:
+            type_info.variables.append(_evaluate_variable(type_info.schema_type_info, var))
+
+    selections = operation.selection_set
+    root_type = type_info.schema_type_info.operation_types[operation.operation.value]
+    assert root_type, f"Make sure you have {operation.operation.name} type defined in your schema"
+    root_proxy_type = _evaluate_object_type(
+        type_info=type_info,
+        concrete=root_type,
+        selection_set=selections,
+        path="",
+    )
+    assert len(root_proxy_type.fields) == 1
+    root_field = root_proxy_type.fields[0]
+    return QtGqlOperationDefinition(
+        root_field=root_field,
+        root_type=root_proxy_type,
+        operation_def=operation,
+        variables=type_info.variables,
+        narrowed_types=tuple(type_info.narrowed_types_map.values()),
+        interfaces=tuple(type_info.narrowed_interfaces_map.values()),
+    )
+
+
+class _OperationsVisitor(visitor.Visitor):
+    def __init__(self, type_info: SchemaTypeInfo):
+        super().__init__()
+        self.schema_type_info = type_info
+        self.operations: dict[str, QtGqlOperationDefinition] = {}
+
+    def enter_operation_definition(self, node, key, parent, path, ancestors) -> None:
+        if operation := is_operation_def_node(node):
+            if operation.operation in (
+                OperationType.QUERY,
+                OperationType.MUTATION,
+                OperationType.SUBSCRIPTION,
+            ):
+                assert operation.name, "QtGql enforces operations to have names."
+                self.operations[operation.name.value] = _evaluate_operation(
+                    operation,
+                    self.schema_type_info,
+                )
+
+
+def evaluate_operations(
+    operations_document: graphql.DocumentNode,
+    type_info: SchemaTypeInfo,
+) -> dict[str, QtGqlOperationDefinition]:
+    operation_visitor = _OperationsVisitor(type_info)
+    graphql.visit(operations_document, operation_visitor)
+    assert operation_visitor.operations
+    return operation_visitor.operations
---
+++ packages/qtgql/qtgql-0.123.0-py3-none-any.whl/qtgqlcodegen/templates/operation.jinja.hpp
@@ -0,0 +1,139 @@
+{%- from "macros/deserialize_concrete_field.jinja.hpp" import  deserialize_concrete_field -%}
+{%- from "macros/proxy_type_fields.jinja.hpp" import  proxy_type_fields -%}
+#pragma once
+#include "./schema.hpp"
+#include <qtgql/gqlwstransport/gqlwstransport.hpp>
+#include <QObject>
+
+namespace 👉 context.config.env_name 👈::👉context.ns👈{
+class 👉 context.operation.name 👈;
+
+namespace deserializers{
+{% for t in context.operation.narrowed_types if not t.concrete.is_root -%}
+std::shared_ptr<👉 t.concrete.name 👈> des_👉 t.name 👈(const QJsonObject& data, const 👉 context.operation.name 👈 * operation);
+{% endfor -%}
+{% for t in context.operation.interfaces -%}
+std::shared_ptr<👉 t.concrete.name 👈> des_👉 t.name 👈(const QJsonObject& data, const 👉 context.operation.name 👈 * operation);
+{% endfor -%}
+};
+
+namespace updaters{
+{% for t in context.operation.narrowed_types -%}
+void update_👉 t.name 👈(👉 t.concrete.member_type_arg 👈 inst, const QJsonObject &data, const 👉 context.operation.name 👈 * operation);
+{% endfor -%}
+};
+
+// ------------ Narrowed Interfaces ------------
+{% for t in context.operation.interfaces -%}
+class 👉 t.name 👈: public QObject{
+👉 proxy_type_fields(t, context) 👈
+public:
+    using QObject::QObject;
+{% for f in t.fields -%}
+[[nodiscard]] inline virtual const 👉 f.property_type 👈  👉 f.concrete.getter_name 👈() const {
+throw qtgql::exceptions::InterfaceDirectAccessError("👉t.concrete.name👈");
+}
+{% endfor %}
+};
+{% endfor %}
+// ------------ Narrowed Object types ------------
+{% for t in context.operation.narrowed_types %}
+class 👉 t.name 👈: public 👉 "QObject" if not t.base_interface else t.base_interface.name 👈{
+👉 proxy_type_fields(t, context) 👈
+public:
+{% if t.concrete.is_root -%}
+👉 t.name 👈(👉 context.operation.name 👈 * operation);
+{% else -%}
+👉 t.name 👈(👉 context.operation.name 👈 * operation, const std::shared_ptr<👉 t.concrete.name 👈> &inst);
+{% endif %}
+public:
+{% for f in t.fields -%}
+[[nodiscard]] inline const 👉 f.property_type 👈  👉 f.concrete.getter_name 👈() const {
+{%- if f.type.is_queried_object_type or f.type.is_model or f.type.is_queried_interface %}
+return m_👉f.name👈;
+{%- else -%}
+return m_inst->👉 f.concrete.getter_name 👈();
+{%- endif -%}
+};
+{% endfor -%}
+
+};
+{% endfor %}
+
+struct 👉 context.operation.generated_variables_type 👈{
+{% for var in context.operation.variables -%}
+std::optional<👉 var.type.member_type 👈> 👉 var.name 👈 = {};
+{% endfor -%}
+
+    QJsonObject to_json() const{
+    QJsonObject ret;
+    {% for var in context.operation.variables -%}
+    if (👉 var.name 👈.has_value()){
+    ret.insert("👉 var.name 👈",  👉 var.json_repr() 👈);
+    }
+    {% endfor -%}
+    return ret;
+    }
+};
+
+class 👉 context.operation.name 👈: public qtgql::gqlwstransport::OperationHandlerABC{
+    Q_OBJECT
+Q_PROPERTY(const 👉 context.operation.root_type.name 👈 * data READ data NOTIFY dataChanged);
+
+std::optional<👉 context.operation.root_type.name 👈 *> m_data = {};
+
+
+
+inline const QString &ENV_NAME() override{
+    static const auto ret = QString("👉 context.config.env_name 👈");
+    return ret;
+    }
+signals:
+    void dataChanged();
+
+public:
+👉 context.operation.generated_variables_type 👈 vars_inst;
+
+👉 context.operation.name 👈(): qtgql::gqlwstransport::OperationHandlerABC(qtgql::gqlwstransport::GqlWsTrnsMsgWithID(qtgql::gqlwstransport::OperationPayload(
+        {%- for line in context.operation.query.splitlines() %}"👉 line 👈"{% endfor -%}
+        ))){
+m_message_template.op_id = m_operation_id;
+};
+
+
+QTGQL_STATIC_MAKE_SHARED(👉 context.operation.name 👈)
+
+inline const QUuid & operation_id() const override{
+return m_operation_id;
+}
+
+
+void on_next(const QJsonObject &message) override{
+    auto data = message.value("data").toObject();
+    if (!m_data){
+        👉 context.operation.root_type.updater_name👈(👉 context.operation.root_type.concrete.name👈::instance(), data, this);
+        m_data = new 👉 context.operation.root_type.name👈(this);
+        emit dataChanged();
+    }
+    else{
+        👉 context.operation.root_type.updater_name👈(👉 context.operation.root_type.concrete.name👈::instance(), data, this);
+    }
+}
+
+inline const 👉 context.operation.root_type.name 👈 * data() const{
+    if (m_data){
+        return m_data.value();
+    }
+    return nullptr;
+}
+
+{% if context.operation.variables %}
+void set_variables(👉 context.operation.generated_variables_type 👈 vars){
+vars_inst = vars;
+m_variables = vars_inst.to_json();
+}
+{% endif %}
+
+};
+};
+
