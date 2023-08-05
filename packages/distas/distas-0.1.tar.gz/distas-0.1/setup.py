624e50471bba6ef14a5eb26c6614cf92f6feb085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:47 +0000

    Add package distas/0.1/distas-0.1.tar.gz

---
+++ packages/distas/distas-0.1.tar.gz/distas-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distas',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distas'],
+      author = 'Clement Victor',
+      zip_safe=False)
