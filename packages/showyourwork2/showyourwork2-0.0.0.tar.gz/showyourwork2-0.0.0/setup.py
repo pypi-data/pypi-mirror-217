11de60f65dc08d4e5de35df0364ac6a17577dfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:22 +0000

    Add package showyourwork2/0.0.0/showyourwork2-0.0.0.tar.gz

---
+++ packages/showyourwork2/showyourwork2-0.0.0.tar.gz/showyourwork2-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='showyourwork2',
+    version='0.0.0',
+    description='',
+    long_description='',
+    author='Dan Foreman-Mackey',
+    author_email='dfm@dfm.io',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
