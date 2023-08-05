ea562c69f6eb0e39cdcf1e9ed6188c1507cd0e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:43 +0000

    Add package torchrl-nightly/2023.7.2/torchrl_nightly-2023.7.2-cp39-cp39-win_amd64.whl

---
+++ packages/torchrl-nightly/torchrl_nightly-2023.7.2-cp39-cp39-win_amd64.whl/torchrl/modules/tensordict_module/__init__.py
@@ -0,0 +1,33 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .actors import (
+    Actor,
+    ActorCriticOperator,
+    ActorCriticWrapper,
+    ActorValueOperator,
+    DistributionalQValueActor,
+    DistributionalQValueHook,
+    DistributionalQValueModule,
+    ProbabilisticActor,
+    QValueActor,
+    QValueHook,
+    QValueModule,
+    TanhModule,
+    ValueOperator,
+)
+from .common import SafeModule
+from .exploration import (
+    AdditiveGaussianWrapper,
+    EGreedyWrapper,
+    OrnsteinUhlenbeckProcessWrapper,
+)
+from .probabilistic import (
+    SafeProbabilisticModule,
+    SafeProbabilisticTensorDictSequential,
+)
+from .rnn import LSTMModule
+from .sequence import SafeSequential
+from .world_models import WorldModelWrapper
---
+++ packages/torchrl-nightly/torchrl_nightly-2023.7.2-cp39-cp39-win_amd64.whl/torchrl/version.py
@@ -0,0 +1,2 @@
+__version__ = '2023.7.2'
+git_version = 'c42ff781215bcc87fe756e61802e954e8a4adffd'
