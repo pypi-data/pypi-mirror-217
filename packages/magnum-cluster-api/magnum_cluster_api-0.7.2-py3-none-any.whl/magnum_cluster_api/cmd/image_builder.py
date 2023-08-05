5ef8b77452de9d205dd0cd499b09055b3301122d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:04 +0000

    Add package magnum-cluster-api/0.7.2/magnum_cluster_api-0.7.2-py3-none-any.whl

---
+++ packages/magnum-cluster-api/magnum_cluster_api-0.7.2-py3-none-any.whl/magnum_cluster_api/cmd/image_builder.py
@@ -0,0 +1,138 @@
+# Copyright (c) 2023 VEXXHOST, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import getpass
+import json
+import os
+import subprocess
+import tarfile
+import tempfile
+import textwrap
+import zlib
+from pathlib import Path
+
+import click
+import requests
+
+QEMU_PACKAGES = [
+    "qemu-kvm",
+    "libvirt-daemon-system",
+    "libvirt-clients",
+    "virtinst",
+    "cpu-checker",
+    "libguestfs-tools",
+    "libosinfo-bin",
+]
+
+
+@click.command()
+@click.option(
+    "--operating-system",
+    show_default=True,
+    default="ubuntu-2004",
+    type=click.Choice(["ubuntu-2004", "ubuntu-2204"]),
+    help="Operating system to build image for",
+)
+@click.option(
+    "--version",
+    show_default=True,
+    default="v1.26.2",
+    help="Kubernetes version",
+)
+@click.option(
+    "--image-builder-version",
+    show_default=True,
+    default="v0.1.14",
+    help="Image builder tag (or commit) to use for building image",
+)
+def main(operating_system, version, image_builder_version):
+    ib_path = f"/tmp/image-builder-{image_builder_version}"
+    output = f"{operating_system}-kube-{version}"
+
+    target = f"{ib_path}/images/capi/output/{output}/{output}"
+    if os.path.exists(target):
+        print(f"Image already exists: {target}")
+        return
+
+    click.echo("- Install QEMU packages")
+    subprocess.run(
+        ["sudo", "/usr/bin/apt", "install", "-y"] + QEMU_PACKAGES, check=True
+    )
+
+    click.echo("- Add current user to KVM group")
+    subprocess.run(
+        ["sudo", "/usr/sbin/usermod", "-a", "-G", "kvm", getpass.getuser()], check=True
+    )
+
+    click.echo("- Update permissions for the KVM device")
+    subprocess.run(["sudo", "/bin/chown", "root:kvm", "/dev/kvm"], check=True)
+
+    # Setup our stream decompressor
+    dec = zlib.decompressobj(32 + zlib.MAX_WBITS)
+
+    click.echo("- Download image-builder")
+    path = f"{ib_path}.tar"
+    with requests.get(
+        f"https://github.com/kubernetes-sigs/image-builder/tarball/{image_builder_version}",
+        stream=True,
+    ) as r:
+        r.raise_for_status()
+        with open(path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                rv = dec.decompress(chunk)
+                if rv:
+                    f.write(rv)
+
+    click.echo("- Extract image-builder")
+    with tarfile.open(path) as tar:
+        members = []
+        for member in tar.getmembers():
+            p = Path(member.path)
+            member.path = p.relative_to(*p.parts[:1])
+            members.append(member)
+
+        tar.extractall(ib_path, members=members)
+
+    click.echo("- Create customization file")
+    kubernetes_series = ".".join(version.split(".")[0:2])
+    customization = {
+        "kubernetes_deb_version": f"{version.replace('v', '')}-00",
+        "kubernetes_semver": f"{version}",
+        "kubernetes_series": f"{kubernetes_series}",
+    }
+    with tempfile.NamedTemporaryFile(suffix=".json") as fp:
+        fp.write(json.dumps(customization).encode("utf-8"))
+        fp.flush()
+
+        click.echo("- Build image")
+        subprocess.run(
+            [
+                "/usr/bin/newgrp",
+                "kvm",
+            ],
+            input=textwrap.dedent(
+                f"""\
+                /usr/bin/make \
+                -C \
+                {ib_path}/images/capi \
+                build-qemu-{operating_system}
+            """
+            ).encode("utf-8"),
+            env={
+                **os.environ,
+                **{
+                    "PACKER_VAR_FILES": fp.name,
+                },
+            },
+        )
---
+++ packages/magnum-cluster-api/magnum_cluster_api-0.7.2-py3-none-any.whl/magnum_cluster_api/cmd/image_loader.py
@@ -0,0 +1,128 @@
+# Copyright (c) 2023 VEXXHOST, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import shutil
+import subprocess
+
+import click
+
+from magnum_cluster_api import conf, image_utils, images
+
+CONF = conf.CONF
+
+
+IMAGES = [
+    "docker.io/calico/cni:v3.24.2",
+    "docker.io/calico/kube-controllers:v3.24.2",
+    "docker.io/calico/node:v3.24.2",
+    "docker.io/k8scloudprovider/cinder-csi-plugin:v1.25.3",
+    "docker.io/k8scloudprovider/manila-csi-plugin:v1.25.3",
+    "docker.io/k8scloudprovider/openstack-cloud-controller-manager:v1.25.3",
+    "registry.k8s.io/sig-storage/nfsplugin:v4.2.0",
+    "registry.k8s.io/coredns/coredns:v1.8.6",
+    "registry.k8s.io/coredns/coredns:v1.9.3",
+    CONF.auto_scaling.v1_22_image,
+    CONF.auto_scaling.v1_23_image,
+    CONF.auto_scaling.v1_24_image,
+    CONF.auto_scaling.v1_25_image,
+    CONF.auto_scaling.v1_26_image,
+    "registry.k8s.io/etcd:3.5.1-0",
+    "registry.k8s.io/etcd:3.5.3-0",
+    "registry.k8s.io/etcd:3.5.3-0",
+    "registry.k8s.io/etcd:3.5.4-0",
+    "registry.k8s.io/etcd:3.5.6-0",
+    "registry.k8s.io/kube-apiserver:v1.23.13",
+    "registry.k8s.io/kube-apiserver:v1.24.7",
+    "registry.k8s.io/kube-apiserver:v1.24.7",
+    "registry.k8s.io/kube-apiserver:v1.25.3",
+    "registry.k8s.io/kube-apiserver:v1.26.2",
+    "registry.k8s.io/kube-controller-manager:v1.23.13",
+    "registry.k8s.io/kube-controller-manager:v1.24.7",
+    "registry.k8s.io/kube-controller-manager:v1.24.7",
+    "registry.k8s.io/kube-controller-manager:v1.25.3",
+    "registry.k8s.io/kube-controller-manager:v1.26.2",
+    "registry.k8s.io/kube-proxy:v1.23.13",
+    "registry.k8s.io/kube-proxy:v1.24.7",
+    "registry.k8s.io/kube-proxy:v1.24.7",
+    "registry.k8s.io/kube-proxy:v1.25.3",
+    "registry.k8s.io/kube-proxy:v1.26.2",
+    "registry.k8s.io/kube-scheduler:v1.23.13",
+    "registry.k8s.io/kube-scheduler:v1.24.7",
+    "registry.k8s.io/kube-scheduler:v1.24.7",
+    "registry.k8s.io/kube-scheduler:v1.25.3",
+    "registry.k8s.io/kube-scheduler:v1.26.2",
+    images.PAUSE,
+    "registry.k8s.io/sig-storage/csi-attacher:v3.4.0",
+    "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.4.0",
+    "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.5.1",
+    "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.6.2",
+    "registry.k8s.io/sig-storage/csi-provisioner:v3.0.0",
+    "registry.k8s.io/sig-storage/csi-provisioner:v3.1.0",
+    "registry.k8s.io/sig-storage/csi-provisioner:v3.3.0",
+    "registry.k8s.io/sig-storage/csi-resizer:v1.4.0",
+    "registry.k8s.io/sig-storage/csi-resizer:v1.8.0",
+    "registry.k8s.io/sig-storage/csi-snapshotter:v5.0.1",
+    "registry.k8s.io/sig-storage/csi-snapshotter:v6.0.1",
+    "registry.k8s.io/sig-storage/livenessprobe:v2.7.0",
+    "registry.k8s.io/sig-storage/livenessprobe:v2.8.0",
+]
+
+
+@click.command()
+@click.option(
+    "--repository",
+    show_default=True,
+    default="quay.io/vexxhost",
+    help="Target image repository",
+)
+@click.option(
+    "--insecure",
+    is_flag=True,
+    help="Allow insecure connections to the registry.",
+)
+def main(repository, insecure):
+    """
+    Load images into a remote registry for `container_infra_prefix` usage.
+    """
+    crane_path = shutil.which("crane")
+
+    if crane_path is None:
+        raise click.UsageError(
+            """Crane is not installed. Please install it before running this command:
+             https://github.com/google/go-containerregistry/blob/main/cmd/crane/README.md#installation"""
+        )
+
+    for image in IMAGES:
+        src = image
+        dst = image_utils.get_image(image, repository)
+
+        try:
+            command = [crane_path]
+            if insecure:
+                command.append("--insecure")
+            command += ["copy", src, dst]
+
+            subprocess.run(command, capture_output=True, check=True)
+        except subprocess.CalledProcessError as e:
+            if "401 Unauthorized" in e.stderr.decode():
+                click.echo(
+                    "Authentication failed. Please ensure you're logged in via Crane.",
+                    err=True,
+                )
+                return
+
+            click.echo(e.stderr.decode(), err=True)
+            return
+
+        click.echo(f"Successfully mirrored {src} to {dst}")
