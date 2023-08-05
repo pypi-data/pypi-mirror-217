efc260d5e82cc4d489db643e9b1ca4e9c86e74e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:33 +0000

    Add package functionlayer/0.1/functionlayer-0.1.tar.gz

---
+++ packages/functionlayer/functionlayer-0.1.tar.gz/functionlayer-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='functionlayer',
+   version='0.1',
+   description='Function Layer',
+   author='Delip Rao',
+   author_email='deliprao@gmail.com',
+   packages=['functionlayer'],  #same as name
+   install_requires=['openai'], # External packages as dependencies
+)
