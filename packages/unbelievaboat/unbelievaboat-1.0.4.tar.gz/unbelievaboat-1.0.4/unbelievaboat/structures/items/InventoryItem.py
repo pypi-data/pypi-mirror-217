31ff8fd050730b2d6c7b2e3ecf5879a6a6c20113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:59 +0000

    Add package unbelievaboat/1.0.4/unbelievaboat-1.0.4.tar.gz

---
+++ packages/unbelievaboat/unbelievaboat-1.0.4.tar.gz/unbelievaboat-1.0.4/unbelievaboat/structures/items/InventoryItem.py
@@ -0,0 +1,15 @@
+from typing import Any, Dict
+
+from .BaseItem import BaseItem
+
+
+class InventoryItem(BaseItem):
+    def __init__(self, data: Dict[str, Any]) -> None:
+        super().__init__(data)
+        self.item_id: str = data["item_id"]
+        self.quantity: int = data["quantity"]
+
+    def __str__(self) -> str:
+        return "<InventoryItem item_id={} quantity={}>".format(
+            self.item_id, self.quantity
+        )
