30fff53d5a7d12922a9e2180335216a391c4c6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:54 +0000

    Add package lucianosyllvapacotepypi/0.0.1/lucianosyllvapacotepypi-0.0.1.tar.gz

---
+++ packages/lucianosyllvapacotepypi/lucianosyllvapacotepypi-0.0.1.tar.gz/lucianosyllvapacotepypi-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='lucianosyllvapacotepypi',
+    version='0.0.1',
+    url='https://github.com/caiocarneloz/pacotepypi',
+    license='MIT License',
+    author='Luciano Syllva',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='caiocarneloz@gmail.com',
+    keywords='Pacote',
+    description=u'Exemplo de pacote PyPI',
+    install_requires=['numpy'],)
