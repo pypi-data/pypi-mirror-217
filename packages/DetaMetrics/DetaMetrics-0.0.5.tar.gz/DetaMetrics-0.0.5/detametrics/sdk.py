b229960fa6869da65b16bd3807d9fab43eec0d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:27 +0000

    Add package DetaMetrics/0.0.5/DetaMetrics-0.0.5.tar.gz

---
+++ packages/DetaMetrics/DetaMetrics-0.0.5.tar.gz/DetaMetrics-0.0.5/detametrics/sdk.py
@@ -0,0 +1,24 @@
+import httpx
+
+
+class DetaMetrics:
+    def __init__(self, urlId: str, apiKey: str) -> None:
+        self.url = "https://" + urlId + ".deta.app"
+        self.__apiKey = apiKey
+        self.__headers = {
+            "User-Agent": "DetaMetrics",
+            "X-Space-App-Key": self.__apiKey,
+        }
+        print(f"Using DetaMetrics Server at {self.url}")
+
+    def __repr__(self) -> str:
+        return f"<DetaMetrics url={self.url}>"
+
+    def __str__(self) -> str:
+        return f"DetaMetrics(url={self.url})"
+
+    def set(self, graph: str, metric: str, value: float) -> None:
+        httpx.get(f"{self.url}/set/{graph}/{metric}/{value}", headers=self.__headers)
+
+    def clear(self) -> None:
+        httpx.get(f"{self.url}/clear", headers=self.__headers)
