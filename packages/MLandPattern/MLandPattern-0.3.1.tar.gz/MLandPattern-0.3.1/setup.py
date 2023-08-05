127be7062dd425af1e87d40d369df3fc4dd756e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:57 +0000

    Add package MLandPattern/0.3.1/MLandPattern-0.3.1.tar.gz

---
+++ packages/MLandPattern/MLandPattern-0.3.1.tar.gz/MLandPattern-0.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MLandPattern",
+    packages=find_packages(include=['MLandPattern']),
+    version='0.3.1',
+    description="Machine Learning and Pattern recognition Library",
+    author="Pablo Munoz Salazar",
+    license="ISC",
+)
+
