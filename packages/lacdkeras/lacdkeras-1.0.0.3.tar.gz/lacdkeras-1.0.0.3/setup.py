9f6f7d3f5237481e873807ce3d7cd73f375454b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:31 +0000

    Add package lacdkeras/1.0.0.3/lacdkeras-1.0.0.3.tar.gz

---
+++ packages/lacdkeras/lacdkeras-1.0.0.3.tar.gz/lacdkeras-1.0.0.3/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+	
+	name = 'lacdkeras',
+	packages = setuptools.find_packages(),
+	version = '1.0.0.3',
+	description = 'ncku AI camp',
+	author = 'HuangTeEn',
+	author_email = 'qaz910048@gmail.com',
+	keywords = ['Keras','Teach','lacd'],
+    license='MIT'
+)
