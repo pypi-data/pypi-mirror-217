0a4e8f4da9f8c704ba53bad2199b1e3baf8e6583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:22 +0000

    Add package repid/1.4.1/repid-1.4.1.tar.gz

---
+++ packages/repid/repid-1.4.1.tar.gz/repid-1.4.1/repid/connections/__init__.py
@@ -0,0 +1,29 @@
+from repid._utils import is_installed
+from repid.connections.abc import BucketBrokerT, ConsumerT, MessageBrokerT
+from repid.connections.in_memory import (
+    DummyBucketBroker,
+    DummyMessageBroker,
+    InMemoryBucketBroker,
+    InMemoryMessageBroker,
+)
+
+__all__ = [
+    "BucketBrokerT",
+    "ConsumerT",
+    "MessageBrokerT",
+    "DummyBucketBroker",
+    "DummyMessageBroker",
+    "InMemoryBucketBroker",
+    "InMemoryMessageBroker",
+]
+
+if is_installed("aiormq"):
+    from repid.connections.rabbitmq import RabbitMessageBroker
+
+    __all__.append("RabbitMessageBroker")
+
+if is_installed("redis"):
+    from repid.connections.redis import RedisBucketBroker, RedisMessageBroker
+
+    __all__.append("RedisBucketBroker")
+    __all__.append("RedisMessageBroker")
