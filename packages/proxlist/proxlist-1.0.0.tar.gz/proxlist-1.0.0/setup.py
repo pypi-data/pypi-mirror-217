d9eb7b2b6b3e77e22591f397a8fdbd2602932ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:43 +0000

    Add package proxlist/1.0.0/proxlist-1.0.0.tar.gz

---
+++ packages/proxlist/proxlist-1.0.0.tar.gz/proxlist-1.0.0/setup.py
@@ -0,0 +1,56 @@
+import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+REQUIREMENTS = [
+    'beautifulsoup4 == 4.*',
+    'requests == 2.*',
+    'woodchips == 0.2.*',
+]
+
+DEV_REQUIREMENTS = [
+    'bandit == 1.7.*',
+    'black == 23.*',
+    'build == 0.10.*',
+    'flake8 == 6.*',
+    'isort == 5.*',
+    'mypy == 1.3.*',
+    'pytest == 7.*',
+    'pytest-cov == 4.*',
+    'twine == 4.*',
+    'types-requests',
+]
+
+setuptools.setup(
+    name='proxlist',
+    version='1.0.0',
+    description='Retrieve free, open-source proxy servers.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/Justintime50/proxlist',
+    author='Justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(
+        exclude=[
+            'examples',
+            'test',
+        ]
+    ),
+    package_data={
+        'proxlist': [
+            'py.typed',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIREMENTS,
+    extras_require={
+        'dev': DEV_REQUIREMENTS,
+    },
+    python_requires='>=3.8, <4',
+)
