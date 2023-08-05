af10d8c779924e1ce55b0c0a291edf50187feea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:47 +0000

    Add package indexify/0.0.2/indexify-0.0.2-py3-none-any.whl

---
+++ packages/indexify/indexify-0.0.2-py3-none-any.whl/indexify/memory.py
@@ -0,0 +1,39 @@
+import requests
+
+from .data_containers import *
+from .utils import _get_payload
+
+
+class Memory:
+
+    def __init__(self, url, index):
+        self._url = url
+        self._index = index
+
+    def create(self) -> str:
+        resp = requests.post(f"{self._url}/memory/create", json={})
+        self.session_id = _get_payload(resp)["session_id"]
+        return self.session_id
+
+    def add(self, *messages: Message) -> None:
+        parsed_messages = []
+        for message in messages:
+            parsed_messages.append(message.to_dict())
+
+        req = {"session_id": self.session_id, "messages": parsed_messages}
+        resp = requests.post(f"{self._url}/memory/add", json=req)
+        if resp.status_code == 200:
+            return
+        _get_payload(resp)
+
+    def all(self) -> list[Message]:
+        req = {"session_id": self.session_id}
+        resp = requests.get(f"{self._url}/memory/get", json=req)
+        if resp.status_code == 200:
+            payload = _get_payload(resp)
+            messages = []
+            for raw_message in payload["messages"]:
+                messages.append(Message(raw_message["role"], raw_message["text"], raw_message["metadata"]))
+            return messages
+        _get_payload(resp)
+
