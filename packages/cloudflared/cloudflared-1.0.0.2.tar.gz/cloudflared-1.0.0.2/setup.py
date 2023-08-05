3f6876bf4404627752b8f82c031c922b367e4e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:24 +0000

    Add package cloudflared/1.0.0.2/cloudflared-1.0.0.2.tar.gz

---
+++ packages/cloudflared/cloudflared-1.0.0.2.tar.gz/cloudflared-1.0.0.2/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def readme() -> str:
+    with open("README.md", encoding='utf-8') as file:
+        file_data = file.read()
+    return file_data
+
+
+setup(
+    name="cloudflared",
+    version="1.0.0.2",
+    description="A Python package to interact with cloudflared",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SigireddyBalaSai/cloudflared.py",
+    author="Sigireddy BalaSai",
+    author_email="sigireddybalasai@gmail.com",
+    license="Apache-2.0",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["cloudflared"],
+    include_package_data=True,
+    install_requires=["setuptools_scm"],
+)
