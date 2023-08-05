8d4a06b4f9328203740e357f67e7541cae2ee564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:45 +0000

    Add package lanablas/0.1.6/lanablas-0.1.6.tar.gz

---
+++ packages/lanablas/lanablas-0.1.6.tar.gz/lanablas-0.1.6/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, Extension
+
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+module_extension = Extension(
+    'matrix',
+    sources=['lanablas/matrix.c'],
+    extra_compile_args=['-std=c11'],
+    include_dirs=['openblas/include'],
+)
+
+
+openblas_lib_files = []
+for root, _, files in os.walk('openblas'):
+    for file in files:
+        openblas_lib_files.append(os.path.relpath(os.path.join(root, file), 'lanablas'))
+
+
+setup(
+    name='lanablas',
+    version='0.1.6',
+    description='Extension module for matrix multiplication',
+    author='Marco Salvalaggio',
+    author_email='mar.salvalaggio@gmail.com',
+    url='https://github.com/marcosalvalaggio/lana-blas',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['lanablas'],
+    include_package_data=True,
+    package_data={
+        'lanablas': openblas_lib_files,  # Include OpenBLAS library files
+    },
+    ext_modules=[module_extension],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
