f6eb9f7d4235675608b15b4f8f3f0d27b5ee589c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:06 +0000

    Add package intex-spa/0.8.4/intex-spa-0.8.4.tar.gz

---
+++ packages/intex-spa/intex-spa-0.8.4.tar.gz/intex-spa-0.8.4/setup.py
@@ -0,0 +1,23 @@
+"""Setup configuration."""
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    README = fh.read()
+setup(
+    name="intex-spa",
+    version="0.8.4",
+    author="Mathieu Payrol",
+    author_email="intex-spa@payrol.fr",
+    description="Intex Spa wifi client",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mathieu-mp/intex-spa",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
