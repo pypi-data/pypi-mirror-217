b5ad2c9c1b85feeaccda10c638161e7e8e98739c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:16 +0000

    Add package bench-cxynb/5.0.0/bench-cxynb-5.0.0.zip

---
+++ packages/bench-cxynb/bench-cxynb-5.0.0.zip/bench-cxynb-5.0.0/bench/__init__.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment, PackageLoader
+
+__version__ = "5.0.0"
+
+env = Environment(loader=PackageLoader('bench.config'))
+
+FRAPPE_VERSION = None
+
+def set_frappe_version(bench_path='.'):
+    print("日志")
+	from .app import get_current_frappe_version
+	global FRAPPE_VERSION
+	if not FRAPPE_VERSION:
+		FRAPPE_VERSION = get_current_frappe_version(bench_path=bench_path)
