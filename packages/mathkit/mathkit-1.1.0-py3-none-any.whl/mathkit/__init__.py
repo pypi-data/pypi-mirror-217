b08ff6efc6baa4498d538f26f4ca67c284b6b593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:11 +0000

    Add package mathkit/1.1.0/mathkit-1.1.0-py3-none-any.whl

---
+++ packages/mathkit/mathkit-1.1.0-py3-none-any.whl/mathkit/__init__.py
@@ -0,0 +1,21 @@
+from .main import add
+from .main import cube
+from .main import cbrt
+from .main import div
+from .main import fact
+from .main import hcf
+from .main import lcm
+from .main import log
+from .main import mod
+from .main import mul
+from .main import per
+from .main import power
+from .main import sqrt
+from .main import sqr
+from .main import sub
+from .main import trig
+
+__all__ = ['add', 'cube', 'cbrt', 'div', 'fact', 'hcf', 'lcm', 'log', 'mod', 'mul', 'per', 'power', 'sqrt', 'sqr', 'sub','trig']
+
+
+
