93de5b6a8ae237ae2ea26d0bfe3c73e2a8a123f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:35 +0000

    Add package meth/1.0.0/meth-1.0.0-py3-none-any.whl

---
+++ packages/meth/meth-1.0.0-py3-none-any.whl/meth/token.py
@@ -0,0 +1,31 @@
+TT_IDENTIFIER = "IDENTIFIER"
+TT_LBRACKET = "LEFT_BRACKET"
+TT_RBRACKET = "RIGHT_BRACKET"
+TT_EQUAL = "EQUAL"
+TT_COMMA = "COMMA"
+
+TT_PLUS = "PLUS"
+TT_MINUS = "MINUS"
+TT_MUL = "MULTIPLY"
+TT_DIV = "DIVIDE"
+TT_MOD = "MODULO"
+TT_POW = "POWER"
+
+TT_FLOAT = "FLOAT"
+TT_INT = "INTEGER"
+
+
+class Token:
+    def __init__(self, token_type, value=None) -> None:
+        """Class for tokens."""
+        self.type = token_type
+        self.value = value
+
+    def __eq__(self, other: object) -> bool:
+        if type(other) == Token:
+            return self.type == other.type and self.value == other.value
+        else:
+            return self.type == other
+
+    def __repr__(self) -> str:
+        return f"{self.type}({self.value})"
---
+++ packages/meth/meth-1.0.0-py3-none-any.whl/meth/utils.py
@@ -0,0 +1,24 @@
+from .nodes import *
+from .token import *
+
+
+def get_final_node(tree: BaseNode, stop_at: list = []):
+    node = tree
+
+    while (
+        getattr((n := getattr(node, "right", None)), "right", None)
+        and n.value not in stop_at
+        and not n.is_paren
+    ):
+        node = node.right
+
+    return node
+
+
+def get_from_depth(tree: BaseNode, depth: int):
+    node = tree
+
+    for _ in range(depth):
+        node = node.left
+
+    return node
