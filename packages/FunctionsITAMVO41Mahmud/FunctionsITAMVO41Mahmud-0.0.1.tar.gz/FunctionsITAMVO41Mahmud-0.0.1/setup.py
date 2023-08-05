72cf7a0eab42de3f434d1af1a6a57e99d94cffd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:52 +0000

    Add package FunctionsITAMVO41Mahmud/0.0.1/FunctionsITAMVO41Mahmud-0.0.1.tar.gz

---
+++ packages/FunctionsITAMVO41Mahmud/FunctionsITAMVO41Mahmud-0.0.1.tar.gz/FunctionsITAMVO41Mahmud-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='FunctionsITAMVO41Mahmud',
+    version='0.0.1',
+    license='MIT License',
+    author='Mahmud',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='mahmud.alineto@gmail.com',
+    keywords='ITA MVO-41',
+    description=u'Functions of MVO-41 from T24',
+    packages=['functions'],
+    install_requires=['numpy'],)
