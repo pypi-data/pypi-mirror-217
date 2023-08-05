01f830df5c6cbdf8f7691ad34f75ac496b7467d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:16 +0000

    Add package djangocms-rawhtml/1.3.0/djangocms-rawhtml-1.3.0.tar.gz

---
+++ packages/djangocms-rawhtml/djangocms-rawhtml-1.3.0.tar.gz/djangocms-rawhtml-1.3.0/djangocms_rawhtml/cms_plugins.py
@@ -0,0 +1,37 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.conf import settings
+from django.utils.translation import gettext as _
+from djangocms_rawhtml.models import RawHTMLPluginData
+
+@plugin_pool.register_plugin
+class RawHTMLPluginPublisher(CMSPluginBase):
+    """
+    Plugin to manage raw HTML text that needs to be managed from the admin interface.
+    """
+    name = _("Raw HTML")
+    model = RawHTMLPluginData
+    change_form_template = "admin/djangocms_rawhtml/change_form.html"
+    render_template = "djangocms_rawhtml/plugins/raw.html"
+    allow_children = False
+    
+    def render(self, context, instance, placeholder):
+        context.update({
+            'instance': instance,
+        })
+        return context
+
+    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+        """
+        If we've set a theme setting, pass that along.
+        """
+        if hasattr(settings, 'CODEMIRROR_THEME'):
+            theme = settings.CODEMIRROR_THEME
+        else:
+            theme = None
+
+        context.update({
+            'theme': theme
+        })
+
+        return super().render_change_form(request, context, add, change, form_url, obj)
