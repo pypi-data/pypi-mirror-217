89c282b2db384d64844ea4f1bb289131cf3ae93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:01 +0000

    Add package ayek-probability/1.0/ayek_probability-1.0.tar.gz

---
+++ packages/ayek-probability/ayek_probability-1.0.tar.gz/ayek_probability-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ayek_probability',
+      version=1.0,
+      description='gaussian probability',
+      packages=['ayek_probability'],
+      author= 'Augustine Ayekor',
+      author_email='augustineayekor@gmail.com',
+      zip_safe = False
+)
