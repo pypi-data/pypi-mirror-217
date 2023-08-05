94dfc67f2521c573f247247de6441f13f673bab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:12 +0000

    Add package owe-python-sdk/0.0.1a7/owe_python_sdk-0.0.1a7.tar.gz

---
+++ packages/owe-python-sdk/owe_python_sdk-0.0.1a7.tar.gz/owe_python_sdk-0.0.1a7/owe_python_sdk/client/__init__.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+from typing import List
+
+from owe_python_sdk import schema as owe_schema
+
+
+class Task:
+    def __init__(self, schema: dict):
+        self.schema = owe_schema.BaseTask(**schema)
+
+    def depends_on(self, task: Task, *tasks: List[Task]) -> None:
+        dependencies = [task, *tasks]
+        for dep in dependencies:
+            if type(dep) != self.__class__.__name__:
+                raise TypeError(f"Task dependencies must be of type {self.__class__.__name__} | Recieved {type(dep)}")
+
+        self.schema.depends_on += dependencies
+
+class Pipeline:
+    def __init__(self, schema: dict):
+        self.schema = owe_schema.BasePipeline(**schema)
+
+    def add_task(self, task: Task, *tasks: List[Task]):
+        new_tasks = [task, *tasks]
+        for new_task in new_tasks:
+            if type(new_task) != Task:
+                raise TypeError(f"Task dependencies must be of type Task | Recieved {type(new_task)}")
+            
+            self.schema.tasks.append(new_task.schema)
+
+    def submit(self):
+        print(self.schema.dict())
+
+    
