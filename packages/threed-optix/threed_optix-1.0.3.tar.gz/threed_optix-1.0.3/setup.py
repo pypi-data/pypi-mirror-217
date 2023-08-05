6db150077ff93e9af577dd3d9c2dbd386b4894a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:56 +0000

    Add package threed-optix/1.0.3/threed_optix-1.0.3.tar.gz

---
+++ packages/threed-optix/threed_optix-1.0.3.tar.gz/threed_optix-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='threed_optix',
+    version='1.0.3',
+    license='',
+    author="Elika Ron",
+    author_email='alikaron@3doptix.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/3doptix/3DOptix-Canvas-API',
+    keywords='',
+    install_requires=[
+        'requests',
+        'pandas',
+        'matplotlib'
+    ]
+)
