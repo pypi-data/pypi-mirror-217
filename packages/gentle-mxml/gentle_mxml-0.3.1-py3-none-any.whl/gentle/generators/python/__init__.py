546242c56005beb91a429854dc29117c18b5aec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:56 +0000

    Add package gentle-mxml/0.3.1/gentle_mxml-0.3.1-py3-none-any.whl

---
+++ packages/gentle-mxml/gentle_mxml-0.3.1-py3-none-any.whl/gentle/generators/python/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: WTFPL
+# SPDX-FileCopyrightText: 2023 Anna <cyber@sysrq.in>
+# No warranty
+
+""" Common data for Python generators """
+
+BUG_TRACKER_LABELS = ["bug tracker", "bugtracker", "bugs", "issues"]
+CHANGELOG_LABELS = ["changelog", "changes"]
+DOCS_LABELS = ["doc", "docs", "documentation"]
+HOME_REPO_LABELS = ["source", "repo", "repository", "home", "homepage"]
