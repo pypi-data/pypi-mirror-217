fa963eac60f63139760cbd136f5a040b0fc5c67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:34 +0000

    Add package adata/0.0.25b0/adata-0.0.25b0-py3-none-any.whl

---
+++ packages/adata/adata-0.0.25b0-py3-none-any.whl/adata/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@desc: adata
+@author: 1nchaos
+@time: 2023/4/4
+"""
+# -*- coding: utf-8 -*-
+
+import logging
+
+from .__version__ import __version__
+from .stock import stock
+from .fund import fund
+from .bond import bond
+from .sentiment import sentiment
+
+# set up logging
+logger = logging.getLogger("adata")
+
+
+def set_logger():
+    format_string = "%(asctime)s - %(levelname)s - %(message)s"
+    formatter = logging.Formatter(format_string, datefmt="%Y-%m-%dT%H:%M:%S")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+
+set_logger()
