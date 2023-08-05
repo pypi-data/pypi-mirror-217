408e99bcd3812a78a85c2e631687bf254d607d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:49 +0000

    Add package trongrid-extractor/0.3.10/trongrid_extractor-0.3.10-py3-none-any.whl

---
+++ packages/trongrid-extractor/trongrid_extractor-0.3.10-py3-none-any.whl/trongrid_extractor/helpers/string_constants.py
@@ -0,0 +1,17 @@
+"""
+String constants.
+"""
+PACKAGE_NAME = 'trongrid_extractor'
+
+DATA = 'data'
+FINGERPRINT = 'fingerprint'
+LINKS = 'links'
+MAINNET = 'mainnet'
+META = 'meta'
+MIN_TIMESTAMP = 'min_timestamp'
+MAX_TIMESTAMP = 'max_timestamp'
+NEXT = 'next'
+PAGE_SIZE = 'page_size'
+RESULT = 'result'
+SUCCESS = 'success'
+TRC20 = 'trc20'
