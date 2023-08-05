f6b8b7f2ef2d93697b61300bd80576b2749449c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:45 +0000

    Add package data-merge/0.1/data_merge-0.1.tar.gz

---
+++ packages/data-merge/data_merge-0.1.tar.gz/data_merge-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_merge',
+    version='0.1',
+    description='Python package for merging Excel and CSV files',
+    author='Ojo Ilesanmi',
+    author_email='ojoilesanmi89@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pytest'
+    ],
+)
