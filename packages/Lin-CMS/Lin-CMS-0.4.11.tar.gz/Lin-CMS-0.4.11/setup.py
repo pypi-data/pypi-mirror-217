42b153c9b4128675f2be666ab7279ce35f6a7a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:45 +0000

    Add package Lin-CMS/0.4.11/Lin-CMS-0.4.11.tar.gz

---
+++ packages/Lin-CMS/Lin-CMS-0.4.11.tar.gz/Lin-CMS-0.4.11/setup.py
@@ -0,0 +1,47 @@
+"""
+Flask-Lin
+------------------
+"""
+import io
+
+import setuptools
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="Lin-CMS",
+    version="0.4.11",
+    url="https://pypi.org/project/Lin-CMS/",
+    license="MIT",
+    author="pedroGao",
+    author_email="1312342604@qq.com",
+    maintainer="sunlin92",
+    maintainer_email="sun.melodies@gmail.com",
+    description="A simple and practical CMS implememted by flask",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["flask", "CMS", "authority", "jwt", "openapi"],
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    zip_safe=False,
+    platforms="any",
+    install_requires=[
+        "Flask==2.0.3",
+        "Flask_JWT_Extended==4.3.1",
+        "Flask_SQLAlchemy==2.5.1",
+        "WTForms==3.0.1",
+        "tablib==3.2.0",
+        "spectree==0.7.6",
+        "SQLAlchemy==1.4.46",
+        "pydantic==1.9.2",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
