c1b63406d497d17c04ed26a7b4dbdc9137804cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:44 +0000

    Add package forks-sync/4.0.0/forks-sync-4.0.0.tar.gz

---
+++ packages/forks-sync/forks-sync-4.0.0.tar.gz/forks-sync-4.0.0/setup.py
@@ -0,0 +1,59 @@
+import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+REQUIREMENTS = [
+    'PyGithub == 1.*',
+    'woodchips == 0.2.*',
+]
+
+DEV_REQUIREMENTS = [
+    'bandit == 1.7.*',
+    'black == 23.*',
+    'build == 0.10.*',
+    'flake8 == 6.*',
+    'isort == 5.*',
+    'mypy == 1.3.*',
+    'pytest == 7.*',
+    'pytest-cov == 4.*',
+    'twine == 4.*',
+]
+
+setuptools.setup(
+    name='forks-sync',
+    version='4.0.0',
+    description='Keep all your git forks up to date with the remote default branch.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/justintime50/forks-sync',
+    author='Justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(
+        exclude=[
+            'examples',
+            'test',
+        ]
+    ),
+    package_data={
+        'forks_sync': [
+            'py.typed',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIREMENTS,
+    extras_require={
+        'dev': DEV_REQUIREMENTS,
+    },
+    entry_points={
+        'console_scripts': [
+            'forks-sync=forks_sync.cli:main',
+        ],
+    },
+    python_requires='>=3.8, <4',
+)
