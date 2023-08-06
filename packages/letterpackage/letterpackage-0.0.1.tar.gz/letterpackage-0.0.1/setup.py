5f04c3d3cf3efaf2460f49823da47ca6244926f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:15 +0000

    Add package letterpackage/0.0.1/letterpackage-0.0.1.tar.gz

---
+++ packages/letterpackage/letterpackage-0.0.1.tar.gz/letterpackage-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='letterpackage',
+    version='0.0.1',
+    license='MIT',
+    description='Paquete DATA SCIENCE FEB23 EDEM',
+    author='Alumnos DATA SCIENCE FEB23 EDEM',
+    install_requires=['os','cv2','nunpy','pandas','sklearn','keras','matplotlib','xgboost','sklearn']
+)
