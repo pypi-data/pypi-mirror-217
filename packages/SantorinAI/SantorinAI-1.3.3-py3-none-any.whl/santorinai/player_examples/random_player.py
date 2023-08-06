2e46dc73630e6fa2b3cb8e0a6d044e7468263fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:16 +0000

    Add package SantorinAI/1.3.3/SantorinAI-1.3.3-py3-none-any.whl

---
+++ packages/SantorinAI/SantorinAI-1.3.3-py3-none-any.whl/santorinai/player_examples/random_player.py
@@ -0,0 +1,35 @@
+from santorinai.player import Player
+
+from random import choice
+
+
+class RandomPlayer(Player):
+    """
+    A player that places his pawns and moves them randomly
+    """
+
+    def name(self):
+        return "Randy Random"
+
+    def place_pawn(self, board, pawn):
+        available_positions = board.get_possible_movement_positions(pawn)
+        my_choice = choice(available_positions)
+        return my_choice
+
+    def play_move(self, board, pawn):
+        # Get movement positions
+        available_positions = board.get_possible_movement_positions(pawn)
+        if len(available_positions) == 0:
+            # The pawn cannot move
+            return None, None
+
+        my_move_choice = choice(available_positions)
+
+        # Simulate the move
+        pawn.move(my_move_choice)
+
+        # Get construction positions for the new position
+        available_positions = board.get_possible_building_positions(pawn)
+        my_build_choice = choice(available_positions)
+
+        return my_move_choice, my_build_choice
