f82dde9c754136349df232d0beb9724b41bf0344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:01 +0000

    Add package jsoner-lib/0.2/jsoner-lib-0.2.tar.gz

---
+++ packages/jsoner-lib/jsoner-lib-0.2.tar.gz/jsoner-lib-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='jsoner-lib',
+    version='0.2',
+    license='MIT',
+    author="kradt",
+    author_email='sidorenkoarem950@gmail.com',
+    packages=find_packages('jsoner'),
+    package_dir={'': 'jsoner'},
+    url='https://github.com/kradt/jsoner',
+    keywords='jsoner',
+    install_requires=[
+          'pydantic',
+      ],
+
+)
