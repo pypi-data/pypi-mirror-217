26883b1e262210b76b3dc2c7b15eb7eac8a5376e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:43 +0000

    Add package api-sentinel/1.0.0/api_sentinel-1.0.0.tar.gz

---
+++ packages/api-sentinel/api_sentinel-1.0.0.tar.gz/api_sentinel-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='api_sentinel',
+    version='1.0.0',
+    author='Omkar Ashrit',
+    description='api_sentinel is a Python module designed to validate all API calls received from partners for modifying orders. It provides a streamlined solution for verifying the integrity and validity of incoming API requests related to order modifications. With api_sentinel, you can ensure that the API calls from partners conform to the required specifications and meet the necessary criteria for order modifications.',
+)
