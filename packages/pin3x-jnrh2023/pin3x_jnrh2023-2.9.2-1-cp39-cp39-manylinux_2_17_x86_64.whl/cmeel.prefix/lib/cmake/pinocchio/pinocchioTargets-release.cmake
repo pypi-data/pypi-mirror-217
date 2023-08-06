1edfcb8980028dbe10cf1e45b11c6c9a023efa4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:46 +0000

    Add package pin3x-jnrh2023/2.9.2/pin3x_jnrh2023-2.9.2-1-cp39-cp39-manylinux_2_17_x86_64.whl

---
+++ packages/pin3x-jnrh2023/pin3x_jnrh2023-2.9.2-1-cp39-cp39-manylinux_2_17_x86_64.whl/cmeel.prefix/lib/cmake/pinocchio/pinocchioTargets-release.cmake
@@ -0,0 +1,39 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pinocchio::pinocchio_double" for configuration "Release"
+set_property(TARGET pinocchio::pinocchio_double APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pinocchio::pinocchio_double PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${PACKAGE_PREFIX_DIR}/lib/libpinocchio_double.so.2.9.2"
+  IMPORTED_SONAME_RELEASE "libpinocchio_double.so.2.9.2"
+  )
+
+list(APPEND _cmake_import_check_targets pinocchio::pinocchio_double )
+list(APPEND _cmake_import_check_files_for_pinocchio::pinocchio_double "${PACKAGE_PREFIX_DIR}/lib/libpinocchio_double.so.2.9.2" )
+
+# Import target "pinocchio::pinocchio_pywrap_default" for configuration "Release"
+set_property(TARGET pinocchio::pinocchio_pywrap_default APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pinocchio::pinocchio_pywrap_default PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${PACKAGE_PREFIX_DIR}/lib/python3.9/site-packages/pinocchio/pinocchio_pywrap_default.cpython-39-x86_64-linux-gnu.so.2.9.2"
+  IMPORTED_SONAME_RELEASE "pinocchio_pywrap_default.cpython-39-x86_64-linux-gnu.so.2.9.2"
+  )
+
+list(APPEND _cmake_import_check_targets pinocchio::pinocchio_pywrap_default )
+list(APPEND _cmake_import_check_files_for_pinocchio::pinocchio_pywrap_default "${PACKAGE_PREFIX_DIR}/lib/python3.9/site-packages/pinocchio/pinocchio_pywrap_default.cpython-39-x86_64-linux-gnu.so.2.9.2" )
+
+# Import target "pinocchio::pinocchio_pywrap_casadi" for configuration "Release"
+set_property(TARGET pinocchio::pinocchio_pywrap_casadi APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pinocchio::pinocchio_pywrap_casadi PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${PACKAGE_PREFIX_DIR}/lib/python3.9/site-packages/pinocchio/pinocchio_pywrap_casadi.cpython-39-x86_64-linux-gnu.so.2.9.2"
+  IMPORTED_SONAME_RELEASE "pinocchio_pywrap_casadi.cpython-39-x86_64-linux-gnu.so.2.9.2"
+  )
+
+list(APPEND _cmake_import_check_targets pinocchio::pinocchio_pywrap_casadi )
+list(APPEND _cmake_import_check_files_for_pinocchio::pinocchio_pywrap_casadi "${PACKAGE_PREFIX_DIR}/lib/python3.9/site-packages/pinocchio/pinocchio_pywrap_casadi.cpython-39-x86_64-linux-gnu.so.2.9.2" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
