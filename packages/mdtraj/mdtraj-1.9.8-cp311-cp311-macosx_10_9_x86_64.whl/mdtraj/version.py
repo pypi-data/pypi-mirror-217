c560d05485ee2a39131f56a61518717b6f39264a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:49 +0000

    Add package mdtraj/1.9.8/mdtraj-1.9.8-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/mdtraj/mdtraj-1.9.8-cp311-cp311-macosx_10_9_x86_64.whl/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.9.8'
+short_version = '1.9.8'
+full_version = '1.9.8'
+git_revision = '073462fa20b7c12a9208b7dfcc9da932267d5e13'
+release = True
