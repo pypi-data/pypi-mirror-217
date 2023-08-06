d9ebe2f1b833b8c2de3ba65ecd75af04041ec1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:34 +0000

    Add package cloudcix/0.9.7/cloudcix-0.9.7.tar.gz

---
+++ packages/cloudcix/cloudcix-0.9.7.tar.gz/cloudcix-0.9.7/cloudcix/api/circuit.py
@@ -0,0 +1,25 @@
+from cloudcix.client import Client
+
+
+class Circuit:
+    """
+    The Circuit Application allows for the management of circuits and devices.
+    """
+    _application_name = 'Circuit'
+
+    circuit = Client(
+        _application_name,
+        'circuit/',
+    )
+    circuit_class = Client(
+        _application_name,
+        'circuit_class/',
+    )
+    property_type = Client(
+        _application_name,
+        'property_type/',
+    )
+    property_value = Client(
+        _application_name,
+        'property_value/{search_term}/',
+    )
