1979924fdab467b804f168a738632b5df2f928e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:13 +0000

    Add package osha.oira/8.3.3/osha.oira-8.3.3.tar.gz

---
+++ packages/osha.oira/osha.oira-8.3.3.tar.gz/osha.oira-8.3.3/src/osha/oira/client/browser/dashboard_banner.py
@@ -0,0 +1,47 @@
+from euphorie.client.country import IClientCountry
+from euphorie.client.model import get_current_account
+from osha.oira.client.model import NewsletterSetting
+from Products.Five import BrowserView
+from z3c.saconfig import Session
+
+
+class View(BrowserView):
+    """View for the dashboard banner."""
+
+    _value = "call-for-action-banner-disabled"
+
+    @property
+    def preferences_url(self):
+        for obj in self.context.aq_chain:
+            if IClientCountry.providedBy(obj):
+                return f"{obj.absolute_url()}/@@preferences#content"
+        return ""
+
+    def available(self):
+        """Check if the user already closed the banner in the past."""
+        account = get_current_account()
+        if not account:
+            return False
+        return (
+            Session.query(NewsletterSetting)
+            .filter(
+                NewsletterSetting.account_id == account.id,
+                NewsletterSetting.value == self._value,
+            )
+            .count()
+            == 0
+        )
+
+    def __call__(self):
+        if (
+            self.request.method == "POST"
+            and self.request.form.get("hide_banner") == "1"
+        ):
+            Session.add(
+                NewsletterSetting(
+                    account_id=get_current_account().getId(),
+                    value=self._value,
+                )
+            )
+            self.request.response.redirect(self.context.absolute_url())
+        return super().__call__()
