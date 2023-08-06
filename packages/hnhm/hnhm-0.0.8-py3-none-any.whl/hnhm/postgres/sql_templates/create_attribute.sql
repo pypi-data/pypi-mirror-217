829965b40ca24d5c925a5ce7689694be8fa4a367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:26 +0000

    Add package hnhm/0.0.8/hnhm-0.0.8-py3-none-any.whl

---
+++ packages/hnhm/hnhm-0.0.8-py3-none-any.whl/hnhm/postgres/sql_templates/create_attribute.sql
@@ -0,0 +1,13 @@
+CREATE TABLE attr__{{ entity_name }}__{{ attribute_name }}(
+    {{ entity_name }}_sk VARCHAR(32) NOT NULL,
+    {{ attribute_name }} {{ attribute_type }},
+    {% for time_column in time_columns -%}
+        {{ time_column }},
+    {% endfor -%}
+    _source VARCHAR(512) NOT NULL,
+    _loaded_at TIMESTAMPTZ NOT NULL,
+    CONSTRAINT fk_{{ entity_name }}_sk
+      FOREIGN KEY({{ entity_name }}_sk) 
+        REFERENCES hub__{{ entity_name }}({{ entity_name }}_sk)
+        ON DELETE NO ACTION
+)
