07e9b9e12555afdef6378b642580368264eb1c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:54 +0000

    Add package tkadw/0.3.0/tkadw-0.3.0.tar.gz

---
+++ packages/tkadw/tkadw-0.3.0.tar.gz/tkadw-0.3.0/tkadw/windows/canvas/__init__.py
@@ -0,0 +1,15 @@
+# 0.2.0加入
+from tkadw.windows.canvas.drawengine import AdwDrawEngine, HORIZONTAL, VERTICAL, ARC, CIRCULAR, CENTRAL
+from tkadw.windows.canvas.button import AdwDrawButton, AdwDrawDarkButton, AdwDrawAccentButton, \
+    AdwDrawRoundButton, AdwDrawRoundDarkButton, AdwDrawRoundAccentButton, \
+    AdwDrawRoundButton2, AdwDrawRoundDarkButton2, AdwDrawRoundAccentButton2, \
+    AdwDrawRoundButton3, AdwDrawRoundDarkButton3, AdwDrawRoundAccentButton3, \
+    AdwDrawCircularButton, AdwDrawCircularDarkButton
+from tkadw.windows.canvas.entry import AdwDrawEntry, AdwDrawDarkEntry, AdwDrawRoundEntry, AdwDrawRoundDarkEntry, \
+    AdwDrawRoundEntry3, AdwDrawRoundDarkEntry3
+from tkadw.windows.canvas.textbox import AdwDrawText, AdwDrawDarkText, AdwDrawRoundText, AdwDrawRoundDarkText, \
+    AdwDrawRoundText3, AdwDrawRoundDarkText3
+
+# 0.3加入
+from tkadw.windows.canvas.widget import AdwWidget
+# from tkadw.canvas.label import AdwDrawLabel, AdwDrawDarkLabel 已移除
