bc6a50f69cce124424b7e34aae11d0a273eb042c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:53 +0000

    Add package noiseflow/0.0.5/noiseflow-0.0.5-cp311-cp311-win_amd64.whl

---
+++ packages/noiseflow/noiseflow-0.0.5-cp311-cp311-win_amd64.whl/noiseflow/__init__.py
@@ -0,0 +1,42 @@
+import os
+import re
+import json
+
+
+env = os.environ.get('CONDA_DEFAULT_ENV')
+env = re.sub('[^a-zA-Z0-9_]', '', env)[0:50]
+config_path = os.path.abspath(os.path.expanduser(f'~/.noiseflow/config_{env}.json'))    
+
+try:
+    from noiseflow.lib import cc_share
+    from noiseflow.lib import signal_share
+    NOISEFLOW_USE_CPP = True
+except:
+    NOISEFLOW_USE_CPP = False
+
+os.makedirs(os.path.expanduser('~/.noiseflow'), exist_ok=True)
+compile_time_env = {"NOISEFLOW_USE_CPP": NOISEFLOW_USE_CPP}
+with open(config_path, 'w') as f:
+    json.dump(compile_time_env, f)
+
+
+from noiseflow.cc.wrapper import rfft, corr, stack
+from noiseflow.cc.rfftdata import RFFTData_Class
+from noiseflow.cc.corrdata import CorrData_Class
+from noiseflow.cc.stackdata import StackData_Class
+from noiseflow.cc.utils_time import time_linspace, get_timestamp, get_stack_timestamp
+from noiseflow.cc.utils_load import load_raw, load_rfft, load_corr, load_stack
+
+from noiseflow.signal.rawdata import RawData_Class
+from noiseflow.signal.wrapper import bandpass, bandstop, lowpass, highpass, detrend, decimate, taper
+
+from noiseflow.client.client import downloader, downloader_https
+
+from noiseflow.dispersion import app_dispersion, test
+
+__all__ = ["wrapper", "dispersion", "tests"]
+
+
+
+
+__version__ = "0.0.1"
