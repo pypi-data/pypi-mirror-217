aeaa579355464957ccc3b41e24971bfbf53ceaeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:14 +0000

    Add package Inde-Abbre/0.0.6/Inde_Abbre-0.0.6-py3-none-any.whl

---
+++ packages/Inde-Abbre/Inde_Abbre-0.0.6-py3-none-any.whl/Inde_Abbre/__init__.py
@@ -0,0 +1,46 @@
+
+import pandas as pd
+import json
+
+# df = pd.read_csv("E:\\Inde_Abbre\\List_of_Abbrevations_Main.csv")
+
+
+# # you can simply use the dataframe (df) to read the columns and covert them as a dict and save it as a jSON file
+# # below is the part
+# abb_dict = {}
+# for i, j in df.iterrows():
+#     abb_dict[j['Unnamed: 0']] = j['List of Abbreviations ']
+
+# now save your dict to JSON
+# with open("output_2.json", 'w') as fp:
+#     json.dump(abb_dict, fp)
+
+# now sload the same json as use it
+
+try:
+    
+    def Abbreviation(arr):
+        """The passing argument should be given in string characters within quotes
+        Abbreviation: "lic"
+        will give an output of list Life Insurance of corporation
+        """
+        with open("Inde_Abbre/output_2.json",) as jfile:
+            loaded_dict = json.load(jfile)
+            arr1=arr.lower().strip()
+            emty_list=[]
+            for i,j in loaded_dict.items():
+                if i==arr1:
+                    emty_list.append(j)
+                    print(emty_list)
+                    break
+            else:
+                print(" The data is not available")
+                
+except Exception as e:
+    print(e)
+
+
+
+
+
+
