50ffecc657b1cce7abceec7d92b7343b339a217f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:47 +0000

    Add package alanapy/0.11/alanapy-0.11.tar.gz

---
+++ packages/alanapy/alanapy-0.11.tar.gz/alanapy-0.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="alanapy",
+    version="0.11",
+    license="MIT",
+    author = "Orkahub Energy",
+	author_email = "orkahub@gmail.com",
+	url = "https://github.com/orkahub/alanapy",
+    long_description="Script for Alana API connection and functions https://alana.tech/open/accounts/login",
+    long_description_content_type = "text/x-rst",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    keywords= ["RESERVOIR", "ENERGY", "OIL", "GAS", "PRODUCTION", "PYTHON","ALANA"],
+    install_requires=[
+        "requests",
+        "matplotlib",
+        "numpy",
+        "pandas"
+    ],
+)
