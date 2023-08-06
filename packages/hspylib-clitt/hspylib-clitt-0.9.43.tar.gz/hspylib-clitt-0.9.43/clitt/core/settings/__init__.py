f37a38ba641621e058532c825a2d97052848a003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:58 +0000

    Add package hspylib-clitt/0.9.43/hspylib-clitt-0.9.43.tar.gz

---
+++ packages/hspylib-clitt/hspylib-clitt-0.9.43.tar.gz/hspylib-clitt-0.9.43/clitt/core/settings/__init__.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-clitt v0.9.43
+#
+# Package: main.clitt.core.settings
+"""Package initialization."""
+
+__all__ = [
+  'settings', 
+  'settings_config', 
+  'settings_entry', 
+  'settings_repository', 
+  'settings_service'
+]
+__version__ = '0.9.43'
