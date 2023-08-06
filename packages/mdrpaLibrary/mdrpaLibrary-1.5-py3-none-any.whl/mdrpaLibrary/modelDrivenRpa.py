d355d80b93971d8872a1328e1a2377a6b03b1961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:21 +0000

    Add package mdrpaLibrary/1.5/mdrpaLibrary-1.5-py3-none-any.whl

---
+++ packages/mdrpaLibrary/mdrpaLibrary-1.5-py3-none-any.whl/mdrpaLibrary/modelDrivenRpa.py
@@ -0,0 +1,50 @@
+from robot.api.deco import keyword
+from robot.libraries.BuiltIn import BuiltIn
+
+class ModelDrivenRpa:
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+
+    def __init__(self):
+        self.builtin = BuiltIn()
+        self.resource_files = [
+            'clickButtonModel.robot',
+            'inputFieldModel.robot',
+            'selectCheckboxModel.robot',
+            'selectFromDropdownModel.robot',
+            'getUiModels.robot'
+            'sendReportModel.robot'
+        ]  
+
+    def import_resource_file(self, resource_file):
+        self.builtin.import_resource(resource_file)
+
+    @keyword
+    def click_button_model(self, *args):
+        self.import_resource_file(self.resource_files[0])  
+        self.builtin.run_keyword('Click Button Model', *args)
+
+    @keyword
+    def input_field_model(self, *args):
+        self.import_resource_file(self.resource_files[1])  
+        self.builtin.run_keyword('Input Field Model', *args)
+
+    @keyword
+    def select_checkbox_model(self, *args):
+        self.import_resource_file(self.resource_files[2])  
+        self.builtin.run_keyword('Select Checkbox Model', *args)
+
+    @keyword
+    def select_from_dropdown_model(self, *args):
+        self.import_resource_file(self.resource_files[3])  
+        self.builtin.run_keyword('Select Value From Dropdown Model', *args)
+    
+    @keyword
+    def get_ui_models(self, *args):
+        self.import_resource_file(self.resource_files[4]) 
+        self.builtin.run_keyword('Get UiModels', *args)
+    
+    @keyword
+    def send_report_to_UI_Modeler(self, *args):
+        self.import_resource_file(self.resource_files[5]) 
+        self.builtin.run_keyword('Send Report to UI Modeler', *args)
+
