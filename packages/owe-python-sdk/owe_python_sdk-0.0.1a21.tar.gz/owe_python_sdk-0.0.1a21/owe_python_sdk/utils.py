e3dc5a543df412a990e1526da74ceaa9b3c1d087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:38 +0000

    Add package owe-python-sdk/0.0.1a21/owe_python_sdk-0.0.1a21.tar.gz

---
+++ packages/owe-python-sdk/owe_python_sdk-0.0.1a21.tar.gz/owe_python_sdk-0.0.1a21/owe_python_sdk/utils.py
@@ -0,0 +1,32 @@
+def adapt_runner(Runner, RunnerClassExtension: type, config_override: dict={}):
+    def __init__(self, *args, **kwargs):
+        Runner.__init__(self)
+        RunnerClassExtension.__init__(self, *args, **kwargs)
+
+    # Raise Exception if RunnerClassExtension does not have a
+    # Config property
+    if not hasattr(RunnerClassExtension, "Config"):
+        raise AttributeError(f"Runner class '{RunnerClassExtension.__name__}' is missing a Config class")
+
+    # Override the config on the RunnerClassExtension
+    for key, val in config_override.items():
+        setattr(RunnerClassExtension.Config, key, val)
+
+    AdaptedRunner = type(
+        "AdaptedRunner",
+        (RunnerClassExtension, Runner),
+        {"__init__": __init__}
+    )
+
+    return AdaptedRunner
+
+def get_schema_extensions(plugins, _type, sub_type=None):
+    schema_extensions = []
+    for plugin in plugins:
+        schema_extensions = schema_extensions + [ 
+            ext.schema for ext in plugin.schema_extensions 
+            if ext.type == _type 
+            and (ext.sub_type == sub_type or sub_type == None)
+        ]
+
+    return schema_extensions
