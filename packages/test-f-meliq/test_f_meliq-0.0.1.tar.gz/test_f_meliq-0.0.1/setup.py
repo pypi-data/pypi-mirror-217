b5c8b231f761022437e403f30c3aa5cedf7c89ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:35 +0000

    Add package test-f-meliq/0.0.1/test_f_meliq-0.0.1.tar.gz

---
+++ packages/test-f-meliq/test_f_meliq-0.0.1.tar.gz/test_f_meliq-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name = "test_f_meliq",
+    version = "0.0.1",
+    author = "Meliq",
+    description = "It,s test Description",
+    packages = ["test_f_meliq"],
+)
