7aa2bb6225c6a39a90e9e740746a4431f93fbbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:51 +0000

    Add package autoconf-language-server/0.0.6/autoconf-language-server-0.0.6.tar.gz

---
+++ packages/autoconf-language-server/autoconf-language-server-0.0.6.tar.gz/autoconf-language-server-0.0.6/tests/server_test.py
@@ -0,0 +1,14 @@
+r"""Test server"""
+from autoconf_language_server.server import get_document
+
+
+class Test:
+    r"""Test."""
+
+    @staticmethod
+    def test_get_document() -> None:
+        r"""Test get document.
+
+        :rtype: None
+        """
+        assert len(get_document().get("AC_INIT", "").splitlines()) > 1
