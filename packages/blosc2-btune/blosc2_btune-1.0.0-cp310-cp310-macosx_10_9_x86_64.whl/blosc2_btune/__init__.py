863a7436152a59800d9f307552800044c2a57125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:32 +0000

    Add package blosc2-btune/1.0.0/blosc2_btune-1.0.0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/blosc2-btune/blosc2_btune-1.0.0-cp310-cp310-macosx_10_9_x86_64.whl/blosc2_btune/__init__.py
@@ -0,0 +1,30 @@
+# Blosc - Blocked Shuffling and Compression Library
+#
+# Copyright (C) 2021  The Blosc Developers <blosc@blosc.org>
+# https://blosc.org
+# License: BSD 3-Clause (see LICENSE.txt)
+#
+# See LICENSE.txt for details about copyright and rights to use.
+
+VERSION = "1.0.0"
+
+import os
+import platform
+from pathlib import Path
+
+def print_libpath():
+    system = platform.system()
+    if system == "Linux":
+        libname = "libblosc2_btune.so"
+    elif system == "Darwin":
+        libname = "libblosc2_btune.so"
+    elif system == "Windows":
+        libname = "libblosc2_btune.dll"
+    else:
+        raise RuntimeError("Unsupported system: ", system)
+    libpath = os.path.abspath(Path(__file__).parent / libname)
+    print(libpath, end="")
+
+
+if __name__ == "__main__":
+    print_libpath()
