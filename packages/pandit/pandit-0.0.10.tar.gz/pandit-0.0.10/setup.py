09028593be001552f7cba5ddfdbb8a73bf583ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:32 +0000

    Add package pandit/0.0.10/pandit-0.0.10.tar.gz

---
+++ packages/pandit/pandit-0.0.10.tar.gz/pandit-0.0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='pandit',
+      version='v0.0.10',
+      description='Pandas with some cool additional features',
+      url='https://github.com/sileod/pandit',
+      author='sileod',
+      license='GPL',
+      install_requires=['pandas'],
+      py_modules=['pandit'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False)
