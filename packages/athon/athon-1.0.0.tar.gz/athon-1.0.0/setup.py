7ebd20c92552bfb212c88dee97d7842438edeeb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:59 +0000

    Add package athon/1.0.0/athon-1.0.0.tar.gz

---
+++ packages/athon/athon-1.0.0.tar.gz/athon-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='athon',
+    version='1.0.0',
+    author='Omkar Ashrit',
+    description='Athon is a Python module that wraps AWS Athena for seamless execution of SQL queries from Python, enabling analysis, visualization, and exporting of data. It supports creating Athena tables directly from DataFrames',
+)
