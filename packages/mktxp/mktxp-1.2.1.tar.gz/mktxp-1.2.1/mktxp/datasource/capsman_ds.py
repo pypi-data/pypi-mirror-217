3570926402a8a329b4fd404a1e469079df38343b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:36 +0000

    Add package mktxp/1.2.1/mktxp-1.2.1.tar.gz

---
+++ packages/mktxp/mktxp-1.2.1.tar.gz/mktxp-1.2.1/mktxp/datasource/capsman_ds.py
@@ -0,0 +1,88 @@
+# coding=utf8
+## Copyright (c) 2020 Arseniy Kuznetsov
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+
+from mktxp.datasource.base_ds import BaseDSProcessor
+from mktxp.datasource.wireless_ds import WirelessMetricsDataSource
+
+
+class CapsmanInfo:
+    @staticmethod
+    def capsman_path(router_entry):
+        if WirelessMetricsDataSource.wifiwave2_installed(router_entry):
+            return '/interface/wifiwave2/capsman'
+        else:
+            return '/caps-man'
+
+    @staticmethod
+    def registration_table_path(router_entry):
+        if WirelessMetricsDataSource.wifiwave2_installed(router_entry):
+            return '/interface/wifiwave2/registration-table'
+        else:
+            return '/caps-man/registration-table'
+
+class CapsmanCapsMetricsDataSource:
+    ''' Caps Metrics data provider
+    '''             
+    @staticmethod
+    def metric_records(router_entry, *, metric_labels = None):
+        if metric_labels is None:
+            metric_labels = []                
+        try:
+            capsman_path = CapsmanInfo.capsman_path(router_entry)
+            remote_caps_records = router_entry.api_connection.router_api().get_resource(f'{capsman_path}/remote-cap').get()
+            return BaseDSProcessor.trimmed_records(router_entry, router_records = remote_caps_records, metric_labels = metric_labels)
+        except Exception as exc:
+            print(f'Error getting CAPsMAN remote caps info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}')
+            return None
+
+
+class CapsmanRegistrationsMetricsDataSource:
+    ''' Capsman Registrations Metrics data provider
+    '''             
+    @staticmethod
+    def metric_records(router_entry, *, metric_labels = None,  add_router_id = True):
+        if metric_labels is None:
+            metric_labels = []                
+        try:
+            registration_table_path = CapsmanInfo.registration_table_path(router_entry)
+            registration_table_records = router_entry.api_connection.router_api().get_resource(f'{registration_table_path}').get()
+            
+            # With wifiwave2, Mikrotik renamed the field 'rx-signal' to 'signal' 
+            # For backward compatibility, including both variants
+            for record in registration_table_records:
+                if 'signal' in record:
+                    record['rx-signal'] = record['signal']
+
+            return BaseDSProcessor.trimmed_records(router_entry, router_records = registration_table_records, metric_labels = metric_labels, add_router_id = add_router_id)
+        except Exception as exc:
+            print(f'Error getting CAPsMAN registration table info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}')
+            return None
+
+
+class CapsmanInterfacesDatasource:
+    ''' Data provider for CAPsMaN interfaces
+    '''
+
+    @staticmethod
+    def metric_records(router_entry, *, metric_labels = None):
+        if WirelessMetricsDataSource.wireless_package(router_entry) == WirelessMetricsDataSource.WIFIWAVE2:
+            return None            
+        if metric_labels is None:
+            metric_labels = []                
+        try:
+            caps_interfaces = router_entry.api_connection.router_api().get_resource('/caps-man/interface').get()
+            return BaseDSProcessor.trimmed_records(router_entry, router_records = caps_interfaces, metric_labels = metric_labels)
+        except Exception as exc:
+            print(f'Error getting CAPsMAN interfaces info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}')
+            return None
---
+++ packages/mktxp/mktxp-1.2.1.tar.gz/mktxp-1.2.1/mktxp/datasource/wireless_ds.py
@@ -0,0 +1,54 @@
+# coding=utf8
+## Copyright (c) 2020 Arseniy Kuznetsov
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+
+from mktxp.datasource.base_ds import BaseDSProcessor
+from mktxp.datasource.package_ds import PackageMetricsDataSource
+
+
+class WirelessMetricsDataSource:
+    ''' Wireless Metrics data provider
+    '''             
+    WIFIWAVE2 = 'wifiwave2'
+    WIRELESS = 'wireless'
+
+    @staticmethod
+    def metric_records(router_entry, *, metric_labels = None, add_router_id = True):
+        if metric_labels is None:
+            metric_labels = []                
+        try:
+            wireless_package = WirelessMetricsDataSource.wireless_package(router_entry)
+            registration_table_records = router_entry.api_connection.router_api().get_resource(f'/interface/{wireless_package}/registration-table').get()
+
+            # With wifiwave2, Mikrotik renamed the field 'signal-strength' to 'signal' 
+            # For backward compatibility, including both variants
+            for record in registration_table_records:
+                if 'signal' in record:
+                    record['signal-strength'] = record['signal']
+
+            return BaseDSProcessor.trimmed_records(router_entry, router_records = registration_table_records, metric_labels = metric_labels, add_router_id = add_router_id,)
+        except Exception as exc:
+            print(f'Error getting wireless registration table info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}')
+            return None
+
+
+    @staticmethod
+    def wireless_package(router_entry):
+        if not router_entry.wifi_package:
+            ww2_installed = PackageMetricsDataSource.is_package_installed(router_entry, package_name = WirelessMetricsDataSource.WIFIWAVE2)
+            router_entry.wifi_package = WirelessMetricsDataSource.WIFIWAVE2 if ww2_installed else WirelessMetricsDataSource.WIRELESS
+        return router_entry.wifi_package
+
+    @staticmethod
+    def wifiwave2_installed(router_entry):
+        return WirelessMetricsDataSource.wireless_package(router_entry) == WirelessMetricsDataSource.WIFIWAVE2
