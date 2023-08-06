6f815ff97727ba9eb609e319bcf7016238070751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:07 +0000

    Add package minty/3.0.9/minty-3.0.9-py3-none-any.whl

---
+++ packages/minty/minty-3.0.9-py3-none-any.whl/minty/config/parser/json_parser.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2020 Mintlab B.V.
+#
+# SPDX-License-Identifier: EUPL-1.2
+
+import json
+from .base import ConfigParserBase
+from typing import Dict
+
+
+class JSONConfigParser(ConfigParserBase):
+    __slots__ = ["content"]
+
+    def parse(self, content: str) -> Dict:
+        """Parse JSON file and return dict.
+
+        :param content: content from JSON file
+        :type content: str
+        :raises ValueError: if content is empty
+        :return: parsed config dict
+        :rtype: dict
+        """
+        preparsed_content = super().parse(content)
+
+        if preparsed_content["content"] == "":
+            raise ValueError("Cannot parse empty configuration")
+
+        config = json.loads(preparsed_content["content"])
+
+        return config
