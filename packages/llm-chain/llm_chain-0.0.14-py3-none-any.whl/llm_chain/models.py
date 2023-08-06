f973198c8198db2aa769541dcfe05c02c4964e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:55 +0000

    Add package llm-chain/0.0.14/llm_chain-0.0.14-py3-none-any.whl

---
+++ packages/llm-chain/llm_chain-0.0.14-py3-none-any.whl/llm_chain/models.py
@@ -0,0 +1,172 @@
+"""Contains models."""
+from collections.abc import Callable
+from llm_chain.base import ChatModel, Document, EmbeddingsRecord, EmbeddingsModel, MemoryBuffer, \
+    MessageRecord, StreamingEvent
+from llm_chain.resources import MODEL_COST_PER_TOKEN
+from llm_chain.utilities import num_tokens, num_tokens_from_messages, retry_handler
+
+
+class OpenAIEmbeddings(EmbeddingsModel):
+    """
+    Input: list of documents
+    Output: list of documents with embeddings.
+    """
+
+    def __init__(
+            self,
+            model_name: str,
+            doc_prep: Callable[[str], str] = lambda x: x.strip().replace('\n', ' '),
+            timeout: int = 10,
+            ) -> None:
+        """
+        TODO.
+
+        NOTE: TODO: cleanup explaination; running this on docs creates side effect and populates
+        .embedding property and returns the same list of objects
+
+        Args:
+            model_name: e.g. 'text-embedding-ada-002'
+            doc_prep:
+                function that cleans the text of each doc before creating embeddings.
+            timeout: TODO
+        """
+        super().__init__()
+        self.model_name = model_name
+        self.doc_prep = doc_prep
+        self.timeout = timeout
+
+    def _run(self, docs: list[Document]) -> tuple[list[list[float]], EmbeddingsRecord]:
+        """TODO."""
+        import openai
+        texts = [self.doc_prep(x.content) for x in docs]
+        response = retry_handler()(
+            openai.Embedding.create,
+            input = texts,
+            model=self.model_name,
+            timeout=self.timeout,
+        )
+        total_tokens = response['usage']['total_tokens']
+        embeddings = [x['embedding'] for x in response['data']]
+        metadata = EmbeddingsRecord(
+            metadata={'model_name': self.model_name},
+            total_tokens=total_tokens,
+            cost=self.cost_per_token * total_tokens,
+        )
+        return embeddings, metadata
+
+    @property
+    def cost_per_token(self) -> dict:
+        """TODO."""
+        return MODEL_COST_PER_TOKEN[self.model_name]
+
+class OpenAIChat(ChatModel):
+    """
+    Input: prompt/query (string).
+    Output: the response (string).
+    """
+
+    def __init__(
+            self,
+            model_name: str,
+            temperature: float = 0,
+            max_tokens: int = 2000,
+            system_message: str = 'You are a helpful assistant.',
+            streaming_callback: Callable[[None], StreamingEvent] | None = None,
+            memory_strategy: MemoryBuffer | None = None,
+            timeout: int = 10,
+            ) -> None:
+        """TODO."""
+        # TODO: doc string model_name e.g. 'gpt-3.5-turbo'
+        # copied from https://github.com/hwchase17/langchain/blob/master/langchain/callbacks/openai_info.py
+        super().__init__()
+        self.model_name = model_name
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.memory_strategy = memory_strategy
+        self.system_message = {'role': 'system', 'content': system_message}
+        self.streaming_callback = streaming_callback
+        self.timeout = timeout
+        self._previous_memory = None
+
+    def _run(self, prompt: str) -> MessageRecord:
+        """
+        `openai.ChatCompletion.create` expects a list of messages with various roles (i.e. system,
+        user, assistant). This function builds the list of messages based on the history of
+        messages and based on an optional 'memory_strategy' that filters the history based on
+        it's own logic. The `system_message` is always the first message regardless if a
+        `memory_strategy` is passed in.
+
+        TODO: explain callback; passing in callback results in turning streaming on; end response
+        should not change though;
+        """
+        import openai
+        # initial message
+        messages = [self.system_message]
+        # build up messages from history
+        memory = self._history.copy()
+        if self.memory_strategy:
+            memory = self.memory_strategy(history=memory)
+        for message in memory:
+            messages += [
+                {'role': 'user', 'content': message.prompt},
+                {'role': 'assistant', 'content': message.response},
+            ]
+        # add latest prompt to messages
+        messages += [{'role': 'user', 'content': prompt}]
+        if self.streaming_callback:
+            response = retry_handler()(
+                openai.ChatCompletion.create,
+                model=self.model_name,
+                messages=messages,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                timeout=self.timeout,
+                stream=True,
+            )
+            def get_delta(chunk):  # noqa
+                delta = chunk['choices'][0]['delta']
+                if 'content' in delta:
+                    return delta['content']
+                return None
+            response_message = ''
+            for chunk in response:
+                delta = get_delta(chunk)
+                if delta:
+                    self.streaming_callback(StreamingEvent(response=delta))
+                    response_message += delta
+
+            prompt_tokens = num_tokens_from_messages(model_name=self.model_name, messages=messages)
+            completion_tokens = num_tokens(model_name=self.model_name, value=response_message)
+            total_tokens = prompt_tokens + completion_tokens
+        else:
+            response = retry_handler()(
+                openai.ChatCompletion.create,
+                model=self.model_name,
+                messages=messages,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                timeout=self.timeout,
+            )
+            response_message = response['choices'][0]['message'].content
+            prompt_tokens = response['usage'].prompt_tokens
+            completion_tokens = response['usage'].completion_tokens
+            total_tokens = response['usage'].total_tokens
+
+        self._previous_memory = messages
+        cost = (prompt_tokens * self.cost_per_token['input']) + \
+            (completion_tokens * self.cost_per_token['output'])
+
+        return MessageRecord(
+            prompt=prompt,
+            response=response_message,
+            metadata={'model_name': self.model_name},
+            prompt_tokens=prompt_tokens,
+            response_tokens=completion_tokens,
+            total_tokens=total_tokens,
+            cost=cost,
+        )
+
+    @property
+    def cost_per_token(self) -> dict:
+        """TODO."""
+        return MODEL_COST_PER_TOKEN[self.model_name]
---
+++ packages/llm-chain/llm_chain-0.0.14-py3-none-any.whl/llm_chain/tools.py
@@ -0,0 +1,194 @@
+"""TODO."""
+import os
+from itertools import islice
+import re
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+from pydantic import BaseModel, Field, validator
+from markdownify import markdownify as html_to_markdown
+from llm_chain.utilities import retry_handler
+from llm_chain.base import Document, HistoricalData, Record, RequestError
+
+
+def split_documents(
+        docs: list[Document],
+        max_chars: int = 500,
+        preserve_words: bool = True) -> list[Document]:
+    """TODO. Mention that it keeps the entire word. Does not return empty documents."""
+    new_docs = []
+    for doc in docs:
+        if doc.content:
+            content = doc.content.strip()
+            metadata = doc.metadata
+            while len(content) > max_chars:
+                # find the last space that is within the limit
+                if preserve_words:
+                    split_at = max_chars
+                    # find the last whitespace that is within the limit
+                    # if no whitespace found, take the whole chunk
+                    # we are going 1 beyond the limit in case it is whitespace so that
+                    # we can keep everything up until that point
+                    for match in re.finditer(r'\s', content[:max_chars + 1]):
+                        split_at = match.start()
+                else:
+                    split_at = max_chars
+                new_doc = Document(
+                    content=content[:split_at].strip(),
+                    metadata=metadata,
+                )
+                new_docs.append(new_doc)
+                content = content[split_at:].strip()  # remove the chunk we added
+            # check for remaining/leftover content
+            if content:
+                new_docs.append(Document(
+                    content=content,
+                    metadata=metadata,
+                ))
+    return new_docs
+
+
+def scrape_url(url: str) -> str:
+    """TODO."""
+    import requests
+    from bs4 import BeautifulSoup
+    response = requests.get(url)
+    if response.status_code != 200:  # let client decide what to do
+        raise RequestError(status_code=response.status_code, reason=response.reason)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup.get_text().strip()
+
+
+class SearchRecord(Record):
+    """TODO."""
+
+    query: str
+    results: list[dict]
+
+
+class DuckDuckGoSearch(HistoricalData):
+    """TODO."""
+
+    def __init__(self, top_n: int = 3):
+        self.top_n = top_n
+        self._history = []
+
+    def __call__(self, query: str) -> list[dict]:
+        """TODO."""
+        from duckduckgo_search import DDGS
+        with DDGS() as ddgs:
+            ddgs_generator = ddgs.text(query, region='wt-wt', safesearch='Off', timelimit='y')
+            results = list(islice(ddgs_generator, self.top_n))
+        self._history.append(SearchRecord(
+            query=query,
+            results=results,
+        ))
+        return results
+
+    @property
+    def history(self) -> list[SearchRecord]:
+        """TODO."""
+        return self._history
+
+
+class StackAnswer(BaseModel):
+    """TODO."""
+
+    answer_id: int
+    is_accepted: bool
+    score: int
+    body: str
+    text: str | None
+    markdown: str | None
+    creation_date: int
+
+    def __init__(self, **data):  # noqa
+        super().__init__(**data)
+        self.text = BeautifulSoup(self.body, 'html.parser').get_text(separator=' ')
+        self.markdown = html_to_markdown(self.body)
+
+
+    @validator('creation_date')
+    def convert_to_datetime(cls, value: str) -> datetime:  # noqa: N805
+        """Convert from string to datetime."""
+        return datetime.fromtimestamp(value)
+
+
+class StackQuestion(BaseModel):
+    """TODO."""
+
+    question_id: int
+    score: int
+    creation_date: int
+    answer_count: int
+    title: str
+    link: str
+    body: str
+    text: str | None
+    markdown: str | None
+    content_license: str | None
+    answers: list[StackAnswer] = Field(default_factory=list)
+
+    def __init__(self, **data):  # noqa
+        super().__init__(**data)
+        self.text = BeautifulSoup(self.body, 'html.parser').get_text(separator=' ')
+        self.markdown = html_to_markdown(self.body)
+
+    @validator('creation_date')
+    def convert_to_datetime(cls, value: str) -> datetime:  # noqa: N805
+        """Convert from string to datetime."""
+        return datetime.fromtimestamp(value)
+
+
+def _get_stack_overflow_answers(question_id: int, max_answers: int = 2) -> list[StackAnswer]:
+    """For a given question_id on Stack Overflow, returns the top `num_answers`."""
+    params = {
+        'site': 'stackoverflow',
+        'key': os.getenv('STACK_OVERFLOW_KEY'),
+        'filter': 'withbody',  # Include the answer body in the response
+        'sort': 'votes',  # Sort answers by votes (highest first)
+        'order': 'desc',
+        'pagesize': max_answers,  # Fetch only the top answers
+    }
+    response = retry_handler()(
+        requests.get,
+        f'https://api.stackexchange.com/2.3/questions/{question_id}/answers',
+        params=params,
+    )
+    if response.status_code != 200:  # let client decide what to do
+        raise RequestError(status_code=response.status_code, reason=response.reason)
+    answers = response.json().get('items', [])
+    return [StackAnswer(**x) for x in answers]
+
+
+def search_stack_overflow(
+        query: str,
+        max_questions: int = 2,
+        max_answers: int = 2) -> list[StackQuestion]:
+    """TODO."""
+    params = {
+        'site': 'stackoverflow',
+        'key': os.getenv('STACK_OVERFLOW_KEY'),
+        'q': query,
+        'sort': 'relevance',
+        'order': 'desc',
+        'filter': 'withbody',  # Include the question body in the response
+        'pagesize': max_questions,
+        'page': 1,
+    }
+    response = retry_handler()(
+        requests.get,
+        'https://api.stackexchange.com/2.3/search/advanced',
+        params=params,
+    )
+    assert response.status_code == 200
+    questions = response.json().get('items', [])
+    questions = [StackQuestion(**x) for x in questions]
+
+    for question in questions:
+        if question.answer_count > 0:
+            question.answers = _get_stack_overflow_answers(
+                question_id=question.question_id,
+                max_answers=max_answers,
+            )
+    return questions
