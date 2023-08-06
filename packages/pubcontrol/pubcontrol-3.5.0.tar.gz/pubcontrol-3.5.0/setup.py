32e1f290425bf7d2464394cce3ec1e1d38ce9932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:55 +0000

    Add package pubcontrol/3.5.0/pubcontrol-3.5.0.tar.gz

---
+++ packages/pubcontrol/pubcontrol-3.5.0.tar.gz/pubcontrol-3.5.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name='pubcontrol',
+	version='3.5.0',
+	description='EPCP library',
+	author='Justin Karneges',
+	author_email='justin@fanout.io',
+	url='https://github.com/fanout/pypubcontrol',
+	license='MIT',
+	package_dir={'pubcontrol': 'src'},
+	packages=['pubcontrol'],
+	install_requires=[
+		'PyJWT>=1,<2;python_version<"3.0"',
+		'PyJWT>=1,<3;python_version>="3.0"',
+		'requests>=2.4,<3'
+	],
+	classifiers=[
+		'Topic :: Utilities',
+		'License :: OSI Approved :: MIT License'
+	]
+)
