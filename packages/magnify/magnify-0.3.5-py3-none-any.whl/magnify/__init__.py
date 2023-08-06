4d86127c29e8b3e76402c67f3966cf5882189917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:53 +0000

    Add package magnify/0.3.5/magnify-0.3.5-py3-none-any.whl

---
+++ packages/magnify/magnify-0.3.5-py3-none-any.whl/magnify/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.3.5"
+
+__all__ = [
+    "load",
+]
+from magnify.registry import load
+import magnify.filter
+import magnify.find
+import magnify.identify
+import magnify.postprocess
+import magnify.preprocess
+import magnify.reader
+import magnify.stitch
