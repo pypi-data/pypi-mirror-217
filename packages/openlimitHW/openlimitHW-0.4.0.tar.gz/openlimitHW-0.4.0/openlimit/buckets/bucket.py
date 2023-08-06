643e08e55e002e7eb0055151080e069d09cf44c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:11 +0000

    Add package openlimitHW/0.4.0/openlimitHW-0.4.0.tar.gz

---
+++ packages/openlimitHW/openlimitHW-0.4.0.tar.gz/openlimitHW-0.4.0/openlimit/buckets/bucket.py
@@ -0,0 +1,35 @@
+# Standard library
+import asyncio
+import time
+
+
+######
+# MAIN
+######
+
+
+class Bucket(object):
+    def __init__(self, rate_limit):
+        # once Per-minute
+        self._sec_per_tick = 60 / rate_limit
+        # Last time the bucket capacity was checked
+        self._last_checked = time.time()
+        self._last_amount = 0
+
+    def _has_capacity(self, amount):
+        current_time = time.time()
+        time_passed = current_time - self._last_checked
+
+        if time_passed > (self._sec_per_tick * self._last_amount):
+            self._last_checked = current_time
+            self._last_amount = amount
+            return True
+        
+        return False
+    
+    def update_last_amount(self, amount):
+        self._last_amount = amount
+        
+    async def wait_for_capacity(self, amount):
+        while not self._has_capacity(amount):
+            await asyncio.sleep(1 / self._sec_per_tick)
---
+++ packages/openlimitHW/openlimitHW-0.4.0.tar.gz/openlimitHW-0.4.0/openlimit/rate_limiters.py
@@ -0,0 +1,84 @@
+# Standard library
+import asyncio
+
+# Local
+import openlimit.utilities as utils
+from openlimit.buckets import Bucket
+
+
+############
+# BASE CLASS
+############
+
+
+class RateLimiter(object):
+    def __init__(self, request_limit, token_limit, token_counter):
+        # Rate limits
+        self.request_limit = request_limit
+        self.token_limit = token_limit
+
+        # Token counter
+        self.token_counter = token_counter
+
+        # Buckets
+        self._request_bucket = Bucket(request_limit)
+        self._token_bucket = Bucket(token_limit)
+ 
+    async def wait_for_capacity(self, num_tokens):
+        await asyncio.gather(
+            self._token_bucket.wait_for_capacity(num_tokens),
+            self._request_bucket.wait_for_capacity(1)
+        )
+    
+    def limit(self, **kwargs):
+        num_tokens = self.token_counter(**kwargs)
+        return utils.ContextManager(num_tokens, self)
+    
+    def is_limited(self):
+        return utils.FunctionDecorator(self)
+
+    def update(self, response):
+        pass
+
+######
+# MAIN
+######
+
+
+class ChatRateLimiter(RateLimiter):
+    def __init__(self, request_limit, token_limit):
+        '''
+        request_limit: number of requests per minute
+        token_limit: number of tokens per minute
+        '''
+        super().__init__(
+            request_limit=request_limit,
+            token_limit=token_limit,
+            token_counter=utils.num_tokens_consumed_by_chat_request
+        )
+
+
+class CompletionRateLimiter(RateLimiter):
+    def __init__(self, request_limit, token_limit):
+        '''
+        request_limit: number of requests per minute
+        token_limit: number of tokens per minute
+        '''
+        super().__init__(
+            request_limit=request_limit,
+            token_limit=token_limit,
+            token_counter=utils.num_tokens_consumed_by_completion_request
+        )
+
+
+class EmbeddingRateLimiter(RateLimiter):
+    def __init__(self, request_limit, token_limit):
+        '''
+        request_limit: number of requests per minute
+        token_limit: number of tokens per minute
+        '''
+        super().__init__(
+            request_limit=request_limit, 
+            token_limit=token_limit, 
+            token_counter=utils.num_tokens_consumed_by_embedding_request
+        )
