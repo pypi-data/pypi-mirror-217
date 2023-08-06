7bd3d032128d91178138b0821d008c20a39e6f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:43 +0000

    Add package pyphetools/0.4.5/pyphetools-0.4.5.tar.gz

---
+++ packages/pyphetools/pyphetools-0.4.5.tar.gz/pyphetools-0.4.5/pyphetools/creation/option_column_mapper.py
@@ -0,0 +1,134 @@
+from .hp_term import HpTerm
+from .column_mapper import ColumnMapper
+from .hpo_cr import HpoConceptRecognizer
+from typing import List
+import pandas as pd
+import re
+from collections import defaultdict
+
+class OptionColumnMapper(ColumnMapper):
+    
+    def __init__(self, concept_recognizer, option_d, negative_symbol=None, negative_label=None):
+        """Mapper to be used if the column has a set of defined items but text mining is not required.
+
+        Args:
+            option_d (dict): key: item (e.g., MVP) in original publication; value: corresponding HPO label, e.g. Mitral Valve Prolapse
+
+        Raises:
+            ValueError: if option_d is not a dictionary
+        """
+        super().__init__()
+        # Either have self._option_d be an empty dictionary or it must be a valid dictionary
+        if option_d is None or not isinstance(option_d, dict):
+            raise ValueError(f"option_d argument must be dictionary but was {type(option_d)}")
+        self._option_d = option_d
+        if not isinstance(concept_recognizer, HpoConceptRecognizer):
+            raise ValueError("concept_recognizer arg must be HpoConceptRecognizer but was {type(concept_recognizer)}")
+        self._hpo_cr = concept_recognizer
+        if negative_symbol is None:
+            negative_symbol = defaultdict()
+        self._negative_symbol = negative_symbol
+        self._negative_label = negative_label
+        self._has_negative = negative_label is not None and negative_symbol is not None
+        
+    def map_cell(self, cell_contents) -> List[HpTerm]:
+        """Map cell contents using the option dictionary 
+        
+        Args:
+            cell_contents (str): contents of one table cell
+        """
+        results = []
+        contents = cell_contents.strip()
+        if self._has_negative and contents == self._negative_symbol:
+            term = self._hpo_cr.get_term_from_label(label=self._negative_label)
+            term.excluded()
+            results.append(term)
+            return results
+        delimiters = ',;|/'
+        regex_pattern = '|'.join(map(re.escape, delimiters))
+        chunks = re.split(regex_pattern, contents)
+        chunks = [chunk.strip() for chunk in chunks]
+
+        hpo_labels = []
+        for c in chunks:
+            for my_key, my_label in self._option_d.items():
+                if my_key in c:
+                    if isinstance(my_label, list):
+                        for itm in my_label:
+                            hpo_labels.append(itm)
+                    else:
+                        hpo_labels.append(my_label)
+        for label in hpo_labels:    
+            term = self._hpo_cr.get_term_from_label(label=label)
+            results.append(term)
+        return results
+        
+    def preview_column(self, column) -> pd.DataFrame:
+        if not isinstance(column, pd.Series):
+            raise ValueError("column argument must be pandas Series, but was {type(column)}")
+        dlist = []
+        for _, value in column.items():
+            column_val = []
+            results  = self.map_cell(str(value))
+            if results is None:
+                print(f"Got None results for {str(value)}")
+                dlist.append({"terms": "n/a"})  
+            elif len(results) > 0:
+                for hpterm in results:
+                    column_val.append(f"{hpterm.id} ({hpterm.label}/{hpterm.display_value})")
+                dlist.append({"terms": "; ".join(column_val)})  
+            else:
+                dlist.append({"terms": "n/a"})  
+        return pd.DataFrame(dlist)   
+    
+    @staticmethod
+    def autoformat(df: pd.DataFrame, concept_recognizer, delimiter=",") -> str:
+        """Autoformat code from the colums so that we can easily copy-paste and change it.
+        This method intends to save time by preformatting code the create OptionMappers.
+        
+        Args:
+            df: data frame with the data about the individuals
+            concept_recognizer: HpoConceptRecognizer for text mining
+            delimiter: the string used to delimit individual items in a cell (default: comma)
+        Return:
+            a string that should be displayed using a print() command in the notebook
+        """  
+        lines = []
+        if not isinstance(df, pd.DataFrame):
+            raise ValueError(f"argument \"df\" must be a pandas DataFrame but was {type(df)}")
+        if not isinstance(concept_recognizer, HpoConceptRecognizer):
+            raise ValueError("concept_recognizer arg must be HpoConceptRecognizer but was {type(concept_recognizer)}")
+        # df.shape[1] gives us the number of columns
+        for y in range(df.shape[1]):
+            temp_dict = {}
+            for i in range(len(df)):
+                if len(str(df.iloc[i, y])) > 1:
+                    for entry in str(df.iloc[i, y]).split(delimiter):
+                        hpo_term = concept_recognizer.parse_cell(entry.strip())
+                        if len(hpo_term) > 0:
+                            temp_dict[entry.strip()] = hpo_term[0].label                
+                        else:
+                            temp_dict[entry.strip()] = 'PLACEHOLDER'
+            col_name = str(df.columns[y]).lower().replace(", ","_").replace(' ', '_')
+            col_name = col_name.lower()
+            # skip columns that are unlikely to be interesting for the OptionColumnMapper
+            if "patient" in col_name:
+                continue
+            if "family" in col_name:
+                continue
+            if "gender" in col_name:
+                continue
+            if "mutation" in col_name or "varia" in col_name:
+                continue
+            if "age" == col_name or "sex" == col_name or "gender" == col_name:
+                continue
+            items_d_name = f"{col_name}_d"
+            items_d_string = str(temp_dict).replace(',', ',\n')
+            lines.append(f"{items_d_name} = {items_d_string}")
+            lines.append(f"{col_name}Mapper = OptionColumnMapper(concept_recognizer=hpo_cr, option_d={items_d_name})")
+            lines.append(f"{col_name}Mapper.preview_column(df['" + str(df.columns[y]) + "']))")
+            lines.append(f"column_mapper_d['{str(df.columns[y])}'] = {col_name}Mapper")
+            lines.append("")
+        return "\n".join(lines)
+        
+   
---
+++ packages/pyphetools/pyphetools-0.4.5.tar.gz/pyphetools-0.4.5/pyphetools/creation/simple_column_mapper.py
@@ -0,0 +1,107 @@
+from .hp_term import HpTerm
+from .column_mapper import ColumnMapper
+from typing import List
+import pandas as pd
+from collections import defaultdict
+
+
+def get_separate_hpos_from_df(df, hpo_cr):
+    """Loop through all the cells in a dataframe or series and try to parse each cell as HPO term.
+    Useful when the separate HPO terms are in the cells themselves.
+
+      Args:
+         df (dataframe): dataframe with phenotypic data
+         hpo_cr (HpoConceptRecognizer): instance of HpoConceptRecognizer to match HPO term and get label/id
+
+      Returns:
+          additional_hpos: list of lists with the additional HPO terms per individual
+      """
+    additional_hpos = []
+
+    for i in range(len(df)):
+        temp_hpos = []
+        for y in range(df.shape[1]):
+            hpo_term = hpo_cr.parse_cell(df.iloc[i, y])
+            if len(hpo_term) > 0:
+                temp_hpos.extend(hpo_term)
+        additional_hpos.append(list(set(temp_hpos)))
+    return additional_hpos
+
+
+def try_mapping_columns(df, observed, excluded, hpo_cr, preview=True):
+    """Try to map the columns in a dataframe by matching the name of the column to correct HPO term.
+    Wrapper for SimpleColumnMapper below
+
+    Args:
+       df (dataframe): dataframe with phenotypic data
+       observed (str): symbol used in table if the phenotypic feature was observed
+       excluded (str): symbol used if the feature was excluded
+       hpo_cr (HpoConceptRecognizer): instance of HpoConceptRecognizer to match HPO term and get label/id
+       preview (bool): whether to print the successfully mapped columns
+
+    Returns:
+        simple_mappers (dict): dict with successfully mapped columns
+        col_not_found (list): columns that were not mapped
+    """
+    col_not_found = []
+    simple_mappers = defaultdict(ColumnMapper)
+    for col in df.columns:
+        hpo_term = hpo_cr.parse_cell(col)
+        if len(hpo_term) > 0:
+            hpo_term = hpo_term[0]
+            simple_mappers[col] = SimpleColumnMapper(hpo_id=hpo_term.id,
+                                                                hpo_label=hpo_term.label,
+                                                                observed=observed,
+                                                                excluded=excluded)
+            if preview:
+                print(simple_mappers[col].preview_column(df[col]))
+        else:
+            col_not_found.append(col)
+    return simple_mappers, col_not_found
+
+
+class SimpleColumnMapper(ColumnMapper):
+
+    def __init__(self, hpo_id, hpo_label, observed=None, excluded=None, non_measured=None):
+        """ColumnMapper for columns that contain information about a single phenotypic abnormality only
+
+        Args:
+            hpo_id (_type_): HPO  id, e.g., HP:0004321
+            hpo_label (_type_): Corresponding term label
+            observed (_type_): symbol used in table if the phenotypic feature was observed
+            excluded (_type_): symbol used if the feature was excluded
+            non_measured (_type_, optional): symbol used if the feature was not measured or is N/A. Defaults to None.
+            constant (bool, optional): If true, all patients have this feature. Defaults to False.
+        """
+        super().__init__()
+        self._hpo_id = hpo_id
+        self._hpo_label = hpo_label
+        if observed is None or excluded is None:
+            raise ValueError(
+                    "Need to provide arguments for both observed and excluded")
+        self._observed = observed
+        self._excluded = excluded
+        self._not_measured = non_measured
+
+    def map_cell(self, cell_contents) -> List[HpTerm]:
+        if not isinstance(cell_contents, str):
+            raise ValueError(
+                f"Error: cell_contents argument ({cell_contents}) must be string but was {type(cell_contents)} -- coerced to string")
+        contents = cell_contents.strip()
+        if contents in self._observed:
+            return [HpTerm(hpo_id=self._hpo_id, label=self._hpo_label)]
+        elif contents in self._excluded:
+            return [HpTerm(hpo_id=self._hpo_id, label=self._hpo_label, observed=False)]
+        else:
+            return [HpTerm(hpo_id=self._hpo_id, label=self._hpo_label, measured=False)]
+
+    def preview_column(self, column) -> pd.DataFrame:
+        if not isinstance(column, pd.Series):
+            raise ValueError("column argument must be pandas Series, but was {type(column)}")
+        dlist = []
+        for _, value in column.items():
+            value = self.map_cell(str(value))
+            hpterm = value[0]
+            dlist.append({"term": hpterm.hpo_term_and_id, "status": hpterm.display_value})
+        return pd.DataFrame(dlist)
+    
