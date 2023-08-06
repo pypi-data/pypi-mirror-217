72d758dc24fdf6deecae86ede2be64120c396e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:05 +0000

    Add package nonebot-plugin-stable-diffusion-diao/0.2.3/nonebot_plugin_stable_diffusion_diao-0.2.3.tar.gz

---
+++ packages/nonebot-plugin-stable-diffusion-diao/nonebot_plugin_stable_diffusion_diao-0.2.3.tar.gz/nonebot_plugin_stable_diffusion_diao-0.2.3/nonebot_plugin_stable_diffusion_diao/utils/save.py
@@ -0,0 +1,23 @@
+from ..config import config
+from pathlib import Path
+import hashlib
+import aiofiles
+import time
+import json
+
+path = Path("data/novelai/output").resolve()
+async def save_img(fifo, img_bytes: bytes, extra: str = "unknown", hash = time.time()):
+    # 存储图片
+    if config.novelai_save:
+        path_ = path / extra
+        path_.mkdir(parents=True, exist_ok=True)
+        hash = hashlib.md5(img_bytes).hexdigest()
+        file = (path_ / hash).resolve()
+        async with aiofiles.open(str(file) + ".jpg", "wb") as f:
+            await f.write(img_bytes)
+        if config.novelai_save==2:
+            async with aiofiles.open(str(file) + ".txt", "w", encoding="utf-8") as f:
+                await f.write(repr(fifo))
+            #     fifo_dict = {key.decode(): value.decode() for key, value in fifo.items()}
+            # async with aiofiles.open(str(file) + ".json", "w", encoding="utf-8") as f:
+            #     await f.write(json.dumps(fifo_dict, ensure_ascii=False, indent=4))
