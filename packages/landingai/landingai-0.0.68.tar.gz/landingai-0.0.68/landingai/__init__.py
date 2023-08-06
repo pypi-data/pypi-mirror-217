bc05d7c03a79056f462771ff3d2509698aa8d76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:09 +0000

    Add package landingai/0.0.68/landingai-0.0.68.tar.gz

---
+++ packages/landingai/landingai-0.0.68.tar.gz/landingai-0.0.68/landingai/__init__.py
@@ -0,0 +1,8 @@
+"""
+.. include:: ../README.md
+.. include:: ../docs/user_guide/1_concepts.md
+.. include:: ../docs/user_guide/2_credentials.md
+.. include:: ../docs/user_guide/3_postprocess.md
+.. include:: ../docs/user_guide/4_pipelines.md
+.. include:: ../docs/developer_guide/1_main.md
+"""
