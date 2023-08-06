b9c3f38f5c67715c9a8c228bed74bbaa316c485f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:42 +0000

    Add package ormlite/0.2.0/ormlite-0.2.0-py3-none-any.whl

---
+++ packages/ormlite/ormlite-0.2.0-py3-none-any.whl/ormlite/errors.py
@@ -0,0 +1,14 @@
+class RegexMatchError(Exception):
+    pass
+
+
+class MissingAdapterError(Exception):
+    pass
+
+
+class InvalidForeignKeyError(Exception):
+    pass
+
+
+class MultiplePrimaryKeysError(Exception):
+    pass
