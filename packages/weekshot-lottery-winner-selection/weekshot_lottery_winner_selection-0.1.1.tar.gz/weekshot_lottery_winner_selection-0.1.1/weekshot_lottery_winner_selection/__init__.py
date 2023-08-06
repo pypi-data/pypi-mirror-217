ecd26fef5fbe6d3425f403a7325d0c19f5617973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:02 +0000

    Add package weekshot-lottery-winner-selection/0.1.1/weekshot_lottery_winner_selection-0.1.1.tar.gz

---
+++ packages/weekshot-lottery-winner-selection/weekshot_lottery_winner_selection-0.1.1.tar.gz/weekshot_lottery_winner_selection-0.1.1/weekshot_lottery_winner_selection/__init__.py
@@ -0,0 +1,31 @@
+import httpx
+
+async def generate_random_numbers(
+    api_key: str,
+    max_number: int,
+    count: int
+) -> list[int]:
+    url = f"https://api.random.org/json-rpc/2/invoke"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "generateIntegers",
+        "params": {
+            "apiKey": api_key,
+            "n": count,
+            "min": 1,
+            "max": max_number,
+            "replacement": True
+        },
+        "id": 1
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json=payload)
+    if response.status_code == 200:
+        data = response.json()
+        random_numbers = data["result"]["random"]["data"]
+        return random_numbers
+    else:
+        raise Exception("Failed to generate random numbers")
