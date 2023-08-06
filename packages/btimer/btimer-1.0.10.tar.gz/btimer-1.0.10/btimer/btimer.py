2242c0f3d8268c7dca9a794b9c15285741df38dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:05 +0000

    Add package btimer/1.0.10/btimer-1.0.10.tar.gz

---
+++ packages/btimer/btimer-1.0.10.tar.gz/btimer-1.0.10/btimer/btimer.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+import random
+import time
+import json
+
+from rich.console import Console
+from rich.panel import Panel
+from rich.progress import Progress
+
+
+def count_down(t: any) -> None:
+    console = Console()
+    with Progress() as progress:
+        task1 = progress.add_task("", total=t, transient=True)
+
+        while not progress.finished:
+            progress.update(task1, advance=1)
+            time.sleep(1)
+
+    console.print("\n")
+
+
+def read_json():
+    f = open('verses.json')
+    return json.load(f)
+
+
+def print_quotes() -> None:
+    console = Console()
+
+    randint = random.randint(0, 2)
+    verses = read_json()
+    quote = verses[randint]
+    console.print(Panel(""" "{}" - {} """.format(quote['quote'], quote['author']), title="Verses"))
+
+
+def active_window() -> None:
+    applescript = """
+    tell application "Terminal"
+        activate
+    end tell
+    """
+    subprocess.call("osascript -e '{}'".format(applescript), shell=True)
+
+
+def main() -> None:
+    try:
+        val = input("Enter the number of minutes: ")
+        count_down(int(val) * 60)
+    except KeyboardInterrupt:
+        # Signal can not handler sleep
+        sys.exit(0)
+
+    active_window()
+    print_quotes()
+
+
+if __name__ == '__main__':
+    main()
