81ad84d8a0907a8f8333daeceb9c7c987f85302e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:31 +0000

    Add package owe-python-sdk/0.0.1a17/owe_python_sdk-0.0.1a17-py3-none-any.whl

---
+++ packages/owe-python-sdk/owe_python_sdk-0.0.1a17-py3-none-any.whl/owe_python_sdk/client/utils.py
@@ -0,0 +1,24 @@
+from client import Runner
+
+
+def adapt_runner(RunnerClassExtension: type, config_override: dict={}):
+    def __init__(self, *args, **kwargs):
+        Runner.__init__(self)
+        RunnerClassExtension.__init__(self, *args, **kwargs)
+
+    # Raise Exception if RunnerClassExtension does not have a
+    # Config property
+    if not hasattr(RunnerClassExtension, "Config"):
+        raise AttributeError(f"Runner class '{RunnerClassExtension.__name__}' is missing a Config class")
+
+    # Override the config on the RunnerClassExtension
+    for key, val in config_override.items():
+        setattr(RunnerClassExtension.Config, key, val)
+
+    AdaptedRunner = type(
+        "AdaptedRunner",
+        (RunnerClassExtension, Runner),
+        {"__init__": __init__}
+    )
+
+    return AdaptedRunner
