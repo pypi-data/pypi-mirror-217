89dd988b4b5a9fe47cdfff352890dbdfe50d4a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:59 +0000

    Add package oarepo-cli/11.1.11/oarepo_cli-11.1.11-py3-none-any.whl

---
+++ packages/oarepo-cli/oarepo_cli-11.1.11-py3-none-any.whl/oarepo_cli/build_command/cli.py
@@ -0,0 +1,42 @@
+import click
+
+from oarepo_cli.config import MonorepoConfig
+from oarepo_cli.site.site_support import SiteSupport
+from .wizard import BuildWizard
+from oarepo_cli.utils import with_config
+from oarepo_cli.wizard.docker import DockerRunner
+
+
+@click.command(name="build")
+@with_config()
+@click.option("--site")
+@click.option("--production/--development")
+@click.pass_context
+def build_command(
+    ctx,
+    cfg: MonorepoConfig = None,
+    step=None,
+    no_input=False,
+    silent=False,
+    verbose=False,
+    steps=False,
+    site=None,
+        production=None,
+    **kwargs,
+):
+    site_support = SiteSupport(cfg, site)
+
+    runner = DockerRunner(cfg, no_input=True)
+    wizard = BuildWizard(runner, site_support, production)
+
+    if site:
+        cfg = cfg.clone(["sites", site])
+        wizard.site_support = SiteSupport(cfg, site)
+
+    if steps:
+        wizard.list_steps()
+        return
+
+    wizard.run_wizard(
+        cfg, selected_steps=step, no_input=no_input, silent=silent, verbose=verbose
+    )
