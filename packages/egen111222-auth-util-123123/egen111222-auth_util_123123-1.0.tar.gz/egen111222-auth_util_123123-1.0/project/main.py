30c92bb65487349ba7ff3dd39f1968ba115c52eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:11 +0000

    Add package egen111222-auth-util-123123/1.0/egen111222-auth_util_123123-1.0.tar.gz

---
+++ packages/egen111222-auth-util-123123/egen111222-auth_util_123123-1.0.tar.gz/egen111222-auth_util_123123-1.0/project/main.py
@@ -0,0 +1,32 @@
+class User:
+    """
+    class Cart for init Nont Get from :class: `AuthUser`
+    
+    :param login: Логін користувача
+    :type login: str
+    :param password: Пароль Користувача
+    :type passsword: str
+    """
+    def __init__(self,login,
+                 password):
+        """Initialized Method
+        """
+        self.login = login
+        self.password = password
+
+    def check_data(self,login,password):
+        """
+        Method Check Data
+
+        :param login: Логін на який перевіряємо
+        :type login: str
+        :param password: Пароль на який перевіряємо
+        :type: str
+
+        :return: True Якщо це логін і пароль цього користувача
+        :rtype: bool
+        """
+        pass
+
+
+
---
+++ packages/egen111222-auth-util-123123/egen111222-auth_util_123123-1.0.tar.gz/egen111222-auth_util_123123-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='egen111222-auth_util_123123',
+      version='1.0',
+      description='Система аутентифікації',
+      author='Kirill',
+      author_email='123@gmail.com',
+      packages=['project'],
+     )
