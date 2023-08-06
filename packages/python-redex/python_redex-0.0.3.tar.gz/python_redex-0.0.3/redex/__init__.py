9ae6428006f09c1ce99a43ba019d31d357ef3472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:34 +0000

    Add package python-redex/0.0.3/python_redex-0.0.3.tar.gz

---
+++ packages/python-redex/python_redex-0.0.3.tar.gz/python_redex-0.0.3/redex/__init__.py
@@ -0,0 +1,38 @@
+__description__ = 'contains the basic functionalities of redex'
+__filename__ = '__init__.py'
+__author__ = 'Timo Kats'
+
+# local imports
+
+from lib.lexical_analysis import *
+from lib.split import *
+
+# main functionalities
+
+def has(query, string, split=' ', granularity=1, threads=2):
+    search = RedexSearch(query, string, split=split, granularity=granularity, threads=threads)
+    search.parse_query()
+    if True in search.get_result():
+        return True
+    else:
+        return False
+
+def find(query, string, split=' ', granularity=1, format='string', threads=2):
+    search = RedexSearch(query, string, split=split, granularity=granularity, threads=threads)
+    search.parse_query()
+    locations = []
+    for index, location in enumerate(redex_split(string, split, granularity)):
+        if search.get_result()[index] and format == 'int':
+            locations.append(index)
+        elif search.get_result()[index] and format == 'string':
+            locations.append(location)
+        elif search.get_result()[index] and format == 'tuple':
+            locations.append((index, location))
+    return locations
+
+def info():
+    print('\n---')
+    print('    ^ ^          Description:        Python library for readable regex.')
+    print('("\(-_-)/")      Version:            0.0.3') 
+    print(' )(  O  )(       Author:             Timo Kats')
+    print('((...)(...))     Last updated:       02/07/2023', end='\n---\n\n')
