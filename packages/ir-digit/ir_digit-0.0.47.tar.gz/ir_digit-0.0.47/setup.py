4f3b1a0c20af785c9e6eac9ef20c3569130abfe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:07 +0000

    Add package ir-digit/0.0.47/ir_digit-0.0.47.tar.gz

---
+++ packages/ir-digit/ir_digit-0.0.47.tar.gz/ir_digit-0.0.47/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+setuptools.setup(
+    name="ir_digit",
+    version="0.0.47",
+    author="FKLiu",
+    author_email="fkliu001@outlook.com",
+    description="digit package",
+    long_description="digit package ……",
+    long_description_content_type="text",
+    url="https://example.com",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+            'console_scripts': [
+                'digit=digit.scripts:main',
+            ],
+        },
+
+)
