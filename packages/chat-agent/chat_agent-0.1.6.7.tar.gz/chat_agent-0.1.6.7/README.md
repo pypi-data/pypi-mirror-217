4f0bc3c86e00c2621dee9b537b6911938da7b86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:32 +0000

    Add package chat-agent/0.1.6.7/chat_agent-0.1.6.7.tar.gz

---
+++ packages/chat-agent/chat_agent-0.1.6.7.tar.gz/chat_agent-0.1.6.7/README.md
@@ -0,0 +1,51 @@
+# 简单的OpenAI chatGPT代理
+
+### 安装
+```
+使用pip安装最新版本，指定到官方pypi源
+pip install chat-agent==0.1.6.7 -i https://pypi.org/simple/
+```
+
+### 环境变量配置项：
+```
+必配：
+OPENAI_ORGANIZATION_KEY: openai对应的组织id
+OPENAI_API_KEY: openai分配的api key
+选配：
+OPENAI_CHAT_ROUND: 上下文保留轮次，默认3
+OPENAI_CHAT_TOKEN_MAX：单次聊天token最大值，适当设置，默认2048
+OPENAI_HISTORY_TOKEN_MAX：历史聊天token最大值，适当设置，默认2048
+OPENAI_MODEL：模型名，默认gpt-3.5-turbo
+OPENAI_TEMPERATURE：采样温度，默认0.7
+
+CHAT_AGENT_HOST_DEFAULT：监听地址，默认0.0.0.0
+CHAT_AGENT_PORT：代理端口，默认8080
+CHAT_AGENT_HTTPS_PORT：http端口，默认8443
+CHAT_AGENT_WEBSOCKET_PORT：websocket端口，默认9080
+CHAT_AGENT_WEBSOCKET_WSS_PORT：websocket wss 端口，默认9443
+CACHE_CHAT_LOG_EXPIRE：缓存聊天信息的过期时间，默认3600
+
+CHAT_AGENT_HTTPS：是否开启https，默认True
+CHAT_AGENT_SSL_CERT_FILE_FULL_PATH：证书文件完整路径，默认cert.pem
+CHAT_AGENT_SSL_KEY_FILE_FULL_PATH：证书key文件完整路径，默认key.pem
+CHAT_AGENT_STATIC_PATH：静态文件路径，用于进行指定路径的隐藏文件路由，可用于进行certbot的CA服务验证使用
+```
+
+### SSL证书
+可参考：https://eff-certbot.readthedocs.io/en/stable/using.html#certbot-commands
+
+### 启动代理
+##### http or https 使用以下方式启动
+```
+python -m chat_agent.app
+```
+##### websocket or websocket wss 使用以下方式启动
+```
+python -m chat_agent.websocket_app
+```
+
+### 使用
+通过在浏览器打开以下链接便可使用
+```
+http://127.0.0.1:8080
+```
---
+++ packages/chat-agent/chat_agent-0.1.6.7.tar.gz/chat_agent-0.1.6.7/chat_agent/handler/openai_handler.py
@@ -0,0 +1,118 @@
+import json
+
+import openai
+import tiktoken
+
+from chat_agent.cache.cache_helper import get_cache, CHAT_LOG, save_cache
+from chat_agent.handler import CHAT_ROUND, CHAT_TOKEN_MAX, HISTORY_TOKEN_MAX, MODEL, TEMPERATURE
+from chat_agent.logger.logger_helper import get_logger
+from chat_agent.util import random
+from chat_agent.util.context import get_thread_context
+
+logger = get_logger(__name__)
+
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo-0301":  # note: future models may deviate from this
+        num_tokens = 0
+        for message in messages:
+            num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+            for key, value in message.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":  # if there's a name, the role is omitted
+                    num_tokens += -1  # role is always required and always 1 token
+        num_tokens += 2  # every reply is primed with <im_start>assistant
+        return num_tokens
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
+  See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+
+
+def get_valid_chat_log(chat_log: []):
+    sid = get_thread_context().get('sid')
+    valid_context_chat_log = []
+    for chat in chat_log:
+        if 'user' == chat['role']:
+            valid_context_chat_log.append(chat)
+        elif 'system' == chat['role']:
+            valid_context_chat_log.append(chat)
+    temp_messages = valid_context_chat_log[-CHAT_ROUND:]
+    while len(temp_messages) > 1:
+        token_count = num_tokens_from_messages(temp_messages)
+        if token_count > HISTORY_TOKEN_MAX:
+            temp_messages = temp_messages[1:]
+        else:
+            logger.debug('sid: {} current token count: {}'.format(sid, token_count))
+            break
+    return temp_messages
+
+
+def send_chat_message_with_steam_response(msg: str, chat_log: [] = None):
+    sid = get_thread_context().get('sid')
+    ip = get_thread_context()['remote_ip']
+    logger.debug('receive msg, sid: {} ip: {} msg: {}'.format(sid, ip, msg))
+    is_need_cache = False
+    if not chat_log:
+        chat_log = get_cache(CHAT_LOG, sid=sid)
+        is_need_cache = True
+    cur_msg = {'role': 'user', 'content': msg}
+    chat_log.append(cur_msg)
+    messages = get_valid_chat_log(chat_log)
+    try:
+        completion = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=messages,
+            temperature=TEMPERATURE,
+            max_tokens=CHAT_TOKEN_MAX,
+            n=1,
+            stop=None,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stream=True
+        )
+        whole_response_msg = ''
+        for chunk in completion:
+            if chunk['choices'][0]['finish_reason']:
+                break
+            if 'role' in chunk['choices'][0]['delta']:
+                continue
+            chunk_text = chunk['choices'][0]['delta']['content']
+            if chunk_text:
+                whole_response_msg += chunk_text
+                yield chunk_text
+            else:
+                break
+        logger.debug('finish chat, sid: {} whole_response: {}'.format(sid, whole_response_msg))
+        chat_log.append({
+            'role': 'system', 'content': whole_response_msg
+        })
+        if is_need_cache:
+            save_cache(CHAT_LOG, sid=sid, data=chat_log)
+    except Exception as e:
+        logger.error('send message has exception, msg: ' + e)
+
+
+def send_chat_message_with_socket_steam_response(msg: str, chat_log: [] = None):
+    msg_id = random.get_random_md5()
+    index = 0
+    sid = get_thread_context().get('sid')
+    for response in send_chat_message_with_steam_response(msg=msg, chat_log=chat_log):
+        response_msg = {
+            'sid': sid,
+            'msg_id': msg_id,
+            'index': index,
+            'data': response,
+            'end': False
+        }
+        index += 1
+        yield json.dumps(response_msg)
+    end_msg = {
+        'sid': sid,
+        'msg_id': msg_id,
+        'end': True
+    }
+    yield json.dumps(end_msg)
