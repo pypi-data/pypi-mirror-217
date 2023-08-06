54339551e2b31dbdde9106f62d9bdb29bfdcde40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:42 +0000

    Add package PrintTestByYinyi/0.0.1/PrintTestByYinyi-0.0.1.tar.gz

---
+++ packages/PrintTestByYinyi/PrintTestByYinyi-0.0.1.tar.gz/PrintTestByYinyi-0.0.1/PrintTestByYinyi/printTest.py
@@ -0,0 +1,2 @@
+def printTest():
+    print("this is yinyi")
---
+++ packages/PrintTestByYinyi/PrintTestByYinyi-0.0.1.tar.gz/PrintTestByYinyi-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PrintTestByYinyi", # Replace with your own username
+    version="0.0.1",
+    author="Yinyi",
+    author_email="905281848@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/zqy0315/pypi-test",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
