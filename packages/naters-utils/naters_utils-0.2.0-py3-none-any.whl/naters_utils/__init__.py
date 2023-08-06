20783ff45cffba7a276b9f8167af1c14289efc1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:11 +0000

    Add package naters-utils/0.2.0/naters_utils-0.2.0-py3-none-any.whl

---
+++ packages/naters-utils/naters_utils-0.2.0-py3-none-any.whl/naters_utils/__init__.py
@@ -0,0 +1,12 @@
+# Naters-Utils
+# Various things i have made that I find useful and want to share
+
+
+# Metadata
+__all__ = ["functions", "hello", "isntinstance", "iterables"]
+__version__ = "0.1.0"
+
+
+# Imports
+from ._hello import hello
+from ._isntinstance import isntinstance
