1d9389aad3848026b8c26c16606d815180cfbe4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:54 +0000

    Add package gonchaya/0.0.2/gonchaya-0.0.2.tar.gz

---
+++ packages/gonchaya/gonchaya-0.0.2.tar.gz/gonchaya-0.0.2/gonchaya/debug.py
@@ -0,0 +1,6 @@
+#!/bin/python3 -i
+
+from gonchaya import *
+data = pd.read_csv('/home/padla/data_science/yandex_lections/08/proj/games.csv')
+#pd.DataFrame.preprocessing__normalization_of_column_names2 = MethodType(gonchaya.src_functions._preprocessing__normalization_of_column_names, data)
+#data.preprocessing__normalization_of_column_names(data, report='con')
