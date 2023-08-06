948915fc6c6f4e7a84b7eedf005eb2870519a9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:09 +0000

    Add package universal-framework-vk/0.3/universal_framework_vk-0.3.tar.gz

---
+++ packages/universal-framework-vk/universal_framework_vk-0.3.tar.gz/universal_framework_vk-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='universal_framework_vk',
+    version='0.3',
+    packages=find_packages(),
+    description='universal framework',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Viral Killer',
+    author_email='jules3313@gmail.com',
+    license='MIT',
+    python_requires='>=3.10.0',
+    install_requires=[
+        # List of your package dependencies
+        # For example: 'numpy>=1.18.1'
+    ],
+)
