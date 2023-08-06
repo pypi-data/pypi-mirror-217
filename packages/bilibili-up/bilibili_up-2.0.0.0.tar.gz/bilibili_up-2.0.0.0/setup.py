0e0b8bd734b3ddbe16048a53f1a039f559b66538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:54 +0000

    Add package bilibili-up/2.0.0.0/bilibili_up-2.0.0.0.tar.gz

---
+++ packages/bilibili-up/bilibili_up-2.0.0.0.tar.gz/bilibili_up-2.0.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='bilibili_up',
+    version='2.0.0.0',
+    author='SuiBo',
+    author_email='534047068@qq.com',
+    description='bilibili_up获取bilibili视频信息的爬虫！',
+    install_requires=[
+        'requests',
+    ],
+    py_modules=["bilibili_up"],
+    packages=find_packages(),
+    url = "https://pypi.org/user/SuiBo/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
