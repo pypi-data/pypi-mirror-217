0bc2b72312d8cfd841a73b1dce83a36f509534f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:22 +0000

    Add package cycode/0.2.6.dev8/cycode-0.2.6.dev8.tar.gz

---
+++ packages/cycode/cycode-0.2.6.dev8.tar.gz/cycode-0.2.6.dev8/cycode/cli/utils/yaml_utils.py
@@ -0,0 +1,29 @@
+from typing import Dict
+
+import yaml
+
+
+def read_file(filename: str) -> Dict:
+    with open(filename, 'r', encoding='UTF-8') as file:
+        return yaml.safe_load(file)
+
+
+def update_file(filename: str, content: Dict):
+    try:
+        with open(filename, 'r', encoding='UTF-8') as file:
+            file_content = yaml.safe_load(file)
+    except FileNotFoundError:
+        file_content = {}
+
+    with open(filename, 'w', encoding='UTF-8') as file:
+        file_content = _deep_update(file_content, content)
+        yaml.safe_dump(file_content, file)
+
+
+def _deep_update(source, overrides):
+    for key, value in overrides.items():
+        if isinstance(value, dict) and value:
+            source[key] = _deep_update(source.get(key, {}), value)
+        else:
+            source[key] = overrides[key]
+    return source
