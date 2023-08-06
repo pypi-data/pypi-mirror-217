55fea3df54d52e4c1bc4359f24214118c5535fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:01 +0000

    Add package volmdlr/0.11.2/volmdlr-0.11.2-pp39-pypy39_pp73-macosx_10_9_x86_64.whl

---
+++ packages/volmdlr/volmdlr-0.11.2-pp39-pypy39_pp73-macosx_10_9_x86_64.whl/volmdlr/utils/common_operations.py
@@ -0,0 +1,45 @@
+"""
+Concatenate common operation for two or more objects.
+
+"""
+import random
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from volmdlr.core import EdgeStyle
+
+
+def plot_circle(circle, ax=None, edge_style: EdgeStyle = EdgeStyle()):
+    """
+    Create a Matplotlib plot for a circle 2d or fullarc 2d.
+
+    :param circle: circle to plot.
+    :param ax: Matplotlib plot axis.
+    :param edge_style: Edge Style to implement.
+    :return: Matplotlib plot axis.
+    """
+    if ax is None:
+        _, ax = plt.subplots()
+    if circle.radius > 0:
+        ax.add_patch(matplotlib.patches.Arc((circle.center.x, circle.center.y),
+                                            2 * circle.radius,
+                                            2 * circle.radius,
+                                            angle=0,
+                                            theta1=0,
+                                            theta2=360,
+                                            color=edge_style.color,
+                                            alpha=edge_style.alpha,
+                                            linestyle=edge_style.linestyle,
+                                            linewidth=edge_style.linewidth))
+    if edge_style.plot_points:
+        ax.plot([circle.start.x], [circle.start.y], 'o',
+                color=edge_style.color, alpha=edge_style.alpha)
+    if edge_style.equal_aspect:
+        ax.set_aspect('equal')
+    return ax
+
+
+def random_color():
+    """Random color generator."""
+    return random.random(), random.random(), random.random()
