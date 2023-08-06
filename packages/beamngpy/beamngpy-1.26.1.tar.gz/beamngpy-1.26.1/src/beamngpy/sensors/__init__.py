638b3c2a6b0af5303228f60eb7123f9941ab3028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:16 +0000

    Add package beamngpy/1.26.1/beamngpy-1.26.1.tar.gz

---
+++ packages/beamngpy/beamngpy-1.26.1.tar.gz/beamngpy-1.26.1/src/beamngpy/sensors/__init__.py
@@ -0,0 +1,19 @@
+"""
+This module implements various sensors that can be attached to vehicles to
+extract data from simulations.
+"""
+
+from .advanced_IMU import AdvancedIMU
+from .camera import Camera
+from .damage import Damage
+from .electrics import Electrics
+from .gforces import GForces
+from .imu import IMU
+from .lidar import Lidar
+from .powertrain_sensor import PowertrainSensor
+from .sensor import Sensor
+from .state import State
+from .timer import Timer
+from .ultrasonic import Ultrasonic
+from .radar import Radar
+from .mesh import Mesh
