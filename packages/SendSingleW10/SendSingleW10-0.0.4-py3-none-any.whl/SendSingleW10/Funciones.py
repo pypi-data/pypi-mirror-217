0154558dbca05ab440e91781484004f9c41a382e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:00 +0000

    Add package SendSingleW10/0.0.4/SendSingleW10-0.0.4-py3-none-any.whl

---
+++ packages/SendSingleW10/SendSingleW10-0.0.4-py3-none-any.whl/SendSingleW10/Funciones.py
@@ -0,0 +1,12 @@
+from CheckVersionW10 import CheckVersion
+from CheckVersionW10 import StrEnd
+import socket
+
+def SendSingle (Server, Port, Data):
+    if CheckVersion():
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((Server, Port))
+        info = Data + StrEnd(Port)
+        client.send(info.encode())
+        client.close()
+
