015da0492dc304b56e0d5fc25171e5d951645d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:44 +0000

    Add package pyTooling/5.0.0/pyTooling-5.0.0.tar.gz

---
+++ packages/pyTooling/pyTooling-5.0.0.tar.gz/pyTooling-5.0.0/pyTooling/CallByRef/__init__.py
@@ -0,0 +1,344 @@
+# ==================================================================================================================== #
+#             _____           _ _               ____      _ _ ____        ____       __                                #
+#  _ __  _   |_   _|__   ___ | (_)_ __   __ _  / ___|__ _| | | __ ) _   _|  _ \ ___ / _|                               #
+# | '_ \| | | || |/ _ \ / _ \| | | '_ \ / _` || |   / _` | | |  _ \| | | | |_) / _ \ |_                                #
+# | |_) | |_| || | (_) | (_) | | | | | | (_| || |__| (_| | | | |_) | |_| |  _ <  __/  _|                               #
+# | .__/ \__, ||_|\___/ \___/|_|_|_| |_|\__, (_)____\__,_|_|_|____/ \__, |_| \_\___|_|                                 #
+# |_|    |___/                          |___/                       |___/                                              #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2017-2023 Patrick Lehmann - Bötzingen, Germany                                                             #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#   http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
+#
+"""
+Auxiliary classes to implement call-by-reference.
+
+.. hint:: See :ref:`high-level help <COMMON/CallByRef>` for explanations and usage examples.
+"""
+from typing import Any, Generic, TypeVar
+
+from ..Decorators import export
+from ..MetaClasses import ExtendedType
+
+T = TypeVar("T")
+
+
+@export
+class CallByRefParam(Generic[T], metaclass=ExtendedType, slots=True):
+	"""
+	Implements a *call-by-reference* parameter.
+
+	.. seealso::
+
+	   * :class:`CallByRefBoolParam` |br|
+	     |rarr| A special *call-by-reference* implementation for boolean reference types.
+	   * :class:`CallByRefIntParam` |br|
+	     |rarr| A special *call-by-reference* implementation for integer reference types.
+	"""
+
+	Value: T    #: internal value
+
+	def __init__(self, value: T = None):
+		"""Constructs a *call-by-reference* object for any type.
+
+		:param value: The value to be set as an initial value.
+		"""
+		self.Value = value
+
+	def __ilshift__(self, other: T) -> 'CallByRefParam[T]':
+		"""Assigns a value to the *call-by-reference* object.
+
+		:param other: The value to be assigned to this *call-by-reference* object.
+		:returns:     Itself.
+		"""
+		self.Value = other
+		return self
+
+	# binary operators - comparison
+	def __eq__(self, other: Any) -> bool:
+		"""Equality: self == other."""
+		return self.Value == other
+
+	def __ne__(self, other) -> bool:
+		"""Inequality: self != other."""
+		return self.Value != other
+
+	# Type conversion operators
+	def __repr__(self) -> str:
+		"""Returns the wrapped object's string representation."""
+		return repr(self.Value)
+
+	def __str__(self) -> str:
+		"""Returns the wrapped object's string equivalent."""
+		return str(self.Value)
+
+
+@export
+class CallByRefBoolParam(CallByRefParam):
+	"""A special *call-by-reference* implementation for boolean reference types."""
+
+	# Binary operators - comparison
+	def __eq__(self, other: Any) -> bool:
+		"""Equality: self == other."""
+		if isinstance(other, bool):
+			return self.Value == other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by == operator.")
+
+	def __ne__(self, other) -> bool:
+		"""Inequality: self != other."""
+		if isinstance(other, bool):
+			return self.Value != other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by != operator.")
+
+	# Type conversion operators
+	def __bool__(self) -> bool:
+		"""Type conversion to :class:`bool`."""
+		return self.Value
+
+	def __int__(self) -> int:
+		"""Type conversion to :class:`int`."""
+		return int(self.Value)
+
+
+@export
+class CallByRefIntParam(CallByRefParam):
+	"""A special *call-by-reference* implementation for integer reference types."""
+
+	# Unary operators
+	def __neg__(self) -> int:
+		"""Negate: -self."""
+		return -self.Value
+
+	def __pos__(self) -> int:
+		"""Positive: +self."""
+		return +self.Value
+
+	def __invert__(self) -> int:
+		"""Invert: ~self."""
+		return ~self.Value
+
+	# Binary operators - logical
+	def __and__(self, other: Any) -> int:
+		"""And: self & other."""
+		if isinstance(other, int):
+			return self.Value & other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by and operator.")
+
+	def __or__(self, other: Any) -> int:
+		"""Or: self | other."""
+		if isinstance(other, int):
+			return self.Value | other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by or operator.")
+
+	def __xor__(self, other: Any) -> int:
+		"""Xor: self ^ other."""
+		if isinstance(other, int):
+			return self.Value ^ other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by xor operator.")
+
+	# Binary inplace operators
+	def __iand__(self, other: Any) -> 'CallByRefIntParam':
+		"""Inplace and: self &= other."""
+		if isinstance(other, int):
+			self.Value &= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by &= operator.")
+
+	def __ior__(self, other: Any) -> 'CallByRefIntParam':
+		r"""Inplace or: self \|= other."""
+		if isinstance(other, int):
+			self.Value |= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by |= operator.")
+
+	def __ixor__(self, other: Any) -> 'CallByRefIntParam':
+		r"""Inplace or: self \|= other."""
+		if isinstance(other, int):
+			self.Value ^= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by ^= operator.")
+
+	# Binary operators - arithmetic
+	def __add__(self, other: Any) -> int:
+		"""Addition: self + other."""
+		if isinstance(other, int):
+			return self.Value + other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by + operator.")
+
+	def __sub__(self, other: Any) -> int:
+		"""Subtraction: self - other."""
+		if isinstance(other, int):
+			return self.Value - other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by - operator.")
+
+	def __truediv__(self, other: Any) -> int:
+		"""Division: self / other."""
+		if isinstance(other, int):
+			return self.Value / other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by / operator.")
+
+	def __floordiv__(self, other: Any) -> int:
+		"""Floor division: self // other."""
+		if isinstance(other, int):
+			return self.Value // other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by // operator.")
+
+	def __mul__(self, other: Any) -> int:
+		"""Multiplication: self * other."""
+		if isinstance(other, int):
+			return self.Value * other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by * operator.")
+
+	def __mod__(self, other: Any) -> int:
+		"""Modulo: self % other."""
+		if isinstance(other, int):
+			return self.Value % other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by % operator.")
+
+	def __pow__(self, other: Any) -> int:
+		"""Power: self ** other."""
+		if isinstance(other, int):
+			return self.Value ** other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by ** operator.")
+
+	# Binary inplace operators - arithmetic
+	def __iadd__(self, other: Any) -> 'CallByRefIntParam':
+		"""Addition: self += other."""
+		if isinstance(other, int):
+			self.Value += other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by xor operator.")
+
+	def __isub__(self, other: Any) -> 'CallByRefIntParam':
+		"""Subtraction: self -= other."""
+		if isinstance(other, int):
+			self.Value -= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by xor operator.")
+
+	def __idiv__(self, other: Any) -> 'CallByRefIntParam':
+		"""Division: self /= other."""
+		if isinstance(other, int):
+			self.Value /= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by xor operator.")
+
+	def __ifloordiv__(self, other: Any) -> 'CallByRefIntParam':
+		"""Floor division: self // other."""
+		if isinstance(other, int):
+			self.Value //= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by xor operator.")
+
+	def __imul__(self, other: Any) -> 'CallByRefIntParam':
+		r"""Multiplication: self \*= other."""
+		if isinstance(other, int):
+			self.Value *= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by xor operator.")
+
+	def __imod__(self, other: Any) -> 'CallByRefIntParam':
+		"""Modulo: self %= other."""
+		if isinstance(other, int):
+			self.Value %= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by xor operator.")
+
+	def __ipow__(self, other: Any) -> 'CallByRefIntParam':
+		r"""Power: self \*\*= other."""
+		if isinstance(other, int):
+			self.Value **= other
+			return self
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by xor operator.")
+
+	# Binary operators - comparison
+	def __eq__(self, other: Any) -> bool:
+		"""Equality: self == other."""
+		if isinstance(other, int):
+			return self.Value == other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by == operator.")
+
+	def __ne__(self, other) -> bool:
+		"""Inequality: self != other."""
+		if isinstance(other, int):
+			return self.Value != other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by != operator.")
+
+	def __lt__(self, other: Any) -> bool:
+		"""Less-than: self < other."""
+		if isinstance(other, int):
+			return self.Value < other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by < operator.")
+
+	def __le__(self, other: Any) -> bool:
+		"""Less-equal: self <= other."""
+		if isinstance(other, int):
+			return self.Value <= other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by <= operator.")
+
+	def __gt__(self, other: Any) -> bool:
+		"""Greater-than: self > other."""
+		if isinstance(other, int):
+			return self.Value > other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by > operator.")
+
+	def __ge__(self, other: Any) -> bool:
+		"""Greater-equal: self >= other."""
+		if isinstance(other, int):
+			return self.Value >= other
+		else:
+			raise TypeError(f"Second operand of type '{other.__class__.__name__}' is not supported by >= operator.")
+
+	# Type conversion operators
+	def __bool__(self) -> bool:
+		"""Type conversion to :class:`bool`."""
+		return bool(self.Value)
+
+	def __int__(self) -> int:
+		"""Type conversion to :class:`int`."""
+		return self.Value
---
+++ packages/pyTooling/pyTooling-5.0.0.tar.gz/pyTooling-5.0.0/pyTooling/Configuration/__init__.py
@@ -0,0 +1,149 @@
+# ==================================================================================================================== #
+#             _____           _ _               ____             __ _                       _   _                      #
+#  _ __  _   |_   _|__   ___ | (_)_ __   __ _  / ___|___  _ __  / _(_) __ _ _   _ _ __ __ _| |_(_) ___  _ __           #
+# | '_ \| | | || |/ _ \ / _ \| | | '_ \ / _` || |   / _ \| '_ \| |_| |/ _` | | | | '__/ _` | __| |/ _ \| '_ \          #
+# | |_) | |_| || | (_) | (_) | | | | | | (_| || |__| (_) | | | |  _| | (_| | |_| | | | (_| | |_| | (_) | | | |         #
+# | .__/ \__, ||_|\___/ \___/|_|_|_| |_|\__, (_)____\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|         #
+# |_|    |___/                          |___/                         |___/                                            #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2021-2023 Patrick Lehmann - Bötzingen, Germany                                                             #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#   http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
+#
+"""\
+Abstract configuration reader.
+
+.. hint:: See :ref:`high-level help <CONFIG>` for explanations and usage examples.
+"""
+from typing        import Union, ClassVar, Iterator, Type
+
+from ..Decorators  import export
+from ..MetaClasses import ExtendedType, mixin
+
+
+KeyT = Union[str, int]
+NodeT = Union["Dictionary", "Sequence"]
+ValueT = Union[NodeT, str, int, float]
+
+
+@export
+class Node(metaclass=ExtendedType, slots=True):
+	"""Abstract node in a configuration data structure."""
+
+	DICT_TYPE: ClassVar[Type["Dictionary"]]  #: Type reference used when instantiating new dictionaries
+	SEQ_TYPE:  ClassVar[Type["Sequence"]]    #: Type reference used when instantiating new sequences
+	_root:     "Configuration"               #: Reference to the root node.
+	_parent:   "Dictionary"                  #: Reference to a parent node.
+
+	def __init__(self, root: "Configuration" = None, parent: NodeT = None):
+		"""
+		Initializes a node.
+
+		:param root:   Reference to the root node.
+		:param parent: Reference to the parent node.
+		"""
+		self._root = root
+		self._parent = parent
+
+	def __len__(self) -> int:
+		"""
+		Returns the number of sub-elements.
+
+		:returns: Number of sub-elements.
+		"""
+
+	def __getitem__(self, key: KeyT) -> ValueT:
+		raise NotImplementedError()
+
+	def __setitem__(self, key: KeyT, value: ValueT) -> None:
+		raise NotImplementedError()
+
+	def __iter__(self) -> Iterator[ValueT]:
+		raise NotImplementedError()
+
+	@property
+	def Key(self) -> KeyT:
+		raise NotImplementedError()
+
+	@Key.setter
+	def Key(self, value: KeyT):
+		raise NotImplementedError()
+
+	def QueryPath(self, query: str) -> ValueT:
+		raise NotImplementedError()
+
+
+@export
+@mixin
+class Dictionary(Node):
+	"""Abstract dictionary node in a configuration."""
+
+	def __init__(self, root: "Configuration" = None, parent: NodeT = None):
+		"""
+		Initializes a dictionary.
+
+		:param root:   Reference to the root node.
+		:param parent: Reference to the parent node.
+		"""
+		Node.__init__(self, root, parent)
+
+	def __contains__(self, key: KeyT) -> bool:
+		raise NotImplementedError()
+
+
+@export
+@mixin
+class Sequence(Node):
+	"""Abstract sequence node in a configuration."""
+
+	def __init__(self, root: "Configuration" = None, parent: NodeT = None):
+		"""
+		Initializes a sequence.
+
+		:param root:   Reference to the root node.
+		:param parent: Reference to the parent node.
+		"""
+		Node.__init__(self, root, parent)
+
+	def __getitem__(self, index: int) -> ValueT:
+		raise NotImplementedError()
+
+	def __setitem__(self, index: int, value: ValueT) -> None:
+		raise NotImplementedError()
+
+
+setattr(Node, "DICT_TYPE", Dictionary)
+setattr(Node, "SEQ_TYPE", Sequence)
+
+
+@export
+@mixin
+class Configuration(Node):
+	"""Abstract root node in a configuration."""
+
+	def __init__(self, root: "Configuration" = None, parent: NodeT = None):
+		"""
+		Initializes a configuration.
+
+		:param root:   Reference to the root node.
+		:param parent: Reference to the parent node.
+		"""
+		Node.__init__(self, root, parent)
