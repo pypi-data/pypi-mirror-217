bf39172d4ed2ad1e31996d1c9050c9f7b8fc59ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:39 +0000

    Add package coder-sage/1.0.0/coder_sage-1.0.0.tar.gz

---
+++ packages/coder-sage/coder_sage-1.0.0.tar.gz/coder_sage-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='coder_sage',
+    version='1.0.0',
+    description='A testing package of codersage',
+    author='Shivazi Biswas',
+    packages=['coder_sage'],
+)
+
