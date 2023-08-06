9d30202405450936a7bc9ab9865b193a3b4fa686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:24 +0000

    Add package mistapi/0.41.12/mistapi-0.41.12-py3-none-any.whl

---
+++ packages/mistapi/mistapi-0.41.12-py3-none-any.whl/mistapi/api/v1/sites/subscriptions.py
@@ -0,0 +1,61 @@
+'''
+--------------------------------------------------------------------------------
+------------------------- Mist API Python CLI Session --------------------------
+
+    Written by: Thomas Munzer (tmunzer@juniper.net)
+    Github    : https://github.com/tmunzer/mistapi_python
+
+    This package is licensed under the MIT License.
+
+--------------------------------------------------------------------------------
+'''
+
+from mistapi import APISession as _APISession
+from mistapi.__api_response import APIResponse as _APIResponse
+import deprecation
+
+def UnsubscribeSite(mist_session:_APISession, site_id:str) -> _APIResponse:
+    """
+    API doc: https://doc.mist-lab.fr/#operation/UnsubscribeSite
+    
+    PARAMS
+    -----------
+    mistapi.APISession : mist_session
+        mistapi session including authentication and Mist host information
+    
+    PATH PARAMS
+    -----------
+    site_id : str        
+    
+    RETURN
+    -----------
+    mistapi.APIResponse
+        response from the API call
+    """
+    uri = f"/api/v1/sites/{site_id}/subscriptions"
+    query_params={}
+    resp = mist_session.mist_delete(uri=uri, query=query_params)
+    return resp
+    
+def SubscribeSite(mist_session:_APISession, site_id:str) -> _APIResponse:
+    """
+    API doc: https://doc.mist-lab.fr/#operation/SubscribeSite
+    
+    PARAMS
+    -----------
+    mistapi.APISession : mist_session
+        mistapi session including authentication and Mist host information
+    
+    PATH PARAMS
+    -----------
+    site_id : str        
+    
+    RETURN
+    -----------
+    mistapi.APIResponse
+        response from the API call
+    """
+    uri = f"/api/v1/sites/{site_id}/subscriptions"
+    resp = mist_session.mist_post(uri=uri)
+    return resp
+    
