6d19dcded8cf6107f7c555020301d3f2c75dbce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:43 +0000

    Add package captchaai/0.0.3/captchaai-0.0.3-py3-none-any.whl

---
+++ packages/captchaai/captchaai-0.0.3-py3-none-any.whl/captchaai/__init__.py
@@ -0,0 +1,5 @@
+from .api import ApiClient
+from .solver import (CaptchaAI, SolverExceptions, ValidationException,
+                     NetworkException, ApiException, TimeoutException)
+
+__version__ = '0.0.3'
