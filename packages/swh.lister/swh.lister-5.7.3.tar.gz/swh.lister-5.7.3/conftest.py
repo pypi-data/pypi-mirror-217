ec411d05bbe6de11e3ba7c6b1e053938676fbb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:29 +0000

    Add package swh.lister/5.7.3/swh.lister-5.7.3.tar.gz

---
+++ packages/swh.lister/swh.lister-5.7.3.tar.gz/swh.lister-5.7.3/conftest.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2020-2022  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import os
+
+import pytest
+
+pytest_plugins = ["swh.scheduler.pytest_plugin", "swh.core.github.pytest_plugin"]
+
+os.environ["LC_ALL"] = "C.UTF-8"
+
+
+@pytest.fixture(autouse=True)
+def tenacity_wait(mocker):
+    # Stops tenacity from blocking lister tests for 50x errors
+    mocker.patch("tenacity.nap.time")
