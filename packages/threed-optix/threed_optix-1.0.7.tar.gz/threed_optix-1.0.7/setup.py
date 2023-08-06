c653b08cc28db1c077dd5c94e542865e88f77c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:18 +0000

    Add package threed-optix/1.0.7/threed_optix-1.0.7.tar.gz

---
+++ packages/threed-optix/threed_optix-1.0.7.tar.gz/threed_optix-1.0.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='threed_optix',
+    version='1.0.7',
+    license='',
+    author="Elika Ron",
+    author_email='alikaron@3doptix.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://3doptix.com',
+    keywords='',
+    install_requires=[
+        'requests',
+        'pandas',
+        'matplotlib'
+    ],
+    long_description_content_type="text/markdown"
+)
