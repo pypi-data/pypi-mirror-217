f406ce69ee15834e1315adaf125b01111034e7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:51 +0000

    Add package particle/0.23.0/particle-0.23.0-py3-none-any.whl

---
+++ packages/particle/particle-0.23.0-py3-none-any.whl/particle/__init__.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2018-2023, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/particle for details.
+
+
+from __future__ import annotations
+
+import sys
+
+from .corsika import Corsika7ID
+from .geant import Geant3ID
+
+# Direct access to Particle literals
+# Direct access to Particle (the CSV file is not read until a particle is accessed)
+from .particle import InvalidParticle, Particle, ParticleNotFound, literals
+from .particle.enums import Charge, Inv, Parity, SpinType, Status
+
+# Direct access to PDGID and other ID classes
+from .pdgid import PDGID
+from .pythia import PythiaID
+
+# Convenient access to the version number
+from .version import version as __version__
+
+sys.modules["particle.literals"] = literals
+
+# Direct access to handy LaTeX to HTML particle name conversions
+# Direct access to kinematics functions
+from .particle import latex_to_html_name, lifetime_to_width, width_to_lifetime
+
+__all__ = (
+    "Charge",
+    "Corsika7ID",
+    "Geant3ID",
+    "Inv",
+    "InvalidParticle",
+    "PDGID",
+    "Parity",
+    "Particle",
+    "ParticleNotFound",
+    "PythiaID",
+    "SpinType",
+    "Status",
+    "latex_to_html_name",
+    "lifetime_to_width",
+    "width_to_lifetime",
+    "__version__",
+)
+
+
+def __dir__() -> tuple[str, ...]:
+    return __all__
