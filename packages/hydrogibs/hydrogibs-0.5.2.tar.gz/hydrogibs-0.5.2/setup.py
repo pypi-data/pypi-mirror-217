2fa69746ff69597e5f7e78bd2f6667a47d853f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:24 +0000

    Add package hydrogibs/0.5.2/hydrogibs-0.5.2.tar.gz

---
+++ packages/hydrogibs/hydrogibs-0.5.2.tar.gz/hydrogibs-0.5.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="hydrogibs",
+    version="0.5.2",
+    description="A personal hydrology and hydraulics package"
+                " based on Christophe Ancey's teaching: "
+                "http://fr.ancey.ch/cours/masterGC/cours-hydraulique.pdf",
+    py_modules="hydrogibs",
+    package_dir={'': 'src'}
+)
