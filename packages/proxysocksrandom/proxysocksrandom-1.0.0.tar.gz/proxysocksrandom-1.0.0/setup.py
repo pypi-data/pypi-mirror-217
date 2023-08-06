3c7058a90f79acf7bc902bbfcdd8012c224b543e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:11 +0000

    Add package proxysocksrandom/1.0.0/proxysocksrandom-1.0.0.tar.gz

---
+++ packages/proxysocksrandom/proxysocksrandom-1.0.0.tar.gz/proxysocksrandom-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='proxysocksrandom',
+	version='1.0.0',
+	author='zelt-dev',
+	author_email='fgytre14@gmail.com',
+	description='proxysocksrandom',
+	packages=['proxysocksrandom'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
