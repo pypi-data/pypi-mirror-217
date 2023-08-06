b82eeb4bba1881b6dc5b920dfb5a08fcc1c0b27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:51 +0000

    Add package particle/0.23.0/particle-0.23.0.tar.gz

---
+++ packages/particle/particle-0.23.0.tar.gz/particle-0.23.0/src/particle/typing.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2018-2023, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/particle for details.
+
+
+from __future__ import annotations
+
+import sys
+from typing import Any, TextIO, Union
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Protocol, runtime_checkable
+else:
+    from typing import Protocol, runtime_checkable
+
+if sys.version_info < (3, 9):
+    from importlib_resources.abc import Traversable
+else:
+    from importlib.abc import Traversable
+
+
+__all__ = (
+    "Protocol",
+    "runtime_checkable",
+    "Traversable",
+    "StringOrIO",
+    "HasOpen",
+    "HasRead",
+)
+
+
+StringOrIO = Union[Traversable, str, TextIO]
+
+
+@runtime_checkable
+class HasOpen(Protocol):
+    def open(self) -> Any:
+        pass
+
+
+@runtime_checkable
+class HasRead(Protocol):
+    def read(self) -> str:
+        pass
