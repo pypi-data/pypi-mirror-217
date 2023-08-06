4836f507a24210399b7824f0174b43946352329b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:56 +0000

    Add package edc-model-admin/0.3.45/edc_model_admin-0.3.45-py3-none-any.whl

---
+++ packages/edc-model-admin/edc_model_admin-0.3.45-py3-none-any.whl/edc_model_admin/mixins/inlines/tabular_inline_mixin.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from django.contrib import admin
+from django_audit_fields.admin import ModelAdminAuditFieldsMixin
+
+
+class TabularInlineMixin(ModelAdminAuditFieldsMixin, admin.TabularInline):
+    insert_before_fieldset: str | None = "Audit"
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj=None, **kwargs)
+        formset.insert_before_fieldset = self.insert_before_fieldset
+        return formset
