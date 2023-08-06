edfe4f99f676a582d493372bb46936816466d950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:18 +0000

    Add package oarepo-runtime/1.3.12/oarepo-runtime-1.3.12.tar.gz

---
+++ packages/oarepo-runtime/oarepo-runtime-1.3.12.tar.gz/oarepo-runtime-1.3.12/oarepo_runtime/cli/__init__.py
@@ -0,0 +1,6 @@
+from .assets import assets
+from .base import as_command, oarepo
+from .index import index
+from .check import check
+
+__all__ = ("oarepo", "index", "as_command", "assets", "check")
