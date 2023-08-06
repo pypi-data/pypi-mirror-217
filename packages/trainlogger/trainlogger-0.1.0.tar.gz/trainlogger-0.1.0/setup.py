daf8f3ee0219c2b80b8d7547df61420592299797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:52 +0000

    Add package trainlogger/0.1.0/trainlogger-0.1.0.tar.gz

---
+++ packages/trainlogger/trainlogger-0.1.0.tar.gz/trainlogger-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="trainlogger",
+    version="0.1.0",    
+    description="A Python package",
+    url="https://github.com/jonas-sth/trainlogger",
+    author="Jonas Steinhäuser",
+    author_email="",
+    license="",
+    packages=["trainlogger"],
+    install_requires=["numpy",                     
+                      "torch",
+                      "torchmetrics",
+                      "tqdm"],
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
