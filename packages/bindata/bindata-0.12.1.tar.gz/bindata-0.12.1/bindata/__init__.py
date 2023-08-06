243ca68e7ef03a0e3d19770fddf70b5647c7bf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:05 +0000

    Add package bindata/0.12.1/bindata-0.12.1.tar.gz

---
+++ packages/bindata/bindata-0.12.1.tar.gz/bindata-0.12.1/bindata/__init__.py
@@ -0,0 +1,15 @@
+"""  Created on 12/03/2023::
+------------- __init__.py -------------
+
+**Authors**: L. Mingarelli
+"""
+
+from bindata.bindata import *
+
+__about__ = "A library for sampling correlated binary variates."
+__version__= '0.12.1'
+__author__ = 'Luca Mingarelli'
+__email__ = "lucamingarelli@me.com"
+__url__ = "https://github.com/LucaMingarelli/bindata"
+
+
