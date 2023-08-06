3e8dafccbf8930fa82fa369de5b126c5aa701d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:49 +0000

    Add package autoprompt/1.0.3/autoprompt-1.0.3.tar.gz

---
+++ packages/autoprompt/autoprompt-1.0.3.tar.gz/autoprompt-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='autoprompt',
+    version='1.0.3',
+    author='Biswanath Das',
+    author_email='cv.biswanath@gmail.com',
+    description='A short description of your package',
+    packages=['autoprompt'],
+    install_requires=[
+        'env',
+    ],
+)
