cb169ad3fdb6d116ce3c0d2c3bc0ddcd5ea73260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:43 +0000

    Add package re-it/1.0.0.0/re_it-1.0.0.0.tar.gz

---
+++ packages/re-it/re_it-1.0.0.0.tar.gz/re_it-1.0.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='re_it',
+    version='1.0.0.0',
+    author='SuiBo',
+    author_email='534047068@qq.com',
+    description='中文的python网络请求库！',
+    install_requires=[
+
+    ],
+    py_modules=["re_it"],
+    packages=find_packages(),
+    url="https://pypi.org/user/SuiBo/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
