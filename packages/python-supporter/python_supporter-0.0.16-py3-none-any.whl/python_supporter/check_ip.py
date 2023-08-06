37bd2b7e0d8d4e84d43ad1183a39c568bc555c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:38 +0000

    Add package python-supporter/0.0.16/python_supporter-0.0.16-py3-none-any.whl

---
+++ packages/python-supporter/python_supporter-0.0.16-py3-none-any.whl/python_supporter/check_ip.py
@@ -0,0 +1,11 @@
+import requests
+
+#https://www.myip.com/api-docs/
+#https://api.myip.com/
+def check_ip():    
+    try:
+        response = requests.get("https://api.myip.com")
+        #print(response.json()) #{"ip":"180.92.96.163","country":"Korea, Republic of","cc":"KR"}
+        return response.json()["ip"]
+    except:
+        return None
