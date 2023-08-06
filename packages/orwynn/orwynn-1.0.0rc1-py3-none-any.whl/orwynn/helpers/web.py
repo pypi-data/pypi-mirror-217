a4f94622fd43574cff989772283cde02ab1b14d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:26 +0000

    Add package orwynn/1.0.0rc1/orwynn-1.0.0rc1-py3-none-any.whl

---
+++ packages/orwynn/orwynn-1.0.0rc1-py3-none-any.whl/orwynn/helpers/web.py
@@ -0,0 +1,50 @@
+# A request of any supported protocol
+from enum import Enum
+from typing import TYPE_CHECKING, Union
+
+from orwynn.utils.protocol import Protocol
+
+if TYPE_CHECKING:
+    from orwynn.http import HttpRequest, HttpResponse
+    from orwynn.websocket import Websocket
+
+
+GenericRequest = Union[
+    "HttpRequest",
+    # The Websocket object is a representation of the request
+    "Websocket"
+]
+GenericResponse = Union[
+    "HttpResponse",
+    # The Websocket protocol doesn't return any response in direct form from a
+    # controller - it sends it over special channel instead.
+    None
+]
+
+
+class RequestMethod(Enum):
+    """
+    All possible request methods.
+    """
+    GET = "get"
+    POST = "post"
+    PUT = "put"
+    DELETE = "delete"
+    PATCH = "patch"
+    OPTIONS = "options"
+    WEBSOCKET = "websocket"
+
+
+REQUEST_METHOD_BY_PROTOCOL: dict[Protocol, list[RequestMethod]] = {
+    Protocol.HTTP: [
+        RequestMethod.GET,
+        RequestMethod.POST,
+        RequestMethod.PUT,
+        RequestMethod.DELETE,
+        RequestMethod.PATCH,
+        RequestMethod.OPTIONS,
+    ],
+    Protocol.WEBSOCKET: [
+        RequestMethod.WEBSOCKET
+    ]
+}
