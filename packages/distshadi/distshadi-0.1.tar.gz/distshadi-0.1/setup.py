2a95c9264f1f97a3215fdc7ed6e385c4f36e4734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:40 +0000

    Add package distshadi/0.1/distshadi-0.1.tar.gz

---
+++ packages/distshadi/distshadi-0.1.tar.gz/distshadi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distshadi',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distshadi'],
+      zip_safe=False)
