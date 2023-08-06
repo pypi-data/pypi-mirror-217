44eac1de049c33118e8bf5f1b4ceb58097f448e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:04 +0000

    Add package raspisump/1.8.2/raspisump-1.8.2-py3-none-any.whl

---
+++ packages/raspisump/raspisump-1.8.2-py3-none-any.whl/raspisump-1.8.2.data/scripts/rsumpmonitor.py
@@ -0,0 +1,21 @@
+#!python
+
+# Raspi-sump, a sump pump monitoring system.
+# Al Audet
+# https://www.linuxnorth.org/raspi-sump/
+#
+# All configuration changes should be done in raspisump.conf
+# MIT License -- https://www.linuxnorth.org/raspi-sump/license.html
+
+from raspisump import checkpid
+
+
+def main():
+    """run checkpid.py module to restart Raspi-Sump if the rsump.py process is
+    stopped or has spawned multiple processes."""
+    process = "/usr/local/bin/rsump.py"
+    checkpid.check_pid(process)
+
+
+if __name__ == "__main__":
+    main()
