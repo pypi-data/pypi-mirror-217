2835d28c4c0be8699f7bc2701cf4f2f2fe13e4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:31 +0000

    Add package ormlite/0.1.0/ormlite-0.1.0.tar.gz

---
+++ packages/ormlite/ormlite-0.1.0.tar.gz/ormlite-0.1.0/src/ormlite/errors.py
@@ -0,0 +1,13 @@
+class RegexMatchError(Exception):
+    pass
+
+
+class MissingAdapterError(Exception):
+    pass
+
+
+class InvalidForeignKeyError(Exception):
+    pass
+
+class MultiplePrimaryKeysError(Exception):
+    pass
