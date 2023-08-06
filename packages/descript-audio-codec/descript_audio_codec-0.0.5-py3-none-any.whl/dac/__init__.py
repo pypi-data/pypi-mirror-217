a77a7df494362636e4b70ebaca010f64a4f18a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:32 +0000

    Add package descript-audio-codec/0.0.5/descript_audio_codec-0.0.5-py3-none-any.whl

---
+++ packages/descript-audio-codec/descript_audio_codec-0.0.5-py3-none-any.whl/dac/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.5"
+
+# preserved here for legacy reasons
+__model_version__ = "latest"
+
+import audiotools
+
+audiotools.ml.BaseModel.INTERN += ["dac.**"]
+audiotools.ml.BaseModel.EXTERN += ["einops"]
+
+
+from . import nn
+from . import model
