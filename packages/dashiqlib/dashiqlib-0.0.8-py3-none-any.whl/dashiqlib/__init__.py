c00bc9fc7ef5dcf21df37f18490d101009dec3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:11 +0000

    Add package dashiqlib/0.0.8/dashiqlib-0.0.8-py3-none-any.whl

---
+++ packages/dashiqlib/dashiqlib-0.0.8-py3-none-any.whl/dashiqlib/__init__.py
@@ -0,0 +1,18 @@
+"""
+# DashIQ Python Library
+
+## Release Notes
+
+- 0.0.7 - Chat functions start
+- 0.0.6 - Clerk User API methods
+- 0.0.5 - Replaced JWT w/ Clerk API
+- 0.0.4 - JWT functions
+- 0.0.3 - Introduced formal testing and linting
+
+"""
+__version__ = '0.0.7'
+
+
+
+class MissingEnvVarException(Exception):
+    """Class to catch missing required envrionment variables"""
