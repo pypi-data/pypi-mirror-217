fab02767024e5cf588aa31e781e948173c3b899e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:22 +0000

    Add package chainsaddiction/0.2.2/chainsaddiction-0.2.2.tar.gz

---
+++ packages/chainsaddiction/chainsaddiction-0.2.2.tar.gz/chainsaddiction-0.2.2/src/vmath/tests/test_rnd.h
@@ -0,0 +1,19 @@
+#ifndef test_rnd_h
+#define test_rnd_h
+
+#include "config.h"
+#include "unittest.h"
+#include "rnd.h"
+
+
+bool test__rnd_int (void);
+bool test__v_rnd_int (void);
+
+bool test__rnd_scalar (void);
+bool test__v_rnd_scalar (void);
+
+bool test__sample (void);
+bool test__v_sample (void);
+
+
+#endif  /* test_rnd_h */
