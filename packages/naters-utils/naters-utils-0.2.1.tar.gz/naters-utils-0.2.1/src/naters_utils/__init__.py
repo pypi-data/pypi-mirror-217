e05362d74dce27649db692e768218d1e76c13a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:22 +0000

    Add package naters-utils/0.2.1/naters-utils-0.2.1.tar.gz

---
+++ packages/naters-utils/naters-utils-0.2.1.tar.gz/naters-utils-0.2.1/src/naters_utils/__init__.py
@@ -0,0 +1,12 @@
+# Naters-Utils
+# Various things i have made that I find useful and want to share
+
+
+# Metadata
+__all__ = ["functions", "hello", "isntinstance", "iterables"]
+__version__ = "0.2.1"
+
+
+# Imports
+from ._hello import hello
+from ._isntinstance import isntinstance
