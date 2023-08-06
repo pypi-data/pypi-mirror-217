8a744d92be6b37e0ed9a867887d5a7567d8d6e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:36 +0000

    Add package dataclass-codec/0.1.8/dataclass_codec-0.1.8-py3-none-any.whl

---
+++ packages/dataclass-codec/dataclass_codec-0.1.8-py3-none-any.whl/dataclass_codec/types_predicates.py
@@ -0,0 +1,13 @@
+from enum import Enum
+from typing import Any, Type
+
+
+def is_generic_dataclass_predicate(_type: Type[Any]) -> bool:
+    return hasattr(_type, "__origin__") and is_dataclass_predicate(_type.__origin__)
+
+def is_dataclass_predicate(_type: Type[Any]) -> bool:
+    return hasattr(_type, "__dataclass_fields__")
+
+
+def is_enum_predicate(_type: Type[Any]) -> bool:
+    return issubclass(_type, Enum)
