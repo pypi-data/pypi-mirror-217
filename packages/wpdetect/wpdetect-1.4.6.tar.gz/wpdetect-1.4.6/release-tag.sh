cf743085a54a089013e92499af410c33df228640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:45 +0000

    Add package wpdetect/1.4.6/wpdetect-1.4.6.tar.gz

---
+++ packages/wpdetect/wpdetect-1.4.6.tar.gz/wpdetect-1.4.6/release-tag.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo "Running release-tag.sh"
+
+# Ensure we're on the master branch
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
+    echo "Not on master branch, aborting"
+    exit 1
+fi
+
+# Generate the changelog
+echo "Generating changelog"
+python utils/generate_changelog.py
+
+
+# Run the version bumping script
+new_version=$(python utils/bump_version.py)
+
+# Commit the updated version
+echo "Committing version $new_version"
+git add pyproject.toml
+git commit -S -m "release $new_version"
+
+# Tag the release
+echo "Tagging version $new_version"
+git tag -s "$new_version" -m "release $new_version"
+
+# Push the commit and tag
+echo "Pushing commit and tag"
+git push --follow-tags origin master
---
+++ packages/wpdetect/wpdetect-1.4.6.tar.gz/wpdetect-1.4.6/utils/bump_version.py
@@ -0,0 +1,41 @@
+"""
+    This file increments the version in pyproject.toml.
+"""
+
+import os
+import toml
+
+
+def increment_version():
+    """Increments the version in pyproject.toml and returns the new version."""
+
+    file_path = os.path.join(os.path.dirname(
+        __file__), '..', 'pyproject.toml')
+
+    with open(file_path, 'r', encoding="utf-8") as file:
+        config = toml.load(file)
+
+    current_version = config['project']['version']
+
+    # Increment the version
+    version_parts = current_version.split('.')
+    version_parts[-1] = str(int(version_parts[-1]) + 1)
+    new_version = '.'.join(version_parts)
+
+    # Update the version in pyproject.toml
+    with open(file_path, 'r', encoding="utf-8") as file:
+        lines = file.readlines()
+
+    with open(file_path, 'w', encoding="utf-8") as file:
+        for line in lines:
+            if line.startswith('version'):
+                file.write(f'version = "{new_version}"\n')
+            else:
+                file.write(line)
+
+    return new_version
+
+
+# Call the increment_version() function
+BUMPED_VERSION = increment_version()
+print(f"v{BUMPED_VERSION}")
