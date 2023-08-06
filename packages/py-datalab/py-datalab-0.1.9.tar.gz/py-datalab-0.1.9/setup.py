7b4a3c303d476d182feac20099fc6ca86cdb4890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:41 +0000

    Add package py-datalab/0.1.9/py-datalab-0.1.9.tar.gz

---
+++ packages/py-datalab/py-datalab-0.1.9.tar.gz/py-datalab-0.1.9/setup.py
@@ -0,0 +1,31 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 6):
+    sys.exit("Sorry, Python < 3.6 is not supported")
+
+setup(
+    name="py-datalab",
+    version="0.1.9",
+    author="Mateusz Nowak",
+    author_email="mateusz.nowak.pol@gmail.com",
+    description="This project provides classes for working with data in python. Actually there are two classes: Matrix and Vector.",
+    url="https://github.com/matuszen/Python-DataLab",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+)
