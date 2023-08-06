667c5c58b247c217fc9ba517cc45e4f1f67c36cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:51 +0000

    Add package nonebot-plugin-alconna/0.9.0/nonebot_plugin_alconna-0.9.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.9.0-py3-none-any.whl/nonebot_plugin_alconna/matcher.pyi
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from datetime import datetime, timedelta
+from typing import Callable
+
+from arclet.alconna import Alconna
+from nonebot.dependencies import Dependent
+from nonebot.matcher import Matcher
+from nonebot.permission import Permission
+from nonebot.rule import Rule
+from nonebot.typing import T_Handler, T_PermissionChecker, T_RuleChecker, T_State
+from nonebot_plugin_alconna.model import CompConfig
+from nonebot_plugin_alconna.typings import TConvert, MReturn
+
+def on_alconna(
+    command: Alconna | str,
+    rule: Rule | T_RuleChecker | None = None,
+    skip_for_unmatch: bool = True,
+    auto_send_output: bool = False,
+    output_converter: TConvert | None = None,
+    aliases: set[str | tuple[str, ...]] | None = None,
+    comp_config: CompConfig | None = None,
+    permission: Permission | T_PermissionChecker | None = ...,
+    *,
+    handlers: list[T_Handler | Dependent] | None = ...,
+    temp: bool = ...,
+    expire_time: datetime | timedelta | None = ...,
+    priority: int = ...,
+    block: bool = ...,
+    state: T_State | None = ...,
+) -> type[Matcher]: ...
+
+
+def funcommand(
+    name: str | None = None,
+    prefixes: list[str] | None = None,
+    description: str | None = None,
+) -> Callable[[Callable[..., MReturn]], type[Matcher]]:
+    ...
