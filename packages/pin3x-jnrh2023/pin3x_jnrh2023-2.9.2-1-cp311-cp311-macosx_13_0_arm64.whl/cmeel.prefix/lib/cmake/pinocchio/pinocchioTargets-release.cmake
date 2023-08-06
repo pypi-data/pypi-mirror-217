ad24d385e5a1f86dcd62af9bd785c8a9e9a34c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:37 +0000

    Add package pin3x-jnrh2023/2.9.2/pin3x_jnrh2023-2.9.2-1-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/pin3x-jnrh2023/pin3x_jnrh2023-2.9.2-1-cp311-cp311-macosx_13_0_arm64.whl/cmeel.prefix/lib/cmake/pinocchio/pinocchioTargets-release.cmake
@@ -0,0 +1,39 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pinocchio::pinocchio_double" for configuration "Release"
+set_property(TARGET pinocchio::pinocchio_double APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pinocchio::pinocchio_double PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${PACKAGE_PREFIX_DIR}/lib/libpinocchio_double.2.9.2.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libpinocchio_double.2.9.2.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets pinocchio::pinocchio_double )
+list(APPEND _cmake_import_check_files_for_pinocchio::pinocchio_double "${PACKAGE_PREFIX_DIR}/lib/libpinocchio_double.2.9.2.dylib" )
+
+# Import target "pinocchio::pinocchio_pywrap_default" for configuration "Release"
+set_property(TARGET pinocchio::pinocchio_pywrap_default APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pinocchio::pinocchio_pywrap_default PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${PACKAGE_PREFIX_DIR}/lib/python3.11/site-packages/pinocchio/pinocchio_pywrap_default.2.9.2.cpython-311-darwin.so"
+  IMPORTED_SONAME_RELEASE "@rpath/pinocchio_pywrap_default.2.9.2.cpython-311-darwin.so"
+  )
+
+list(APPEND _cmake_import_check_targets pinocchio::pinocchio_pywrap_default )
+list(APPEND _cmake_import_check_files_for_pinocchio::pinocchio_pywrap_default "${PACKAGE_PREFIX_DIR}/lib/python3.11/site-packages/pinocchio/pinocchio_pywrap_default.2.9.2.cpython-311-darwin.so" )
+
+# Import target "pinocchio::pinocchio_pywrap_casadi" for configuration "Release"
+set_property(TARGET pinocchio::pinocchio_pywrap_casadi APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pinocchio::pinocchio_pywrap_casadi PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${PACKAGE_PREFIX_DIR}/lib/python3.11/site-packages/pinocchio/pinocchio_pywrap_casadi.2.9.2.cpython-311-darwin.so"
+  IMPORTED_SONAME_RELEASE "@rpath/pinocchio_pywrap_casadi.2.9.2.cpython-311-darwin.so"
+  )
+
+list(APPEND _cmake_import_check_targets pinocchio::pinocchio_pywrap_casadi )
+list(APPEND _cmake_import_check_files_for_pinocchio::pinocchio_pywrap_casadi "${PACKAGE_PREFIX_DIR}/lib/python3.11/site-packages/pinocchio/pinocchio_pywrap_casadi.2.9.2.cpython-311-darwin.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
