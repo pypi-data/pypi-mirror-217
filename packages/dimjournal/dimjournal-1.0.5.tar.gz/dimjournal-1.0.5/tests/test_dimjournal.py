92eb54e1ec13891e885b063f4ec238eca0bf340a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:51 +0000

    Add package dimjournal/1.0.5/dimjournal-1.0.5.tar.gz

---
+++ packages/dimjournal/dimjournal-1.0.5.tar.gz/dimjournal-1.0.5/tests/test_dimjournal.py
@@ -0,0 +1,12 @@
+import pytest
+from dimjournal.dimjournal import (
+    MidjourneyAPI,
+    MidjourneyJobCrawler,
+    MidjourneyDownloader,
+)
+from pathlib import Path
+import undetected_chromedriver as webdriver
+
+
+def test():
+    assert True
