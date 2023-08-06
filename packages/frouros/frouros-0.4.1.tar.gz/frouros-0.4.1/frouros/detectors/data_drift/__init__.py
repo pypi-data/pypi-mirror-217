e4c95204596a068f5a45a7b786c4096764e979f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:12 +0000

    Add package frouros/0.4.1/frouros-0.4.1.tar.gz

---
+++ packages/frouros/frouros-0.4.1.tar.gz/frouros-0.4.1/frouros/detectors/data_drift/__init__.py
@@ -0,0 +1,36 @@
+"""Data drift detection methods init."""
+
+from .batch import (  # noqa: F401
+    BhattacharyyaDistance,
+    ChiSquareTest,
+    CVMTest,
+    EMD,
+    HellingerDistance,
+    HINormalizedComplement,
+    JS,
+    KL,
+    KSTest,
+    PSI,
+    MannWhitneyUTest,
+    MMD,
+    WelchTTest,
+)
+
+from .streaming import IncrementalKSTest, MMD as MMDStreaming  # noqa: N811
+
+__all__ = [
+    "BhattacharyyaDistance",
+    "ChiSquareTest",
+    "CVMTest",
+    "EMD",
+    "HellingerDistance",
+    "HINormalizedComplement",
+    "IncrementalKSTest",
+    "JS",
+    "KL",
+    "KSTest",
+    "PSI",
+    "MannWhitneyUTest",
+    "MMDStreaming",
+    "WelchTTest",
+]
