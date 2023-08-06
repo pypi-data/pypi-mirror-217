324a3abe18f67c3b69a88c09dac15a0270146daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:19 +0000

    Add package sdqrcode/0.3.6/sdqrcode-0.3.6.tar.gz

---
+++ packages/sdqrcode/sdqrcode-0.3.6.tar.gz/sdqrcode-0.3.6/src/sdqrcode/Engines/Engine.py
@@ -0,0 +1,12 @@
+class Engine:
+    def __init__(self, config):
+        self.config = config
+
+    def init_backend():
+        pass
+
+    def generate_sd_qrcode():
+        pass
+
+
+# remove controlnet images
