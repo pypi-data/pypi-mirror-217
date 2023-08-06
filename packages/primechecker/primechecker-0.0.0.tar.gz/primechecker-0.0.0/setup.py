3b0f0c72144671798f6e22526a0ebb48712a3762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:38 +0000

    Add package primechecker/0.0.0/primechecker-0.0.0.tar.gz

---
+++ packages/primechecker/primechecker-0.0.0.tar.gz/primechecker-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='primechecker',
+    author='Titus K.',
+    author_email='tituskamunya@gmail.com',
+    description='A package for checking prime numbers',
+    packages=find_packages(),
+)
