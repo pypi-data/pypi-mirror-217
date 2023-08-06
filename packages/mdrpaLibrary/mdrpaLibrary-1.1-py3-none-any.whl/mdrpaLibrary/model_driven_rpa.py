fdb28488dfea7faebef5dde2b5902d9bcebf5cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:33 +0000

    Add package mdrpaLibrary/1.1/mdrpaLibrary-1.1-py3-none-any.whl

---
+++ packages/mdrpaLibrary/mdrpaLibrary-1.1-py3-none-any.whl/mdrpaLibrary/model_driven_rpa.py
@@ -0,0 +1,44 @@
+from robot.api.deco import keyword
+from robot.libraries.BuiltIn import BuiltIn
+
+class ModelDrivenRpa:
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+
+    def __init__(self):
+        self.builtin = BuiltIn()
+        self.resource_files = [
+            'clickButtonModel.robot',
+            'inputFieldModel.robot',
+            'selectCheckboxModel.robot',
+            'selectFromDropdownModel.robot',
+            'getUiModel.robot'
+        ]  # Update with the actual resource file names
+
+    def import_resource_file(self, resource_file):
+        # Use the BuiltIn library's `Import Resource` keyword to import the specified resource file
+        self.builtin.import_resource(resource_file)
+
+    @keyword
+    def click_button_model_keyword(self, *args):
+        self.import_resource_file(self.resource_files[0])  # Import the first resource file
+        self.builtin.run_keyword('Click Button Model', *args)
+
+    @keyword
+    def input_field_model_keyword(self, *args):
+        self.import_resource_file(self.resource_files[1])  # Import the second resource file
+        self.builtin.run_keyword('Input Field Model', *args)
+
+    @keyword
+    def select_checkbox_model_keyword(self, *args):
+        self.import_resource_file(self.resource_files[2])  # Import the third resource file
+        self.builtin.run_keyword('Select Checkbox Model', *args)
+
+    @keyword
+    def select_from_dropdown_model_keyword(self, *args):
+        self.import_resource_file(self.resource_files[3])  # Import the fourth resource file
+        self.builtin.run_keyword('Select From Dropdown Model', *args)
+    
+    @keyword
+    def get_ui_models_keyword(self, *args):
+        self.import_resource_file(self.resource_files[4])  # Import the fifth resource file
+        self.builtin.run_keyword('Get UiModels', *args)
