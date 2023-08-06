a3d1f015b53178ab02d4d2e4fdae7ca16074bc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:35 +0000

    Add package genworlds/0.0.3/genworlds-0.0.3.tar.gz

---
+++ packages/genworlds/genworlds-0.0.3.tar.gz/genworlds-0.0.3/genworlds/objects/base_object.py
@@ -0,0 +1,34 @@
+from uuid import uuid4
+from genworlds.events.websocket_event_handler import WebsocketEventHandler
+from genworlds.events.basic_events import (
+    AgentGetsObjectInfoEvent,
+    ObjectSendsInfoToAgentEvent,
+)
+
+
+class BaseObject(WebsocketEventHandler):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        id: str = None,
+    ):
+        self.id = id if id else str(uuid4())
+        self.name = name
+        self.description = description
+
+        super().__init__(self.id)
+        self.register_event_listeners(
+            [
+                # (AgentGetsObjectInfoEvent, self.agent_gets_object_info_listener)
+            ]
+        )
+
+    def agent_gets_object_info_listener(self, event: AgentGetsObjectInfoEvent):
+        self.send_event(
+            ObjectSendsInfoToAgentEvent,
+            target_id=event.agent_id,
+            object_name=self.name,
+            object_description=self.description,
+            possible_events=self.event_classes.keys(),
+        )
