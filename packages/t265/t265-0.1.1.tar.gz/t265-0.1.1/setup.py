8f0679365da39298de510e997ef8ff5bca77aaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:26 +0000

    Add package t265/0.1.1/t265-0.1.1.tar.gz

---
+++ packages/t265/t265-0.1.1.tar.gz/t265-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# from pathlib import Path
+# this_directory = Path(__file__).parent
+# long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name='t265',
+    version='0.1.1',
+    description='t265 Tracking camera API wrapper',
+    author='Yusuke Tanaka',
+    license='LGPLv3',
+    project_urls={'GitHub':'https://github.com/Suke0811/Localization_T265'},
+    packages=find_packages(include=['t265', 't265.*']),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Robot Framework',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
