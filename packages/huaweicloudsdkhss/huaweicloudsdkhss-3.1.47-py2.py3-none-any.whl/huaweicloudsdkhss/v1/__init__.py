751bb32438678a789d052999edd08be99655a620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:15 +0000

    Add package huaweicloudsdkhss/3.1.47/huaweicloudsdkhss-3.1.47-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkhss/huaweicloudsdkhss-3.1.47-py2.py3-none-any.whl/huaweicloudsdkhss/v1/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+# import HssClient
+from huaweicloudsdkhss.v1.hss_client import HssClient
+from huaweicloudsdkhss.v1.hss_async_client import HssAsyncClient
+# import models into sdk package
+from huaweicloudsdkhss.v1.model.event import Event
+from huaweicloudsdkhss.v1.model.host import Host
+from huaweicloudsdkhss.v1.model.list_events_request import ListEventsRequest
+from huaweicloudsdkhss.v1.model.list_events_response import ListEventsResponse
+from huaweicloudsdkhss.v1.model.list_hosts_request import ListHostsRequest
+from huaweicloudsdkhss.v1.model.list_hosts_response import ListHostsResponse
+
