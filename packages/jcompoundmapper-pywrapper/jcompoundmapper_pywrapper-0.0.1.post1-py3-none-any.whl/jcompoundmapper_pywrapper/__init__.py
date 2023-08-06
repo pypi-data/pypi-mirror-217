7dbe4d89e40687184dffc46560bc23b2aa341fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:30 +0000

    Add package jcompoundmapper-pywrapper/0.0.1.post1/jcompoundmapper_pywrapper-0.0.1.post1-py3-none-any.whl

---
+++ packages/jcompoundmapper-pywrapper/jcompoundmapper_pywrapper-0.0.1.post1-py3-none-any.whl/jcompoundmapper_pywrapper/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8
+
+"""Wrapper for jCompoundMapper molecular fingerprints"""
+
+from .jcompoundmapper import JCompoundMapper, AtomType, FpParamType, DEFAULT_FP_PARAMETERS
+
+__version__ = "0.0.1-post1"
