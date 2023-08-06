107ec3cdeece4fd56ce33059c1c89c09ad1dd4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:16 +0000

    Add package teamanchorhello/0.0.12/teamanchorhello-0.0.12.tar.gz

---
+++ packages/teamanchorhello/teamanchorhello-0.0.12.tar.gz/teamanchorhello-0.0.12/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="teamanchorhello",
+    version="0.0.12",
+    author="fanis",
+    description="teamanchor say hello using Pypi package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["teamanchorhello"],
+    package_dir={'':'teamanchorhello/src'},
+    install_requires=[]
+)
