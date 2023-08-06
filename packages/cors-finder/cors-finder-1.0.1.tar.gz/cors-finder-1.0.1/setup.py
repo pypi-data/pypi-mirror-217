40d298e98bd9735d11e4735584b9d9f88a420508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:52 +0000

    Add package cors-finder/1.0.1/cors-finder-1.0.1.tar.gz

---
+++ packages/cors-finder/cors-finder-1.0.1.tar.gz/cors-finder-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cors-finder",
+    version= '1.0.1',
+    author= 'Hariharan',
+    description= 'The cors-finder package is used to find the vulnerable CORS domains',
+    keywords= ['cors', 'cors-finder', 'cors misconfiguration', 'cors exploit', 'cors vulnerability'],
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'colorama',
+    ],
+    
+)
