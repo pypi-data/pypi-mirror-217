4d77301a7cdd108bc3ba89712aef4dc1d90abd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:46 +0000

    Add package lfinancial/0.1.5/lfinancial-0.1.5.tar.gz

---
+++ packages/lfinancial/lfinancial-0.1.5.tar.gz/lfinancial-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+excluded_packages = ["docs", "tests", "tests.*"]
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='lfinancial',
+    version='0.1.5',
+    author='zaneliu',
+    description='Generate financial test data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    readme="README.md",
+    author_email='lzy291980138@163.com',
+    packages=['lfinancial'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+    ],
+)
