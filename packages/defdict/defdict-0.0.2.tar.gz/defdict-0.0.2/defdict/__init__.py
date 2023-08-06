426a978d3bff826bf1d1dfe1812b6ec64333453a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:00 +0000

    Add package defdict/0.0.2/defdict-0.0.2.tar.gz

---
+++ packages/defdict/defdict-0.0.2.tar.gz/defdict-0.0.2/defdict/__init__.py
@@ -0,0 +1,11 @@
+from .DefDict import DefDict
+from .UnitType import UnitType
+from .MapRule import MapRule
+from .StdUnit import *
+import SpeedTest
+
+
+__author__ = 'UCLA LEMUR'
+__credits__ = 'The Laboratory for Embedded Machines and Ubiquitous Robots'
+__url__ = 'https://github.com/Suke0811/DefDict/'
+__description__ = 'Defined Dictionary'
---
+++ packages/defdict/defdict-0.0.2.tar.gz/defdict-0.0.2/defdict/_static_version.py
@@ -0,0 +1,2 @@
+# This file has been created by setup.py.
+version = 'unknown+g3d827be.dirty'
