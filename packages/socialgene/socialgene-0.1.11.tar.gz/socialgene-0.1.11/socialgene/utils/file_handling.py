ec7876f64f6bd30b899097023b688f533f64e6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:14 +0000

    Add package socialgene/0.1.11/socialgene-0.1.11.tar.gz

---
+++ packages/socialgene/socialgene-0.1.11.tar.gz/socialgene-0.1.11/socialgene/utils/file_handling.py
@@ -0,0 +1,121 @@
+from typing import IO
+
+import bz2
+import gzip
+import lzma
+import shutil
+import tarfile
+from contextlib import contextmanager
+from enum import Enum, auto
+from pathlib import Path
+
+from socialgene.utils.logging import log
+
+
+class Compression(Enum):
+    bzip2 = auto()
+    gzip = auto()
+    xz = auto()
+    uncompressed = auto()
+
+
+def is_compressed(filepath: Path) -> Compression:
+    with open(filepath, "rb") as f:
+        signature = f.peek(8)[:8]
+        if tuple(signature[:2]) == (0x1F, 0x8B):
+            return Compression.gzip
+        elif tuple(signature[:3]) == (0x42, 0x5A, 0x68):
+            return Compression.bzip2
+        elif tuple(signature[:7]) == (0xFD, 0x37, 0x7A, 0x58, 0x5A, 0x00, 0x00):
+            return Compression.xz
+        else:
+            return Compression.uncompressed
+
+
+def gunzip(filepath: Path) -> None:
+    if is_compressed(filepath).name == "gzip":
+        # remove ".gz" for the new filepath
+        new_hmm_path = Path(filepath.parents[0], filepath.stem)
+        log.info(f"Start decompressing: {str(Path(filepath).stem)}")
+        # open gz, decompress, write back out to new file
+        with gzip.open(filepath, "rb") as f_in:
+            with open(new_hmm_path, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        log.info(f"Finish decompressing: {str(Path(filepath).stem)}")
+
+
+@contextmanager
+def open_read(filepath: Path) -> IO:
+    filepath_compression = is_compressed(filepath)
+    if filepath_compression == Compression.gzip:
+        f = gzip.open(filepath, "rt")
+    elif filepath_compression == Compression.bzip2:
+        f = bz2.open(filepath, "rt")
+    elif filepath_compression == Compression.xz:
+        f = lzma.open(filepath, "rt")
+    else:
+        f = open(filepath, "rt")
+    try:
+        yield f
+    finally:
+        f.close()
+
+
+@contextmanager
+def open_write(filepath: str, mode="w", compression: str = None) -> IO:
+    """Open a file for writing
+
+    Args:
+        filepath (str): input filepath
+        mode (str, optional): modes to open file for writing (only use "a", "w", or "r"- "b" will auto-applied). Defaults to "w".
+        compression (str, optional): which compression method to use ("gzip", "bzip", "xz", or None). Defaults to None.
+    Yields:
+        Iterator[IO]: context manager
+    """
+    filepath = Path(filepath)
+    if mode not in ["w", "a", "r"]:
+        raise ValueError
+    match compression:
+        case "gzip":
+            _open = gzip.open(filepath.with_suffix(".gz"), f"{mode}")
+        case "bzip":
+            _open = bz2.open(filepath.with_suffix(".bz2"), f"{mode}")
+        case "xz":
+            _open = lzma.open(filepath.with_suffix(".xz"), f"{mode}")
+        case None:
+            _open = open(filepath, mode)
+        case _:
+            raise ValueError(
+                f'compression variable must be "gzip", "bzip", "xz", or None; was {compression}'
+            )
+    try:
+        yield _open
+    finally:
+        _open.close()
+
+
+def check_if_tar(filepath):
+    return tarfile.is_tarfile(filepath)
+
+
+def guess_filetype(filepath):
+    """Guess what type of file it is
+    Args:
+        filepath: file path of file to guess
+    Returns:
+        bool: true/false
+    """
+    with open_read(filepath) as f:
+        l1 = f.readline()
+
+    if l1.startswith("LOCUS "):
+        return "genbank"
+    if l1.startswith(">"):
+        return "fasta"
+    if l1.startswith("##gff-version"):
+        return "gff"
+    if (
+        l1.replace(" ", "")
+        == "#---fullsequence-----------------thisdomain-------------hmmcoordalicoordenvcoord\n"
+    ):
+        return "domtblout"
---
+++ packages/socialgene/socialgene-0.1.11.tar.gz/socialgene-0.1.11/socialgene/utils/goterms.py
@@ -0,0 +1,176 @@
+import argparse
+import csv
+from pathlib import Path
+import requests
+import re
+from copy import deepcopy
+from socialgene.utils.logging import log
+
+parser = argparse.ArgumentParser(
+    description="Download and create edgelist of all Go terms"
+)
+
+parser.add_argument(
+    "--outdir",
+    metavar="filepath",
+    help="Output directory filepath",
+    required=True,
+)
+
+OBO_URL = "http://purl.obolibrary.org/obo/go/go-basic.obo"
+
+goterm_pattern = re.compile("GO:[0-9]{7}", flags=0)
+
+
+def extract_goterm_int_as_str(id):
+    if goterm := goterm_pattern.search(id):
+        return str(goterm.group().removeprefix("GO:"))
+
+
+class GoNode:
+    __slots__ = ["id", "name", "namespace", "definition"]
+
+    def __init__(self) -> None:
+        self.id = None
+        self.name = None
+        self.namespace = None
+        self.definition = None
+
+    def add_id(self, x):
+        if goterm := extract_goterm_int_as_str(x):
+            self.id = goterm
+        else:
+            print(f"'{x}' isn't a goterm")
+            raise
+
+    def add_name(self, x):
+        self.name = x.removeprefix("name:").strip()
+
+    def add_namespace(self, x):
+        self.namespace = x.removeprefix("namespace:").strip()
+
+    def add_definition(self, x):
+        self.definition = x.removeprefix("definition:").strip()
+
+    def assign(self, line):
+        if line.startswith("id:"):
+            self.add_id(line)
+        elif line.startswith("name:"):
+            self.add_name(line)
+        elif line.startswith("namespace:"):
+            self.add_namespace(line)
+        elif line.startswith("definition:"):
+            self.add_definition(line)
+
+    def output(self):
+        return (self.id, self.name, self.namespace, self.definition)
+
+
+class GoRelationship:
+    __slots__ = [
+        "start",
+        "end",
+        "type",
+    ]
+
+    def __init__(self) -> None:
+        self.start = None
+        self.end = None
+        self.type = None
+
+    def add_end(self, id):
+        if goterm := extract_goterm_int_as_str(id):
+            self.end = goterm
+        else:
+            print(f"'{id}' isn't a goterm")
+
+    def add_type(self, line):
+        key = line.split(":", 1)[0]
+        if key == "relationship":
+            # 'relationship: part_of GO:0000785 ! chromatin'
+            # should return: "part of"
+            self.type = line.split(":", 1)[1].strip().split(" ", 1)[0]
+        elif key == "is_a":
+            self.type = key
+        self.type = f"GO_{self.type.upper()}"
+        self.end = extract_goterm_int_as_str(line)
+
+    def assign(self, start, line):
+        self.start = start
+        if line.startswith("is_a") or line.startswith("relationship"):
+            self.add_type(line)
+
+    def output(self):
+        if all((self.start, self.end, self.type)):
+            return (self.start, self.end, self.type)
+
+
+class AltRel(GoRelationship):
+    def __init__(
+        self,
+    ):
+        super().__init__()
+
+    def assign(self, main_id, line):
+        if line.startswith("alt_id"):
+            self.start = main_id
+            self.end = extract_goterm_int_as_str(line)
+            self.type = "GO_ALTERNATE"
+
+
+def write(
+    input: list[tuple[str, ...]],
+    outpath: str,
+) -> None:
+    with open(outpath, "w") as h:
+        writer = csv.writer(
+            h, quoting=csv.QUOTE_MINIMAL, delimiter="\t", dialect="unix"
+        )
+        writer.writerows(input)
+
+
+def main():
+    args = parser.parse_args()
+    nodes = []
+    relationships = []
+    with requests.get(OBO_URL, stream=True) as r:
+        r.raise_for_status()
+        term_open = False
+        for line in r.iter_lines():
+            line = line.decode(r.encoding)
+            if line == "[Term]":
+                term_open = True
+                nodeobj = GoNode()
+                relobj = GoRelationship()
+            if not term_open:
+                # skip lines before first term
+                continue
+            nodeobj.assign(line=line)
+            # some goterms have "alt_ids" these are sometimes referenced from other sources but only exist
+            # in the obo file as alt ids (no primary entry)
+            if line.startswith("alt_id"):
+                # deepcopy so name and namespace will carry if present
+                alt_node = deepcopy(nodeobj)
+                alt_node.add_id(line)
+                alt_rel = AltRel()
+                alt_rel.assign(main_id=nodeobj.id, line=line)
+                nodes.append(alt_node.output())
+                relationships.append(alt_rel.output())
+                del alt_node, alt_rel
+            relobj.assign(start=nodeobj.id, line=line)
+            if line == "":
+                term_open = False
+                nodes.append(nodeobj.output())
+                # only output full relationhip tuples
+                if out := relobj.output():
+                    relationships.append(out)
+
+    log.info("Writing goterms nodes")
+    write(nodes, Path(args.outdir, "goterms"))
+    log.info("Writing goterms relationships")
+    write(relationships, Path(args.outdir, "goterm_edgelist"))
+    log.info("Finished reading/writing goterms")
+
+
+if __name__ == "__main__":
+    main()
