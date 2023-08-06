f77370e6bf36c12f410e21e92767835f329b56a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:52 +0000

    Add package fastapi-amis-admin/0.6.0a1/fastapi_amis_admin-0.6.0a1.tar.gz

---
+++ packages/fastapi-amis-admin/fastapi_amis_admin-0.6.0a1.tar.gz/fastapi_amis_admin-0.6.0a1/fastapi_amis_admin/crud/_sqlmodel.py
@@ -0,0 +1,6 @@
+from ._sqlalchemy import SqlalchemyCrud, SqlalchemySelector
+
+SQLModelSelector = SqlalchemySelector
+"""Deprecated, use SqlalchemySelector instead."""
+SQLModelCrud = SqlalchemyCrud
+"""Deprecated, use SqlalchemyCrud instead."""
