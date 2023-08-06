8f6657537b608927d63719848c442473f49edbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:38 +0000

    Add package pyqtorch/0.3.1/pyqtorch-0.3.1.tar.gz

---
+++ packages/pyqtorch/pyqtorch-0.3.1.tar.gz/pyqtorch-0.3.1/pyqtorch/modules/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from pyqtorch.modules.circuit import (
+    EntanglingLayer,
+    FeaturemapLayer,
+    QuantumCircuit,
+    VariationalLayer,
+    uniform_state,
+    zero_state,
+)
+from pyqtorch.modules.hamevo import HamEvo, HamEvoEig, HamEvoExp, HamEvoType, HamiltonianEvolution
+from pyqtorch.modules.parametric import CPHASE, CRX, CRY, CRZ, RX, RY, RZ, U
+from pyqtorch.modules.primitive import CNOT, CY, CZ, SWAP, H, I, S, Sdagger, T, X, Y, Z
