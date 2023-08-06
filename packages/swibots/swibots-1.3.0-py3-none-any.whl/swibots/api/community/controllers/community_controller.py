b3e9a8fc80545d1a1f9564f8aef2c197606784bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:22 +0000

    Add package swibots/1.3.0/swibots-1.3.0-py3-none-any.whl

---
+++ packages/swibots/swibots-1.3.0-py3-none-any.whl/swibots/api/community/controllers/community_controller.py
@@ -0,0 +1,20 @@
+import logging
+from typing import TYPE_CHECKING
+from swibots.api.community.models import Channel, Community, Group
+
+if TYPE_CHECKING:
+    from swibots.api.community import CommunityClient
+
+log = logging.getLogger(__name__)
+
+BASE_PATH = "/v1/community"
+
+
+class CommunityController:
+    def __init__(self, client: "CommunityClient"):
+        self.client = client
+
+    async def get_community(self, community_id: str):
+        """Get a community by id"""
+        response = await self.client.get(f"{BASE_PATH}?communityId={community_id}")
+        return self.client.build_object(Community, response.data.get("result"))
