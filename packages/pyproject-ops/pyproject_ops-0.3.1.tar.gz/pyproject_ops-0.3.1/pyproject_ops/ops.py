f810e3525a97c4130ccbde5dee01874151869e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:31 +0000

    Add package pyproject-ops/0.3.1/pyproject_ops-0.3.1.tar.gz

---
+++ packages/pyproject-ops/pyproject_ops-0.3.1.tar.gz/pyproject_ops-0.3.1/pyproject_ops/ops.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+The namespace for all the pyproject_ops automation methods.
+"""
+
+import dataclasses
+
+from .pyproject_paths import PyProjectPaths
+from .pyproject_venv import PyProjectVenv
+from .pyproject_deps import PyProjectDeps
+from .pyproject_tests import PyProjectTests
+from .pyproject_docs import PyProjectDocs
+from .pyproject_build import PyProjectBuild
+from .pyproject_publish import PyProjectPublish
+from .pyproject_config_management import PyProjectConfigManagement
+from .pyproject_aws import PyProjectAWS
+from .pyproject_aws_lambda import PyProjectAWSLambda
+from .pyproject_aws_glue import PyProjectAWSGlue
+
+
+@dataclasses.dataclass
+class PyProjectOps(
+    PyProjectPaths,
+    PyProjectVenv,
+    PyProjectDeps,
+    PyProjectTests,
+    PyProjectDocs,
+    PyProjectBuild,
+    PyProjectPublish,
+    PyProjectConfigManagement,
+    PyProjectAWS,
+    PyProjectAWSLambda,
+    PyProjectAWSGlue,
+):
+    """
+    The namespace for all the pyproject_ops automation methods.
+    """
+    def __post_init__(self):
+        self._validate_paths()
+        self._validate_python_version()
