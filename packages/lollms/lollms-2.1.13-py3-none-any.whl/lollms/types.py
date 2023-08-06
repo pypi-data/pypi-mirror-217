85cdab7b236fb1ae7f0827e736d44c399d9a7956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:22 +0000

    Add package lollms/2.1.13/lollms-2.1.13-py3-none-any.whl

---
+++ packages/lollms/lollms-2.1.13-py3-none-any.whl/lollms/types.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class MSG_TYPE(Enum):
+    # Messaging
+    MSG_TYPE_CHUNK=0        # A chunk of a message (used for classical chat)
+    MSG_TYPE_FULL=1         # A full message (for some personality the answer is sent in bulk)
+
+    # Informations
+    MSG_TYPE_INFO=3             # An information to be shown to user
+    MSG_TYPE_EXCEPTION=2        # An exception occured
+
+    # Steps
+    MSG_TYPE_STEP=4             # An instant step (a step that doesn't need time to be executed)
+    MSG_TYPE_STEP_START=4       # A step has started (the text contains an explanation of the step done by he personality)
+    MSG_TYPE_STEP_PROGRESS=5    # The progress value (the text contains a percentage and can be parsed by the reception)
+    MSG_TYPE_STEP_END=6         # A step has been done (the text contains an explanation of the step done by he personality)
+
+    #Extra
+    MSG_TYPE_JSON_INFOS=7       # A JSON output that is useful for summarizing the process of generation used by personalities like chain of thoughts and tree of thooughts
+    MSG_TYPE_REF=8              # References (in form of  [text](path))
+    MSG_TYPE_CODE=9             # A javascript code to execute
+    MSG_TYPE_UI=10              # A vue.js component to show (we need to build some and parse the text to show it)
