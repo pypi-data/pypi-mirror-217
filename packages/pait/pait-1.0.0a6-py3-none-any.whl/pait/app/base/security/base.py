b8fa15cdddc86d0d25dbe3e4861d4993bf02cd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:12 +0000

    Add package pait/1.0.0a6/pait-1.0.0a6-py3-none-any.whl

---
+++ packages/pait/pait-1.0.0a6-py3-none-any.whl/pait/app/base/security/base.py
@@ -0,0 +1,20 @@
+from typing import Any, Callable, Dict, Optional
+
+from any_api.openapi import SecurityModelType
+
+
+class BaseSecurity:
+    model: SecurityModelType
+    security_name: str
+
+    def pait_handler(self, *args: Any, **kwargs: Any) -> Any:
+        pass
+
+    def set_pait_handler(self, func: Callable) -> None:
+        if hasattr(func, "pait_handler"):
+            raise ValueError("'func' already has pait_handler")  # pragma: no cover
+        setattr(self, "pait_handler", func)
+
+    @classmethod
+    def get_exception(cls, *, status_code: int, message: str, headers: Optional[Dict] = None) -> Exception:
+        return NotImplementedError()  # pragma: no cover
---
+++ packages/pait/pait-1.0.0a6-py3-none-any.whl/pait/types.py
@@ -0,0 +1,42 @@
+import sys
+from typing import Any, Callable, Union
+
+# copy from https://github.com/agronholm/typeguard/blob/master/src/typeguard/__init__.py#L64
+if sys.version_info >= (3, 10):
+    from typing import ParamSpec  # pragma: no cover
+    from typing import is_typeddict  # pragma: no cover
+    from typing import Literal, Protocol
+else:
+    from typing_extensions import ParamSpec  # type: ignore
+    from typing_extensions import Literal, Protocol
+
+    _typed_dict_meta_types = ()
+    if sys.version_info >= (3, 8):
+        from typing import _TypedDictMeta  # pragma: no cover
+
+        _typed_dict_meta_types += (_TypedDictMeta,)  # pragma: no cover
+
+    try:
+        from typing_extensions import _TypedDictMeta  # type: ignore
+
+        _typed_dict_meta_types += (_TypedDictMeta,)  # type: ignore
+    except ImportError:  # pragma: no cover
+        pass
+
+    def is_typeddict(tp) -> bool:  # type: ignore
+        return isinstance(tp, _typed_dict_meta_types)
+
+
+class _CallType(Protocol):
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        ...
+
+
+class _PaitCallType(Protocol):
+    def pait_handler(self, *args: Any, **kwargs: Any) -> Any:
+        ...
+
+
+CallType = Union[Callable, _CallType, _PaitCallType]
+
+__all__ = ["ParamSpec", "Literal", "is_typeddict", "CallType"]
