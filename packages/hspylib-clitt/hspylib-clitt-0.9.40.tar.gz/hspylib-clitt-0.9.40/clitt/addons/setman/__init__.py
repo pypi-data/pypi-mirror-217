3e16e727b2e85f93e0bfe246db3cc17a6406f082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:59 +0000

    Add package hspylib-clitt/0.9.40/hspylib-clitt-0.9.40.tar.gz

---
+++ packages/hspylib-clitt/hspylib-clitt-0.9.40.tar.gz/hspylib-clitt-0.9.40/clitt/addons/setman/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-clitt v0.9.40
+#
+# Package: main.clitt.addons.setman
+"""Package initialization."""
+
+__all__ = [
+  'setman', 
+  'setman_config', 
+  'setman_enums'
+]
+__version__ = '0.9.40'
