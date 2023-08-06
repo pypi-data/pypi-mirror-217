cc6006a79785c2b3ca80daba5a5d5c4439372bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:16 +0000

    Add package ttcal/2.0.4/ttcal-2.0.4-py2.py3-none-any.whl

---
+++ packages/ttcal/ttcal-2.0.4-py2.py3-none-any.whl/ttcal/__init__.py
@@ -0,0 +1,24 @@
+"""
+Date classes (originally from TikTok).
+"""
+__version__ = '2.0.4'
+from .day import Day, Days, Today   # noqa  
+from .duration import Duration, Period  # noqa
+from .calfns import chop, isoweek   # noqa
+from .month import Month
+from .week import Week
+from .year import Year
+
+
+def from_idtag(idtag):
+    """Return a class from idtag.
+    """
+    assert len(idtag) > 1
+    assert idtag[0] in 'wdmy'
+
+    return {
+        'w': Week,
+        'd': Day,
+        'm': Month,
+        'y': Year,
+    }[idtag[0]].from_idtag(idtag)
