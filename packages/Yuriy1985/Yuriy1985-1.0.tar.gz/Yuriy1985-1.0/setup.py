43b7ac5cddd45048fb4bc3601de9d48668d12c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:54 +0000

    Add package Yuriy1985/1.0/Yuriy1985-1.0.tar.gz

---
+++ packages/Yuriy1985/Yuriy1985-1.0.tar.gz/Yuriy1985-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Yuriy1985',
+      version='1.0',
+      description='Система аутентифікації',
+      author='Yuriy1985',
+      author_email='gward@python.net',
+      packages=['project'],
+     )
