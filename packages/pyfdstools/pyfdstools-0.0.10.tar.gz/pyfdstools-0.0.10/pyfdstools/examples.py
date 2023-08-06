c6b335885216153e95ca8157508a7932de2290a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:42 +0000

    Add package pyfdstools/0.0.10/pyfdstools-0.0.10.tar.gz

---
+++ packages/pyfdstools/pyfdstools-0.0.10.tar.gz/pyfdstools-0.0.10/pyfdstools/examples.py
@@ -0,0 +1,353 @@
+#-----------------------------------------------------------------------
+# Copyright (C) 2020, All rights reserved
+#
+# Jonathan L. Hodges
+#
+#-----------------------------------------------------------------------
+#=======================================================================
+# 
+# DESCRIPTION:
+# This software is part of a python library to assist in developing and
+# analyzing simulation results from Fire Dynamics Simulator (FDS).
+# FDS is an open source software package developed by NIST. The source
+# code is available at: https://github.com/firemodels/fds
+#
+#=======================================================================
+# # IMPORTS
+#=======================================================================
+import pyfdstools as fds
+import os
+from collections import defaultdict
+import numpy as np
+import pandas as pd
+
+def exampleImportFile(fdsPath=None):
+    if fdsPath is None:
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        fdsPath = os.path.join(systemPath, "examples", "case001.fds")
+    fdsFile = fds.fdsFileOperations()
+    print("\tStarting to import file from %s"%(fdsPath))
+    fdsFile.importFile(fdsPath)
+    print("\tFinished importing file.")
+    return fdsFile
+
+
+def exampleSaveFile(file=None, outdir=None, outname=None):
+    if file is None:
+        file = exampleImportFile()
+    if outdir is None:
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        outdir = os.path.join(systemPath, "generated")
+    if outname is None:
+        location = os.path.join(outdir, '%s.fds'%(file.head['ID']['CHID']))
+    print("\tStarting to save model.")
+    file.saveModel(1, location, allowMeshSplitting=False)
+    print("\tFinished saving model.")
+    return location
+
+
+def exampleErrorCalculation():
+    data, keys = fds.readErrorTable()
+    
+    errorvalues = fds.calculatePercentile([100, 200, 300, 400, 500, 600], 'Plume Temperature', 0.95, fdsVersion='6.7.1')
+    fig, ax1 = fds.plotPercentile(500, 'Plume Temperature', fdsVersion='6.7.1')
+    return errorvalues
+
+
+def exampleReadSlcf2dResults(resultDir=None, chid=None,
+                             fdsQuantities = ['TEMPERATURE'],
+                             tStart=0, tEnd=120):
+    if (resultDir is None) and (chid is None):
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        chid = "case001"        
+        resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+    
+    datas = defaultdict(bool)
+    
+    for qty in fdsQuantities:
+        grid, data, times = fds.readSLCF2Ddata(chid, resultDir, qty)
+        tStartInd = np.argwhere(times >= tStart)[0][0]
+        tEndInd = np.argwhere(times <= tEnd)[-1][0]
+        data_tavg = np.nanmean(data[:, :, :, tStartInd:tEndInd], axis=3)
+        datas[qty] = data_tavg.copy()
+    datas['GRID'] = grid
+    datas['TIMES'] = times
+    
+    return datas
+
+
+def exampleExtract2dFromSlcf2d(datas,
+                               fdsQuantities = ['TEMPERATURE'],
+                               fdsUnits = ['C'],
+                               axis=1, value=2.5,
+                               qnty_mn=20, qnty_mx=150):
+    datas2D = defaultdict(bool)    
+    for qty, unit in zip(fdsQuantities, fdsUnits):
+        x, z, data_slc = fds.findSliceLocation(datas['GRID'], datas[qty], axis, value)
+        datas2D[qty] = data_slc
+        fig = fds.plotSlice(x, z, data_slc, axis,
+                            qnty_mn=qnty_mn, qnty_mx=qnty_mx,
+                            clabel="%s (%s)"%(qty, unit),
+                            cbarticks=[20, 40, 60, 80, 100, 120, 140])
+    datas2D['X'] = x
+    datas2D['Z'] = z
+    
+    return datas2D, fig
+
+def exampleReadSlcf3dResults(resultDir=None, chid=None,
+                             fdsQuantities = ['TEMPERATURE'],
+                             tStart=0, tEnd=120):
+    if (resultDir is None) and (chid is None):
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        chid = "case001"        
+        resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+    
+    datas = defaultdict(bool)
+    
+    for qty in fdsQuantities:
+        grid, data, times = fds.readSLCF3Ddata(chid, resultDir, qty)
+        tStartInd = np.argwhere(times >= tStart)[0][0]
+        tEndInd = np.argwhere(times <= tEnd)[-1][0]
+        data_tavg = np.nanmean(data[:, :, :, tStartInd:tEndInd], axis=3)
+        datas[qty] = data_tavg.copy()
+    datas['GRID'] = grid
+    datas['TIMES'] = times
+    
+    return datas
+
+def exampleExtract2dFromSlcf3d(datas,
+                               fdsQuantities = ['TEMPERATURE'],
+                               fdsUnits = ['C'],
+                               axis=2, value=4.4,
+                               qnty_mn=20, qnty_mx=150):
+    datas2D = defaultdict(bool)    
+    for qty, unit in zip(fdsQuantities, fdsUnits):
+        x, z, data_slc = fds.findSliceLocation(datas['GRID'], datas[qty], axis, value)
+        datas2D[qty] = data_slc
+        fig = fds.plotSlice(x, z, data_slc, axis,
+                            qnty_mn=qnty_mn, qnty_mx=qnty_mx,
+                            clabel="%s (%s)"%(qty, unit),
+                            cbarticks=[20, 40, 60, 80, 100, 120, 140])
+    datas2D['X'] = x
+    datas2D['Z'] = z
+    
+    return datas2D, fig
+
+def exampleImportBndf(resultDir=None, chid=None, fdsFile=None,
+                       fdsQuantities = ['WALL TEMPERATURE'],
+                       fdsUnits = ['C'],
+                       tStart=0, tEnd=120,
+                       axis=-2, value=4.4,
+                       qnty_mn=20, qnty_mx=100):
+    if (resultDir is None) and (chid is None):
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        chid = "case001"        
+        resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+    
+    if fdsFile is None: fdsFilePath = fds.getFileList(resultDir, chid, 'fds')[0]
+    
+    datas, times = fds.queryBndf(resultDir, chid, fdsFilePath, fdsQuantities, fdsUnits, axis, value)
+    tStartInd = np.argwhere(times >= tStart)[0][0]
+    tEndInd = np.argwhere(times <= tEnd)[-1][0]
+    
+    for qty, unit in zip(fdsQuantities, fdsUnits):
+        for mesh in list(datas[qty].keys()):
+            data = datas[qty]['DATA']
+            x = datas[qty]['X']
+            z = datas[qty]['Z']
+        meanData = np.mean(data[:, :, tStartInd:tEndInd], axis=2)
+        fig = fds.plotSlice(x, z, meanData, axis,
+                            qnty_mn=qnty_mn, qnty_mx=qnty_mx,
+                            clabel="%s (%s)"%(qty, unit),
+                            cbarticks=[20, 40, 60, 80, 100, 120, 140])
+    return datas, fig
+
+def exampleExtractBndfMax(resultDir=None, chid=None, outDir=None,
+                          fdsFile=None, smvFile=None, outputNamespace=None,
+                          fdsQuantities = ['WALL TEMPERATURE'], fdsUnits = ['C'],
+                          tStart=0, tEnd=120, tInt=1, tBand=3, orientations=[0],
+                          axis=-2, value=4.4,
+                          qnty_mn=20, qnty_mx=100,
+                          yticks=[20, 50, 100, 150, 200, 250, 300, 350]):
+    if (resultDir is None) and (chid is None) and (outDir is None):
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        chid = "case001"        
+        resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+        outDir = os.path.join(systemPath, "generated")
+    
+    if fdsFile is None: fdsFilePath = fds.getFileList(resultDir, chid, 'fds')[0]
+    if smvFile is None: smvFilePath = fds.getFileList(resultDir, chid, 'smv')[0]
+    if outputNamespace is None: outputNamespace = "%s_max_"%(chid)
+    
+    outputName = os.path.join(outDir, outputNamespace)
+    
+    datas = fds.extractMaxBndfValues(fdsFilePath, smvFilePath, resultDir, chid, fdsQuantities,
+                    tStart=tStart, tEnd=tEnd, tInt=tInt, tBand=tBand, orientations=orientations)
+    
+    figs = []
+    for qty in fdsQuantities:
+        times = datas[qty]['TIMES']
+        mPts = datas[qty]['DATA']
+        names = datas[qty]['NAMES']
+        outName = "%s%s"%(outputName, qty)
+        fds.maxValueCSV(times, mPts, names, outName)
+        fig = fds.maxValuePlot(times, mPts, names, outName, vName=qty, yticks=yticks)
+        figs.append(fig)
+    return datas, figs
+
+
+def example2dSliceToCsv(resultDir=None, outDir=None, chid=None,
+                        quantity=None, unit=None, axis=None, value=None,
+                        time=None, dt=None):
+    if (resultDir is None) and (chid is None) and (outDir is None):
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        chid = "case001"        
+        resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+        outDir = os.path.join(systemPath, "generated")
+    try:
+        os.mkdir(outDir)
+    except:
+        pass
+    data = fds.query2dAxisValue(resultDir, chid, quantity, axis, value, time=time, dt=dt)
+    fds.renderSliceCsvs(data, chid, outDir)
+    fig, ax = fds.plotSlice(data['x'], data['z'], data['datas'][:, :, -1], axis,
+                        clabel="%s (%s)"%(quantity, unit))
+    fig.savefig(os.path.join(outDir, '%s_%s_%0.0f_%0.4f_final_frame.png'%(chid, quantity, axis, value)))
+    return data, fig
+
+
+def exampleBndfTimeAverage(resultDir=None, outDir=None, chid=None,
+                           quantity=None, dt=None):
+    if (resultDir is None) and (chid is None) and (outDir is None):
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        chid = "case001"        
+        resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+        outDir = os.path.join(systemPath, "generated")
+    try:
+        os.mkdir(outDir)
+    except:
+        pass
+    
+    outFiles, outQty, refFiles, newSmvFile = fds.bndfsTimeAverage(
+        resultDir, chid, quantity, dt, outDir=outDir)
+    
+
+def exampleWriteToNetCDF4():
+    import netCDF4
+    
+    # Get case information from examples
+    systemPath = os.path.dirname(os.path.abspath(__file__))
+    chid = "case001"
+    resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+    qty = 'TEMPERATURE'
+    
+    # Read data
+    grid, data, times = fds.readSLCF3Ddata(chid, resultDir, qty)
+    
+    # Convert axes to netcdf4 ordering
+    data2 = np.moveaxis(data, [3,2,1,0], [0,1,2,3])
+    grid2 = np.moveaxis(grid, [2,1,0,3], [0,1,2,3])
+    
+    # Open netcdf4 file
+    rootgrp = netCDF4.Dataset('output.nc','w')
+    
+    # Establish basic dimension information for netcdf file
+    time = rootgrp.createDimension('time', None)
+    lon = rootgrp.createDimension('lon', grid.shape[0])
+    lat = rootgrp.createDimension('lat', grid.shape[1])
+    level = rootgrp.createDimension('level', grid.shape[2])
+    
+    # Establish netcdf variable
+    temp = rootgrp.createVariable("temp","f4",("time","level","lat","lon"))
+    temp.units = "K"
+    temp[:] = data2 + 273 # Fill temperature variable with values from FDS in K
+    
+    # Close file
+    rootgrp.close()
+    
+def stretchedMeshExample(resultDir=None, outDir=None, chid=None,
+                           quantity="TEMPERATURE", dt=None, time=None):
+    if (resultDir is None) and (chid is None) and (outDir is None):
+        systemPath = os.path.dirname(os.path.abspath(__file__))
+        chid = "stretched_mesh_example"        
+        resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+        outDir = os.path.join(systemPath, "generated")
+    try:
+        os.mkdir(outDir)
+    except:
+        pass
+    
+    grid, data, times = fds.readSLCF3Ddata(chid, resultDir, quantity, time=time, dt=dt)
+    
+    x, z, data_slc = fds.findSliceLocation(grid, data[:, :, :, -1], 1, 0)
+    fds.plotSlice(x, z, data_slc, 1, figsize=(10, 4))
+    
+    x, z, data_slc = fds.findSliceLocation(grid, data[:, :, :, -1], 3, 885)
+    fds.plotSlice(x, z, data_slc, 3, figsize=(10, 10))
+
+
+def exampleAddOccupantFedDevices(indir,outdir):
+    name = 3
+    height_above_floor = 1.8
+    occupants = pd.read_csv(os.path.join(indir,'fed_example_occupants.csv'), header=[0], skiprows=[1])
+    t = occupants.loc[occupants['name'] == name,'t'].values
+    x = occupants.loc[occupants['name'] == name,'x'].values
+    y = occupants.loc[occupants['name'] == name,'y'].values
+    z = occupants.loc[occupants['name'] == name,'z'].values + height_above_floor
+    
+    fdsFile = fds.fdsFileOperations()
+    fdsFile.importFile(os.path.join(indir, 'fed_example.fds'))
+    fdsFile.addOccupant('Occupant %d'%(name), x, y, z, t, output=True)
+    fdsFile.saveModel(1, os.path.join(outdir, "fed_example_out.fds"), allowMeshSplitting=False)
+    
+
+def runExamples():
+    systemPath = os.path.dirname(os.path.abspath(__file__))
+    exampleInputFdsFile = os.path.join(systemPath, "examples", "case001.fds")
+    exampleOutputDir = os.path.join(systemPath, "generated")
+    chid = "case001"
+    resultDir = os.path.join(systemPath, "examples", "%s.zip"%(chid))
+    
+    print("Importing model example", flush=True)
+    file = exampleImportFile(exampleInputFdsFile)
+    
+    print("Saving model example", flush=True)
+    exampleSaveFile(file=file, outdir=exampleOutputDir)
+    
+    print("Plotting error example", flush=True)
+    exampleErrorCalculation()
+    
+    print("Importing SLCF2D results example", flush=True)
+    datas = exampleReadSlcf2dResults(resultDir=resultDir, chid=chid)
+    
+    print("Extracting slice from SLCF2D results example", flush=True)
+    datas2D, fig = exampleExtract2dFromSlcf2d(datas)
+    
+    print("Importing SLCF3D results example", flush=True)
+    datas = exampleReadSlcf3dResults(resultDir=resultDir, chid=chid)
+    
+    print("Extracting 2-D slice from SLCF3D results example", flush=True)
+    datas2D, fig = exampleExtract2dFromSlcf3d(datas)
+    
+    print("Importing BNDF results example", flush=True)
+    datas, fig = exampleImportBndf(resultDir=resultDir, chid=chid)
+    
+    print("Extracting max value from BNDF results example", flush=True)
+    datas, figs = exampleExtractBndfMax(resultDir=resultDir, chid=chid, outDir=exampleOutputDir)
+    
+    print("Rendering 2d slice to csv example.", flush=True)
+    datas = example2dSliceToCsv(resultDir=resultDir, chid=chid, outDir=exampleOutputDir,
+                                axis=1, value=2.45, time=30, dt=60, quantity='TEMPERATURE', unit='C')
+    
+    print("Time-averaging a boundary file example.", flush=True)
+    exampleBndfTimeAverage(dt=30, quantity='WALL TEMPERATURE')
+    
+    print("Read SL3D with stretched mesh example", flush=True)
+    stretchedMeshExample()
+    
+    print("Add Occupant FED calculation example", flush=True)
+    exampleAddOccupantFedDevices(os.path.join(systemPath, "examples"),
+                                 os.path.join(systemPath, "generated"))
+
+if __name__ == '__main__':
+    runExamples()
---
+++ packages/pyfdstools/pyfdstools-0.0.10.tar.gz/pyfdstools-0.0.10/pyfdstools/utilities.py
@@ -0,0 +1,346 @@
+#-----------------------------------------------------------------------
+# Copyright (C) 2020, All rights reserved
+#
+# Jonathan L. Hodges
+#
+#-----------------------------------------------------------------------
+#=======================================================================
+# 
+# DESCRIPTION:
+# This software is part of a python library to assist in developing and
+# analyzing simulation results from Fire Dynamics Simulator (FDS).
+# FDS is an open source software package developed by NIST. The source
+# code is available at: https://github.com/firemodels/fds
+# 
+# This script has utilities used to throughout the package for tasks
+# associated with visualization and filtering.
+#
+#=======================================================================
+# # IMPORTS
+#=======================================================================
+import numpy as np
+import matplotlib.colors as pltc
+import scipy.spatial as scsp
+import mpl_toolkits.mplot3d as a3
+import matplotlib.pyplot as plt
+import os
+import zipfile
+import glob
+import struct
+from .colorSchemes import getVTcolors
+
+def kalmanFilter(z, Q=1e-5, R=0.5**2):
+    # This subroutine applies a kalman filter to an input set of data.
+    #
+    #Inputs:
+    #    z: series of data to be filtered
+    #    Q: process variance
+    #    R: measurement variance
+    #Outputs:
+    #    xhat: filtered series of data
+    
+    # intial parameters
+    sz = z.shape[0] # size of array
+    
+    # allocate space for arrays
+    xhat=np.zeros(sz)      # a posteri estimate of x
+    P=np.zeros(sz)         # a posteri error estimate
+    xhatminus=np.zeros(sz) # a priori estimate of x
+    Pminus=np.zeros(sz)    # a priori error estimate
+    K=np.zeros(sz)         # gain or blending factor
+    
+    # intial guesses
+    xhat[0] = z[0]
+    P[0] = 1.0
+    
+    for k in range(1,sz):
+        # time update
+        xhatminus[k] = xhat[k-1]
+        Pminus[k] = P[k-1]+Q
+    
+        # measurement update
+        K[k] = Pminus[k]/( Pminus[k]+R )
+        xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
+        P[k] = (1-K[k])*Pminus[k]
+    
+    return xhat
+
+def smvVisual(obstructions,surfaces,namespace,fs=16,fig=None,ax=None,
+              limits=[0,15,0,8,0,5]):
+    if fig is None: fig = plt.figure(figsize=(12,12))
+    if ax is None: ax = a3.Axes3D(fig)
+    
+    for obst in obstructions:
+        pts, colors = getPtsFromObst(obst,surfaces)
+        print(pts)
+        print(colors)
+        for pt, color in zip(pts,colors):
+            f = a3.art3d.Poly3DCollection(pt)
+            f.set_color(color)
+            f.set_edgecolor('k')
+            #f.set_alpha(1)
+            ax.add_collection3d(f)
+    plt.xlim(limits[0],limits[1])
+    plt.ylim(limits[2],limits[3])
+    ax.set_zlim(limits[4],limits[5])
+    plt.xlabel('x (m)',fontsize=fs)
+    plt.ylabel('y (m)',fontsize=fs)
+    ax.set_zlabel('z (m)',fontsize=fs)
+    plt.tick_params(labelsize=fs)
+    plt.savefig('%s_smvvisual.png'%(namespace),dpi=300)
+    
+    return fig, ax
+
+def buildSMVgeometry(file):
+    with open(file,'r') as f:
+        lines = f.readlines()
+    inds = []
+    for i in range(0,len(lines)):
+        if lines[i][0] != ' ' and lines[i][0] != '-':
+            inds.append(i)
+    surfaces = []
+    obstructions = []
+    for ind in inds:
+        if 'SURFACE' in lines[ind]:
+            sname = ' '.join(lines[ind+1].split())
+            (Tign,eps) = (lines[ind+2].split()[0],lines[ind+2].split()[1])
+            (stype,t_width,t_height) = (lines[ind+3].split()[0],lines[ind+3].split()[1],lines[ind+3].split()[2])
+            (c1,c2,c3,c4) = (lines[ind+3].split()[3],lines[ind+3].split()[4],lines[ind+3].split()[5],lines[ind+3].split()[6])
+            surfaces.append([sname,Tign,eps,stype,t_width,t_height,c1,c2,c3,c4])
+        if 'OBST' in lines[ind] and '_OBST' not in lines[ind]:
+            nObst = int(lines[ind+1].split()[0])
+            for i in range(0,nObst):
+                obst = [float(x) for x in lines[ind+i+2].split()]
+                obstructions.append(obst)
+    return surfaces, obstructions
+
+def getPtsFromObst(obst,surfaces):
+    pts = []
+    colors = []
+    pts = np.array([[obst[0],obst[2],obst[4]],
+                   [obst[0],obst[2],obst[5]],
+                   [obst[1],obst[2],obst[5]],
+                   [obst[1],obst[2],obst[4]],
+                   [obst[0],obst[3],obst[5]],
+                   [obst[1],obst[3],obst[5]],
+                   [obst[1],obst[3],obst[4]],
+                   [obst[0],obst[3],obst[4]]])
+    
+    # y-negative surface
+    #pts.append([(obst[0],obst[2],obst[4]),(obst[1],obst[2],obst[4]),
+    #            (obst[1],obst[2],obst[5]),(obst[0],obst[2],obst[5])])
+    surf = surfaces[int(obst[7])]
+    colors.append((float(surf[6]),float(surf[7]),float(surf[8]),float(surf[9])))
+    # y-positive surface
+    #pts.append([(obst[0],obst[3],obst[4]),(obst[1],obst[3],obst[4]),
+    #            (obst[1],obst[3],obst[5]),(obst[0],obst[3],obst[5])])
+    surf = surfaces[int(obst[8])]
+    colors.append((float(surf[6]),float(surf[7]),float(surf[8]),float(surf[9])))
+    # x-negative surface
+    #pts.append([(obst[0],obst[2],obst[4]),(obst[0],obst[2],obst[5]),
+    #            (obst[0],obst[3],obst[5]),(obst[0],obst[3],obst[4])])
+    surf = surfaces[int(obst[9])]
+    colors.append((float(surf[6]),float(surf[7]),float(surf[8]),float(surf[9])))
+    # x-positive surface
+    #pts.append([(obst[1],obst[2],obst[4]),(obst[1],obst[2],obst[5]),
+    #            (obst[1],obst[3],obst[5]),(obst[1],obst[3],obst[4])])
+    surf = surfaces[int(obst[10])]
+    colors.append((float(surf[6]),float(surf[7]),float(surf[8]),float(surf[9])))
+    # z-negative surface
+    #pts.append([(obst[0],obst[2],obst[4]),(obst[1],obst[2],obst[4]),
+    #            (obst[1],obst[3],obst[4]),(obst[0],obst[3],obst[4])])
+    surf = surfaces[int(obst[11])]
+    colors.append((float(surf[6]),float(surf[7]),float(surf[8]),float(surf[9])))
+    # z-positive surface
+    #pts.append([(obst[0],obst[2],obst[5]),(obst[1],obst[2],obst[5]),
+    #            (obst[1],obst[3],obst[5]),(obst[0],obst[3],obst[5])])
+    surf = surfaces[int(obst[12])]
+    colors.append((float(surf[6]),float(surf[7]),float(surf[8]),float(surf[9])))
+    
+    return pts, colors
+
+def maxValueCSV(times, mPts, names, namespace):
+    '''  mPts rows correlated to times, columns correlated to different groups. '''
+    numberOfGroups = mPts.shape[1]
+    header = 'Time,'
+    for i in range(0,numberOfGroups):
+        name = names[i].replace(',','_')
+        header = header+name+','
+    header = header[:-1]+'\n'
+    data = np.append(np.reshape(times,(times.shape[0],1)),mPts,axis=1)
+    csvName = '%s.csv'%(namespace)
+    print("Saving max value csv to %s"%(csvName))
+    np.savetxt(csvName, data, delimiter=',', header=header)
+    return '%s.csv'%(namespace)   
+
+def maxValuePlot(times, mPts, names, namespace, fs=16, lw=3, pcs=None, vName='',
+                 yticks=None, xticks=None):
+    '''  mPts rows correlated to times, columns correlated to different groups. '''
+    numberOfGroups = mPts.shape[1]
+    if pcs is None:
+        pcs = getVTcolors()
+        if len(pcs) < numberOfGroups: pcs = getPlotColors(numberOfGroups)
+    fig = plt.figure(figsize=(12,8))
+    for i in range(0,numberOfGroups):
+        plt.plot(times,mPts[:,i],color=pcs[i],label=names[i],linewidth=lw)
+    if yticks is not None: plt.yticks(yticks)
+    if xticks is not None: plt.xticks(xticks)
+    plt.legend(fontsize=fs)
+    plt.xlabel('time (s)',fontsize=fs)
+    plt.ylabel('%s'%(vName),fontsize=fs)
+    plt.tick_params(labelsize=fs)
+    plt.tight_layout()
+    figName = '%s_maxTPlot.png'%(namespace)
+    print("Saving max value figure to %s"%(figName))
+    plt.savefig(figName, dpi=300)
+    plt.show()
+    return fig
+
+def getPlotColors(numberOfGroups):
+    pcs = []
+    print(numberOfGroups)
+    for i in range(0,numberOfGroups):
+        v = np.random.rand(3)
+        su = np.sum(v)
+        while (su > 2.7) or (su < 0.3):
+            v = np.random.rand(3)
+            su = np.sum(v)
+        tmp = pltc.rgb2hex(v)
+        pcs.append(tmp)
+    return pcs
+
+def pointsFromXB(XB,extend=[0,0,0]):
+    ''' This routine builds a list of XYZ points from an obstruction XB
+    
+    Inputs:
+        XB: Septuplet containing [xmin, xmax, ymin, ymax, zmin, zmax]
+        extend: Float array containing amount to extend polygon along each axis
+    Outputs:
+        pts: List of corner points
+    '''
+    pts = [[XB[0]-extend[0],XB[2]-extend[1],XB[4]-extend[2]],
+           [XB[0]-extend[0],XB[2]-extend[1],XB[5]+extend[2]],
+           [XB[0]-extend[0],XB[3]+extend[1],XB[4]-extend[2]],
+           [XB[0]-extend[0],XB[3]+extend[1],XB[5]+extend[2]],
+           [XB[1]+extend[0],XB[2]-extend[1],XB[4]-extend[2]],
+           [XB[1]+extend[0],XB[2]-extend[1],XB[5]+extend[2]],
+           [XB[1]+extend[0],XB[3]+extend[1],XB[4]-extend[2]],
+           [XB[1]+extend[0],XB[3]+extend[1],XB[5]+extend[2]]]
+    return pts
+
+def in_hull(p, hull):
+    if not isinstance(hull,scsp.Delaunay):
+        hull = scsp.Delaunay(hull)
+    return hull.find_simplex(p)>=0
+
+def pts2polygons(groups):
+    '''
+    Build polygons from series of points.
+    '''
+    polygons = []
+    
+    for group in groups:
+        linkedPolygons = []
+        for pts in group:
+            try:
+                linkedPolygons.append(scsp.ConvexHull(pts))
+            except:
+                print("Failed points:")
+                print(pts)
+        polygons.append(linkedPolygons)
+        
+    return polygons, len(polygons)
+
+def getFileList(resultDir, chid, extension):
+    if '.zip' in resultDir:
+        files = getFileListFromZip(resultDir, chid, extension)
+    else:
+        path = os.path.join(resultDir, '%s*.%s'%(chid, extension))
+        files = glob.glob(path)
+    return files
+
+def getFileListFromZip(filename, chid, extension):
+    filelist = []
+    with zipfile.ZipFile(filename, 'r') as zip:
+        for info in zip.infolist():
+            if info.filename.split('.')[-1] == extension:
+                if chid in info.filename:
+                    filelist.append("%s%s%s"%(filename, os.sep, info.filename))
+    return filelist
+
+def zreadlines(file):
+    f = zopen(file, readtype='r')
+    lines = f.readlines()
+    if '.zip' in file:
+        lines = [line.decode("utf-8").replace('\r','').replace('\n','') for line in lines]
+    f.close()
+    return lines
+
+def zopen(file, readtype='rb'):
+    if '.zip' in file:
+        zname = '%s.zip'%(file.split('.zip')[0])
+        fname = file.split('.zip%s'%(os.sep))[1]
+        zip = zipfile.ZipFile(zname, 'r')
+        f = zip.open(fname)
+    else:
+        f = open(file, readtype)
+    return f
+
+def getEndianness(resultDir, chid):
+    endFiles = getFileList(resultDir, chid, 'end')
+    if len(endFiles) == 0:
+        print('Unable to find endianness file, %s.'%(os.path.join(resultDir, chid+'.end')))
+        print("Assuming little-endian.")
+        return "<"
+        
+    f = zopen(endFiles[0])
+    data = f.read()
+    f.close()
+    if struct.unpack('<i',data[4:8])[0] == 1:
+        return '<'
+    elif struct.unpack('>i',data[4:8])[0] == 1:
+        return ">"
+    else:
+        print("Unable to determine endianness from file, %s."%(os.path.join(resultDir, chid+'.end')))
+        print("Assuming little-endian.")
+        return "<"
+
+def getDatatypeByEndianness(datatype1, endianness):
+    if endianness == '>':
+        datatype2 = np.dtype(datatype1).newbyteorder('>')
+    elif endianness == '<':
+        datatype2 = np.dtype(datatype1).newbyteorder('<')
+    else:
+        print("Endianness unknown, could not determine datatype")
+    return datatype2
+
+def getTwoZone(z, val, lowtohigh=True):
+    if lowtohigh:
+        z = z[::-1]
+        val = val[::-1]
+        val_low = val[-1]
+    else:
+        val_low = val[0]
+    H = z.max()
+    H0 = z.min()
+    tmpZ = np.linspace(0, H, 101)
+    tmpV = np.interp(tmpZ, z, val)
+    if np.isclose(np.nanmin(tmpV), np.nanmax(tmpV), atol=1e-1):
+        zInt = H
+        return val_low, val_low, zInt
+    
+    I1 = np.trapz(tmpV, tmpZ)
+    I2 = np.trapz(1/tmpV, tmpZ)
+    zInt = val_low*(I1*I2-H**2)/(I1+I2*val_low**2-2*val_low*H)
+    
+    zU = np.linspace(zInt, H, num=50)
+    
+    val_high_tmp = np.interp(zU, z, val)
+    val_high = np.trapz(val_high_tmp, zU)/(H-zInt)
+    
+    zL = np.linspace(0, zInt, num=50)
+    
+    val_low_tmp = np.interp(zL, z, val)
+    val_low = np.trapz(val_low_tmp, zL)/(zInt-H0)
+    
+    return val_low, val_high, zInt
