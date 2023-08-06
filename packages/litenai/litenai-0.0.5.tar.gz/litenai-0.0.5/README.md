5703a14c6afefcfc9f846aa1de85d0e538dceb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:48 +0000

    Add package litenai/0.0.5/litenai-0.0.5.tar.gz

---
+++ packages/litenai/litenai-0.0.5.tar.gz/litenai-0.0.5/README.md
@@ -0,0 +1,3 @@
+## Liten AI Data 
+### Release Notes
+
---
+++ packages/litenai/litenai-0.0.5.tar.gz/litenai-0.0.5/setup.py
@@ -0,0 +1,55 @@
+import sys
+import setuptools
+#from setuptools import setup, Extension, Distribution
+#from distutils import sysconfig
+#import numpy
+
+long_description="Liten AI"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # This is the module name
+    name='litenai',
+    # This is the version of the module
+    version='0.0.5',
+    # Name of Author
+    author='HK Verma',
+    # Author email address
+    author_email='hkverma@litendata.com',
+    # Small description
+    description='Liten AI Data Platform',
+    # Long description
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # URL for github
+    # url='https://github.com/litenai/',
+    # Find packages
+    package_dir={'liten': 'liten','resources':'resources'},
+    packages=['liten','resources'],
+    include_package_data=True,
+    package_data={
+        "liten": ['*.pyc'],
+        "resources" : ['*.ipynb'],
+    },
+    # Add all the packages required by liten
+    install_requires = [
+        'botocore',
+        'boto3',
+        'langchain',
+       # 'minio',
+        'openai',
+        'pandas',
+        'panel',
+        'hvplot',
+        'python-dotenv',
+        'tiktoken'
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
