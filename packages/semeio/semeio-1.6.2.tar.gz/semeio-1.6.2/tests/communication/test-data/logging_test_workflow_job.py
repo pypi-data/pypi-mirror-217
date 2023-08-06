7e4bb6c82b8b183486a3c8dc29125f4bd7c16436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:59 +0000

    Add package semeio/1.6.2/semeio-1.6.2.tar.gz

---
+++ packages/semeio/semeio-1.6.2.tar.gz/semeio-1.6.2/tests/communication/test-data/logging_test_workflow_job.py
@@ -0,0 +1,26 @@
+import logging
+from threading import Thread
+
+from semeio.communication import SemeioScript
+
+
+class TestWorkflowJob(SemeioScript):
+    # pylint: disable=method-hidden
+    def run(self, *args):
+        # pylint: disable=unused-argument
+        self.reporter.publish("test_data", list(range(10)))
+
+        # The mission of this code is to simulate that something outside the
+        # thread of the workflow (e.g. ERT itself), is logging. This should not
+        # end up in the workflow log itself.
+        thread = Thread(
+            target=lambda: logging.error(
+                "Log statement from outside the workflow thread."
+            )
+        )
+        thread.start()
+        thread.join()
+
+        logging.error(
+            "I finished without any problems - hence I'm not a failure after all!"
+        )
