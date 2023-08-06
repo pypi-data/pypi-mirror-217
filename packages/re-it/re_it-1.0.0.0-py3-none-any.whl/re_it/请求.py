1b41358942c1d0a2cb84b226c02f27830a6b23a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:43 +0000

    Add package re-it/1.0.0.0/re_it-1.0.0.0-py3-none-any.whl

---
+++ packages/re-it/re_it-1.0.0.0-py3-none-any.whl/re_it/请求.py
@@ -0,0 +1,24 @@
+import http.client
+
+class 我的请求:
+    def __init__(self):
+        self.headers = {}
+
+    def 添加头部(self, 键, 值):
+        self.headers[键] = 值
+
+    def 获取(self, 网址):
+        conn = http.client.HTTPSConnection(网址)
+        conn.request("GET", "/", headers=self.headers)
+        response = conn.getresponse()
+        响应内容 = response.read().decode()
+        conn.close()
+        return 响应内容
+
+    def 提交(self, 网址, 数据=None):
+        conn = http.client.HTTPSConnection(网址)
+        conn.request("POST", "/", body=数据, headers=self.headers)
+        response = conn.getresponse()
+        响应内容 = response.read().decode()
+        conn.close()
+        return 响应内容
