c91bbdbc2db306897f5f1ba25d061eb2a544c005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:18 +0000

    Add package lol-esp-api/0.3/lol_esp_api-0.3-py2.py3-none-any.whl

---
+++ packages/lol-esp-api/lol_esp_api-0.3-py2.py3-none-any.whl/lol_esp_api/apis/eventsAPI.py
@@ -0,0 +1,40 @@
+from lol_esp_api.apis.supfunc import request
+
+
+def getSchedule(leagueID, PageToken=""):
+    url = (
+        "https://esports-api.lolesports.com/persisted/gw/getSchedule?leagueId={0}&PageToken={1}"
+        "&hl=en-US".format(leagueID, PageToken)
+    )
+
+    return request(url)
+
+
+def getLive():
+    url = "https://esports-api.lolesports.com/persisted/gw/getLive?hl=en-US"
+
+    return request(url)
+
+
+def getCompeletedEvents(eventID):
+    url = "https://esports-api.lolesports.com/persisted/gw/getCompletedEvents?tournamentId={0}&hl=en-US".format(
+        eventID
+    )
+
+    return request(url)
+
+
+def getEventDetails(eventID):
+    url = "https://esports-api.lolesports.com/persisted/gw/getEventDetails?Id={0}&hl=en-US".format(
+        eventID
+    )
+
+    return request(url)
+
+
+def getGames(gameID=""):
+    url = "https://esports-api.lolesports.com/persisted/gw/getGames?id={}&hl=en-US".format(
+        gameID
+    )
+
+    return request(url)
