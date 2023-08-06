e3b22c32dea2a3d23a73ca91e800110583ba66a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:24 +0000

    Add package taibun/0.0.3/taibun-0.0.3.tar.gz

---
+++ packages/taibun/taibun-0.0.3.tar.gz/taibun-0.0.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+# python setup.py sdist
+# twine upload dist/*
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding='utf-8') as fh:
+    LONG_DESCRIPTION = '\n' + fh.read()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Convert Chinese characters to Taiwanese'
+
+# Setting up
+setup(
+    name="taibun",
+    version=VERSION,
+    author="Andrei Harbachov",
+    author_email="<andrei.harbachov@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    package_dir={'taibun': 'taibun'},
+    package_data={'taibun': ['data/*.json']},
+    license='MIT',
+    url='https://github.com/andreihar/taibun',
+    install_requires=[],
+    keywords=['python', 'taiwan', 'taiwanese', 'taigi', 'hokkien', 'romanization', 'transliteration'],
+    classifiers=[
+        'Topic :: Text Processing :: Linguistic',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
