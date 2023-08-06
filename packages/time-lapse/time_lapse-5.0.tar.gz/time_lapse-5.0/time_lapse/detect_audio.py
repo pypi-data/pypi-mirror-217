6c7a2eac09c4941b9421d62d12a5972735684d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:56 +0000

    Add package time-lapse/5.0/time_lapse-5.0.tar.gz

---
+++ packages/time-lapse/time_lapse-5.0.tar.gz/time_lapse-5.0/time_lapse/detect_audio.py
@@ -0,0 +1,12 @@
+import glob
+
+from collections.abc import Iterator
+
+import ffmpeg
+
+
+def files_with_audio(pattern: str = '*.mp4') -> Iterator[str]:
+    for filename in glob.glob(pattern):
+        for stream in ffmpeg.probe(filename)['streams']:
+            if stream['codec_type'] == 'audio':
+                yield filename
