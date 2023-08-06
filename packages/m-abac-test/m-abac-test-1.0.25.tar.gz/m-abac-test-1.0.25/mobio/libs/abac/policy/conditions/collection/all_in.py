d3f27efe24e1bfaa533b335eb1bbf97ca8664dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:59 +0000

    Add package m-abac-test/1.0.25/m-abac-test-1.0.25.tar.gz

---
+++ packages/m-abac-test/m-abac-test-1.0.25.tar.gz/m-abac-test-1.0.25/mobio/libs/abac/policy/conditions/collection/all_in.py
@@ -0,0 +1,27 @@
+"""
+    All of the values in collection conditions
+"""
+
+from marshmallow import post_load
+
+from .base import CollectionCondition, CollectionConditionSchema
+
+
+class AllIn(CollectionCondition):
+    """
+        Condition for all values of `what` in `values`
+    """
+
+    def _is_satisfied(self) -> bool:
+        return set(self.what).issubset(self.values)
+
+
+class AllInSchema(CollectionConditionSchema):
+    """
+        JSON schema for all in collection condition
+    """
+
+    @post_load
+    def post_load(self, data, **_):  # pylint: disable=missing-docstring,no-self-use
+        # self.validate(data)
+        return AllIn(**data)
---
+++ packages/m-abac-test/m-abac-test-1.0.25.tar.gz/m-abac-test-1.0.25/mobio/libs/abac/policy/conditions/string/not_contains.py
@@ -0,0 +1,44 @@
+"""
+    String not contains conditions
+"""
+
+from marshmallow import post_load
+
+from .base import StringCondition, StringConditionSchema
+
+
+class NotContains(StringCondition):
+    """
+        Condition for string `self.what` not contains `value`
+    """
+
+    def _is_satisfied(self) -> bool:
+        if self.qualifier == self.Qualifier.ForAnyValue:
+            for i in self.values:
+                if self.case_insensitive:
+                    if i.lower() in self.what.lower():
+                        return False
+                else:
+                    if i in self.what:
+                        return False
+            return True
+        else:
+            for i in self.values:
+                if self.case_insensitive:
+                    if i.lower() in self.what.lower():
+                        return False
+                else:
+                    if i in self.what:
+                        return False
+            return True
+
+
+class NotContainsSchema(StringConditionSchema):
+    """
+        JSON schema for not contains string condition
+    """
+
+    @post_load
+    def post_load(self, data, **_):  # pylint: disable=missing-docstring,no-self-use
+        # self.validate(data)
+        return NotContains(**data)
