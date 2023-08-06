0539761b425d24031e8884dd974b1db59e1c7e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:17 +0000

    Add package datatransform/1.0/datatransform-1.0.tar.gz

---
+++ packages/datatransform/datatransform-1.0.tar.gz/datatransform-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='datatransform',
+      version='1.0',
+      description='datatransform',
+      packages=['datatransform'],
+      author_email='aaravsharma927@gmail.com',
+      zip_safe=False)
