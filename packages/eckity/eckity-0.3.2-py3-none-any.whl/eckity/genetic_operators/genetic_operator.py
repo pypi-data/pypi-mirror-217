939cb64f1629fe74147cb789af96d374702577c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:14 +0000

    Add package eckity/0.3.2/eckity-0.3.2-py3-none-any.whl

---
+++ packages/eckity/eckity-0.3.2-py3-none-any.whl/eckity/genetic_operators/genetic_operator.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod
+from random import uniform
+
+from eckity.event_based_operator import Operator
+
+
+class GeneticOperator(Operator):
+    def __init__(self, probability=0.05, arity=0, events=None):
+        super().__init__(events=events, arity=arity)
+        self.probability = probability
+
+    def apply_operator(self, individuals):
+        if uniform(0, 1) <= self.probability:
+            for individual in individuals:
+                individual.set_fitness_not_evaluated()
+            op_res = self.apply(individuals)
+            for ind in op_res:
+                ind.applied_operators.append(type(self).__name__)
+            return op_res
+        return individuals
+
+    @abstractmethod
+    def apply(self, individuals):
+        pass
