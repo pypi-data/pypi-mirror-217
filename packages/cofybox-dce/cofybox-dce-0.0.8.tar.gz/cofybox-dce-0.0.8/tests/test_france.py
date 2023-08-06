eb47ff93497949a49845f1fbfaf13f25ad1b343b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:13 +0000

    Add package cofybox-dce/0.0.8/cofybox-dce-0.0.8.tar.gz

---
+++ packages/cofybox-dce/cofybox-dce-0.0.8.tar.gz/cofybox-dce-0.0.8/tests/test_france.py
@@ -0,0 +1,42 @@
+"""Test France collector"""
+
+from unittest.mock import MagicMock, patch
+
+import pandas as pd
+import pytest
+
+pytest_plugins = ("pytest_asyncio",)
+
+from cofybox_dce.pricing_collectors import Enercoop
+
+
+@pytest.fixture
+def enercoop():
+    """Fixture for Enercoop collector"""
+    collector = Enercoop(tariff="dynamic_tariff")
+
+    collector.session = MagicMock()
+    collector.session.get.return_value.content = open(
+        "tests/data/enercoop_sample.csv", "rb"
+    ).read()
+
+    return collector
+
+
+def test_enercoop(enercoop):
+    """Test Enercoop collector"""
+    assert enercoop.country == "FR"
+    assert enercoop.supplier == "Enercoop"
+    assert enercoop.timezone == "Europe/Paris"
+    assert enercoop.unit == "c€/kWh"
+    assert enercoop.tariff == "dynamic_tariff"
+    assert enercoop.price_name == "Enercoop_dynamic_tariff"
+
+
+@pytest.mark.asyncio
+async def test_enercoop_get_data(enercoop):
+    """Test Enercoop get_data"""
+    df = await enercoop.get_data()
+    assert df.columns.tolist() == ["value_inc_vat", "Unit"]
+    assert df.index.tz.zone == "Europe/Paris"
+    assert df["Unit"].iloc[0] == "c€/kWh"
---
+++ packages/cofybox-dce/cofybox-dce-0.0.8.tar.gz/cofybox-dce-0.0.8/tests/test_spain.py
@@ -0,0 +1,62 @@
+"""Tests for Spain Dynamic Prices"""
+
+from unittest.mock import MagicMock
+
+import pandas as pd
+import pytest
+
+from cofybox_dce.pricing_collectors import SomEnergiaInjection, SomEnergiaOfftake
+
+
+@pytest.fixture
+def somenergia_injection():
+    """Fixture for SomEnergia collector"""
+    collector = SomEnergiaInjection(entsoe_api_key="test")
+    collector.entsoe_client = MagicMock()
+    collector.entsoe_client.query_day_ahead_prices.return_value = pd.read_pickle(
+        "tests/data/da_prices_sample.pkl"
+    ).tz_convert("Europe/Madrid")
+    return collector
+
+
+@pytest.fixture
+def somenergia_offtake():
+    """Fixture for SomEnergia collector"""
+    collector = SomEnergiaOfftake()
+    return collector
+
+
+def test_somenergia_injection(somenergia_injection):
+    """Test SomEnergia collector"""
+    assert somenergia_injection.country == "ES"
+    assert somenergia_injection.supplier == "SomEnergia"
+    assert somenergia_injection.timezone == "Europe/Madrid"
+    assert somenergia_injection.unit == "c€/kWh"
+    assert somenergia_injection.price_name == "SomEnergia_injection"
+
+
+def test_somenergia_offtake(somenergia_offtake):
+    """Test SomEnergia collector"""
+    assert somenergia_offtake.country == "ES"
+    assert somenergia_offtake.supplier == "SomEnergia"
+    assert somenergia_offtake.timezone == "Europe/Madrid"
+    assert somenergia_offtake.unit == "c€/kWh"
+    assert somenergia_offtake.price_name == "SomEnergia_consumption"
+
+
+@pytest.mark.asyncio
+async def test_somenergia_injection_get_data(somenergia_injection):
+    """Test SomEnergia get_data"""
+    df = await somenergia_injection.get_data()
+    assert df.columns.tolist() == ["value_inc_vat", "Unit"]
+    assert df.index.tz.zone == "Europe/Madrid"
+    assert df["Unit"].iloc[0] == "c€/kWh"
+
+
+@pytest.mark.asyncio
+async def test_somenergia_offtake_get_data(somenergia_offtake):
+    """Test SomEnergia get_data"""
+    df = await somenergia_offtake.get_data()
+    assert df.columns.tolist() == ["value_inc_vat", "Unit"]
+    assert df.index.tz.zone == "Europe/Madrid"
+    assert df["Unit"].iloc[0] == "c€/kWh"
