d31961abec7a0a2323a017914cbc0872842733b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:33 +0000

    Add package devvit-manager/1.0.1/devvit_manager-1.0.1-py3-none-any.whl

---
+++ packages/devvit-manager/devvit_manager-1.0.1-py3-none-any.whl/devvit_mgr/const.py
@@ -0,0 +1,11 @@
+"""Devvit Manager Constants."""
+from pathlib import Path
+
+__version__ = "1.0.1"
+
+DEVVIT_CLIENT_ID = "Bep8X2RRjuoyuxkKsKxFuQ"
+DEVVIT_COPY_PASTE_CLIENT_ID = "TWTsqXa53CexlrYGBWaesQ"
+DEVVIT_MGR_PATH = Path("~/.devvit-mgr").expanduser()
+DEVVIT_PATH = Path("~/.devvit").expanduser()
+PROFILES_FILE = DEVVIT_MGR_PATH / "profiles.json"
+TOKEN_PATH = DEVVIT_PATH / "token"
