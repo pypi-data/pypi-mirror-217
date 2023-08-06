e57b07b51788bc088734f347998b37f089aa413f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:11 +0000

    Add package seinpdf/1.1/seinpdf-1.1-py3-none-any.whl

---
+++ packages/seinpdf/seinpdf-1.1-py3-none-any.whl/seinpdf/pdf2text.py
@@ -0,0 +1,18 @@
+""" This module provides functions to convert a PDF to Text."""
+
+
+class Converter:
+    """A simple converter for converting PDFs to text."""
+    def convert(self, path):
+        # Docstring:
+        """
+        Convert the given PDF to text.
+
+        Parameters:
+        path (str): The path to a PDF file.
+
+        Returns:
+        str: Content of the PDF file as text.
+        :param path:
+        """
+        print("pdf2text")
