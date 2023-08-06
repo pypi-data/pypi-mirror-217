5a33d27d1b7fbf6e03ac8aaee7a96e7dc1e7a1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:01 +0000

    Add package retake/0.1.5.dev0/retake-0.1.5.dev0-py3-none-any.whl

---
+++ packages/retake/retake-0.1.5.dev0-py3-none-any.whl/core/load/opensearch.py
@@ -0,0 +1,14 @@
+from typing import List
+
+# TODO: Implement OpenSearch interface
+
+
+class OpenSearchLoader:
+    def __init__(self):
+        pass
+
+    def upsert_embedding(self):
+        pass
+
+    def bulk_upsert_embeddings(self):
+        pass
---
+++ packages/retake/retake-0.1.5.dev0-py3-none-any.whl/retake/target.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+
+class Similarity:
+    L2_NORM = "l2_norm"
+    DOT_PRODUCT = "dot_product"
+    COSINE = "cosine"
+
+
+class ElasticSearchTarget(BaseModel):
+    index_name: str
+    field_name: str
+    index_field: bool 
+    similarity: Similarity = None
+
+
+class Target:
+    @classmethod
+    def ElasticSearch(
+        cls,
+        index_name: str,
+        field_name: str,
+        index_field: bool = True,
+        similarity: Similarity = None,
+    ) -> ElasticSearchTarget:
+        return ElasticSearchTarget(
+            index_name=index_name,
+            field_name=field_name,
+            index_field=index_field,
+            similarity=similarity,
+        )
