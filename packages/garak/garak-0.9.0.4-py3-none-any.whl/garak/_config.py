76ea5ba378b40465c9930c6d9bc04a443d92584e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:37 +0000

    Add package garak/0.9.0.4/garak-0.9.0.4-py3-none-any.whl

---
+++ packages/garak/garak-0.9.0.4-py3-none-any.whl/garak/_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""garak global config"""
+
+import pathlib
+
+basedir = pathlib.Path(__file__).parents[0]
+
+args = None
+reportfile = None
+hitlogfile = None
+seed = None
+run_id = None
+generator_model = None
+generator_name = None
