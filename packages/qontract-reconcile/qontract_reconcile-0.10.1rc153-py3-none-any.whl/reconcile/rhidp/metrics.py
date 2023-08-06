1ebb75677a7f18a1fb5750ff639ed38f46752e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:51 +0000

    Add package qontract-reconcile/0.10.1rc153/qontract_reconcile-0.10.1rc153-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc153-py3-none-any.whl/reconcile/rhidp/metrics.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+from reconcile.utils.metrics import GaugeMetric
+
+
+class RhIdpBaseMetric(BaseModel):
+    """Base class for RhIdp metrics"""
+
+    integration: str
+
+
+class RhIdpClusterCounter(RhIdpBaseMetric, GaugeMetric):
+    """Number of managed clusters per organization."""
+
+    org_id: str
+
+    @classmethod
+    def name(cls) -> str:
+        return "rhidp_managed_clusters"
