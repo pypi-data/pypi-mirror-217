cca05e426fbc51f311c3a1cfb102195e8f813ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:44 +0000

    Add package ai-images/0.1.1/ai_images-0.1.1.tar.gz

---
+++ packages/ai-images/ai_images-0.1.1.tar.gz/ai_images-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ai_images',
+    version='0.1.1',
+    author="Keith Burroughs",
+    author_email='keithburroughs33@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/keithhb33/AI-Image-Bot/',
+    keywords=["python", "images", "ai", "ai generated", "movies", "photography", "modules"],
+    install_requires=[
+          'requests'
+      ],
+
+)
