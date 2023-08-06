24b1ed8bdba008b098741ccc1d5937ee597a3293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:35 +0000

    Add package wpp-bavapi/0.4.1/wpp_bavapi-0.4.1-py3-none-any.whl

---
+++ packages/wpp-bavapi/wpp_bavapi-0.4.1-py3-none-any.whl/bavapi_refs/countries.py
@@ -0,0 +1,64 @@
+"""Countries class for holding countries IDs."""
+
+# This file was generated from active countries in the Fount.
+# Countries retrieved on 2023-06-19 20:12:53
+
+from bavapi.reference._int_enum import IntEnum
+
+
+class Countries(IntEnum):
+    """Countries IDs for Fount API queries."""
+
+    ARGENTINA = 1
+    AUSTRALIA = 7
+    AUSTRIA = 20
+    BELGIUM = 29
+    BRAZIL = 47
+    CANADA = 63
+    CHILE = 73
+    CHINA = 287
+    COLOMBIA = 91
+    CZECH_REPUBLIC = 100
+    DENMARK = 595
+    ECUADOR = 129
+    EGYPT = 686
+    FINLAND = 131
+    FRANCE = 132
+    GERMANY = 147
+    GREECE = 161
+    GUATEMALA = 166
+    HONG_KONG = 300
+    HUNGARY = 188
+    INDIA = 198
+    INDONESIA = 207
+    IRELAND = 214
+    ITALY = 215
+    JAPAN = 230
+    JORDAN = 239
+    MALAYSIA = 242
+    MEXICO = 245
+    NETHERLANDS = 169
+    NEW_ZEALAND = 261
+    NORWAY = 263
+    PERU = 265
+    PHILIPPINES = 270
+    POLAND = 272
+    PORTUGAL = 281
+    PUERTO_RICO = 302
+    SAUDI_ARABIA = 312
+    SINGAPORE = 313
+    SOUTH_AFRICA = 560
+    SOUTH_KOREA = 320
+    SPAIN = 321
+    SWEDEN = 333
+    SWITZERLAND = 337
+    TAIWAN = 348
+    THAILAND = 349
+    TURKIYE = 359
+    UNITED_ARAB_EMIRATES = 367
+    UNITED_KINGDOM = 705
+    URUGUAY = 384
+    USA = 726
+    US_HISPANICS = 43
+    VENEZUELA = 535
+    VIETNAM = 624
