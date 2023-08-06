5b318b913b56fb11f36953c3527227e8468e505e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:32 +0000

    Add package kappadata/1.2.63/kappadata-1.2.63-py3-none-any.whl

---
+++ packages/kappadata/kappadata-1.2.63-py3-none-any.whl/kappadata/wrappers/sample_wrappers/label_smoothing_wrapper.py
@@ -0,0 +1,25 @@
+import torch
+
+from kappadata.datasets.kd_wrapper import KDWrapper
+
+
+class LabelSmoothingWrapper(KDWrapper):
+    def __init__(self, dataset, smoothing):
+        super().__init__(dataset=dataset)
+        assert isinstance(smoothing, (int, float)) and 0. < smoothing < 1.
+        self.smoothing = smoothing
+
+    def getitem_class(self, idx, ctx=None):
+        y = self.dataset.getitem_class(idx, ctx)
+        assert isinstance(y, int) or (torch.is_tensor(y) and y.ndim == 0)
+        n_classes = max(2, self.dataset.getdim_class())
+
+        # can't smooth missing label in semi-supervised
+        if y == -1:
+            return torch.full(size=(n_classes,), fill_value=-1.)
+
+        off_value = self.smoothing / n_classes
+        on_value = 1. - self.smoothing + off_value
+        y_vector = torch.full(size=(n_classes,), fill_value=off_value)
+        y_vector[y] = on_value
+        return y_vector
