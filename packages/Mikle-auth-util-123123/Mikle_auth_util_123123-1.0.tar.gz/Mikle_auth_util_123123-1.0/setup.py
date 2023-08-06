320a1602f6dfe2a29b52b6948931feeba18c7130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:57 +0000

    Add package Mikle-auth-util-123123/1.0/Mikle_auth_util_123123-1.0.tar.gz

---
+++ packages/Mikle-auth-util-123123/Mikle_auth_util_123123-1.0.tar.gz/Mikle_auth_util_123123-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Mikle_auth_util_123123',
+      version='1.0',
+      description='Система аутентифікації',
+      author='Mikle',
+      author_email='fedorkomm@gmail.com',
+      packages=['project'],
+     )
