78d4023407da55b1577249f4333c8b10fb420cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:43 +0000

    Add package ir-digit/0.0.49/ir_digit-0.0.49-py3-none-any.whl

---
+++ packages/ir-digit/ir_digit-0.0.49-py3-none-any.whl/digit/scripts.py
@@ -0,0 +1,41 @@
+import argparse
+from .core import Core
+
+def main():
+    parser = argparse.ArgumentParser(prog="digit")
+    subparsers = parser.add_subparsers(dest='command')
+    parser.add_mutually_exclusive_group("-h","--help",help="帮助")
+    parser.add_mutually_exclusive_group("-u", "--update-api-token", help="设置/更新个人的API-token")
+    parser.add_mutually_exclusive_group("-c", "--category", help="查看分类体系中的编码与含义",default=None)
+
+    query_parser = subparsers.add_parser('query', help='子命令：查看平台现有资源')
+    query_parser.add_argument("-t","--type",help="查看哪种类型的资源，data/card/useraccount/websetting/dataid", default='data')
+    query_parser.add_argument("-i","--id",help="选择特定id的资源查看",default=None)
+    query_parser.add_argument("-d","--detail", help="是否查看详情，默认为否",default=False)
+
+    # 创建 download 子命令的解析器
+    download_parser = subparsers.add_parser('download', help='子命令：下载资源到本地')
+    download_parser.add_argument("-i", "--id-or-name", help="根据data_id_or_name下载对应的资源至本地, 默认如果已经存在则不更新资源")
+    download_parser.add_argument("-u","--update",help="更新下载的资源")
+
+    args = parser.parse_args()
+    core = Core()
+    if args.command=="query":
+        core.get_resources(api_type=args.type,id=args.id,detail=args.detail)
+    elif args.command =="download":
+        core.download_repo(data_id_or_name=args.id_or_name,update=args.update)
+
+
+    else:
+
+        if args.update_api_token:
+            core.update_api_token(new_token=args.update_api_token)
+
+        elif args.category:
+            core.get_category()
+
+        elif args.help:
+            args.print_help()
+        else:
+            print("参数错误/无参数:, 帮助如下：")
+            args.print_help()
