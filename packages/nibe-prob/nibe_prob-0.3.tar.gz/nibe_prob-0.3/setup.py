392edd38d5bd47a6b8cb9a47f4ffd8fcc0ade850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:00 +0000

    Add package nibe-prob/0.3/nibe_prob-0.3.tar.gz

---
+++ packages/nibe-prob/nibe_prob-0.3.tar.gz/nibe_prob-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nibe_prob',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['nibe_prob'],
+      authon = 'Nibedan Banerjee',
+      zip_safe=False)
