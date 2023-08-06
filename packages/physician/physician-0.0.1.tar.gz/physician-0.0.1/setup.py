ff6e24c7b8efdded1fd178bbf01ff52afc01e206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:55 +0000

    Add package physician/0.0.1/physician-0.0.1.tar.gz

---
+++ packages/physician/physician-0.0.1.tar.gz/physician-0.0.1/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+import pathlib
+setup(
+    name="physician",
+    version="0.0.1",
+    description="In the works.",
+    url="https://github.com/",
+    author="approximate",
+    author_email="test@exmaple.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        
+    ],
+    
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    
+    python_requires=">=3.7",
+    project_urls={
+      
+    },
+)
