b7ad1aa90961754aba90efb8330442b7d0c29ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:03 +0000

    Add package lander/2.0.0a8/lander-2.0.0a8.tar.gz

---
+++ packages/lander/lander-2.0.0a8.tar.gz/lander-2.0.0a8/src/lander/__init__.py
@@ -0,0 +1,24 @@
+"""HTML landing page generator for LSST PDF documentation deployed from Git to
+LSST the Docs.
+
+https://github.com/lsst-sqre/lander
+"""
+
+__all__ = ["__version__"]
+
+import sys
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import PackageNotFoundError, version
+else:
+    from importlib.metadata import PackageNotFoundError, version
+
+
+__version__: str
+"""The application version string of (PEP 440 / SemVer compatible)."""
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "0.0.0"
