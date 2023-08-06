9b1dffbfffdda8152a40e49d81173dd30e2dcf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:46 +0000

    Add package spellbot/8.11.7/spellbot-8.11.7-py3-none-any.whl

---
+++ packages/spellbot/spellbot-8.11.7-py3-none-any.whl/spellbot/cogs/tasks_cog.py
@@ -0,0 +1,59 @@
+import logging
+
+from ddtrace import tracer
+from discord.ext import commands, tasks
+
+from .. import SpellBot
+from ..actions import TasksAction
+from ..environment import running_in_pytest
+from ..settings import Settings
+
+logger = logging.getLogger(__name__)
+settings = Settings()
+
+
+async def wait_until_ready(bot: SpellBot) -> None:
+    while True:
+        try:
+            await bot.wait_until_ready()
+            break
+        except BaseException as e:  # Catch EVERYTHING so tasks don't die
+            logger.exception("error: exception in task before loop: %s", e)
+
+
+class TasksCog(commands.Cog):  # pragma: no cover
+    def __init__(self, bot: SpellBot) -> None:
+        self.bot = bot
+        if not running_in_pytest():
+            self.cleanup_old_voice_channels.start()  # pylint: disable=no-member
+            self.expire_inactive_games.start()  # pylint: disable=no-member
+
+    @tasks.loop(minutes=settings.VOICE_CLEANUP_LOOP_M)
+    async def cleanup_old_voice_channels(self) -> None:
+        try:
+            with tracer.trace(name="command", resource="cleanup_old_voice_channels"):
+                async with TasksAction.create(self.bot) as action:
+                    await action.cleanup_old_voice_channels()
+        except BaseException as e:  # Catch EVERYTHING so tasks don't die
+            logger.exception("error: exception in task cog: %s", e)
+
+    @cleanup_old_voice_channels.before_loop
+    async def before_cleanup_old_voice_channels(self) -> None:
+        await wait_until_ready(self.bot)
+
+    @tasks.loop(minutes=settings.EXPIRE_GAMES_LOOP_M)
+    async def expire_inactive_games(self) -> None:
+        try:
+            with tracer.trace(name="command", resource="expire_inactive_games"):
+                async with TasksAction.create(self.bot) as action:
+                    await action.expire_inactive_games()
+        except BaseException as e:  # Catch EVERYTHING so tasks don't die
+            logger.exception("error: exception in task cog: %s", e)
+
+    @expire_inactive_games.before_loop
+    async def before_expire_inactive_games(self) -> None:
+        await wait_until_ready(self.bot)
+
+
+async def setup(bot: SpellBot) -> None:  # pragma: no cover
+    await bot.add_cog(TasksCog(bot), guild=bot.settings.GUILD_OBJECT)
