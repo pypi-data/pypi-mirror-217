5ededcaacd196ae60b3a2edfd5634aaf89e3114f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:19 +0000

    Add package biobear/0.6.3/biobear-0.6.3-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/biobear/biobear-0.6.3-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/biobear/genbank_reader.py
@@ -0,0 +1,32 @@
+"""Genbank file reader."""
+import os
+
+from biobear.reader import Reader
+from biobear.compression import Compression
+
+from .biobear import _ExonReader
+
+
+class GenbankReader(Reader):
+    """Genbank file reader."""
+
+    def __init__(
+        self, path: os.PathLike, compression: Compression = Compression.INFERRED
+    ):
+        """Read a fasta file.
+
+        Args:
+            path (Path): Path to the fasta file.
+
+        """
+        self.compression = compression.infer_or_use(path)
+
+        if self.compression == Compression.GZIP:
+            self._reader = _ExonReader(str(path), "GENBANK", "GZIP")
+        else:
+            self._reader = _ExonReader(str(path), "GENBANK", None)
+
+    @property
+    def inner(self):
+        """Return the inner reader."""
+        return self._reader
