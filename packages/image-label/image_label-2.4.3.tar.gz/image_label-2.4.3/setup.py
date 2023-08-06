7bc0ae453a37fe4dc726bd9099ee41461ca896aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:31 +0000

    Add package image-label/2.4.3/image_label-2.4.3.tar.gz

---
+++ packages/image-label/image_label-2.4.3.tar.gz/image_label-2.4.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="image_label",
+    version="2.4.3",
+    author="sks",
+    author_email="shubhaman47@gmail.com",
+    description="This is an Image Labeling Component for streamlit",
+    long_description="The image_label package provides a custom Streamlit component for image annotation. It allows users to manually annotate images with bounding boxes and labels, making it a great tool for data labeling tasks, especially in machine learning projects. for more info go to https://github.com/SksOp/image_label",
+    long_description_content_type="text/plain",
+    url="https://github.com/SksOp/image_label",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
