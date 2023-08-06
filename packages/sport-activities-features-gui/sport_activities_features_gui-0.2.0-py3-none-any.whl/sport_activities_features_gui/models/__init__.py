5be204448161c0726371f33a818efc2ab347f4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:26 +0000

    Add package sport-activities-features-gui/0.2.0/sport_activities_features_gui-0.2.0-py3-none-any.whl

---
+++ packages/sport-activities-features-gui/sport_activities_features_gui-0.2.0-py3-none-any.whl/sport_activities_features_gui/models/__init__.py
@@ -0,0 +1,7 @@
+from sport_activities_features_gui.models import User
+
+__all__ = [
+    'User',
+]
+
+__version__ = '0.1.0'
---
+++ packages/sport-activities-features-gui/sport_activities_features_gui-0.2.0-py3-none-any.whl/sport_activities_features_gui/widgets/__init__.py
@@ -0,0 +1,13 @@
+from widgets.ImportDataWidget import Ui_ImportDataWidget
+from widgets.CalendarWidget import Ui_CalendarWidget
+from widgets.TransformationsWidget import Ui_TransformationsWidget
+from widgets.GraphsWidget import Ui_GraphsWidget
+
+__all__ = [
+    'Ui_ImportDataWidget',
+    'Ui_CalendarWidget',
+    'Ui_TransformationsWidget',
+    'Ui_GraphsWidget'
+]
+
+__version__ = '0.1.0'
