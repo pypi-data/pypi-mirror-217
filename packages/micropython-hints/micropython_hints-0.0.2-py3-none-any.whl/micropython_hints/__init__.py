0a2a44aaebad34fd7663f99f97b3010fe8cdb827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:27 +0000

    Add package micropython-hints/0.0.2/micropython_hints-0.0.2-py3-none-any.whl

---
+++ packages/micropython-hints/micropython_hints-0.0.2-py3-none-any.whl/micropython_hints/__init__.py
@@ -0,0 +1,24 @@
+import os
+TGT_DIR = os.path.join(os.path.split(os.path.abspath(os.__file__))[0], "site-packages")
+DIR = os.path.split(os.path.abspath(__file__))[0]
+
+def run():
+    print(f"\033[31m[WARNING]: Please note that this package will be installed at `{TGT_DIR}` in a destructive manner. \
+        \n\rIt is strongly recommended to install it within a virtual environment.\033[0m", end='')
+
+    if input("continue? [y/N]: ").strip() not in ['yes', 'Y', 'y']:
+        exit()
+
+    from tqdm import tqdm
+    from shutil import copyfile
+    for name in tqdm(os.listdir(DIR)):
+        if name.startswith("__") and name.endswith("__"):
+            continue
+        fpath = os.path.join(DIR, name)
+        tpath = os.path.join(TGT_DIR, name)
+        copyfile(fpath, tpath)
+    print("done")
+    
+if __name__ == '__main__':
+    # run()
+    ...
