ab077258193f038859350a5d0d92ce9485c566df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:10 +0000

    Add package BabitMF/0.0.5/BabitMF-0.0.5-cp37-cp37m-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/BabitMF/BabitMF-0.0.5-cp37-cp37m-manylinux_2_17_s390x.manylinux2014_s390x.whl/bmf/include/bmf_nlohmann/detail/meta/void_t.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace bmf_nlohmann
+{
+namespace detail
+{
+template <typename ...Ts> struct make_void
+{
+    using type = void;
+};
+template <typename ...Ts> using void_t = typename make_void<Ts...>::type;
+} // namespace detail
+}  // namespace bmf_nlohmann
