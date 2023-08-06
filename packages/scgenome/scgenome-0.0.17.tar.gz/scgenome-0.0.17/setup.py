53b589b75cc424bec8a9c59e0ca23194bd9e98cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:31 +0000

    Add package scgenome/0.0.17/scgenome-0.0.17.tar.gz

---
+++ packages/scgenome/scgenome-0.0.17.tar.gz/scgenome-0.0.17/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='scgenome',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Code for analyzing single cell whole genomes',
+    author='Shah Lab',
+    url='https://github.com/shahcompbio/scgenome',
+    packages=find_packages(),
+    install_requires=[
+        'adjustText',
+        'anndata',
+        'bamread',
+        'biopython',
+        'bokeh',
+        'brewer2mpl',
+        'Click',
+        'csverve>=0.3.1',
+        'hdbscan',
+        'hmmlearn',
+        'ipython!=8.7.0',
+        'jupyter',
+        'lda',
+        'matplotlib',
+        'nose',
+        'numba',
+        'numexpr',
+        'numpy',
+        'oauthlib',
+        'pandas',
+        'pyBigWig',
+        'pyfaidx',
+        'pyranges',
+        'pysam',
+        'PyYAML',
+        'scikit-learn',
+        'scipy',
+        'seaborn',
+        'statsmodels',
+        'umap-learn',
+        'wgs_analysis',
+        'nose',
+        'biopython',
+        'pypeliner',
+        'joblib==1.1.0',
+        'sphinx==5.1.1'
+    ],
+    package_data={
+        'scgenome': [
+            'data/*',
+            'dtypes/*.yaml',
+            'datasets/data/*'
+        ],
+    },
+)
