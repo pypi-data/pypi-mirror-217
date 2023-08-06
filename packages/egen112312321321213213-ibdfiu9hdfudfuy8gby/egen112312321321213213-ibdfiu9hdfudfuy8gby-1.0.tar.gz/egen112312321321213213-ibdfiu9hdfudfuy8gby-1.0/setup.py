828e76855c2615fe2b3af6d2f7766102ed1a01f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:56 +0000

    Add package egen112312321321213213-ibdfiu9hdfudfuy8gby/1.0/egen112312321321213213-ibdfiu9hdfudfuy8gby-1.0.tar.gz

---
+++ packages/egen112312321321213213-ibdfiu9hdfudfuy8gby/egen112312321321213213-ibdfiu9hdfudfuy8gby-1.0.tar.gz/egen112312321321213213-ibdfiu9hdfudfuy8gby-1.0/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='egen112312321321213213-ibdfiu9hdfudfuy8gby',
+      version='1.0',
+      description='Мій проєкт',
+      author='Kirill',
+      author_email='123@gmail.com',
+      packages=['project'],
+     )
