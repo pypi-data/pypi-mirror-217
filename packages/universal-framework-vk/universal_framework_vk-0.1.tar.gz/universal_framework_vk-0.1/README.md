360562e8c9d73b32277a3d455c2b97a1f7e53d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:52 +0000

    Add package universal-framework-vk/0.1/universal_framework_vk-0.1.tar.gz

---
+++ packages/universal-framework-vk/universal_framework_vk-0.1.tar.gz/universal_framework_vk-0.1/README.md
@@ -0,0 +1,2 @@
+# _Universal_Framework
+universal framework for HTML, CSS, databases and payment processing, along with coding practices
---
+++ packages/universal-framework-vk/universal_framework_vk-0.1.tar.gz/universal_framework_vk-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='universal_framework_vk',
+    version='0.1',
+    packages=find_packages(),
+    description='universal framework',
+    author='Viral Killer',
+    author_email='jules3313@gmail.com',
+    license='MIT',
+    install_requires=[
+        # List of your package dependencies
+        # For example: 'numpy>=1.18.1'
+    ],
+)
