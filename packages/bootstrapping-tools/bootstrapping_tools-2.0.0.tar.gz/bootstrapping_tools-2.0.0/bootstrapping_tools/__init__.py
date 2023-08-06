c57ef8e439bc187b45bccc96947b701dd3849fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:03 +0000

    Add package bootstrapping-tools/2.0.0/bootstrapping_tools-2.0.0.tar.gz

---
+++ packages/bootstrapping-tools/bootstrapping_tools-2.0.0.tar.gz/bootstrapping_tools-2.0.0/bootstrapping_tools/__init__.py
@@ -0,0 +1,6 @@
+"""GECI Tools for bootstrapping"""
+
+__version__ = "2.0.0"
+from .bootstrapping import *  # noqa
+from .n0_and_lambdas_intervals import *  # noqa
+from .resample_by_blocks import *  # noqa
