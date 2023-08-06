7b4c4f487570d91baee8c605d2c205b6f5c2e8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:24 +0000

    Add package SC-Track/0.0.2/SC-Track-0.0.2.tar.gz

---
+++ packages/SC-Track/SC-Track-0.0.2.tar.gz/SC-Track-0.0.2/setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# @FileName: setup.py
+# @Author: Li Chengxin 
+# @Time: 2023/7/4 13:43
+
+import setuptools
+
+with open('readme.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as fr:
+    pkg_requirements = fr.read().split('\n')
+    pkg_requirements.remove('')
+
+VERSION = '0.0.2'
+
+setuptools.setup(
+    name='SC-Track',  # package name
+    author="Li Chengxin",
+    author_email="914814442@qq.com",
+    url="https://github.com/frozenleaves/SC-Track",
+    version=VERSION,  # package version
+    description='single cell tracking package',  # package description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    python_requires=">=3.7",
+    install_requires=pkg_requirements,
+    entry_points={
+        'console_scripts': [
+            'sctrack = SCTrack.sctrack:main',
+        ],
+    },
+)
