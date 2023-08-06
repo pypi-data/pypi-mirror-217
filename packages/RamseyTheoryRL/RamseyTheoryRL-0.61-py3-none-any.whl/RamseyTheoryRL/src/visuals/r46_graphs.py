32462d40ea0ed2fead2203964bd229ce8bc683d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:58 +0000

    Add package RamseyTheoryRL/0.61/RamseyTheoryRL-0.61-py3-none-any.whl

---
+++ packages/RamseyTheoryRL/RamseyTheoryRL-0.61-py3-none-any.whl/RamseyTheoryRL/src/visuals/r46_graphs.py
@@ -0,0 +1,37 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import json
+import sys
+sys.path.append("..")
+from utils.features import *
+import timeit
+
+
+# Obtain feature table for each of the 37 R(4,6,35) graphs
+
+# 4 subgraph, K_2, 2K_2 P_3, K_{1,3}, P_4, K_3, K_3 + e, C_4, K_4 - e, K_4
+
+# Right now we do it for graph 1
+
+def obtain_counts():
+  start_time = timeit.default_timer()
+  filename = '../../data/r46/r46_35some.g6'
+  graphs = nx.read_graph6(filename)
+  for idx, g in enumerate(graphs):
+    feature_counts = dict()
+    counters = count_subgraph_structures(g)
+    print(counters)
+    # Output to file
+    with open('feature_counts.txt', 'a') as convert_file:
+      convert_file.write(json.dumps(counters))
+      convert_file.write('\n')
+  print(f"Time elapsed: {timeit.default_timer()-start_time} seconds")
+
+
+
+def main():
+  obtain_counts()
+
+
+if __name__ == '__main__':
+  main()
