c5489de4b3d7dc858db1ecde8e0af873cb2b136e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:08 +0000

    Add package qinyuanZheng/0.0.1/qinyuanZheng-0.0.1.tar.gz

---
+++ packages/qinyuanZheng/qinyuanZheng-0.0.1.tar.gz/qinyuanZheng-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+name="qinyuanZheng", # Replace with your own username
+version="0.0.1",
+author="aishan1",
+author_email="18094773132@189.cn",
+description="A small example package",
+long_description=long_description,
+long_description_content_type="text/markdown",
+url="https://github.com/pypa/sampleproject",
+packages=setuptools.find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires='>=3.6',
+)
