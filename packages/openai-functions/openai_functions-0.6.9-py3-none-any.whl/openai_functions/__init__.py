5b16f613e1e800c2fb91ad3802030111735df88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:22 +0000

    Add package openai-functions/0.6.9/openai_functions-0.6.9-py3-none-any.whl

---
+++ packages/openai-functions/openai_functions-0.6.9-py3-none-any.whl/openai_functions/__init__.py
@@ -0,0 +1,42 @@
+"""ChatGPT function calling based on function docstrings."""
+from .conversation import Conversation
+from .functions import (
+    BasicFunctionSet,
+    FunctionNotFoundError,
+    FunctionResult,
+    FunctionSet,
+    FunctionWrapper,
+    MutableFunctionSet,
+    OpenAIFunction,
+    RawFunctionResult,
+    TogglableSet,
+    UnionSkillSet,
+    WrapperConfig,
+)
+from .nlp import NaturalLanguageAnnotated, Wrapper, nlp
+from .openai_types import FinalResponseMessage, FunctionCall, GenericMessage, Message
+from .parsers import ArgSchemaParser, defargparsers
+
+__all__ = [
+    "Conversation",
+    "BasicFunctionSet",
+    "FunctionNotFoundError",
+    "FunctionSet",
+    "defargparsers",
+    "ArgSchemaParser",
+    "FunctionWrapper",
+    "MutableFunctionSet",
+    "OpenAIFunction",
+    "FunctionResult",
+    "RawFunctionResult",
+    "TogglableSet",
+    "UnionSkillSet",
+    "WrapperConfig",
+    "NaturalLanguageAnnotated",
+    "nlp",
+    "Wrapper",
+    "FinalResponseMessage",
+    "FunctionCall",
+    "GenericMessage",
+    "Message",
+]
