bfe70fdaf5a671c8f40c74fb954aa82e03ff2e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:20 +0000

    Add package overhave/4.0.3/overhave-4.0.3.tar.gz

---
+++ packages/overhave/overhave-4.0.3.tar.gz/overhave-4.0.3/overhave/entities/auth_managers/default.py
@@ -0,0 +1,18 @@
+import logging
+
+from pydantic import SecretStr
+
+from overhave.entities.auth_managers.secret_mixin import AdminSecretMixin
+from overhave.storage import SystemUserModel
+
+logger = logging.getLogger(__name__)
+
+
+class DefaultAdminAuthorizationManager(AdminSecretMixin):
+    """Class for user auth_managers.
+
+    Manager authorize users with registered credentials.
+    """
+
+    def authorize_user(self, username: str, password: SecretStr) -> SystemUserModel | None:
+        return self._system_user_storage.get_user_by_credits(login=username, password=password)
