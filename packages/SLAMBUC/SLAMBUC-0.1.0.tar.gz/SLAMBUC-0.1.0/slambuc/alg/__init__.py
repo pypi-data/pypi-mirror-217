764e92b573a293377b799caf581300fc973efe06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:06 +0000

    Add package SLAMBUC/0.1.0/SLAMBUC-0.1.0.tar.gz

---
+++ packages/SLAMBUC/SLAMBUC-0.1.0.tar.gz/SLAMBUC-0.1.0/slambuc/alg/__init__.py
@@ -0,0 +1,23 @@
+# Copyright 2023 Janos Czentye
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Constants for attribute indices in DP matrix
+import math
+
+BARR, COST, LAT = 0, 1, 2
+# Constant for block cache index
+MEM, CPU = 0, 3
+# Constant for ILP model names
+LP_LAT = 'C_LAT'
+# Values of infeasible solution [partitioning, opt_cost, opt_lat]
+INFEASIBLE = ([], math.inf, None)
