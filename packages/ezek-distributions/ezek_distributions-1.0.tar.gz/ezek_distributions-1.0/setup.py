c98738c59c52684de191064637e05c86b9a0ca23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:41 +0000

    Add package ezek-distributions/1.0/ezek_distributions-1.0.tar.gz

---
+++ packages/ezek-distributions/ezek_distributions-1.0.tar.gz/ezek_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ezek_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ezek_distributions'],
+      author = 'Ezekiel Ekanem',
+      author_email = "ezekiel.een111@gmail.com",
+      zip_safe=False)
