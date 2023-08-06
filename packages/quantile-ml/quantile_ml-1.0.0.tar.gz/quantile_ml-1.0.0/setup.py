5ce94250b532b837294d619f6d51e214ffc150fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:49 +0000

    Add package quantile-ml/1.0.0/quantile_ml-1.0.0.tar.gz

---
+++ packages/quantile-ml/quantile_ml-1.0.0.tar.gz/quantile_ml-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='quantile_ml', #package name
+      version='1.0.0',
+      description='Python Package By Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False
+      )
