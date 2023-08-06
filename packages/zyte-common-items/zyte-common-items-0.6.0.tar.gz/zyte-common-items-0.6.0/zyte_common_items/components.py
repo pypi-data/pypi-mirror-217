e222bac3089189ce4312d4a4fbffa6fcfeaea1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:51 +0000

    Add package zyte-common-items/0.6.0/zyte-common-items-0.6.0.tar.gz

---
+++ packages/zyte-common-items/zyte-common-items-0.6.0.tar.gz/zyte-common-items-0.6.0/zyte_common_items/components.py
@@ -0,0 +1,333 @@
+"""Classes for data nested within items."""
+from typing import Optional
+
+import attrs
+
+from zyte_common_items.base import Item
+from zyte_common_items.util import url_to_str
+
+
+@attrs.define
+class _Media(Item):
+    #: URL.
+    #:
+    #: When multiple URLs exist for a given media element, pointing to
+    #: different-quality versions, the highest-quality URL should be used.
+    #:
+    #: `Data URIs`_ are not allowed in this attribute.
+    #:
+    #: .. _Data URIs: https://en.wikipedia.org/wiki/Data_URI_scheme
+    url: str = attrs.field(converter=url_to_str)
+
+
+@attrs.define
+class AdditionalProperty(Item):
+    """A name-value pair.
+
+    See :attr:`Product.additionalProperties
+    <zyte_common_items.Product.additionalProperties>`.
+    """
+
+    #: Name.
+    name: str
+
+    #: Value.
+    value: str
+
+
+@attrs.define(kw_only=True)
+class AggregateRating(Item):
+    """Aggregate data about reviews and ratings.
+
+    At least one of :attr:`ratingValue` or :attr:`reviewCount` is required.
+
+    See :attr:`Product.aggregateRating
+    <zyte_common_items.Product.aggregateRating>`.
+    """
+
+    #: Maximum value of the rating system.
+    bestRating: Optional[float] = None
+
+    #: Average value of all ratings.
+    ratingValue: Optional[float] = None
+
+    #: Review count.
+    reviewCount: Optional[int] = None
+
+
+@attrs.define
+class Audio(_Media):
+    """Audio.
+
+    See :class:`Article.audios <zyte_common_items.Article.audios>`.
+    """
+
+
+@attrs.define(kw_only=True)
+class Author(Item):
+    """Author of an article.
+
+    See :attr:`Article.authors <zyte_common_items.Article.authors>`.
+    """
+
+    #: Email.
+    email: Optional[str] = None
+
+    #: URL of the details page of the author.
+    url: Optional[str] = attrs.field(
+        default=None, converter=attrs.converters.optional(url_to_str), kw_only=True
+    )
+
+    #: Full name.
+    name: Optional[str] = None
+
+    #: Text from which :attr:`~zyte_common_items.Author.name` was
+    #: extracted.
+    nameRaw: Optional[str] = None
+
+
+@attrs.define
+class Brand(Item):
+    """Brand.
+
+    See :attr:`Product.brand <zyte_common_items.Product.brand>`.
+    """
+
+    #: Name as it appears on the source webpage (no post-processing).
+    name: str
+
+
+@attrs.define(kw_only=True)
+class Breadcrumb(Item):
+    """A breadcrumb from the `breadcrumb trail`_ of a webpage.
+
+    See :attr:`Product.breadcrumbs
+    <zyte_common_items.Product.breadcrumbs>`.
+
+    .. _breadcrumb trail: https://en.wikipedia.org/wiki/Breadcrumb_navigation
+    """
+
+    #: Displayed name.
+    name: Optional[str] = None
+
+    #: Target URL.
+    url: Optional[str] = attrs.field(
+        default=None, converter=attrs.converters.optional(url_to_str), kw_only=True
+    )
+
+
+@attrs.define
+class Gtin(Item):
+    """GTIN_ type-value pair.
+
+    See :class:`Product.gtin <zyte_common_items.Product.gtin>`.
+
+    .. _GTIN: https://en.wikipedia.org/wiki/Global_Trade_Item_Number
+    """
+
+    #: Identifier of the GTIN format of ``value``.
+    #:
+    #: One of: ``"gtin13"``, ``"gtin8"``, ``"gtin14"``, ``"isbn10"``,
+    #: ``"isbn13"``, ``"ismn"``, ``"issn"``, ``"upc"``.
+    type: str
+
+    #: Value.
+    #:
+    #: It should only contain digits.
+    value: str
+
+
+@attrs.define
+class Image(_Media):
+    """Image.
+
+    See for example
+    :class:`Product.images <zyte_common_items.Product.images>` and
+    :class:`Product.mainImage <zyte_common_items.Product.mainImage>`.
+    """
+
+
+@attrs.define(kw_only=True)
+class Link(Item):
+    """A link from a webpage to another webpage."""
+
+    #: Displayed text.
+    text: Optional[str] = None
+
+    #: Target URL.
+    url: Optional[str] = attrs.field(
+        default=None, converter=attrs.converters.optional(url_to_str), kw_only=True
+    )
+
+
+@attrs.define(kw_only=True)
+class NamedLink(Item):
+    """A link from a webpage to another webpage."""
+
+    #: The name of the link.
+    name: Optional[str] = None
+
+    #: Target URL.
+    url: Optional[str] = attrs.field(
+        default=None, converter=attrs.converters.optional(url_to_str), kw_only=True
+    )
+
+
+@attrs.define(kw_only=True)
+class DateDownloadedMetadata(Item):
+    """Data extraction process metadata that only indicates the download date.
+
+    See
+    :class:`ArticleList.metadata <zyte_common_items.ArticleList.metadata>`.
+    """
+
+    #: Date and time when the product data was downloaded, in UTC timezone and
+    #: the following format: ``YYYY-MM-DDThh:mm:ssZ``.
+    dateDownloaded: Optional[str] = None
+
+
+@attrs.define(kw_only=True)
+class Metadata(DateDownloadedMetadata):
+    #: The probability (0 for 0%, 1 for 100%) that the webpage features the
+    #: requested data type.
+    #:
+    #: For example, if the extraction of a product from a given URL is
+    #: requested, and that URL points to the webpage of a product with complete
+    #: certainty, the value should be `1`. If with complete certainty the
+    #: webpage features a job listing instead of a product, the value should be
+    #: `0`. When there is no complete certainty, the value could be anything in
+    #: between (e.g. `0.96`).
+    probability: Optional[float] = None
+
+
+@attrs.define(kw_only=True)
+class BusinessPlaceMetadata(Metadata):
+    """Data extraction process metadata.
+
+    See :class:`BusinessPlace.metadata <zyte_common_items.BusinessPlace.metadata>`.
+    """
+
+    #: The search text the place was found with.
+    searchText: Optional[str] = None
+
+
+@attrs.define(kw_only=True)
+class Address(Item):
+    """Address item."""
+
+    #: The raw address information, as it appears on the website.
+    addressRaw: Optional[str] = None
+
+    #: The street address of the place.
+    streetAddress: Optional[str] = None
+
+    #: The city the place is located in.
+    addressCity: Optional[str] = None
+
+    #: The locality to which the place belongs.
+    addressLocality: Optional[str] = None
+
+    #: The region of the place.
+    addressRegion: Optional[str] = None
+
+    #: The country the place is located in.
+    #:
+    #: The country name or the `ISO 3166-1 alpha-2 country code
+    #: <https://en.wikipedia.org/wiki/ISO_3166-1>`__.
+    addressCountry: Optional[str] = None
+
+    #: The postal code of the address.
+    postalCode: Optional[str] = None
+
+    #: The auxiliary part of the postal code.
+    #:
+    #: It may include a state abbreviation or town name, depending on local standards.
+    postalCodeAux: Optional[str] = None
+
+    #: Geographical latitude of the place.
+    latitude: Optional[float] = None
+
+    #: Geographical longitude of the place.
+    longitude: Optional[float] = None
+
+
+@attrs.define(kw_only=True)
+class Amenity(Item):
+    """An amenity that a business place has"""
+
+    #: Name of amenity.
+    name: str
+
+    #: Availability of the amenity.
+    value: bool
+
+
+@attrs.define(kw_only=True)
+class StarRating(Item):
+    """Official star rating of a place."""
+
+    #: Star rating of the place, as it appears on the page, without processing.
+    raw: Optional[str] = None
+
+    #: Star rating value of the place.
+    ratingValue: Optional[float] = None
+
+
+@attrs.define(kw_only=True)
+class ParentPlace(Item):
+    """If the place is located inside another place, these are the details of the parent place."""
+
+    #: Name of the parent place.
+    name: str
+
+    #: Identifier of the parent place.
+    placeId: str
+
+
+@attrs.define(kw_only=True)
+class OpeningHoursItem(Item):
+    """Specification of opening hours of a business place."""
+
+    #: English weekday name.
+    dayOfWeek: Optional[str] = None
+
+    #: Opening time in ISO 8601 format, local time.
+    opens: Optional[str] = None
+
+    #: Closing time in ISO 8601 format, local time.
+    closes: Optional[str] = None
+
+    #: Day of the week, as it appears on the page, without processing.
+    rawDayOfWeek: Optional[str] = None
+
+    #: Opening time, as it appears on the page, without processing.
+    rawOpens: Optional[str] = None
+
+    #: Closing time, as it appears on the page, without processing.
+    rawCloses: Optional[str] = None
+
+
+@attrs.define(kw_only=True)
+class RealEstateArea(Item):
+    """Area of a place, with type, units, value and raw value."""
+
+    #: Area
+    value: float
+
+    #: Unit of the value field, one of: SQMT (square meters), SQFT (square
+    #: feet), ACRE (acres).
+    unitCode: str
+
+    #: Type of area, one of: LOT, FLOOR
+    areaType: Optional[str] = None
+
+    #: Area in the raw format, as it appears on the website.
+    raw: str
+
+
+@attrs.define
+class Video(_Media):
+    """Video.
+
+    See :class:`Article.videos <zyte_common_items.Article.videos>`.
+    """
---
+++ packages/zyte-common-items/zyte-common-items-0.6.0.tar.gz/zyte-common-items-0.6.0/zyte_common_items/pages.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+
+import attrs
+from web_poet import ItemPage, RequestUrl, Returns, WebPage, field
+from web_poet.pages import ItemT
+
+from .components import Metadata
+from .items import Article, ArticleList, BusinessPlace, Product, ProductList, RealEstate
+from .util import format_datetime
+
+
+def _date_downloaded_now():
+    return format_datetime(datetime.utcnow())
+
+
+class _BasePage(ItemPage[ItemT]):
+    @field
+    def metadata(self) -> Metadata:
+        return Metadata(
+            dateDownloaded=_date_downloaded_now(),
+            probability=1.0,
+        )
+
+    def no_item_found(self) -> ItemT:
+        """Return an item with the current url and probability=0,
+        indicating that the passed URL doesn't contain the expected item.
+
+        Use it in your .validate_input implementation.
+        """
+        return self.item_cls(  # type: ignore
+            url=self.url,  # type: ignore[attr-defined]
+            metadata=Metadata(probability=0, dateDownloaded=_date_downloaded_now()),
+        )
+
+
+@attrs.define
+class BasePage(_BasePage):
+    request_url: RequestUrl
+
+    @field
+    def url(self) -> str:
+        return str(self.request_url)
+
+
+class BaseArticlePage(BasePage, Returns[Article]):
+    pass
+
+
+class BaseArticleListPage(BasePage, Returns[ArticleList]):
+    pass
+
+
+class BaseBusinessPlacePage(BasePage, Returns[BusinessPlace]):
+    pass
+
+
+class BaseProductPage(BasePage, Returns[Product]):
+    pass
+
+
+class BaseProductListPage(BasePage, Returns[ProductList]):
+    pass
+
+
+class BaseRealEstatePage(BasePage, Returns[RealEstate]):
+    pass
+
+
+@attrs.define
+class Page(_BasePage, WebPage):
+    @field
+    def url(self) -> str:
+        return str(self.response.url)
+
+
+class ArticlePage(Page, Returns[Article]):
+    pass
+
+
+class ArticleListPage(Page, Returns[ArticleList]):
+    pass
+
+
+class BusinessPlacePage(Page, Returns[BusinessPlace]):
+    pass
+
+
+class ProductPage(Page, Returns[Product]):
+    pass
+
+
+class ProductListPage(Page, Returns[ProductList]):
+    pass
+
+
+class RealEstatePage(Page, Returns[RealEstate]):
+    pass
