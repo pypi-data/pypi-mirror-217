cb214ac8a0b20986f9088ff2ed0562302d0d5757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:24 +0000

    Add package zlx7.6/0.0.1/zlx7.6-0.0.1.tar.gz

---
+++ packages/zlx7.6/zlx7.6-0.0.1.tar.gz/zlx7.6-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zlx7.6", # Replace with your own username
+    version="0.0.1",
+    author="Zlx-0429",
+    author_email="1533340709@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/zqy0315/pypi-test",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
