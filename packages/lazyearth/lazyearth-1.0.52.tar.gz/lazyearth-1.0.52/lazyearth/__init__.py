4e180f6d1f118e173d1b5062403774b40319d47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:27 +0000

    Add package lazyearth/1.0.52/lazyearth-1.0.52.tar.gz

---
+++ packages/lazyearth/lazyearth-1.0.52.tar.gz/lazyearth-1.0.52/lazyearth/__init__.py
@@ -0,0 +1,11 @@
+from lazyearth.common import objearth
+from lazyearth.remote_sensing_index import index
+from lazyearth.virtual import *
+from lazyearth.water import water
+# from lazy.cloud import cloud
+
+__author__  = 'tul kedsaro'
+__email__   = 'Tun.k@ku.th'
+__version__ = '1.0.52'
+
+print(f'Lazyearth version : {__version__} Krub')
