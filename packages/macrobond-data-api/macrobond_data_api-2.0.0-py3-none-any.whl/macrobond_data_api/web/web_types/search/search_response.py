c5dd25296f8e256b45279f686f18503e1e6ac029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:14 +0000

    Add package macrobond-data-api/2.0.0/macrobond_data_api-2.0.0-py3-none-any.whl

---
+++ packages/macrobond-data-api/macrobond_data_api-2.0.0-py3-none-any.whl/macrobond_data_api/web/web_types/search/search_response.py
@@ -0,0 +1,12 @@
+from typing import Any, Optional, List, Dict
+from typing_extensions import TypedDict
+
+
+class SearchResponse(TypedDict):
+    """Entity search response"""
+
+    isTruncated: Optional[bool]
+    """If True, the search response is truncated"""
+
+    results: List[Dict[str, Any]]
+    """The matched entites"""
