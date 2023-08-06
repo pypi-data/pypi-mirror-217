9fbc0d741817465f03e3b97e3094507c7a968646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:43 +0000

    Add package junifer/0.0.3.dev131/junifer-0.0.3.dev131-py3-none-any.whl

---
+++ packages/junifer/junifer-0.0.3.dev131-py3-none-any.whl/junifer/__init__.py
@@ -0,0 +1,21 @@
+"""Provide imports for junifer package."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+from . import (
+    api,
+    configs,
+    data,
+    datagrabber,
+    datareader,
+    markers,
+    pipeline,
+    preprocess,
+    stats,
+    storage,
+    utils,
+    external,
+)
+from ._version import __version__
