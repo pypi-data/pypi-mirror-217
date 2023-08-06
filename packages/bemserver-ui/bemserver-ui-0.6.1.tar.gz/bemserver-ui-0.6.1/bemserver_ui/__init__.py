bf2702eb314d9bd62fc1bb733c211ac5e37e1cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:26 +0000

    Add package bemserver-ui/0.6.1/bemserver-ui-0.6.1.tar.gz

---
+++ packages/bemserver-ui/bemserver-ui-0.6.1.tar.gz/bemserver-ui-0.6.1/bemserver_ui/__init__.py
@@ -0,0 +1,26 @@
+"""BEMServer UI"""
+import flask
+
+from . import extensions
+from . import internal_api
+from . import views
+
+
+__version__ = "0.6.1"
+
+
+def create_app(config_override=None):
+    """Create application"""
+    app = flask.Flask(__name__)
+    app.config.from_object("bemserver_ui.settings.Config")
+    app.config.from_envvar("BEMSERVER_UI_SETTINGS_FILE", silent=True)
+    app.config.from_object(config_override)
+
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
+
+    extensions.init_app(app)
+    internal_api.init_app(app)
+    views.init_app(app)
+
+    return app
