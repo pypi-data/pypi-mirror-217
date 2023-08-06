891821bd636d0609c14194810426ee34d82df358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:19 +0000

    Add package discord-ansi/0.0/discord-ansi-0.0.tar.gz

---
+++ packages/discord-ansi/discord-ansi-0.0.tar.gz/discord-ansi-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='discord-ansi',
+      version='0.0',
+      description='Discord coloured messages!',
+      packages=['discord_ansi'],
+      author_email='geoegii200155@gmail.com',
+      zip_safe=False,
+      install_requires=[
+            "setuptools"
+      ])
