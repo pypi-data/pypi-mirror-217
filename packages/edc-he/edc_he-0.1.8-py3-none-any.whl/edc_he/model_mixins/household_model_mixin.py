d49515b498a86510da4eadb399e16bab77579493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:01 +0000

    Add package edc-he/0.1.8/edc_he-0.1.8-py3-none-any.whl

---
+++ packages/edc-he/edc_he-0.1.8-py3-none-any.whl/edc_he/model_mixins/household_model_mixin.py
@@ -0,0 +1,20 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
+class HouseholdModelMixin(models.Model):
+    hh_count = models.IntegerField(
+        verbose_name="What is the total number of people who live in your household?",
+        validators=[MinValueValidator(1), MaxValueValidator(25)],
+    )
+
+    hh_minors_count = models.IntegerField(
+        verbose_name=(
+            "What is the total number of people 14 years or under "
+            "who live in your household?"
+        ),
+        validators=[MinValueValidator(0), MaxValueValidator(25)],
+    )
+
+    class Meta:
+        abstract = True
