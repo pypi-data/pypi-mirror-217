3f2ee5e37941e6389ea1af5e2e75fe92377ff8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:43 +0000

    Add package mads-datasets/0.1.6/mads_datasets-0.1.6.tar.gz

---
+++ packages/mads-datasets/mads_datasets-0.1.6.tar.gz/mads_datasets-0.1.6/mads_datasets/__init__.py
@@ -0,0 +1,6 @@
+from mads_datasets.datasetfactory import DatasetFactoryProvider
+from mads_datasets.settings import DatasetType
+
+__all__ = ["DatasetFactoryProvider", "DatasetType"]
+
+__version__ = "0.1.6"
