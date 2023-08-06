ed2ec89828061f4df82da8d34b407bfe181a2dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:35 +0000

    Add package ibis-framework/6.0.0/ibis_framework-6.0.0-py3-none-any.whl

---
+++ packages/ibis-framework/ibis_framework-6.0.0-py3-none-any.whl/ibis/backends/tests/test_binary.py
@@ -0,0 +1,39 @@
+import contextlib
+
+import pytest
+import sqlalchemy.exc
+
+import ibis
+import ibis.common.exceptions as com
+
+BINARY_BACKEND_TYPES = {
+    'bigquery': "BYTES",
+    'clickhouse': 'String',
+    'duckdb': 'BLOB',
+    'snowflake': "BINARY",
+    'sqlite': "blob",
+    'trino': 'STRING',
+    "postgres": "bytea",
+}
+
+pytestmark = pytest.mark.broken(["druid"], raises=AssertionError)
+
+
+@pytest.mark.broken(
+    ['trino'],
+    "(builtins.AttributeError) 'bytes' object has no attribute 'encode'",
+    raises=sqlalchemy.exc.StatementError,
+)
+@pytest.mark.broken(
+    ['clickhouse', 'impala'],
+    "Unsupported type: Binary(nullable=True)",
+    raises=NotImplementedError,
+)
+def test_binary_literal(con, backend):
+    expr = ibis.literal(b"A")
+    result = con.execute(expr)
+    assert result == b"A"
+
+    with contextlib.suppress(com.OperationNotDefinedError):
+        backend_name = backend.name()
+        assert con.execute(expr.typeof()) == BINARY_BACKEND_TYPES[backend_name]
