ed5058d98cebc744c9016d82db8266c781d8664a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:41 +0000

    Add package bfieldtools/0.11.3/bfieldtools-0.11.3.tar.gz

---
+++ packages/bfieldtools/bfieldtools-0.11.3.tar.gz/bfieldtools-0.11.3/tests/test_utils.py
@@ -0,0 +1,29 @@
+from bfieldtools import utils
+
+import numpy as np
+
+import pytest
+
+
+def test_mesh_utils():
+
+    mesh1 = utils.load_example_mesh("unit_disc")
+    mesh2 = utils.load_example_mesh("10x10_plane")
+
+    mesh2.vertices += np.array([[0, 0, 20]])
+
+    mesh3 = utils.combine_meshes((mesh1, mesh2))
+
+    assert len(mesh3.vertices) == len(mesh1.vertices) + len(mesh2.vertices)
+
+
+@pytest.mark.xfail
+def test_load_mesh_fail():
+    mesh = utils.load_example_mesh("this mesh does not exist")
+
+
+def test_cylinder_points():
+
+    cp = utils.cylinder_points()
+    cp = utils.cylinder_points(alpha=90)
+    cp = utils.cylinder_points(orientation=np.array([0, 1, 0]))
