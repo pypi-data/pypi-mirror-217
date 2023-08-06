96f09a6e660ebb33bb9d1cb4be482ae98e96cc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:14 +0000

    Add package codedapertures/0.1.1/codedapertures-0.1.1.tar.gz

---
+++ packages/codedapertures/codedapertures-0.1.1.tar.gz/codedapertures-0.1.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(name='codedapertures',
+      version='0.1.1',
+      description='Coded Aperture Production in PYthon (CAPPY)',
+      long_description='Coded Aperture Production in PYthon (CAPPY)',
+      url='https://github.com/bpops/cappy',
+      author='bpops',
+      license='MIT',
+      install_requires=['pyprimes',
+                'numpy',
+                'matplotlib'],
+      zip_safe=False)
