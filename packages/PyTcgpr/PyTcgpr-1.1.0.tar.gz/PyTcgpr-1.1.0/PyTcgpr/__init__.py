8c41ee4cbe4ed23f6f62890d0d590a47cce92acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:11 +0000

    Add package PyTcgpr/1.1.0/PyTcgpr-1.1.0.tar.gz

---
+++ packages/PyTcgpr/PyTcgpr-1.1.0.tar.gz/PyTcgpr-1.1.0/PyTcgpr/__init__.py
@@ -0,0 +1,11 @@
+import datetime
+now = datetime.datetime.now()
+formatted_date_time = now.strftime('%Y-%m-%d %H:%M:%S')
+
+
+print('A Machine Learning Algorithms for Screening Abnormal Data and Important features of Small DataSet')
+print('TCGPR, Bin Cao, MGI, Shanghai University, Shanghai, CHINA.')
+print('Intro : https://github.com/Bin-Cao/TCGPR/blob/main/Intro/TCGPR.pdf')
+print('URL : https://github.com/Bin-Cao/TCGPR')
+print('Executed on :',formatted_date_time, ' | Have a great day.')
+print('\n')
