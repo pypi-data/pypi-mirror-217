911f6dca12418e27b144526bcffcf6b4931ea156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:32 +0000

    Add package pyoverload/1.1.28/pyoverload-1.1.28.tar.gz

---
+++ packages/pyoverload/pyoverload-1.1.28.tar.gz/pyoverload-1.1.28/pyoverload/__init__.py
@@ -0,0 +1,131 @@
+
+from pycamia import info_manager
+
+__info__ = info_manager(
+    project = 'PyCAMIA',
+    package = 'pyoverload',
+    author = 'Yuncheng Zhou',
+    create = '2021-12',
+    version = '1.1.27',
+    contact = 'bertiezhou@163.com',
+    keywords = ['overload'],
+    description = "'pyoverload' overloads the functions by simply using typehints and adding decorator '@overload'. ",
+    requires = ['pycamia'],
+    update = '2023-07-05 14:57:54'
+).check()
+
+from .typehint import isofsubclass, inheritable, isitertype, iterable, isarray, isdtype, isatype, isoftype, isclassmethod, typename, Type, params, Bool, Int, Float, Str, Set, List, Tuple, Dict, Class, Callable, Function, Method, Lambda, Functional, Real, real, Null, null, Sequence, sequence, Array, Iterable, Scalar, IntScalar, FloatScalar #*
+from .override import override, overload, params #*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
