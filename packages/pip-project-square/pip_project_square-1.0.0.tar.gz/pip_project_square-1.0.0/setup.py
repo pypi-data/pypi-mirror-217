366a3cd436af459548512963320da9e2a6ab54a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:49 +0000

    Add package pip-project-square/1.0.0/pip_project_square-1.0.0.tar.gz

---
+++ packages/pip-project-square/pip_project_square-1.0.0.tar.gz/pip_project_square-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pip_project_square',
+    version='1.0.0',
+    description='A project that calculates the square of a number',
+    packages=['pip_project_square'],
+    author='Jalla Vidya Sagar',
+    author_email='jallavidya.sagar@icloud.com',
+    install_requires=[],
+)
+
