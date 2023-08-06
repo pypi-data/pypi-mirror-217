5b6e458785b71e87ff0f30447e5514f32752e654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:33 +0000

    Add package viviLibrary/1.1.1/viviLibrary-1.1.1.tar.gz

---
+++ packages/viviLibrary/viviLibrary-1.1.1.tar.gz/viviLibrary-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='viviLibrary',
+    version='1.1.1',
+    author='test user',
+    description='A library for basic math operations',
+    packages=find_packages()
+    #install_requires
+)
