4feba870ba8fc1463474ea7fb2a8f16e8afb2943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:47 +0000

    Add package myFirstLibrary/1.0.0/myFirstLibrary-1.0.0.tar.gz

---
+++ packages/myFirstLibrary/myFirstLibrary-1.0.0.tar.gz/myFirstLibrary-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='myFirstLibrary',
+    version='1.0.0',
+    author='test user',
+    description='A library for basic math operations',
+    packages=find_packages()
+    #install_requires
+)
