e8866b88d76962f8a98ef5e13f386ab7d924e8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:46 +0000

    Add package offat/0.1.0/offat-0.1.0.tar.gz

---
+++ packages/offat/offat-0.1.0.tar.gz/offat-0.1.0/offat/openapi.py
@@ -0,0 +1,35 @@
+from prance import ResolvingParser
+from .logger import create_logger
+
+
+logger = create_logger(__name__)
+
+
+class OpenAPIParser:
+    ''''''
+    def __init__(self, fpath:str) -> None:
+        self._parser = ResolvingParser(fpath, backend = 'openapi-spec-validator')
+        if self._parser.valid:
+            logger.info('Specification file is valid')
+        else:
+            logger.error('Specification file is invalid!')
+
+        self._spec = self._parser.specification
+        self.host = self._spec.get('host')
+        self.base_url = f"http://{self.host}{self._spec.get('basePath','')}"
+
+
+    def _get_endpoints(self):
+        '''Returns list of endpoint paths along with HTTP methods allowed'''
+        endpoints = []
+
+        for endpoint in self._spec.get('paths', {}).keys():
+            methods = list(self._spec['paths'][endpoint].keys())
+            if 'parameters' in methods:
+                methods.remove('parameters')
+            endpoints.append((endpoint, methods))
+
+        return endpoints
+
+    
+
