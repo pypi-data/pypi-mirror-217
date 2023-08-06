9a35807ba41360a6cb81ddb2ad2e0d951029508c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:11 +0000

    Add package mypermobil/0.1.3/mypermobil-0.1.3.tar.gz

---
+++ packages/mypermobil/mypermobil-0.1.3.tar.gz/mypermobil-0.1.3/mypermobil/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.3"
+
+from mypermobil.mypermobil import (
+    MyPermobil,
+    create_session,
+    MyPermobilAPIException,
+    MyPermobilClientException,
+)
+from mypermobil.const import *
