c895f2479c0e11607f0c687de419ab4a4f469fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:50 +0000

    Add package oenv2config/1.2.1/oenv2config-1.2.1.tar.gz

---
+++ packages/oenv2config/oenv2config-1.2.1.tar.gz/oenv2config-1.2.1/tests/test_section/test_misc_section.py
@@ -0,0 +1,45 @@
+import unittest
+
+from odoo_env_config.api import Env
+from odoo_env_config.section import MiscSection
+
+
+class TestMiscSection(unittest.TestCase):
+    def test_default(self):
+        conf = MiscSection()
+        self.assertFalse(conf.unaccent)
+        self.assertEqual([], conf.without_demo)
+        self.assertFalse(conf.stop_after_init)
+        self.assertFalse(conf.save_config_file)
+
+        flags = conf.to_values()
+        self.assertFalse(flags)
+
+    def test_global(self):
+        conf = MiscSection().init(
+            Env(
+                {
+                    "UNACCENT": str(True),
+                    "WITHOUT_DEMO": "all,account",
+                    "STOP_AFTER_INIT": str(True),
+                    "SAVE_CONFIG_FILE": str(True),
+                }
+            )
+        )
+        self.assertTrue(conf.unaccent)
+        self.assertListEqual(["all", "account"], conf.without_demo)
+        self.assertTrue(conf.stop_after_init)
+        self.assertTrue(conf.save_config_file)
+
+        flags = conf.to_values()
+        self.assertIn("unaccent", flags)
+        self.assertTrue(flags["unaccent"])
+
+        self.assertIn("without-demo", flags)
+        self.assertEqual("all,account", flags["without-demo"])
+
+        self.assertIn("save", flags)
+        self.assertTrue(flags["save"])
+
+        self.assertIn("stop-after-init", flags)
+        self.assertTrue(flags["stop-after-init"])
