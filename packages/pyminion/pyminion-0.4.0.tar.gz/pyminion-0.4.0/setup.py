e2594a2d603554a30e6d8a159b83ffcdee6fe184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:38 +0000

    Add package pyminion/0.4.0/pyminion-0.4.0.tar.gz

---
+++ packages/pyminion/pyminion-0.4.0.tar.gz/pyminion-0.4.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyminion",
+    version="0.4.0",
+    author="Evan Slack",
+    author_email="evan.slack@outlook.com",
+    description="Dominion but make it python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/evanofslack/pyminion",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(
+        exclude=(
+            "tests",
+            "tests.*",
+            "examples",
+            "examples.*",
+        )
+    ),
+    python_requires=">=3.8",
+)
