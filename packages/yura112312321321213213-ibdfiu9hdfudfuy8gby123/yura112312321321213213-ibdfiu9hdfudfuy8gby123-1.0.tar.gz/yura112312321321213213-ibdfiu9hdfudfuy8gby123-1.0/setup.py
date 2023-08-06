92a85dd0d52252af83c38a467304456b18c8b2c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:04 +0000

    Add package yura112312321321213213-ibdfiu9hdfudfuy8gby123/1.0/yura112312321321213213-ibdfiu9hdfudfuy8gby123-1.0.tar.gz

---
+++ packages/yura112312321321213213-ibdfiu9hdfudfuy8gby123/yura112312321321213213-ibdfiu9hdfudfuy8gby123-1.0.tar.gz/yura112312321321213213-ibdfiu9hdfudfuy8gby123-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='yura112312321321213213-ibdfiu9hdfudfuy8gby123',
+      version='1.0',
+      description='Мій проєкт',
+      author='Yurii',
+      author_email='123@gmail.com',
+      packages=['project'],
+     )
