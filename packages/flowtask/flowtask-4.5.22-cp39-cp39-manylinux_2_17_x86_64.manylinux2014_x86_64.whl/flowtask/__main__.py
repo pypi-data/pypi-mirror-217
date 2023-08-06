6f77178948878a11f961442db7b31526cf8f046c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:24 +0000

    Add package flowtask/4.5.22/flowtask-4.5.22-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/flowtask/flowtask-4.5.22-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/flowtask/__main__.py
@@ -0,0 +1,45 @@
+"""Data Integration Executor."""
+import logging
+import asyncio
+from flowtask.runner import TaskRunner
+from flowtask.utils import cPrint
+
+
+def install_uvloop():
+    try:
+        import uvloop
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+        uvloop.install()
+    except ImportError:
+        pass
+
+async def task(loop):
+    runner = None
+    try:
+        runner = TaskRunner(loop=loop)
+        async with runner as job:
+            if await job.start():
+                await job.run()
+    except Exception as e:  # pylint: disable=W0718
+        logging.exception(e, stack_info=True)
+    finally:
+        return runner
+
+def main():
+    install_uvloop()
+    loop = asyncio.get_event_loop()
+    loop.slow_callback_duration = 0.5  # Set threshold to 0.5 seconds
+    try:
+        result = loop.run_until_complete(
+            task(loop)
+        )
+        if result:
+            cPrint(' === RESULT === ', level="DEBUG")
+            print(result.result)
+            cPrint('== Task stats === ', level="INFO")
+            print(result.stats)
+    finally:
+        loop.close()
+
+if __name__ == '__main__':
+    main()
