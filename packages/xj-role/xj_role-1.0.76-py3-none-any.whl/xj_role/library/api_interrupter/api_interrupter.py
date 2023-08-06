18495c1507c23414f0394a29e5ed2107c786bc48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:09 +0000

    Add package xj-role/1.0.76/xj_role-1.0.76-py3-none-any.whl

---
+++ packages/xj-role/xj_role-1.0.76-py3-none-any.whl/xj_role/library/api_interrupter/api_interrupter.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+"""
+@project: djangoModel->api_interrupter
+@author: 孙楷炎
+@Email: sky4834@163.com
+@synopsis: API组短期
+@created_time: 2023/7/5 10:30
+"""
+
+
+class APIInterrupter():
+    """API接口阻断器"""
+
+    def open_api(self):
+        """获取open API"""
+        pass
+
+    def get_role_permission(self, user_id=None):
+        """获取角色的路由"""
+        pass
+
+    def match_apis(self):
+        pass
