901b43a87bb303decb5428496d9becfb124db854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:08 +0000

    Add package edc-sites/0.3.21/edc-sites-0.3.21.tar.gz

---
+++ packages/edc-sites/edc-sites-0.3.21.tar.gz/edc-sites-0.3.21/edc_sites/tests/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from ..models import CurrentSiteManager, SiteModelMixin
+
+
+class TestModelWithSite(SiteModelMixin, models.Model):
+    f1 = models.CharField(max_length=10, default="1")
+
+    on_site = CurrentSiteManager()
+
+    objects = models.Manager()
