94ddb5ee3ea4a0fc392d0fa483b868bf6c4aaf21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:59 +0000

    Add package rdf-doctor/0.8.5/rdf_doctor-0.8.5-py3-none-any.whl

---
+++ packages/rdf-doctor/rdf_doctor-0.8.5-py3-none-any.whl/doctor/consts.py
@@ -0,0 +1,24 @@
+VERSION_FILE = "__init__.py"
+
+# Report export format
+REPORT_FORMAT_SHEX = "shex"
+REPORT_FORMAT_MD = "md"               # same as markdown
+REPORT_FORMAT_MARKDOWN = "markdown"   # same as md
+
+# Target classes
+TARGET_CLASS_ALL = "all"
+
+# Input file extensions
+EXTENSION_NT = ".nt"
+EXTENSION_TTL = ".ttl"
+EXTENSION_GZ = ".gz"
+
+# Correct prefix list file path
+CORRECT_PREFIXES_FILE_PATH = "reference/correct-prefixes.tsv"
+
+# Errata
+CLASS_ERRATA_FILE_PATH = "reference/refine-classes.tsv"
+PREFIX_ERRATA_FILE_PATH = "reference/refine-prefixes.tsv"
+
+# Help link URL
+HELP_LINK_URL = "https://github.com/dbcls/rdf-doctor#output-description"
