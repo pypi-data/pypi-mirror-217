9cf7876644b35afee7947275fbf5336e2c3fe93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:21 +0000

    Add package libros/1.0.0/libros-1.0.0.tar.gz

---
+++ packages/libros/libros-1.0.0.tar.gz/libros-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='libros',
+    version='1.0.0',
+    author='itzel franco ',
+    author_email='itzel_fd1@tesch.edu.mx',
+    description='busqueda de libros por medio de isbn',
+    install_requires=[
+        'pandas',
+        'tkinter'
+    ],
+)
+
