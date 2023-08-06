7b4ab0d3a47de2d97de5673b0166446f7f79815d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:47 +0000

    Add package chai-guanaco/1.0.1/chai-guanaco-1.0.1.tar.gz

---
+++ packages/chai-guanaco/chai-guanaco-1.0.1.tar.gz/chai-guanaco-1.0.1/src/chai_guanaco/__init__.py
@@ -0,0 +1,8 @@
+from .submit import (
+    ModelSubmitter,
+    display_leaderboard,
+    deactivate_model,
+    get_my_submissions
+)
+from .feedback import get_feedback
+from .login_cli import developer_login
