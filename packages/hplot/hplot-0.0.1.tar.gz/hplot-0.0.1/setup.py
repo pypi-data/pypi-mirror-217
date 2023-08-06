f78947ebfa7a5b6dd28f5aafa09f210439b52dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:06 +0000

    Add package hplot/0.0.1/hplot-0.0.1.tar.gz

---
+++ packages/hplot/hplot-0.0.1.tar.gz/hplot-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup
+
+
+setup(
+    name="hplot",
+    version="0.0.1",
+    description="Some plot utils for research",
+    author="AeroH",
+    author_email='zhang.yuh@outlook.com', 
+    install_requires=[
+        "numpy==1.23.1",
+        "pytest==7.1.2",
+        "matplotlib==3.5.1",
+        "pandas==1.4.2",
+        "scipy==1.9.0",
+        "seaborn==0.11.2",
+        "wrapt==1.14.1",
+        "tabulate==0.8.10",
+        "psutil==5.9.1",
+        "tqdm"
+    ],
+    python_requires=">=3.8",
+)
