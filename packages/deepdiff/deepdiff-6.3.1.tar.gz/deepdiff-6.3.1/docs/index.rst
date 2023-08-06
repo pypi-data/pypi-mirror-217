2966436cbcf92ec330412bc83bd7505674a04497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:39 +0000

    Add package deepdiff/6.3.1/deepdiff-6.3.1.tar.gz

---
+++ packages/deepdiff/deepdiff-6.3.1.tar.gz/deepdiff-6.3.1/docs/index.rst
@@ -0,0 +1,162 @@
+.. DeepDiff documentation master file, created by
+   sphinx-quickstart on Mon Jul 20 06:06:44 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+
+DeepDiff 6.3.1 documentation!
+=============================
+
+*******
+Modules
+*******
+
+The DeepDiff library includes the following modules:
+
+- **DeepDiff** For Deep Difference of 2 objects. :doc:`/diff`
+
+    It return the deep Difference of python objects. It can also be used to take the distance between objects. :doc:`/deep_distance`
+
+- **DeepSearch** Search for objects within other objects. :doc:`/dsearch`
+
+- **DeepHash** Hash any object based on their content even if they are not "hashable" in Python's eyes.  :doc:`/deephash`
+
+- **Delta** Delta of objects that can be applied to other objects. Imagine git commits but for structured data.  :doc:`/delta`
+
+- **Extract** For extracting a path from an object  :doc:`/extract`
+
+- **Commandline** Most of the above functionality is also available via the commandline module  :doc:`/commandline`
+
+***********
+What is New
+***********
+
+
+DeepDiff 6-3-1
+--------------
+
+This release includes many bug fixes.
+
+-  Bugfix deephash for paths by
+  `maggelus <https://github.com/maggelus>`__
+-  Bugfix deephash compiled regex
+  `maggelus <https://github.com/maggelus>`__
+-  Fix tests dependent on toml by
+  `martin-kokos <https://github.com/martin-kokos>`__
+-  Bugfix for ``include_paths`` for nested dictionaries by
+  `kor4ik <https://github.com/kor4ik>`__
+-  Use tomli and tomli-w for dealing with tomli files by
+  `martin-kokos <https://github.com/martin-kokos>`__
+-  Bugfix for ``datetime.date`` by `Alex
+  Sauer-Budge <https://github.com/amsb>`__
+
+
+DeepDiff 6-3-0
+--------------
+
+-  :ref:`prefix_or_suffix_operator_label`: This operator will skip strings that are
+   suffix or prefix of each other.
+-  :ref:`include_obj_callback_label` and :ref:`include_obj_callback_strict_label` are
+   added by `Håvard Thom <https://github.com/havardthom>`__.
+-  Fixed a corner case where numpy’s ``np.float32`` nans are not ignored
+   when using ``ignore_nan_equality`` by `Noam
+   Gottlieb <https://github.com/noamgot>`__
+-  ``orjson`` becomes optional again.
+-  Fix for ``ignore_type_in_groups`` with numeric values so it does not report number changes when the number types are different.
+
+*********
+Tutorials
+*********
+
+Tutorials can be found on `Zepworks blog <https://zepworks.com/tags/deepdiff/>`_
+
+************
+Installation
+************
+
+Install from PyPi::
+
+    pip install deepdiff
+
+If you want to use DeepDiff from commandline::
+
+    pip install "deepdiff[cli]"
+
+If you want to improve the performance of DeepDiff with certain processes such as json serialization::
+
+    pip install "deepdiff[optimize]"
+
+Read about DeepDiff optimizations at :ref:`optimizations_label`
+
+Importing
+~~~~~~~~~
+
+.. code:: python
+
+    >>> from deepdiff import DeepDiff  # For Deep Difference of 2 objects
+    >>> from deepdiff import grep, DeepSearch  # For finding if item exists in an object
+    >>> from deepdiff import DeepHash  # For hashing objects based on their contents
+    >>> from deepdiff import Delta  # For creating delta of objects that can be applied later to other objects.
+    >>> from deepdiff import extract  # For extracting a path from an object
+
+
+.. note:: if you want to use DeepDiff via commandline, make sure to run:: 
+    pip install "deepdiff[cli]"
+
+Then you can access the commands via:
+
+- DeepDiff
+
+.. code:: bash
+
+    $ deep diff --help
+
+- Delta
+
+.. code:: bash
+
+    $ deep patch --help
+
+- grep
+
+.. code:: bash
+
+    $ deep grep --help
+- extract
+
+.. code:: bash
+
+    $ deep extract --help
+
+
+Supported data types
+~~~~~~~~~~~~~~~~~~~~
+
+int, string, unicode, dictionary, list, tuple, set, frozenset, OrderedDict, NamedTuple, Numpy, custom objects and more!
+
+
+
+References
+==========
+
+.. toctree::
+   :maxdepth: 4
+
+   diff
+   dsearch
+   deephash
+   delta
+   extract
+   commandline
+   changelog
+   authors
+   faq
+   support
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/deepdiff/deepdiff-6.3.1.tar.gz/deepdiff-6.3.1/tests/test_operators.py
@@ -0,0 +1,242 @@
+import math
+
+from typing import List
+from deepdiff import DeepDiff
+from deepdiff.operator import BaseOperator, PrefixOrSuffixOperator
+
+
+class TestOperators:
+    def test_custom_operators_prevent_default(self):
+        t1 = {
+            "coordinates": [
+                {"x": 5, "y": 5},
+                {"x": 8, "y": 8}
+            ]
+        }
+
+        t2 = {
+            "coordinates": [
+                {"x": 6, "y": 6},
+                {"x": 88, "y": 88}
+            ]
+        }
+
+        class L2DistanceDifferWithPreventDefault(BaseOperator):
+            def __init__(self, regex_paths: List[str], distance_threshold: float):
+                super().__init__(regex_paths)
+                self.distance_threshold = distance_threshold
+
+            def _l2_distance(self, c1, c2):
+                return math.sqrt(
+                    (c1["x"] - c2["x"]) ** 2 + (c1["y"] - c2["y"]) ** 2
+                )
+
+            def give_up_diffing(self, level, diff_instance):
+                l2_distance = self._l2_distance(level.t1, level.t2)
+                if l2_distance > self.distance_threshold:
+                    diff_instance.custom_report_result('distance_too_far', level, {
+                        "l2_distance": l2_distance
+                    })
+                return True
+
+        ddiff = DeepDiff(t1, t2, custom_operators=[L2DistanceDifferWithPreventDefault(
+            ["^root\\['coordinates'\\]\\[\\d+\\]$"],
+            1
+        )])
+
+        expected = {
+            'distance_too_far': {
+                "root['coordinates'][0]": {'l2_distance': 1.4142135623730951},
+                "root['coordinates'][1]": {'l2_distance': 113.13708498984761}
+            }
+        }
+        assert expected == ddiff
+
+    def test_custom_operators_not_prevent_default(self):
+        t1 = {
+            "coordinates": [
+                {"x": 5, "y": 5},
+                {"x": 8, "y": 8}
+            ]
+        }
+
+        t2 = {
+            "coordinates": [
+                {"x": 6, "y": 6},
+                {"x": 88, "y": 88}
+            ]
+        }
+
+        class L2DistanceDifferWithPreventDefault(BaseOperator):
+            def __init__(self, regex_paths, distance_threshold):
+                super().__init__(regex_paths)
+                self.distance_threshold = distance_threshold
+
+            def _l2_distance(self, c1, c2):
+                return math.sqrt(
+                    (c1["x"] - c2["x"]) ** 2 + (c1["y"] - c2["y"]) ** 2
+                )
+
+            def give_up_diffing(self, level, diff_instance):
+                l2_distance = self._l2_distance(level.t1, level.t2)
+                if l2_distance > self.distance_threshold:
+                    diff_instance.custom_report_result('distance_too_far', level, {
+                        "l2_distance": l2_distance
+                    })
+                #
+                return False
+
+        ddiff = DeepDiff(t1, t2, custom_operators=[L2DistanceDifferWithPreventDefault(
+            ["^root\\['coordinates'\\]\\[\\d+\\]$"],
+            1
+        )
+        ])
+        expected = {
+            'values_changed': {
+                "root['coordinates'][0]['x']": {'new_value': 6, 'old_value': 5},
+                "root['coordinates'][0]['y']": {'new_value': 6, 'old_value': 5},
+                "root['coordinates'][1]['x']": {'new_value': 88, 'old_value': 8},
+                "root['coordinates'][1]['y']": {'new_value': 88, 'old_value': 8}
+            },
+            'distance_too_far': {
+                "root['coordinates'][0]": {'l2_distance': 1.4142135623730951},
+                "root['coordinates'][1]": {'l2_distance': 113.13708498984761}
+            }
+        }
+        assert expected == ddiff
+
+    def test_custom_operators_should_not_equal(self):
+        t1 = {
+            "id": 5,
+            "expect_change_pos": 10,
+            "expect_change_neg": 10,
+        }
+
+        t2 = {
+            "id": 5,
+            "expect_change_pos": 100,
+            "expect_change_neg": 10,
+        }
+
+        class ExpectChangeOperator(BaseOperator):
+            def __init__(self, regex_paths):
+                super().__init__(regex_paths)
+
+            def give_up_diffing(self, level, diff_instance):
+                if level.t1 == level.t2:
+                    diff_instance.custom_report_result('unexpected:still', level, {
+                        "old": level.t1,
+                        "new": level.t2
+                    })
+
+                return True
+
+        ddiff = DeepDiff(t1, t2, custom_operators=[
+            ExpectChangeOperator(regex_paths=["root\\['expect_change.*'\\]"])
+        ])
+
+        assert ddiff == {'unexpected:still': {"root['expect_change_neg']": {'old': 10, 'new': 10}}}
+
+    def test_custom_operator2(self):
+
+        class CustomClass:
+
+            def __init__(self, d: dict, l: list):
+                self.dict = d
+                self.dict['list'] = l
+
+            def __repr__(self):
+                return "Class list is " + str(self.dict['list'])
+
+        custom1 = CustomClass(d=dict(a=1, b=2), l=[1, 2, 3])
+        custom2 = CustomClass(d=dict(c=3, d=4), l=[1, 2, 3, 2])
+        custom3 = CustomClass(d=dict(a=1, b=2), l=[1, 2, 3, 4])
+
+        class ListMatchOperator(BaseOperator):
+
+            def give_up_diffing(self, level, diff_instance):
+                if set(level.t1.dict['list']) == set(level.t2.dict['list']):
+                    return True
+
+        ddiff = DeepDiff(custom1, custom2, custom_operators=[
+            ListMatchOperator(types=[CustomClass])
+        ])
+
+        assert {} == ddiff
+
+        ddiff2 = DeepDiff(custom2, custom3, custom_operators=[
+            ListMatchOperator(types=[CustomClass])
+        ])
+
+        expected = {
+            'dictionary_item_added': ["root.dict['a']", "root.dict['b']"],
+            'dictionary_item_removed': ["root.dict['c']", "root.dict['d']"],
+            'values_changed': {"root.dict['list'][3]": {'new_value': 4, 'old_value': 2}}}
+
+        assert expected == ddiff2
+
+    def test_include_only_certain_path(self):
+
+        class MyOperator:
+
+            def __init__(self, include_paths):
+                self.include_paths = include_paths
+
+            def match(self, level) -> bool:
+                return True
+
+            def give_up_diffing(self, level, diff_instance) -> bool:
+                return level.path() not in self.include_paths
+
+        t1 = {'a': [10, 11], 'b': [20, 21], 'c': [30, 31]}
+        t2 = {'a': [10, 22], 'b': [20, 33], 'c': [30, 44]}
+
+        ddiff = DeepDiff(t1, t2, custom_operators=[
+            MyOperator(include_paths="root['a'][1]")
+        ])
+
+        expected = {'values_changed': {"root['a'][1]": {'new_value': 22, 'old_value': 11}}}
+        assert expected == ddiff
+
+    def test_give_up_diffing_on_first_diff(self):
+
+        class MyOperator:
+
+            def match(self, level) -> bool:
+                return True
+
+            def give_up_diffing(self, level, diff_instance) -> bool:
+                return any(diff_instance.tree.values())
+
+        t1 = [[1, 2], [3, 4], [5, 6]]
+        t2 = [[1, 3], [3, 5], [5, 7]]
+
+        ddiff = DeepDiff(t1, t2, custom_operators=[
+            MyOperator()
+        ])
+
+        expected = {'values_changed': {'root[0][1]': {'new_value': 3, 'old_value': 2}}}
+        assert expected == ddiff
+
+    def test_prefix_or_suffix_diff(self):
+
+        t1 = {
+            "key1": ["foo", "bar's food", "jack", "joe"]
+        }
+        t2 = {
+            "key1": ["foo", "bar", "jill", "joe'car"]
+        }
+
+        ddiff = DeepDiff(t1, t2, custom_operators=[
+            PrefixOrSuffixOperator()
+        ])
+
+        expected = {'values_changed': {"root['key1'][2]": {'new_value': 'jill', 'old_value': 'jack'}}}
+        assert expected == ddiff
+
+        ddiff2 = DeepDiff(t1, t2, ignore_order=True, custom_operators=[
+            PrefixOrSuffixOperator()
+        ])
+
+        expected2 = {'values_changed': {"root['key1'][2]": {'new_value': 'jill', 'old_value': 'jack'}}}
+        assert expected2 == ddiff2
