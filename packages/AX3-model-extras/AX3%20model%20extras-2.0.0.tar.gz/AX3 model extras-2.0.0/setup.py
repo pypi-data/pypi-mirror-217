d017e6131548037a6575909883b4af2c30f27288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:49 +0000

    Add package AX3-model-extras/2.0.0/AX3%20model%20extras-2.0.0.tar.gz

---
+++ packages/AX3-model-extras/AX3%20model%20extras-2.0.0.tar.gz/AX3 model extras-2.0.0/setup.py
@@ -0,0 +1,33 @@
+import os
+
+import setuptools
+
+__VERSION__ = '2.0.0'
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setuptools.setup(
+    name='AX3 model extras',
+    version=__VERSION__,
+    author='Axiacore',
+    author_email='info@axiacore.com',
+    description='Django app extras for AX3 models',
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/axiacore/ax3-model-extras',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'django >= 3.2',
+        'python-resize-image',
+        'python_magic',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
