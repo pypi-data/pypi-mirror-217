f5272b0da0eb9f43b8ab8bac6296c60fc4bedaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:21 +0000

    Add package stats-probalytics/1.3/stats_probalytics-1.3.tar.gz

---
+++ packages/stats-probalytics/stats_probalytics-1.3.tar.gz/stats_probalytics-1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="stats_probalytics",
+    version="1.3",
+    description="Package for Gaussian and Binomial statistical distributions",
+    author="Sumaiya Jelle Adan",
+    author_email="jellerayyann@gmail.com",
+    packages=["stats_probalytics"],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+)
