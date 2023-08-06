cc8fdfa3098895bfc2e50b8c4c70905a52ca91d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:53 +0000

    Add package plutous-trade-crypto/0.0.49/plutous_trade_crypto-0.0.49-py3-none-any.whl

---
+++ packages/plutous-trade-crypto/plutous_trade_crypto-0.0.49-py3-none-any.whl/plutous/trade/crypto/alerts/__init__.py
@@ -0,0 +1,3 @@
+from .base import BaseAlert, BaseAlertConfig
+from .oi_variation import OIVariationAlert, OIVariationAlertConfig
+from .price_variation import PriceVariationAlert, PriceVariationAlertConfig
