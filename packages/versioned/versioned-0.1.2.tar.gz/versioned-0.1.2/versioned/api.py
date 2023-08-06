6b8ab5bc8c881089dcbdad5cfe755e3353b234dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:44 +0000

    Add package versioned/0.1.2/versioned-0.1.2.tar.gz

---
+++ packages/versioned/versioned-0.1.2.tar.gz/versioned-0.1.2/versioned/api.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Public API.
+"""
+
+from . import exc
+from .constants import DYNAMODB_TABLE_NAME
+from .constants import BUCKET_NAME
+from .constants import S3_PREFIX
+from .constants import LATEST_VERSION
+from .constants import VERSION_ZFILL
+from .bootstrap import bootstrap
+from .core import Artifact
+from .core import Alias
+from .core import put_artifact
+from .core import get_artifact_version
+from .core import list_artifact_versions
+from .core import publish_artifact_version
+from .core import delete_artifact_version
+from .core import put_alias
+from .core import get_alias
+from .core import list_aliases
+from .core import delete_alias
+from .core import purge_artifact
