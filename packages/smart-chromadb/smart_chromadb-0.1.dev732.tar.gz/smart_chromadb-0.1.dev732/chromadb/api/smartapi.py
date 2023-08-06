6f19f0838e28d984dfb022cbf0209af0af7ddf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:02 +0000

    Add package smart-chromadb/0.1.dev732/smart_chromadb-0.1.dev732.tar.gz

---
+++ packages/smart-chromadb/smart_chromadb-0.1.dev732.tar.gz/smart_chromadb-0.1.dev732/chromadb/api/smartapi.py
@@ -0,0 +1,33 @@
+from uuid import UUID
+from chromadb.api.types import (
+    Embeddings,
+    Include,
+    QueryResult,
+    Where,
+    WhereDocument,
+)
+from overrides import override
+from chromadb.api.local import LocalAPI
+
+
+class SmartAPI(LocalAPI):
+
+    @override
+    def _query(
+        self,
+        collection_id: UUID,
+        query_embeddings: Embeddings,
+        n_results: int = 10,
+        where: Where = {},
+        where_document: WhereDocument = {},
+        include: Include = ["documents", "metadatas", "distances"],
+    ) -> QueryResult:
+        query_result = self._db.get_nearest_neighbors_smart(
+            collection_uuid=collection_id,
+            where=where,
+            where_document=where_document,
+            embeddings=query_embeddings,
+            n_results=n_results,
+        )
+
+        return query_result
