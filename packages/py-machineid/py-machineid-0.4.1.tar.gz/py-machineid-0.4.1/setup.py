d8029df29e5b085a2a54a0c7ee2cc7e236b7ca5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:32 +0000

    Add package py-machineid/0.4.1/py-machineid-0.4.1.tar.gz

---
+++ packages/py-machineid/py-machineid-0.4.1.tar.gz/py-machineid-0.4.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+  readme = fh.read()
+
+setup(
+  name='py-machineid',
+  version='0.4.1',
+  description='Get the unique machine ID of any host (without admin privileges)',
+  long_description_content_type='text/markdown',
+  long_description=readme,
+  url='https://github.com/keygen-sh/py-machineid',
+  author='Zeke Gabrielse',
+  author_email='oss@keygen.sh',
+  license='MIT',
+  install_requires=['winregistry'],
+  packages=['machineid'],
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+)
