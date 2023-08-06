86a4129206a242df2d40de93c9063642f357c633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:33 +0000

    Add package jys/0.0.1/jys-0.0.1.tar.gz

---
+++ packages/jys/jys-0.0.1.tar.gz/jys-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jys", # Replace with your own username
+    version="0.0.1",
+    author="18501112703",
+    author_email="1084748556@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/zqy0315/pypi-test",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
