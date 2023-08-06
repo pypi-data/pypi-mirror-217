ee809d1f61af10ca58d2780f0f20191f41007dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:44 +0000

    Add package GailBot/0.1a10/GailBot-0.1a10.tar.gz

---
+++ packages/GailBot/GailBot-0.1a10.tar.gz/GailBot-0.1a10/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: Jason Y. Wu
+# @Date:   2023-07-03 22:44:39
+# @Last Modified by:   Jason Y. Wu
+# @Last Modified time: 2023-07-03 23:06:25
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Gailbot_Testing_Suite",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["d"],
+    include_package_data=True,
+)
