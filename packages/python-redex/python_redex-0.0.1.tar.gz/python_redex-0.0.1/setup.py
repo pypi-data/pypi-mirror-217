70e020e8e444cc7731009104b8fd3e37986efd04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:50 +0000

    Add package python-redex/0.0.1/python_redex-0.0.1.tar.gz

---
+++ packages/python-redex/python_redex-0.0.1.tar.gz/python_redex-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='python_redex',
+    version='0.0.1',
+    description='User friendly version of regex. More information available at https://github.com/TimoKats/redex',
+    url='',  
+    author='Timo Kats',
+    author_email='tpakats@gmail.com',
+    license='MIT', 
+    classifiers=classifiers,
+    keywords='regex', 
+    packages=find_packages(),
+    install_requires=[''] 
+)
