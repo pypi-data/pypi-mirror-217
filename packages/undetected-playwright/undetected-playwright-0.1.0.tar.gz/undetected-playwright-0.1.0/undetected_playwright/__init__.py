f351b22a327cff5bcccb16d1e532a553e2e2c269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:19 +0000

    Add package undetected-playwright/0.1.0/undetected-playwright-0.1.0.tar.gz

---
+++ packages/undetected-playwright/undetected-playwright-0.1.0.tar.gz/undetected-playwright-0.1.0/undetected_playwright/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Time       : 2022/10/22 23:35
+# Author     : QIN2DIM
+# Github     : https://github.com/QIN2DIM
+# Description:
+from .ninja import StealthConfig, stealth_async, stealth_sync
+
+__version__ = "0.1.0"
+__all__ = ["StealthConfig", "stealth_async", "stealth_sync"]
