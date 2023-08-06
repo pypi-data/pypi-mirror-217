583866e0a67660bf026a0d71e23e0ed422552234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:18 +0000

    Add package infrahouse-toolkit/1.4.0/infrahouse_toolkit-1.4.0-py3-none-any.whl

---
+++ packages/infrahouse-toolkit/infrahouse_toolkit-1.4.0-py3-none-any.whl/infrahouse_toolkit/__init__.py
@@ -0,0 +1,16 @@
+"""
+Top-level package for InfraHouse Toolkit.
+
+:Author: Oleksandr Kuzminskyi
+:Version: 1.4.0
+:Email: aleks@infrahouse.com
+"""
+
+__author__ = """Oleksandr Kuzminskyi"""
+__email__ = "aleks@infrahouse.com"
+__version__ = "1.4.0"
+
+from logging import getLogger
+
+DEFAULT_OPEN_ENCODING = "utf8"
+LOG = getLogger(__name__)
