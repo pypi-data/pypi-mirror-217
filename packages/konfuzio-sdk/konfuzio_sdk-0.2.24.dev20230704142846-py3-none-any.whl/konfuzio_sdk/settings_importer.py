dd8d3fedaab50faf6670d518c07eb02bc3e639e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:29 +0000

    Add package konfuzio-sdk/0.2.24.dev20230704142846/konfuzio_sdk-0.2.24.dev20230704142846-py3-none-any.whl

---
+++ packages/konfuzio-sdk/konfuzio_sdk-0.2.24.dev20230704142846-py3-none-any.whl/konfuzio_sdk/settings_importer.py
@@ -0,0 +1,43 @@
+"""Central place to collect settings from Projects and make them available in the konfuzio_sdk package."""
+import logging
+import os
+import sys
+
+from decouple import AutoConfig
+
+sys.path.append(os.getcwd())
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+config = AutoConfig(search_path=os.getcwd())
+KONFUZIO_HOST = config('KONFUZIO_HOST', default="https://app.konfuzio.com")
+KONFUZIO_USER = config('KONFUZIO_USER', default=None)
+KONFUZIO_TOKEN = config('KONFUZIO_TOKEN', default=None)
+
+PDF_FILE = 1
+IMAGE_FILE = 2
+OFFICE_FILE = 3
+
+SUPPORTED_FILE_TYPES = {PDF_FILE: 'PDF', IMAGE_FILE: 'IMAGE', OFFICE_FILE: 'OFFICE'}
+
+LOG_FILE_PATH = os.path.join(os.getcwd(), 'konfuzio_sdk.log')
+LOG_FORMAT = (
+    "%(asctime)s [%(name)-20.20s] [%(threadName)-10.10s] [%(levelname)-8.8s] "
+    "[%(funcName)-20.20s][%(lineno)-4.4d] %(message)-10s"
+)
+
+
+def get_handlers():
+    """Get logging handlers based on environment variables."""
+    handlers = [logging.StreamHandler()]
+
+    if config('LOG_TO_FILE', cast=bool, default=True):
+        with open(LOG_FILE_PATH, "a") as f:
+            if f.writable():
+                handlers.append(logging.FileHandler(LOG_FILE_PATH))
+
+    return handlers
+
+
+logging.basicConfig(
+    level=config('LOGGING_LEVEL', default=logging.INFO, cast=int), format=LOG_FORMAT, handlers=get_handlers()
+)
