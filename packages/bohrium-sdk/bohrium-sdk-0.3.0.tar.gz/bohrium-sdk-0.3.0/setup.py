7a7392f68c41ec9f5c240763294c8a24aea79e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:43 +0000

    Add package bohrium-sdk/0.3.0/bohrium-sdk-0.3.0.tar.gz

---
+++ packages/bohrium-sdk/bohrium-sdk-0.3.0.tar.gz/bohrium-sdk-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import setuptools
+setup(
+    name="bohrium-sdk",
+    version="0.3.0",
+    author="dingzhaohan",
+    author_email="dingzh@dp.tech",
+    url="https://github.com/dingzhoahan",
+    description="bohrium openapi python sdk",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+    entry_points={}
+)
+
