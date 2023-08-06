30b2ff40698faf5a3c49bc1ed174e58f0bae9b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:26 +0000

    Add package PyGauss-Binomial/1.0.0/PyGauss_Binomial-1.0.0.tar.gz

---
+++ packages/PyGauss-Binomial/PyGauss_Binomial-1.0.0.tar.gz/PyGauss_Binomial-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='PyGauss_Binomial',
+      version='1.0.0',
+      description= 'Gaussian distributions and Binomial Distribution',
+      packages=['PyGauss_Binomial'],
+      author = "Mohammad Omar Adde",
+      author_email = "mohaomar495@gmail.com",
+      zip_safe=False)
