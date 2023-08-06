02d3f7a442f6b2d48b8d1caf8701798543ea9edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:14 +0000

    Add package taibun/0.0.2/taibun-0.0.2.tar.gz

---
+++ packages/taibun/taibun-0.0.2.tar.gz/taibun-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding='utf-8') as fh:
+    LONG_DESCRIPTION = '\n' + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Convert Chinese characters to Taiwanese'
+#LONG_DESCRIPTION = 'Taiwanese Hokkien transliterator from Chinese characters.'
+
+# Setting up
+setup(
+    name="taibun",
+    version=VERSION,
+    author="Andrei Harbachov",
+    author_email="<andrei.harbachov@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'taiwan', 'taiwanese', 'taigi', 'hokkien', 'romanization'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
