2c114bdd1c14747bc8e6536b0e1cafdd164306bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:38 +0000

    Add package fanbookbotapi/1.1.2/fanbookbotapi-1.1.2.tar.gz

---
+++ packages/fanbookbotapi/fanbookbotapi-1.1.2.tar.gz/fanbookbotapi-1.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='fanbookbotapi',
+    version='1.1.2',
+    author='王大哥',
+    author_email='fanbookwdg1122@outlook.com',
+    description='方便给fanbook bot api发送请求',
+    long_description='具体可参考函数描述',
+    packages=['fanbookbotapi'],
+    install_requires=[
+        'requests',
+    ],
+)
