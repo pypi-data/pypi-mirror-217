75151c9aa563ce7efe6be033a369d8b0fd91dc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:55 +0000

    Add package adaptlm/0.0.1/adaptlm-0.0.1.tar.gz

---
+++ packages/adaptlm/adaptlm-0.0.1.tar.gz/adaptlm-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="adaptlm",
+    version="0.0.1",
+    url="http://github.com/vietanhdev/adaptlm",
+    license="MIT",
+    author="Viet Anh Nguyen",
+    author_email="vietanh.dev@gmail.com",
+    description="Easy library for adapting and optimizing large models for production on specific domains",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
