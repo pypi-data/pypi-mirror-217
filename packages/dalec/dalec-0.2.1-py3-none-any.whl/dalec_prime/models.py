3f837a16db975464ea604224aa9f79d8c95a39f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:59 +0000

    Add package dalec/0.2.1/dalec-0.2.1-py3-none-any.whl

---
+++ packages/dalec/dalec-0.2.1-py3-none-any.whl/dalec_prime/models.py
@@ -0,0 +1,12 @@
+from dalec.models import ContentBase
+from dalec.models import FetchHistoryBase
+
+__all__ = ["Content", "FetchHistory"]
+
+
+class Content(ContentBase):
+    pass
+
+
+class FetchHistory(FetchHistoryBase):
+    pass
