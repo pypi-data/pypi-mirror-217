88f735778279c2400627fddb96e2e5beb0849d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:29 +0000

    Add package TypeDal/1.2.2/typedal-1.2.2.tar.gz

---
+++ packages/TypeDal/typedal-1.2.2.tar.gz/typedal-1.2.2/CHANGELOG.md
@@ -0,0 +1,61 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v1.2.2 (2023-07-06)
+### Fix
+
+* Allow using `TypedFieldType`s in .select() ([`2945d15`](https://github.com/trialandsuccess/TypeDAL/commit/2945d15b3856f4881d27bd00504b3a2b7363b7ed))
+
+## v1.2.1 (2023-06-27)
+### Fix
+
+* Typehint query options for db() and update_or_insert() ([`2b65f58`](https://github.com/trialandsuccess/TypeDAL/commit/2b65f58ccf93f12713877fd420ec43454911efe9))
+
+## v1.2.0 (2023-06-27)
+### Feature
+
+* **set:** Type hints for .count() and .select() ([`1f23581`](https://github.com/trialandsuccess/TypeDAL/commit/1f2358169c6690e36153e99983c53a6c370f5e40))
+
+## v1.1.2 (2023-06-27)
+### Fix
+
+* **query:** A class with postponed db.define could not be used to make queries ([`2e55363`](https://github.com/trialandsuccess/TypeDAL/commit/2e553632312fd759702e220ae71de9e0abb3d5a9))
+
+## v1.1.1 (2023-06-22)
+### Fix
+
+* **mypy:** Made the package properly typed ([`a242895`](https://github.com/trialandsuccess/TypeDAL/commit/a242895d9d8a2d04add9c863105c2b1c756a3b6a))
+
+## v1.1.0 (2023-06-22)
+### Feature
+
+* **types:** You should use `param = TypedField()` instead of `param: TypedField()` from now on for better mypy support ([`ed50273`](https://github.com/trialandsuccess/TypeDAL/commit/ed50273ded4a178a30ec8c9d6f492b871853a568))
+
+### Fix
+
+* **version:** We were already on 1.0.0 ([`d6a8d0d`](https://github.com/trialandsuccess/TypeDAL/commit/d6a8d0d17274a5a89e7c0260ba00bb916533866c))
+
+### Documentation
+
+* More info about new release ([`ddfffe2`](https://github.com/trialandsuccess/TypeDAL/commit/ddfffe2c5e6a8f7799449d059e5a2b8dd9c95dab))
+
+## v1.0.0 (2023-06-05)
+Refactored pre-1.0 code and added [su6](https://github.com/robinvandernoord/su6-checker) checker tool.  
+Moved to `pyproject.toml` based build system.
+
+### Feature
+* **su6:** Add github workflow with checkers ([`d462387`](https://github.com/trialandsuccess/TypeDAL/commit/d46238705b27137ed60806eca5c53d8c2940052f))
+* **tests:** Moved tests.py to pytest ([`de33d20`](https://github.com/trialandsuccess/TypeDAL/commit/de33d2053e6499c4a8628cfc4b8813b5a649c584))
+* **define:** Pass extra kwargs (e.g. format) to define_table ([`bb692dc`](https://github.com/trialandsuccess/TypeDAL/commit/bb692dc1b2a9c18e45bcb70771b7435df8a39c8e))
+* **TypedRows:** Subscriptable return type for .select() ([`7b674b0`](https://github.com/trialandsuccess/TypeDAL/commit/7b674b07d4e61f4ead5d3f006e018b1aa66c10e7))
+* Add specific Fields, experimented with __future__.annotations (does not work right now) and general improvements ([`9af27c2`](https://github.com/trialandsuccess/TypeDAL/commit/9af27c254bf2ad7bd3bfa80e66b6e08e82f8f13a))
+
+### Fix
+* **gh:** Also install current project with dependencies on checking ([`60a1010`](https://github.com/trialandsuccess/TypeDAL/commit/60a101064b32cead856dd1a0d1a78a2e74c94abf))
+* **ruff:** Make linter happy ([`b961054`](https://github.com/trialandsuccess/TypeDAL/commit/b9610542d199a42b527a58aaf08ddf70025abe6e))
+* **..Field:** Use typing.Type[] instead of type() for better hinting ([`308ea14`](https://github.com/trialandsuccess/TypeDAL/commit/308ea14f38d4611b626c0edaf737ee53467b25b2))
+* **core:** Table(id) works now just like Table(id=id) ([`c19e170`](https://github.com/trialandsuccess/TypeDAL/commit/c19e17074edfba06287402d0c52d52127fe15499))
+* **intfield:** Return int type, not int instance ([`334c3ac`](https://github.com/trialandsuccess/TypeDAL/commit/334c3acd9f0e38badb742a69181e19ebf2088cb1))
+* **core:** Notnull > required, fix ReferenceField with cls instead of string ([`1cc1482`](https://github.com/trialandsuccess/TypeDAL/commit/1cc148209572ce1d457bfde51f86c9167b2da904))
+* Actually use custom type in TypedField ([`98c8fc3`](https://github.com/trialandsuccess/TypeDAL/commit/98c8fc30c311136b4804ed3211884b70dd7a98af))
---
+++ packages/TypeDal/typedal-1.2.2.tar.gz/typedal-1.2.2/tests/test_main.py
@@ -0,0 +1,304 @@
+import re
+import typing
+from sqlite3 import IntegrityError
+
+import pydal
+import pytest
+
+from src.typedal import *
+from src.typedal.__about__ import __version__
+from src.typedal.fields import *
+
+
+def test_about():
+    version_re = re.compile(r"\d+\.\d+\.\d+.*")
+    assert version_re.findall(__version__)
+
+
+db = TypeDAL("sqlite:memory")
+
+
+def test_mixed_defines():
+    ### DEFINE
+
+    # before:
+
+    db.define_table("relation")
+
+    db.define_table(
+        "old_syntax",
+        pydal.Field("name", "string", notnull=True),
+        pydal.Field("age", "float", notnull=False),
+        pydal.Field("location", "text", default="Amsterdam"),
+        pydal.Field("relation", "reference relation"),
+    )
+
+    # after:
+
+    @db.define
+    class NewRelation(TypedTable):
+        ...
+
+    class SecondNewRelation(TypedTable):
+        ...
+
+    # db.define can be used as decorator or later on
+    db.define(SecondNewRelation)
+
+    # you can use native types or TypedField (if more settings are required, otherwise default are used)
+
+    @db.define
+    class FirstNewSyntax(TypedTable):
+        # simple:
+        name: str
+        # optional: (sets required=False and notnull=False)
+        age: float | None
+        # with extra options (and non-native type 'text'):
+        location = TypedField(str, type="text", default="Amsterdam")
+        # references:
+        # can only be made optional with typing.Optional, not '| None'
+        first_new_relation: typing.Optional[NewRelation]
+        second_new_relation: typing.Optional[SecondNewRelation]
+        # backwards compatible:
+        old_relation: typing.Optional[db.relation]
+        # generics:
+        tags: list[str]
+
+    # instead of using just a native type, TypedField can also always be used:
+    class SecondNewSyntax(TypedTable):
+        # simple:
+        name = TypedField(str)
+        # optional: (sets required=False and notnull=False)
+        # note: TypedField can NOT be used with typing.Optional or '| None' !!
+        age = TypedField(float, notnull=False)
+        # with extra options (and non-native type 'text'):
+        location = TextField(default="Rotterdam")
+        first_new_relation = ReferenceField(NewRelation)
+        second_new_relation = ReferenceField(db.second_new_relation)
+        # backwards compatible:
+        old_relation = TypedField(db.relation, notnull=False)
+        # generics:
+        tags = TypedField(list[str])
+
+    db.define(SecondNewSyntax)
+
+    ### INSERTS
+    db.relation.insert()
+
+    db.new_relation.insert()
+    # OR
+    NewRelation.insert()
+    SecondNewRelation.insert()
+
+    ## insert without all required:
+
+    with pytest.raises(IntegrityError):
+        db.old_syntax.insert()
+
+    with pytest.raises(IntegrityError):
+        db.first_new_syntax.insert()
+
+    # equals:
+
+    with pytest.raises(IntegrityError):
+        FirstNewSyntax.insert()
+
+    with pytest.raises(IntegrityError):
+        SecondNewSyntax.insert()
+
+    ## insert normal
+    db.old_syntax.insert(name="First", age=99, location="Norway", relation=db.relation(id=1))
+    db.first_new_syntax.insert(
+        name="First", age=99, location="Norway", old_relation=db.relation(id=1), tags=["first", "second"]
+    )
+    # equals
+    FirstNewSyntax.insert(
+        name="First", age=99, location="Norway", old_relation=db.relation(id=1), tags=["first", "second"]
+    )
+    # similar
+    SecondNewSyntax.insert(
+        name="Second",
+        age=101,
+        first_new_relation=NewRelation(id=1),
+        second_new_relation=SecondNewRelation(id=1),
+        tags=["first", "second"],
+    )
+
+    ### Select
+    from pprint import pprint
+
+    assert db(FirstNewSyntax.name == "First").count()
+    assert db(FirstNewSyntax.location == "Norway").count()
+    assert not db(FirstNewSyntax.location == "Nope").count()
+    assert not db(FirstNewSyntax.location == "Nope").count()
+
+    assert db(SecondNewSyntax.name == "Second").count()
+    assert db(SecondNewSyntax.location == "Rotterdam").count()
+    assert not db(SecondNewSyntax.location == "Nope").count()
+    assert not db(SecondNewSyntax.location == "Nope").count()
+
+    def _print_and_assert_len(lst, exp):
+        pprint(lst)
+        real = len(lst)
+        assert real == exp, f"{real} != {exp}"
+
+    _print_and_assert_len(db(db.old_syntax).select().as_list(), 1)
+    _print_and_assert_len(db(db.old_syntax.id > 0).select().as_list(), 1)
+
+    _print_and_assert_len(db(db.first_new_syntax).select().as_list(), 2)
+    _print_and_assert_len(db(db.first_new_syntax.id > 0).select().as_list(), 2)
+
+    _print_and_assert_len(db(FirstNewSyntax).select().as_list(), 2)
+    _print_and_assert_len(db(FirstNewSyntax.id > 0).select().as_list(), 2)
+
+    assert SecondNewSyntax(id=1) is not None
+    assert SecondNewSyntax(1) is not None
+    assert SecondNewSyntax(id=2) is None
+    assert SecondNewSyntax(2) is None
+    _print_and_assert_len(db(SecondNewSyntax).select().as_list(), 1)
+    _print_and_assert_len(db(SecondNewSyntax.id > 0).select().as_list(), 1)
+
+    assert SecondNewSyntax(1).location == "Rotterdam"
+
+
+def test_dont_allow_bool_in_query():
+    with pytest.raises(ValueError):
+        db(True)
+
+
+def test_invalid_union():
+    with pytest.raises(NotImplementedError):
+        @db.define
+        class Invalid(TypedTable):
+            valid: int | None
+            invalid: int | str
+
+    with pytest.raises(NotImplementedError):
+        @db.define
+        class Invalid(TypedTable):
+            valid: list[int]
+            invalid: dict[str, int]
+
+
+def test_using_model_without_define():
+    class Invalid(TypedTable):
+        name: str
+
+    # no db.define used
+    with pytest.raises(EnvironmentError):
+        Invalid.insert(name="error")
+
+    with pytest.raises(EnvironmentError):
+        Invalid(name="error")
+
+
+def test_typedfield_reprs():
+    # str() and repr()
+
+    assert str(TypedField(str)) == "TypedField.str"
+    assert str(TypedField(str | None)) == "TypedField.str"
+    assert str(TypedField(typing.Optional[str])) == "TypedField.str"
+    assert repr(TypedField(str)) == "<TypedField.str with options {}>"
+    assert str(TypedField(str, type="text")) == "TypedField.text"
+    assert repr(TypedField(str, type="text", default="def")) == "<TypedField.text with options {'default': 'def'}>"
+    assert str(TextField()) == "TypedField.text"
+    assert repr(TextField()) == "<TypedField.text with options {}>"
+
+
+def test_typedfield_to_field_type():
+    @db.define
+    class SomeTable(TypedTable):
+        name = TypedField(str)  # basic mapping
+
+    @db.define
+    class OtherTable(TypedTable):
+        second = ReferenceField(SomeTable)  # reference to TypedTable
+        third = ReferenceField(db.some_table)  # reference to pydal table
+        fourth = TypedField(list[str])  # generic alias
+        optional_one = TypedField(typing.Optional[str])
+        optional_two = TypedField(str | None)
+
+    with pytest.raises(NotImplementedError):
+        @db.define
+        class Invalid(TypedTable):
+            third = TypedField(dict[str, int])  # not supported
+
+
+def test_fields():
+    @db.define
+    class SomeNewTable(TypedTable):
+        name: str
+        name_alt = TypedField(str)
+
+    class OtherNewTable(TypedTable):
+        name: str
+
+    db.define(OtherNewTable)
+
+    assert str(StringField()) == "TypedField.string"
+    assert str(BlobField()) == "TypedField.blob"
+    assert str(Boolean()) == "TypedField.boolean"
+    assert str(IntegerField()) == "TypedField.integer"
+    assert str(DoubleField()) == "TypedField.double"
+    assert str(DecimalField(1, 1)) == "TypedField.decimal(1, 1)"
+    assert str(DateField()) == "TypedField.date"
+    assert str(TimeField()) == "TypedField.time"
+    assert str(DatetimeField()) == "TypedField.datetime"
+    assert str(PasswordField()) == "TypedField.password"
+    assert str(UploadField()) == "TypedField.upload"
+    assert str(ReferenceField("other")) == "TypedField.reference other"
+    assert str(ReferenceField(db.some_new_table)) == "TypedField.reference some_new_table"
+    assert str(ReferenceField(SomeNewTable)) == "TypedField.reference some_new_table"
+    assert str(ReferenceField(OtherNewTable)) == "TypedField.reference other_new_table"
+    with pytest.raises(ValueError):
+        ReferenceField(object())
+
+    assert str(ListStringField()) == "TypedField.list:string"
+    assert str(ListIntegerField()) == "TypedField.list:integer"
+    assert str(ListReferenceField("somenewtable")) == "TypedField.list:reference somenewtable"
+    assert str(JSONField()) == "TypedField.json"
+    assert str(BigintField()) == "TypedField.bigint"
+
+    # test typedset:
+    counted1 = db(SomeNewTable).count()
+    counted2 = db(OtherNewTable).count()
+    counted3 = db(db.some_new_table).count()
+
+    assert counted1 == counted2 == counted3 == 0
+
+    select2: TypedRows[SomeNewTable] = db(SomeNewTable.id > 0).select(SomeNewTable.name, SomeNewTable.name_alt)
+
+    for row in select2:
+        raise ValueError("no rows should exist")
+
+    SomeNewTable.update_or_insert(
+        SomeNewTable.name == "Hendrik",
+        name="Hendrik 2",
+        name_alt="Hendrik II"
+    )
+
+    idx = OtherNewTable.update_or_insert(
+        OtherNewTable.name == "Hendrik",
+        name="Hendrik 2",
+    )
+    assert idx
+
+    OtherNewTable.update_or_insert(
+        OtherNewTable.name == "Hendrik",
+        name="Hendrik 2",
+    )
+
+    assert db(SomeNewTable.name == "Hendrik").count() == 0
+    assert db(SomeNewTable.name == "Hendrik 2").count() == 1
+
+    assert db(OtherNewTable.name == "Hendrik").count() == 0
+    assert db(OtherNewTable.name == "Hendrik 2").count() == 2
+
+    no_idx = OtherNewTable.update_or_insert(
+        OtherNewTable.name == "Hendrik 2",
+        name="Hendrik 3",
+    )  # should only update one and return None!
+
+    assert no_idx is None
+    assert db(OtherNewTable.name == "Hendrik 2").count() == 1
+    assert db(OtherNewTable.name == "Hendrik 3").count() == 1
