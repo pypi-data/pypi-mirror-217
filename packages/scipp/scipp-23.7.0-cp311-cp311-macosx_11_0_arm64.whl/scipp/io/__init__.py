55f11856a9c8db6eacc819b89c2b40255b10008f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:49 +0000

    Add package scipp/23.7.0/scipp-23.7.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/scipp/scipp-23.7.0-cp311-cp311-macosx_11_0_arm64.whl/scipp/io/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# @file
+# @author Simon Heybrock
+
+# flake8: noqa
+
+from .hdf5 import load_hdf5, open_hdf5
+
+__all__ = ['load_hdf5', 'open_hdf5']
