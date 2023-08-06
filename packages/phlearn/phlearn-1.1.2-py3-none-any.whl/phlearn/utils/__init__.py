63d07ac285ec4c2adea3b0084d8a565b4815d679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:48 +0000

    Add package phlearn/1.1.2/phlearn-1.1.2-py3-none-any.whl

---
+++ packages/phlearn/phlearn-1.1.2-py3-none-any.whl/phlearn/utils/__init__.py
@@ -0,0 +1,15 @@
+"""
+The utils subpackage contains functionality that are handy when
+using the other phlearn subpackages.
+
+Functions present in phlearn.utils
+-------------------------------------------
+
+   :py:meth:`~.derivatives.time_derivative`
+
+   :py:meth:`~.utils.to_tensor`
+
+"""
+
+from .derivatives import *
+from .utils import *
