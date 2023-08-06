0d26ff5c42f5d63f29edc55b2cac75b524fa2c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:05 +0000

    Add package openai-functions/0.6.11/openai_functions-0.6.11.tar.gz

---
+++ packages/openai-functions/openai_functions-0.6.11.tar.gz/openai_functions-0.6.11/openai_functions/parsers/none_parser.py
@@ -0,0 +1,25 @@
+"""Parser for null types"""
+from __future__ import annotations
+from typing import Any, TYPE_CHECKING, Type
+
+from .abc import ArgSchemaParser
+
+if TYPE_CHECKING:
+    from ..json_type import JsonType
+    from typing_extensions import TypeGuard
+
+
+class NoneParser(ArgSchemaParser[None]):
+    """Parser for null types"""
+
+    @classmethod
+    def can_parse(cls, argtype: Any) -> TypeGuard[Type[None]]:
+        return argtype in [None, type(None)]
+
+    @property
+    def argument_schema(self) -> dict[str, JsonType]:
+        return {"type": "null"}
+
+    def parse_value(self, value: JsonType) -> None:
+        if value is not None:
+            raise TypeError(f"Expected None, got {type(value)}")
