fc0268cd291964ac4002e382e5c77c6e5cd59b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:27 +0000

    Add package scikit-tree/0.1.4/scikit_tree-0.1.4-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/scikit-tree/scikit_tree-0.1.4-cp310-cp310-macosx_10_9_x86_64.whl/sktree/ensemble/__init__.py
@@ -0,0 +1,8 @@
+from ._honest_forest import HonestForestClassifier
+from ._supervised_forest import (
+    ObliqueRandomForestClassifier,
+    ObliqueRandomForestRegressor,
+    PatchObliqueRandomForestClassifier,
+    PatchObliqueRandomForestRegressor,
+)
+from ._unsupervised_forest import UnsupervisedObliqueRandomForest, UnsupervisedRandomForest
