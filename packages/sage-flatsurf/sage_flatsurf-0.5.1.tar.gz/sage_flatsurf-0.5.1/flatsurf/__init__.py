3d593c7de17b3363ac0164810a3119dfbba69bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:48 +0000

    Add package sage-flatsurf/0.5.1/sage_flatsurf-0.5.1.tar.gz

---
+++ packages/sage-flatsurf/sage_flatsurf-0.5.1.tar.gz/sage_flatsurf-0.5.1/flatsurf/__init__.py
@@ -0,0 +1,38 @@
+r"""
+Flat Surfaces in SageMath
+"""
+from flatsurf.version import version as __version__
+
+from flatsurf.geometry.polygon import (
+    Polygon,
+    polygons,
+    EquiangularPolygons,
+    EuclideanPolygonsWithAngles,
+    EuclideanPolygons as Polygons,
+    ConvexPolygons,
+)
+
+from flatsurf.geometry.similarity_surface_generators import (
+    similarity_surfaces,
+    dilation_surfaces,
+    half_translation_surfaces,
+    translation_surfaces,
+)
+
+from flatsurf.geometry.surface import MutableOrientedSimilaritySurface
+
+from flatsurf.geometry.gl2r_orbit_closure import GL2ROrbitClosure
+
+from flatsurf.geometry.hyperbolic import HyperbolicPlane
+
+from flatsurf.geometry.surface_legacy import (
+    Surface_list,
+    Surface_dict,
+    SimilaritySurface,
+    HalfDilationSurface,
+    DilationSurface,
+    ConeSurface,
+    RationalConeSurface,
+    HalfTranslationSurface,
+    TranslationSurface,
+)
