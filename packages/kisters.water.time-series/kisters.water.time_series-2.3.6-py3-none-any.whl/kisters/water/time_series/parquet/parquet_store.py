bba1f4e41be47445df1a9614d45016eaccd6217f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:41 +0000

    Add package kisters.water.time-series/2.3.6/kisters.water.time_series-2.3.6-py3-none-any.whl

---
+++ packages/kisters.water.time-series/kisters.water.time_series-2.3.6-py3-none-any.whl/kisters/water/time_series/parquet/parquet_store.py
@@ -0,0 +1,22 @@
+from kisters.water.time_series.core import TimeSeriesStore
+
+from .parquet_time_series_client import ParquetTimeSeriesClient
+
+
+class ParquetStore(TimeSeriesStore):
+    """ParquetStore provides a TimeSeriesStore for time series stored in Parquet files.
+
+    Args:
+        filename: The path to the Parquet file used for storage.
+
+    Examples:
+        .. code-block:: python
+
+            from kisters.water.time_series.parquet import ParquetStore
+
+            ts_store = ParquetStore("test.pq")
+            ts = ts_store.get_by_path("validation/inner_consistency1/station1/H")
+    """
+
+    def __init__(self, filename: str):
+        self.client = ParquetTimeSeriesClient(filename)
