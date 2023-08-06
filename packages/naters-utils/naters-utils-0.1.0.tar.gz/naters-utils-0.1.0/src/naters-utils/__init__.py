354c4581ffc7b5f91cd41a2b16f11953fa5f9c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:14 +0000

    Add package naters-utils/0.1.0/naters-utils-0.1.0.tar.gz

---
+++ packages/naters-utils/naters-utils-0.1.0.tar.gz/naters-utils-0.1.0/src/naters-utils/__init__.py
@@ -0,0 +1,11 @@
+# Naters-Utils
+# Various things i have made that I find useful and want to share
+
+
+# Metadata
+__all__ = ["hello"]
+__version__ = "0.1.0"
+
+
+# Imports
+from .hello import hello
