0386d43ad9124ac4caa3733819ae305c55971d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:31 +0000

    Add package micropython-magic/0.4.0/micropython_magic-0.4.0-py3-none-any.whl

---
+++ packages/micropython-magic/micropython_magic-0.4.0-py3-none-any.whl/micropython_magic/__init__.py
@@ -0,0 +1,26 @@
+"""MicroPython magics for Jupyter Notebooks and JupyterLabs"""
+__version__ = "0.4.0"
+__author__ = "Jos Verlinde"
+
+
+from IPython.core.interactiveshell import InteractiveShell
+from loguru import logger as log
+
+from .magic_transformer import comment_magic_transformer
+from .octarine import MpyMagics
+from .test_magics import TestMagics
+
+
+def load_ipython_extension(ipython: InteractiveShell):
+    # register the input transformer to allow %%cell_magics in comments
+    ipython.input_transformers_cleanup.append(comment_magic_transformer)
+    # register the magics
+    ipython.register_magics(MpyMagics)
+    # ipython.register_magics(TestMagics)
+
+
+def unload_ipython_extension(ipython: InteractiveShell):
+    # unregister the magics, allows to unload / reload the extension
+    # ipython.magics_manager.magics["cell"].pop("mpy", None)
+    # TODO
+    pass
