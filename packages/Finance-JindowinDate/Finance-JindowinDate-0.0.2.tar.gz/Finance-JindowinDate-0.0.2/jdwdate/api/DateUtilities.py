695328d314f736ed996f0ec339855c7e6b34a477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:51 +0000

    Add package Finance-JindowinDate/0.0.2/Finance-JindowinDate-0.0.2.tar.gz

---
+++ packages/Finance-JindowinDate/Finance-JindowinDate-0.0.2.tar.gz/Finance-JindowinDate-0.0.2/jdwdate/api/DateUtilities.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+import pdb
+from jdwdate.DateUtilities.Calendar import Calendar
+from jdwdate.DateUtilities.Date import Date
+from jdwdate.DateUtilities.Date import check_date
+from jdwdate.DateUtilities.Period import Period
+from jdwdate.DateUtilities.Period import check_period
+from jdwdate.DateUtilities.Schedule import Schedule
+from jdwdate.Enums import BizDayConventions
+from jdwdate.Enums import DateGeneration
+from jdwdate.Enums import TimeUnits
+
+
+def isBizDay(holidayCenter, ref):
+    cal = Calendar(holidayCenter)
+    ref = check_date(ref)
+    return cal.isBizDay(ref)
+
+
+def datesList(fromDate, toDate):
+    fromDate = check_date(fromDate)
+    toDate = check_date(toDate)
+    return [
+        Date.fromExcelSerialNumber(serial).toDateTime()
+        for serial in range(fromDate.serialNumber, toDate.serialNumber + 1)
+    ]
+
+
+def bizDatesList(holidayCenter, fromDate, toDate):
+    cal = Calendar(holidayCenter)
+    fromDate = check_date(fromDate)
+    toDate = check_date(toDate)
+    return [d.toDateTime() for d in cal.bizDatesList(fromDate, toDate)]
+
+
+def holDatesList(holidayCenter, fromDate, toDate, includeWeekend=True):
+    cal = Calendar(holidayCenter)
+    fromDate = check_date(fromDate)
+    toDate = check_date(toDate)
+    return [
+        d.toDateTime()
+        for d in cal.holDatesList(fromDate, toDate, includeWeekend)
+    ]
+
+
+def advanceDate(referenceDate, period):
+    d = check_date(referenceDate) + period
+    return d.toDateTime()
+
+
+def adjustDateByCalendar(holidayCenter,
+                         referenceDate,
+                         convention=BizDayConventions.Following):
+    cal = Calendar(holidayCenter)
+    refer = check_date(referenceDate)
+    return cal.adjustDate(refer, convention).toDateTime()
+
+
+def advanceDateByCalendar(holidayCenter,
+                          referenceDate,
+                          period,
+                          convention=BizDayConventions.Following):
+    cal = Calendar(holidayCenter)
+    refer = check_date(referenceDate)
+    period = check_period(period)
+    return cal.advanceDate(refer, period, convention).toDateTime()
+
+
+def nthWeekDay(nth, dayOfWeek, month, year):
+    date = Date.nthWeekday(nth, dayOfWeek, month, year)
+    return date.toDateTime()
+
+
+def makeSchedule(firstDate,
+                 endDate,
+                 tenor,
+                 calendar='NullCalendar',
+                 dateRule=BizDayConventions.Following,
+                 dateGenerationRule=DateGeneration.Forward):
+
+    cal = Calendar(calendar)
+    firstDate = check_date(firstDate)
+    endDate = check_date(endDate)
+    tenor = check_period(tenor)
+
+    if tenor.units() == TimeUnits.BDays:
+        schedule = []
+        if dateGenerationRule == DateGeneration.Forward:
+            d = cal.adjustDate(firstDate, dateRule)
+            while d <= endDate:
+                schedule.append(d)
+                d = cal.advanceDate(d, tenor, dateRule)
+        elif dateGenerationRule == DateGeneration.Backward:
+            d = cal.adjustDate(endDate, dateRule)
+            while d >= firstDate:
+                schedule.append(d)
+                d = cal.advanceDate(d, -tenor, dateRule)
+            schedule = sorted(schedule)
+    else:
+        schedule = Schedule(firstDate,
+                            endDate,
+                            tenor,
+                            cal,
+                            convention=dateRule,
+                            dateGenerationRule=dateGenerationRule)
+    return [d.toDateTime() for d in schedule]
+
+
+__all__ = [
+    "datesList", "bizDatesList", "holDatesList", "isBizDay", "advanceDate",
+    "BizDayConventions", "DateGeneration", "adjustDateByCalendar",
+    "advanceDateByCalendar", "nthWeekDay", "makeSchedule"
+]
