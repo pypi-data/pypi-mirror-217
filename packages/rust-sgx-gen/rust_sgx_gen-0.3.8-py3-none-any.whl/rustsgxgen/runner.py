490756bee520589bda6c497dc374a6097b833e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:44 +0000

    Add package rust-sgx-gen/0.3.8/rust_sgx_gen-0.3.8-py3-none-any.whl

---
+++ packages/rust-sgx-gen/rust_sgx_gen-0.3.8-py3-none-any.whl/rustsgxgen/runner.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+
+class Error(Exception):
+    pass
+
+
+class Runner(Enum):
+    SGX = 1
+    NATIVE = 2
+
+    @staticmethod
+    def from_str(name):
+        lower_str = name.lower()
+
+        if lower_str == "sgx":
+            return Runner.SGX
+        if lower_str == "native":
+            return Runner.NATIVE
+
+        raise Error(f"No matching runner for {name}")
+
+    def to_str(self):
+        if self == Runner.SGX:
+            return "runner_sgx"
+        if self == Runner.NATIVE:
+            return "runner_native"
+
+        raise Error("No to_str for some Runners")
+
+    def has_hardcoded_key(self):
+        if self == Runner.NATIVE:
+            return True
+
+        return False
