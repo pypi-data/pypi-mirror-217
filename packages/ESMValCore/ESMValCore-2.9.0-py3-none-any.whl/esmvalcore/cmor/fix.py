d3237898525f557085f246f4747e9139ab359d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:45 +0000

    Add package ESMValCore/2.9.0/ESMValCore-2.9.0-py3-none-any.whl

---
+++ packages/ESMValCore/ESMValCore-2.9.0-py3-none-any.whl/esmvalcore/cmor/fix.py
@@ -0,0 +1,278 @@
+"""Apply automatic fixes for known errors in cmorized data.
+
+All functions in this module will work even if no fixes are available
+for the given dataset. Therefore is recommended to apply them to all
+variables to be sure that all known errors are fixed.
+"""
+from __future__ import annotations
+
+import logging
+from collections import defaultdict
+from pathlib import Path
+from typing import TYPE_CHECKING, Optional
+
+from iris.cube import CubeList
+
+from ._fixes.fix import Fix
+from .check import CheckLevels, _get_cmor_checker
+
+if TYPE_CHECKING:
+    from ..config import Session
+
+logger = logging.getLogger(__name__)
+
+
+def fix_file(
+    file: Path,
+    short_name: str,
+    project: str,
+    dataset: str,
+    mip: str,
+    output_dir: Path,
+    add_unique_suffix: bool = False,
+    session: Optional[Session] = None,
+    **extra_facets,
+) -> Path:
+    """Fix files before ESMValTool can load them.
+
+    This fixes are only for issues that prevent iris from loading the cube or
+    that cannot be fixed after the cube is loaded.
+
+    Original files are not overwritten.
+
+    Parameters
+    ----------
+    file: Path
+        Path to the original file.
+    short_name: str
+        Variable's short name.
+    project: str
+        Project of the dataset.
+    dataset: str
+        Name of the dataset.
+    mip: str
+        Variable's MIP.
+    output_dir: Path
+        Output directory for fixed files.
+    add_unique_suffix: bool, optional (default: False)
+        Adds a unique suffix to `output_dir` for thread safety.
+    session: Session, optional
+        Current session which includes configuration and directory information.
+    **extra_facets: dict, optional
+        Extra facets are mainly used for data outside of the big projects like
+        CMIP, CORDEX, obs4MIPs. For details, see :ref:`extra_facets`.
+
+    Returns
+    -------
+    Path:
+        Path to the fixed file.
+    """
+    # Update extra_facets with variable information given as regular arguments
+    # to this function
+    extra_facets.update({
+        'short_name': short_name,
+        'project': project,
+        'dataset': dataset,
+        'mip': mip,
+    })
+
+    for fix in Fix.get_fixes(project=project,
+                             dataset=dataset,
+                             mip=mip,
+                             short_name=short_name,
+                             extra_facets=extra_facets,
+                             session=session):
+        file = fix.fix_file(
+            file, output_dir, add_unique_suffix=add_unique_suffix
+        )
+    return file
+
+
+def fix_metadata(cubes,
+                 short_name,
+                 project,
+                 dataset,
+                 mip,
+                 frequency=None,
+                 check_level=CheckLevels.DEFAULT,
+                 session: Optional[Session] = None,
+                 **extra_facets):
+    """Fix cube metadata if fixes are required and check it anyway.
+
+    This method collects all the relevant fixes for a given variable, applies
+    them and checks the resulting cube (or the original if no fixes were
+    needed) metadata to ensure that it complies with the standards of its
+    project CMOR tables.
+
+    Parameters
+    ----------
+    cubes: iris.cube.CubeList
+        Cubes to fix.
+    short_name: str
+        Variable's short name.
+    project: str
+        Project of the dataset.
+    dataset: str
+        Name of the dataset.
+    mip: str
+        Variable's MIP.
+    frequency: str, optional
+        Variable's data frequency, if available.
+    check_level: CheckLevels
+        Level of strictness of the checks. Set to default.
+    session: Session, optional
+        Current session which includes configuration and directory information.
+    **extra_facets: dict, optional
+        Extra facets are mainly used for data outside of the big projects like
+        CMIP, CORDEX, obs4MIPs. For details, see :ref:`extra_facets`.
+
+    Returns
+    -------
+    iris.cube.Cube:
+        Fixed and checked cube.
+
+    Raises
+    ------
+    CMORCheckError
+        If the checker detects errors in the metadata that it can not fix.
+    """
+    # Update extra_facets with variable information given as regular arguments
+    # to this function
+    extra_facets.update({
+        'short_name': short_name,
+        'project': project,
+        'dataset': dataset,
+        'mip': mip,
+        'frequency': frequency,
+    })
+
+    fixes = Fix.get_fixes(project=project,
+                          dataset=dataset,
+                          mip=mip,
+                          short_name=short_name,
+                          extra_facets=extra_facets,
+                          session=session)
+    fixed_cubes = []
+    by_file = defaultdict(list)
+    for cube in cubes:
+        by_file[cube.attributes.get('source_file', '')].append(cube)
+
+    for cube_list in by_file.values():
+        cube_list = CubeList(cube_list)
+        for fix in fixes:
+            cube_list = fix.fix_metadata(cube_list)
+
+        cube = _get_single_cube(cube_list, short_name, project, dataset)
+        checker = _get_cmor_checker(frequency=frequency,
+                                    table=project,
+                                    mip=mip,
+                                    short_name=short_name,
+                                    check_level=check_level,
+                                    fail_on_error=False,
+                                    automatic_fixes=True)
+        cube = checker(cube).check_metadata()
+        cube.attributes.pop('source_file', None)
+        fixed_cubes.append(cube)
+    return fixed_cubes
+
+
+def _get_single_cube(cube_list, short_name, project, dataset):
+    if len(cube_list) == 1:
+        return cube_list[0]
+    cube = None
+    for raw_cube in cube_list:
+        if raw_cube.var_name == short_name:
+            cube = raw_cube
+            break
+    if not cube:
+        raise ValueError(
+            f'More than one cube found for variable {short_name} in '
+            f'{project}:{dataset} but none of their var_names match the '
+            f'expected.\nFull list of cubes encountered: {cube_list}'
+        )
+    logger.warning(
+        'Found variable %s in %s:%s, but there were other present in '
+        'the file. Those extra variables are usually metadata '
+        '(cell area, latitude descriptions) that was not saved '
+        'according to CF-conventions. It is possible that errors appear '
+        'further on because of this. \nFull list of cubes encountered: %s',
+        short_name, project, dataset, cube_list)
+    return cube
+
+
+def fix_data(cube,
+             short_name,
+             project,
+             dataset,
+             mip,
+             frequency=None,
+             check_level=CheckLevels.DEFAULT,
+             session: Optional[Session] = None,
+             **extra_facets):
+    """Fix cube data if fixes add present and check it anyway.
+
+    This method assumes that metadata is already fixed and checked.
+
+    This method collects all the relevant fixes for a given variable, applies
+    them and checks resulting cube (or the original if no fixes were
+    needed) metadata to ensure that it complies with the standards of its
+    project CMOR tables.
+
+    Parameters
+    ----------
+    cube: iris.cube.Cube
+        Cube to fix.
+    short_name: str
+        Variable's short name.
+    project: str
+        Project of the dataset.
+    dataset: str
+        Name of the dataset.
+    mip: str
+        Variable's MIP.
+    frequency: str, optional
+        Variable's data frequency, if available.
+    check_level: CheckLevels
+        Level of strictness of the checks. Set to default.
+    session: Session, optional
+        Current session which includes configuration and directory information.
+    **extra_facets: dict, optional
+        Extra facets are mainly used for data outside of the big projects like
+        CMIP, CORDEX, obs4MIPs. For details, see :ref:`extra_facets`.
+
+    Returns
+    -------
+    iris.cube.Cube:
+        Fixed and checked cube.
+
+    Raises
+    ------
+    CMORCheckError
+        If the checker detects errors in the data that it can not fix.
+    """
+    # Update extra_facets with variable information given as regular arguments
+    # to this function
+    extra_facets.update({
+        'short_name': short_name,
+        'project': project,
+        'dataset': dataset,
+        'mip': mip,
+        'frequency': frequency,
+    })
+
+    for fix in Fix.get_fixes(project=project,
+                             dataset=dataset,
+                             mip=mip,
+                             short_name=short_name,
+                             extra_facets=extra_facets,
+                             session=session):
+        cube = fix.fix_data(cube)
+    checker = _get_cmor_checker(frequency=frequency,
+                                table=project,
+                                mip=mip,
+                                short_name=short_name,
+                                fail_on_error=False,
+                                automatic_fixes=True,
+                                check_level=check_level)
+    cube = checker(cube).check_data()
+    return cube
---
+++ packages/ESMValCore/ESMValCore-2.9.0-py3-none-any.whl/esmvalcore/preprocessor/_volume.py
@@ -0,0 +1,406 @@
+"""Volume and z coordinate operations on data cubes.
+
+Allows for selecting data subsets using certain volume bounds; selecting
+depth or height regions; constructing volumetric averages;
+"""
+import logging
+
+import dask.array as da
+import iris
+import numpy as np
+
+from ._shared import get_iris_analysis_operation, operator_accept_weights
+from ._supplementary_vars import register_supplementaries
+
+logger = logging.getLogger(__name__)
+
+
+def extract_volume(
+    cube,
+    z_min,
+    z_max,
+    interval_bounds='open',
+    nearest_value=False
+):
+    """Subset a cube based on a range of values in the z-coordinate.
+
+    Function that subsets a cube on a box of (z_min, z_max),
+    (z_min, z_max], [z_min, z_max) or [z_min, z_max]
+    Note that this requires the requested z-coordinate range to be the
+    same sign as the iris cube. ie, if the cube has z-coordinate as
+    negative, then z_min and z_max need to be negative numbers.
+    If nearest_value is set to `False`, the extraction will be
+    performed with the given z_min and z_max values.
+    If nearest_value is set to `True`, the cube extraction will be
+    performed taking into account the z_coord values that are closest
+    to the z_min and z_max values.
+
+    Parameters
+    ----------
+    cube: iris.cube.Cube
+        input cube.
+    z_min: float
+        minimum depth to extract.
+    z_max: float
+        maximum depth to extract.
+    interval_bounds: str
+        sets left bound of the interval to either 'open', 'closed',
+        'left_closed' or 'right_closed'.
+    nearest_value: bool
+        extracts considering the nearest value of z-coord to z_min and z_max.
+    Returns
+    -------
+    iris.cube.Cube
+        z-coord extracted cube.
+    """
+    if z_min > z_max:
+        # minimum is below maximum, so switch them around
+        zmax = float(z_min)
+        zmin = float(z_max)
+    else:
+        zmax = float(z_max)
+        zmin = float(z_min)
+
+    z_coord = cube.coord(axis='Z')
+
+    if nearest_value:
+        min_index = np.argmin(np.abs(z_coord.core_points() - zmin))
+        max_index = np.argmin(np.abs(z_coord.core_points() - zmax))
+        zmin = z_coord.core_points()[min_index]
+        zmax = z_coord.core_points()[max_index]
+
+    if interval_bounds == 'open':
+        coord_values = {z_coord: lambda cell: zmin < cell.point < zmax}
+    elif interval_bounds == 'closed':
+        coord_values = {z_coord: lambda cell: zmin <= cell.point <= zmax}
+    elif interval_bounds == 'left_closed':
+        coord_values = {z_coord: lambda cell: zmin <= cell.point < zmax}
+    elif interval_bounds == 'right_closed':
+        coord_values = {z_coord: lambda cell: zmin < cell.point <= zmax}
+    else:
+        raise ValueError(
+            'Depth extraction bounds can be set to "open", "closed", '
+            f'"left_closed", or "right_closed". Got "{interval_bounds}".')
+
+    z_constraint = iris.Constraint(coord_values=coord_values)
+
+    return cube.extract(z_constraint)
+
+
+def calculate_volume(cube):
+    """Calculate volume from a cube.
+
+    This function is used when the volume ancillary variables can't be found.
+
+    Parameters
+    ----------
+    cube: iris.cube.Cube
+        input cube.
+
+    Returns
+    -------
+    float
+        grid volume.
+    """
+    # ####
+    # Load depth field and figure out which dim is which.
+    depth = cube.coord(axis='z')
+    z_dim = cube.coord_dims(cube.coord(axis='z'))[0]
+
+    # ####
+    # Load z direction thickness
+    thickness = depth.bounds[..., 1] - depth.bounds[..., 0]
+
+    # ####
+    # Calculate grid volume:
+    area = da.array(iris.analysis.cartography.area_weights(cube))
+    if thickness.ndim == 1 and z_dim == 1:
+        grid_volume = area * thickness[None, :, None, None]
+    if thickness.ndim == 4 and z_dim == 1:
+        grid_volume = area * thickness[:, :]
+
+    return grid_volume
+
+
+@register_supplementaries(
+    variables=['volcello'],
+    required='prefer_at_least_one',
+)
+def volume_statistics(cube, operator):
+    """Apply a statistical operation over a volume.
+
+    The volume average is weighted according to the cell volume.
+
+    Parameters
+    ----------
+    cube: iris.cube.Cube
+        Input cube. The input cube should have a
+        :class:`iris.coords.CellMeasure` with standard name ``'ocean_volume'``,
+        unless it has regular 1D latitude and longitude coordinates so the cell
+        volumes can be computed by using
+        :func:`iris.analysis.cartography.area_weights` to compute the cell
+        areas and multiplying those by the cell thickness, computed from the
+        bounds of the vertical coordinate.
+    operator: str
+        The operation to apply to the cube, options are: 'mean'.
+
+    Returns
+    -------
+    iris.cube.Cube
+        collapsed cube.
+
+    Raises
+    ------
+    ValueError
+        if input cube shape differs from grid volume cube shape.
+    """
+    # TODO: Test sigma coordinates.
+    # TODO: Add other operations.
+
+    if operator != 'mean':
+        raise ValueError(f'Volume operator {operator} not recognised.')
+
+    try:
+        grid_volume = cube.cell_measure('ocean_volume').core_data()
+    except iris.exceptions.CellMeasureNotFoundError:
+        logger.debug('Cell measure "ocean_volume" not found in cube. '
+                     'Check fx_file availability.')
+        logger.debug('Attempting to calculate grid cell volume...')
+        grid_volume = calculate_volume(cube)
+    else:
+        grid_volume = da.broadcast_to(grid_volume, cube.shape)
+
+    if cube.data.shape != grid_volume.shape:
+        raise ValueError('Cube shape ({}) doesn`t match grid volume shape '
+                         f'({cube.shape, grid_volume.shape})')
+
+    masked_volume = da.ma.masked_where(da.ma.getmaskarray(cube.lazy_data()),
+                                       grid_volume)
+    result = cube.collapsed(
+        [cube.coord(axis='Z'),
+         cube.coord(axis='Y'),
+         cube.coord(axis='X')],
+        iris.analysis.MEAN,
+        weights=masked_volume)
+
+    return result
+
+
+def axis_statistics(cube, axis, operator):
+    """Perform statistics along a given axis.
+
+    Operates over an axis direction. If weights are required,
+    they are computed using the coordinate bounds.
+
+    Arguments
+    ---------
+    cube: iris.cube.Cube
+        Input cube.
+    axis: str
+        Direction over where to apply the operator. Possible values
+        are 'x', 'y', 'z', 't'.
+    operator: str
+        Statistics to perform. Available operators are:
+        'mean', 'median', 'std_dev', 'sum', 'variance',
+        'min', 'max', 'rms'.
+
+    Returns
+    -------
+    iris.cube.Cube
+        collapsed cube.
+    """
+    try:
+        coord = cube.coord(axis=axis)
+    except iris.exceptions.CoordinateNotFoundError as err:
+        raise ValueError(f'Axis {axis} not found in cube '
+                         f'{cube.summary(shorten=True)}') from err
+    coord_dims = cube.coord_dims(coord)
+    if len(coord_dims) > 1:
+        raise NotImplementedError('axis_statistics not implemented for '
+                                  'multidimensional coordinates.')
+    operation = get_iris_analysis_operation(operator)
+    if operator_accept_weights(operator):
+        coord_dim = coord_dims[0]
+        expand = list(range(cube.ndim))
+        expand.remove(coord_dim)
+        bounds = coord.core_bounds()
+        weights = np.abs(bounds[..., 1] - bounds[..., 0])
+        weights = np.expand_dims(weights, expand)
+        weights = da.broadcast_to(weights, cube.shape)
+        result = cube.collapsed(coord, operation, weights=weights)
+    else:
+        result = cube.collapsed(coord, operation)
+
+    return result
+
+
+def depth_integration(cube):
+    """Determine the total sum over the vertical component.
+
+    Requires a 3D cube. The z-coordinate
+    integration is calculated by taking the sum in the z direction of the
+    cell contents multiplied by the cell thickness.
+
+    Arguments
+    ---------
+    cube: iris.cube.Cube
+        input cube.
+
+    Returns
+    -------
+    iris.cube.Cube
+        collapsed cube.
+    """
+    result = axis_statistics(cube, axis='z', operator='sum')
+    result.rename('Depth_integrated_' + str(cube.name()))
+    # result.units = Unit('m') * result.units # This doesn't work:
+    # TODO: Change units on cube to reflect 2D concentration (not 3D)
+    # Waiting for news from iris community.
+    return result
+
+
+def extract_transect(cube, latitude=None, longitude=None):
+    """Extract data along a line of constant latitude or longitude.
+
+    Both arguments, latitude and longitude, are treated identically.
+    Either argument can be a single float, or a pair of floats, or can be
+    left empty.
+    The single float indicates the latitude or longitude along which the
+    transect should be extracted.
+    A pair of floats indicate the range that the transect should be
+    extracted along the secondairy axis.
+
+    For instance `'extract_transect(cube, longitude=-28)'` will produce a
+    transect along 28 West.
+
+    Also, `'extract_transect(cube, longitude=-28, latitude=[-50, 50])'` will
+    produce a transect along 28 West  between 50 south and 50 North.
+
+    This function is not yet implemented for irregular arrays - instead
+    try the extract_trajectory function, but note that it is currently
+    very slow. Alternatively, use the regrid preprocessor to regrid along
+    a regular grid and then extract the transect.
+
+    Parameters
+    ----------
+    cube: iris.cube.Cube
+        input cube.
+    latitude: None, float or [float, float], optional
+        transect latiude or range.
+    longitude:  None, float or [float, float], optional
+        transect longitude or range.
+
+    Returns
+    -------
+    iris.cube.Cube
+        collapsed cube.
+
+    Raises
+    ------
+    ValueError
+        slice extraction not implemented for irregular grids.
+    ValueError
+        latitude and longitude are both floats or lists; not allowed
+        to slice on both axes at the same time.
+    """
+    # ###
+    coord_dim2 = False
+    second_coord_range = False
+    lats = cube.coord('latitude')
+    lons = cube.coord('longitude')
+
+    if lats.ndim == 2:
+        raise ValueError(
+            'extract_transect: Not implemented for irregular arrays!' +
+            '\nTry regridding the data first.')
+
+    if isinstance(latitude, float) and isinstance(longitude, float):
+        raise ValueError(
+            "extract_transect: Can't slice along lat and lon at the same time")
+
+    if isinstance(latitude, list) and isinstance(longitude, list):
+        raise ValueError(
+            "extract_transect: Can't reduce lat and lon at the same time")
+
+    for dim_name, dim_cut, coord in zip(['latitude', 'longitude'],
+                                        [latitude, longitude], [lats, lons]):
+        # ####
+        # Look for the first coordinate.
+        if isinstance(dim_cut, float):
+            coord_index = coord.nearest_neighbour_index(dim_cut)
+            coord_dim = cube.coord_dims(dim_name)[0]
+
+        # ####
+        # Look for the second coordinate.
+        if isinstance(dim_cut, list):
+            coord_dim2 = cube.coord_dims(dim_name)[0]
+            second_coord_range = [
+                coord.nearest_neighbour_index(dim_cut[0]),
+                coord.nearest_neighbour_index(dim_cut[1])
+            ]
+    # ####
+    # Extracting the line of constant longitude/latitude
+    slices = [slice(None) for i in cube.shape]
+    slices[coord_dim] = coord_index
+
+    if second_coord_range:
+        slices[coord_dim2] = slice(second_coord_range[0],
+                                   second_coord_range[1])
+    return cube[tuple(slices)]
+
+
+def extract_trajectory(cube, latitudes, longitudes, number_points=2):
+    """Extract data along a trajectory.
+
+    latitudes and longitudes are the pairs of coordinates for two points.
+    number_points is the number of points between the two points.
+
+    This version uses the expensive interpolate method, but it may be
+    necceasiry for irregular grids.
+
+    If only two latitude and longitude coordinates are given,
+    extract_trajectory will produce a cube will extrapolate along a line
+    between those two points, and will add `number_points` points between
+    the two corners.
+
+    If more than two points are provided, then
+    extract_trajectory will produce a cube which has extrapolated the data
+    of the cube to those points, and `number_points` is not needed.
+
+    Parameters
+    ----------
+    cube: iris.cube.Cube
+        input cube.
+    latitudes: list
+        list of latitude coordinates (floats).
+    longitudes: list
+        list of longitude coordinates (floats).
+    number_points: int
+        number of points to extrapolate (optional).
+
+    Returns
+    -------
+    iris.cube.Cube
+        collapsed cube.
+
+    Raises
+    ------
+    ValueError
+        if latitude and longitude have different dimensions.
+    """
+    from iris.analysis.trajectory import interpolate
+
+    if len(latitudes) != len(longitudes):
+        raise ValueError(
+            'Longitude & Latitude coordinates have different lengths')
+
+    if len(latitudes) == len(longitudes) == 2:
+        minlat, maxlat = np.min(latitudes), np.max(latitudes)
+        minlon, maxlon = np.min(longitudes), np.max(longitudes)
+
+        longitudes = np.linspace(minlon, maxlon, num=number_points)
+        latitudes = np.linspace(minlat, maxlat, num=number_points)
+
+    points = [('latitude', latitudes), ('longitude', longitudes)]
+    interpolated_cube = interpolate(cube, points)  # Very slow!
+    return interpolated_cube
