2dc565dde8645cb27b8db55fc461ac520a7b918b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:35 +0000

    Add package pybfbc2stats/0.3.16/pybfbc2stats-0.3.16-py3-none-any.whl

---
+++ packages/pybfbc2stats/pybfbc2stats-0.3.16-py3-none-any.whl/pybfbc2stats/__init__.py
@@ -0,0 +1,23 @@
+from .asyncio_client import AsyncFeslClient, AsyncTheaterClient
+from .asyncio_connection import AsyncConnection, AsyncSecureConnection
+from .client import FeslClient, TheaterClient
+from .connection import Connection, SecureConnection
+from .constants import Platform, Namespace, DEFAULT_LEADERBOARD_KEYS, STATS_KEYS
+from .exceptions import Error, TimeoutError, ParameterError, \
+    PlayerNotFoundError, AuthError, ConnectionError, SearchError, \
+    NotFoundError, ServerNotFoundError, LobbyNotFoundError
+
+"""
+pybfbc2stats.
+Python library for retrieving statistics of Battlefield: Bad Company 2 players.
+"""
+
+__version__ = '0.3.16'
+__author__ = 'cetteup'
+__credits__ = 'nemo, Luigi Auriemma'
+__all__ = ['Connection', 'SecureConnection', 'FeslClient', 'TheaterClient',
+           'AsyncConnection', 'AsyncSecureConnection', 'AsyncFeslClient', 'AsyncTheaterClient',
+           'Platform', 'Namespace', 'DEFAULT_LEADERBOARD_KEYS', 'STATS_KEYS',
+           'Error', 'ConnectionError', 'ParameterError', 'TimeoutError',
+           'AuthError', 'NotFoundError', 'PlayerNotFoundError',
+           'ServerNotFoundError', 'LobbyNotFoundError', 'SearchError']
