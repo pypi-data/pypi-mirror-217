837c8de8f555d5ef5aedac114a6ad03b269ea59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:27 +0000

    Add package nodered.py/0.2.4/nodered.py-0.2.4-py3-none-any.whl

---
+++ packages/nodered.py/nodered.py-0.2.4-py3-none-any.whl/noderedpy/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from ._nodered import RED
+from ._property import (
+    InputProperty, ListProperty, DictProperty,
+    SpinnerProperty, ComboBoxProperty, CodeProperty
+)
+
+
+__version__ = "0.2.4"
+__author__ = "oyajiDev"
+__email__ = "this.dev.somehit@gmail.com"
+
+__all__ = [
+    "RED",
+    "InputProperty", "ListProperty", "DictProperty",
+    "SpinnerProperty", "ComboBoxProperty", "CodeProperty",
+]
