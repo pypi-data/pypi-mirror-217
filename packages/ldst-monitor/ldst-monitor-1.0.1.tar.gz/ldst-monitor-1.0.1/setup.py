8c1c0157ce62da41f1a883091d70dee3f29449c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:38 +0000

    Add package ldst-monitor/1.0.1/ldst-monitor-1.0.1.tar.gz

---
+++ packages/ldst-monitor/ldst-monitor-1.0.1.tar.gz/ldst-monitor-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ldst-monitor',
+    version='1.0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='happy',
+    install_requires=["requests", "python-dotenv"],
+    author_email='',
+    description='Monitor server status'
+)
