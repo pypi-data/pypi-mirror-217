94fd95519bc68f69cab0d9101e802c1b09b2932c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:52 +0000

    Add package arcan/1.3.7/arcan-1.3.7-py3-none-any.whl

---
+++ packages/arcan/arcan-1.3.7-py3-none-any.whl/arcan/__init__.py
@@ -0,0 +1,48 @@
+# %%
+from modal import Stub, web_endpoint
+from modal import Image, Stub, web_endpoint
+
+__version__ = "1.3.7"
+
+
+# %%
+# %%
+def get_arcan_version():
+    try:
+        import arcan
+
+        return arcan.__version__
+    except Exception as e:
+        print(e)
+        return f"No arcan package is installed"
+
+
+# %%
+image = Image.debian_slim().pip_install(
+    "fastapi", "uvicorn", "databricks_session", "arcan"
+)
+# api = FastAPI()
+stub = Stub(
+    name="arcan",
+    image=image,
+)
+
+
+@stub.function()
+@web_endpoint(method="GET")
+# @api.get("/")
+def entrypoint():
+    return {"message": "Arcan is running"}
+
+
+@stub.function()
+@web_endpoint(method="GET")
+# @api.get("/api/version")
+def version():
+    print("Arcan is installed")
+    # return the installed version of Arcan package from the pyproject.toml file
+    version = get_arcan_version()
+    return {"message": f"Arcan version {version} is installed"}
+
+
+# %%
