980c1800917052eb1f14a61cc377684bcf163bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:25 +0000

    Add package event-dispatching/0.1.1/event_dispatching-0.1.1.tar.gz

---
+++ packages/event-dispatching/event_dispatching-0.1.1.tar.gz/event_dispatching-0.1.1/event_dispatcher/sync_dispatcher.py
@@ -0,0 +1,26 @@
+"""Sync implementation of event dispatcher
+
+This module provide sync implementation for BaseEventDispatcher.
+All callbacks are executed sequentially and callbacks sequential execution is guaranteed.
+
+Typical usage example:
+
+  dispatcher = SyncEventDispatcher()
+  dispatcher.subscribe("event.name", async_callback)
+  dispatcher.dispatch("event.name", {"event": "data"})
+"""
+
+from event_dispatcher import _dispatcher, types
+
+
+class SyncEventDispatcher(_dispatcher.BaseEventDispatcher[types.SyncCallback]):
+    def dispatch(self, event_name: str, data: types.EventData | None = None) -> bool:
+        subscribers = self._subscribers[event_name]
+
+        if not subscribers:
+            return False
+
+        for subscriber in subscribers:
+            subscriber(data)
+
+        return True
