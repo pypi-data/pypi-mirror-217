9867fcf6d3006254962efc8dbfda3d3295cb9af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:00 +0000

    Add package enigmacrypt/0.0.7/enigmacrypt-0.0.7.tar.gz

---
+++ packages/enigmacrypt/enigmacrypt-0.0.7.tar.gz/enigmacrypt-0.0.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = '0.0.7'
+DESCRIPTION = 'Encoding and decoding strings using keys'
+
+# Setting up
+setup(
+    name="enigmacrypt",
+    version=VERSION,
+    author="WooksCode (Wojciech Karwowski)",
+    author_email="<wookscode.kontakt@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/Wooks08/EnigmaCrypt",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.07",
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"]
+    },
+    keywords=['python', 'encode', 'decode', 'decoder',
+              'encoder', 'string encryption', 'string decryption'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
