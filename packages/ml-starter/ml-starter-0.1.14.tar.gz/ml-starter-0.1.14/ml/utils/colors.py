e2ac221d76825caa7997d493d8ecb79f53633c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:34 +0000

    Add package ml-starter/0.1.14/ml-starter-0.1.14.tar.gz

---
+++ packages/ml-starter/ml-starter-0.1.14.tar.gz/ml-starter-0.1.14/ml/utils/colors.py
@@ -0,0 +1,63 @@
+"""Defines helper functions for printing colored text to the terminal.
+
+Colors can be disabled by setting ``DISABLE_COLORS=1`` in the environment.
+"""
+
+from typing import Literal
+
+from ml.core.env import are_colors_disabled
+
+RESET_SEQ = "\033[0m"
+REG_COLOR_SEQ = "\033[%dm"
+BOLD_COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+
+Color = Literal[
+    "black",
+    "red",
+    "green",
+    "yellow",
+    "blue",
+    "magenta",
+    "cyan",
+    "white",
+    "grey",
+    "light-red",
+    "light-green",
+    "light-yellow",
+    "light-blue",
+    "light-magenta",
+    "light-cyan",
+]
+
+COLOR_INDEX: dict[Color, int] = {
+    "black": 30,
+    "red": 31,
+    "green": 32,
+    "yellow": 33,
+    "blue": 34,
+    "magenta": 35,
+    "cyan": 36,
+    "white": 37,
+    "grey": 90,
+    "light-red": 91,
+    "light-green": 92,
+    "light-yellow": 93,
+    "light-blue": 94,
+    "light-magenta": 95,
+    "light-cyan": 96,
+}
+
+
+def get_colorize_parts(color: Color, bold: bool = False) -> tuple[str, str]:
+    if bold:
+        return BOLD_COLOR_SEQ % COLOR_INDEX[color], RESET_SEQ
+    return REG_COLOR_SEQ % COLOR_INDEX[color], RESET_SEQ
+
+
+def colorize(s: str, color: Color, bold: bool = False) -> str:
+    if are_colors_disabled():
+        return s
+    start, end = get_colorize_parts(color, bold=bold)
+    return start + s + end
