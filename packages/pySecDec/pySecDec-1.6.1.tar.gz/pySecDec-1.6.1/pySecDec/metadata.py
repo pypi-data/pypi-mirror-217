98c4d383a4d46135474801a1859e05bd3649fac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:28 +0000

    Add package pySecDec/1.6.1/pySecDec-1.6.1.tar.gz

---
+++ packages/pySecDec/pySecDec-1.6.1.tar.gz/pySecDec-1.6.1/pySecDec/metadata.py
@@ -0,0 +1,3 @@
+__version__ = version = "1.6.1"
+__authors__ = authors = "Sophia Borowka, Gudrun Heinrich, Stephan Jahn, Stephen Jones, Matthias Kerner, Florian Langer, Vitaly Magerya, Anton Olsson, Andres Poldaru, Johannes Schlenk, Emilio Villa, Tom Zirke"
+__commit__ = git_id = "47e62592dfa1252cf390b8422e672bcd3d05e5ca"
