c0e0394457aee9ab6eef8ea8a1a90b3ca9a59d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:13 +0000

    Add package feedancy-client/0.0.1/feedancy_client-0.0.1.tar.gz

---
+++ packages/feedancy-client/feedancy_client-0.0.1.tar.gz/feedancy_client-0.0.1/feedancy-client/feedancy_client/apis/api/api_v1_region_create.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import datetime
+import pydantic
+import typing
+
+from pydantic import BaseModel
+
+from feedancy_client.lib.base import BaseApi
+from feedancy_client.lib.request import ApiRequest
+from feedancy_client.lib import json
+def make_request(self: BaseApi,
+
+
+) -> None:
+    
+
+    
+    body = None
+    
+
+    m = ApiRequest(
+        method="POST",
+        path="/api/v1/region/".format(
+            
+        ),
+        content_type=None,
+        body=body,
+        headers=self._only_provided({
+        }),
+        query_params=self._only_provided({
+        }),
+        cookies=self._only_provided({
+        }),
+    )
+    return self.make_request({
+    
+        "200": {
+            
+                "default": None,
+            
+        },
+    
+    }, m)
---
+++ packages/feedancy-client/feedancy_client-0.0.1.tar.gz/feedancy_client-0.0.1/feedancy-client/feedancy_client/apis/api/api_v1_stopkeywords_retrieve.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import datetime
+import pydantic
+import typing
+
+from pydantic import BaseModel
+
+from feedancy_client.lib.base import BaseApi
+from feedancy_client.lib.request import ApiRequest
+from feedancy_client.lib import json
+def make_request(self: BaseApi,
+
+
+) -> None:
+    
+
+    
+    body = None
+    
+
+    m = ApiRequest(
+        method="GET",
+        path="/api/v1/stopkeywords/".format(
+            
+        ),
+        content_type=None,
+        body=body,
+        headers=self._only_provided({
+        }),
+        query_params=self._only_provided({
+        }),
+        cookies=self._only_provided({
+        }),
+    )
+    return self.make_request({
+    
+        "200": {
+            
+                "default": None,
+            
+        },
+    
+    }, m)
