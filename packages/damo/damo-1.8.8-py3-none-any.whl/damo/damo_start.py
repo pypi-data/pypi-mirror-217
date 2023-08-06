cc226fd972a9d3a3c5c8d5e18f24f50c3d3345c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:24 +0000

    Add package damo/1.8.8/damo-1.8.8-py3-none-any.whl

---
+++ packages/damo/damo-1.8.8-py3-none-any.whl/damo/damo_start.py
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: GPL-2.0
+
+"""
+Start DAMON with given parameters.
+"""
+
+import _damon
+import _damon_args
+
+def set_argparser(parser):
+    return _damon_args.set_argparser(parser, add_record_options=False)
+
+def main(args=None):
+    if not args:
+        parser = set_pargparser(None)
+        args = parser.parse_args()
+
+    _damon.ensure_root_and_initialized(args)
+
+    err, kdamonds = _damon_args.turn_damon_on(args)
+    if err:
+        print('could not turn on damon (%s)' % err)
+        exit(1)
+
+if __name__ == '__main__':
+    main()
