2a3483515ea3edbbf9c72d89724afb02b88c876d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:20 +0000

    Add package sama-probability/0.1/sama_probability-0.1.tar.gz

---
+++ packages/sama-probability/sama_probability-0.1.tar.gz/sama_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sama_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sama_probability'],
+      author = 'SilEye',
+      author_email = 'samaemail@something.com',
+    zip_safe=False)
