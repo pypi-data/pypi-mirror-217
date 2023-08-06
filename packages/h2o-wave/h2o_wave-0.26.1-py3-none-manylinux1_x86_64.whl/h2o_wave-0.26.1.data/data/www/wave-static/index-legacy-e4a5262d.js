1601bf5bff0cea81f1c36253b7367073f5265e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:24 +0000

    Add package h2o-wave/0.26.1/h2o_wave-0.26.1-py3-none-manylinux1_x86_64.whl

---
+++ packages/h2o-wave/h2o_wave-0.26.1-py3-none-manylinux1_x86_64.whl/h2o_wave-0.26.1.data/data/www/wave-static/index-legacy-e4a5262d.js
@@ -0,0 +1,1 @@
+System.register(["./LocalizationProvider-legacy-7eae8523.js","./index-legacy-7f1e43ab.js","./useThemeProps-legacy-f925c615.js"],(function(e,t){"use strict";return{setters:[function(t){e({LocalizationProvider:t.L,MuiPickersAdapterContext:t.M})},null,null],execute:function(){}}}));
