38845c135fa31f1baaf847d77cec8945c85ad83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:05 +0000

    Add package consumptioncli/1.1.0/consumptioncli-1.1.0-py3-none-any.whl

---
+++ packages/consumptioncli/consumptioncli-1.1.0-py3-none-any.whl/consumptioncli/__main__.py
@@ -0,0 +1,16 @@
+from argparse import ArgumentError
+from .parsers import main_parser
+from consumptionbackend.Database import SQLiteTableInstantiator
+
+def main():
+    args = vars(main_parser.parse_args())
+    subdict = args.pop("subdict") if "subdict" in args else dict() # Default value is empty dict, see SubdictAction.py
+    try:
+        SQLiteTableInstantiator.run()
+        print(args["_handler"].handle(args["_ent"], subdict, **args))
+        return 0
+    except ArgumentError as e:
+        main_parser.error(e.message)
+
+if __name__ == "__main__":
+    main()
