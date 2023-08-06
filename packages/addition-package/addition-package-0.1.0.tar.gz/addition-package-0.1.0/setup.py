5a50408d60ab1617bd18717c12d52853f9269a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:44 +0000

    Add package addition-package/0.1.0/addition-package-0.1.0.tar.gz

---
+++ packages/addition-package/addition-package-0.1.0.tar.gz/addition-package-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='addition-package',
+    version='0.1.0',
+    description='A simple package for addition',
+    author='Rakshitha',
+    author_email='rakshithaprakasam@gmail.com',
+    url='https://github.com/rakshitha/addition-package',
+    packages=find_packages(),
+    install_requires=[
+        # No dependencies for this example
+    ],
+)
