0516187f646224f7e7df0d80a2e2373b308b88c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:02 +0000

    Add package pykrx/1.0.43/pykrx-1.0.43-py3-none-any.whl

---
+++ packages/pykrx/pykrx-1.0.43-py3-none-any.whl/pykrx/__init__.py
@@ -0,0 +1,27 @@
+import platform
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+from . import bond
+from . import stock
+
+os = platform.system()
+
+if os == "Darwin":
+    plt.rc('font', family="AppleGothic")
+
+else:
+    fe = fm.FontEntry(
+        fname="pykrx/NanumBarunGothic.ttf",
+        name='NanumBarunGothic'
+    )
+    fm.fontManager.ttflist.insert(0, fe)
+    plt.rc('font', family=fe.name)
+
+plt.rcParams['axes.unicode_minus'] = False
+
+__all__ = [
+    'bond',
+    'stock'
+]
+
+__version__ = '1.0.43'
