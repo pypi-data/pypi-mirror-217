fec2b6e00ffe614346ebbfea52c05f06b558495e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:01 +0000

    Add package chroma-datasets/0.1.3/chroma_datasets-0.1.3-py3-none-any.whl

---
+++ packages/chroma-datasets/chroma_datasets-0.1.3-py3-none-any.whl/chroma_datasets/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.3"
+
+from .datasets.state_of_the_union import StateOfTheUnion
+from .datasets.glue import Glue
+from .datasets.paul_graham_essay import PaulGrahamEssay
+from .datasets.sciq import SciQ
