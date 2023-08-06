1f9b22af94c2ef83538d0189655f209694d3f4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:51 +0000

    Add package nonebot-plugin-alconna/0.9.0/nonebot-plugin-alconna-0.9.0.tar.gz

---
+++ packages/nonebot-plugin-alconna/nonebot-plugin-alconna-0.9.0.tar.gz/nonebot-plugin-alconna-0.9.0/src/nonebot_plugin_alconna/__init__.py
@@ -0,0 +1,51 @@
+from nonebot import __version__ as nonebot_version
+from nonebot.plugin import PluginMetadata
+
+from .argv import MessageArgv as MessageArgv
+from .consts import ALCONNA_RESULT as ALCONNA_RESULT
+from .matcher import on_alconna as on_alconna
+from .matcher import funcommand as funcommand
+from .model import CommandResult as CommandResult
+from .model import Match as Match
+from .model import Query as Query
+from .params import AlcMatches as AlcMatches
+from .params import AlcResult as AlcResult
+from .params import AlconnaDuplication as AlconnaDuplication
+from .params import AlconnaMatch as AlconnaMatch
+from .params import AlconnaMatches as AlconnaMatches
+from .params import AlconnaQuery as AlconnaQuery
+from .params import AlconnaResult as AlconnaResult
+from .params import Check as Check
+from .params import assign as assign
+from .params import match_path as match_path
+from .params import match_value as match_value
+from .rule import alconna as alconna
+from .rule import set_output_converter as set_output_converter
+from .config import Config
+
+__version__ = "0.9.0"
+
+_meta_source = {
+    "name": "Alconna 插件",
+    "description": "提供 [Alconna](https://github.com/ArcletProject/Alconna) 的 Nonebot2 适配版本与工具",
+    "usage": "matcher = on_alconna(...)",
+    "homepage": "https://github.com/ArcletProject/Alconna",
+    "type": "library",
+    "supported_adapters": None,
+    "config": Config,
+    "extra": {
+        "author": "RF-Tar-Railt",
+        "priority": 1,
+        "version": __version__,
+    }
+}
+
+
+if not nonebot_version.split(".")[-1].isdigit():
+    _meta_source["extra"]["homepage"] = _meta_source.pop("homepage")
+    _meta_source["extra"]["type"] = _meta_source.pop("type")
+    _meta_source["extra"]["config"] = _meta_source.pop("config")
+    _meta_source["extra"]["supported_adapters"] = _meta_source.pop("supported_adapters")
+
+
+__plugin_meta__ = PluginMetadata(**_meta_source)
---
+++ packages/nonebot-plugin-alconna/nonebot-plugin-alconna-0.9.0.tar.gz/nonebot-plugin-alconna-0.9.0/src/nonebot_plugin_alconna/model.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass, field
+from typing import Generic, Optional, TypedDict, TypeVar
+from typing_extensions import NotRequired
+
+from arclet.alconna import Alconna, Arparma, Empty
+from arclet.alconna.duplication import Duplication
+
+T = TypeVar("T")
+T_Duplication = TypeVar("T_Duplication", bound=Duplication)
+
+
+@dataclass
+class Match(Generic[T]):
+    """
+    匹配项，表示参数是否存在于 `all_matched_args` 内
+    result (T): 匹配结果
+    available (bool): 匹配状态
+    """
+
+    result: T
+    available: bool
+
+
+class Query(Generic[T]):
+    """
+    查询项，表示参数是否可由 `Arparma.query` 查询并获得结果
+
+    result (T): 查询结果
+
+    available (bool): 查询状态
+
+    path (str): 查询路径
+    """
+    result: T
+    available: bool
+    path: str
+
+    def __init__(self, path: str, default: T = Empty):
+        self.path = path
+        self.result = default
+        self.available = False
+
+    def __repr__(self):
+        return f"Query({self.path}, {self.result})"
+
+
+@dataclass(frozen=True)
+class CommandResult:
+    source: Alconna
+    result: Arparma
+    output: Optional[str] = field(default=None)
+
+    @property
+    def matched(self) -> bool:
+        return self.result.matched
+
+
+class CompConfig(TypedDict):
+    priority: NotRequired[int]
+    tab: NotRequired[str]
+    enter: NotRequired[str]
+    exit: NotRequired[str]
+    timeout: NotRequired[int]
