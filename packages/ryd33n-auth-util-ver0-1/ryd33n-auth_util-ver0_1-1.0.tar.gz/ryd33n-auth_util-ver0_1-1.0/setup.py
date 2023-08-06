336f54b192bd361a3706edd4209b3451613f826b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:56 +0000

    Add package ryd33n-auth-util-ver0-1/1.0/ryd33n-auth_util-ver0_1-1.0.tar.gz

---
+++ packages/ryd33n-auth-util-ver0-1/ryd33n-auth_util-ver0_1-1.0.tar.gz/ryd33n-auth_util-ver0_1-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ryd33n-auth_util-ver0_1',
+      version='1.0',
+      description='Auth system',
+      author='Dmitry "ryd33n" Kozlovsky',
+      author_email='rem.dk83@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['project'],
+      )
