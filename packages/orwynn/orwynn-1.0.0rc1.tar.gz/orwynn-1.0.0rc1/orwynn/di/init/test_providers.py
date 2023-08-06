5bba3613a8e3bfe3263b9c5d59461de9b3df73c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:26 +0000

    Add package orwynn/1.0.0rc1/orwynn-1.0.0rc1.tar.gz

---
+++ packages/orwynn/orwynn-1.0.0rc1.tar.gz/orwynn-1.0.0rc1/orwynn/di/init/test_providers.py
@@ -0,0 +1,21 @@
+from orwynn.base.module.module import Module
+from orwynn.boot.boot import Boot
+from orwynn.di.collecting.providerdependencies.map import (
+    ProviderDependenciesMap,
+)
+from orwynn.di.container import DiContainer
+from orwynn.di.init.providers import init_providers
+from tests.std.assertion import Assertion
+
+
+def test_std(
+    std_struct: Module,
+    std_provider_dependencies_map: ProviderDependenciesMap
+):
+    Boot(std_struct)
+    container: DiContainer = init_providers(
+        std_provider_dependencies_map
+    )
+
+    for P in Assertion.COLLECTED_PROVIDERS:
+        isinstance(container.find(P.__name__), P)
