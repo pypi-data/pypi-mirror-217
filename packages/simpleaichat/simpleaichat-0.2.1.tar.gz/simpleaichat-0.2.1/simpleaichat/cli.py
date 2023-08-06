b1a57ee1b8cac6d72371b898ad11996217978399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:49 +0000

    Add package simpleaichat/0.2.1/simpleaichat-0.2.1.tar.gz

---
+++ packages/simpleaichat/simpleaichat-0.2.1.tar.gz/simpleaichat-0.2.1/simpleaichat/cli.py
@@ -0,0 +1,20 @@
+import fire
+import os
+from getpass import getpass
+from .simpleaichat import AIChat
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def interactive_chat(character=None, character_command=None, prime=True):
+    gpt_api_key = os.getenv("OPENAI_API_KEY")
+    if not gpt_api_key:
+        gpt_api_key = getpass("Input your OpenAI key here: ")
+    assert gpt_api_key, "An API key was not defined."
+    _ = AIChat(character=character, character_command=character_command, prime=prime)
+    return
+
+
+if __name__ == "__main__":
+    fire.Fire(interactive_chat)
