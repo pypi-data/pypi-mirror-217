4b36cb7637c1596e33c2a5b95faf5f6b13c5a10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:08 +0000

    Add package tecoradors-elunico/5.2.1/tecoradors-elunico-5.2.1.tar.gz

---
+++ packages/tecoradors-elunico/tecoradors-elunico-5.2.1.tar.gz/tecoradors-elunico-5.2.1/tecoradors/__init__.py
@@ -0,0 +1,30 @@
+from tecoradors.tecoradors import (
+    accepts, dataclass, deprecated, equatable, final, freeze, FrozenClassError, hashable, json_serializable, log, Self,
+    spread, squash, stringable, synchronized, timed, count_calls, returns, interruptable, lazy, precompute, PrecomputeStorage, NoSuchValue
+)
+
+__all__ = [
+    "accepts",
+    "json_serializable",
+    "spread",
+    "timed",
+    "squash",
+    "stringable",
+    "equatable",
+    "hashable",
+    "dataclass",
+    "Self",
+    "final",
+    "deprecated",
+    "freeze",
+    "FrozenClassError",
+    "log",
+    "synchronized",
+    "count_calls",
+    "returns",
+    "interruptable",
+    "lazy",
+    "precompute",
+    "NoSuchValue",
+    "PrecomputeStorage"
+]
