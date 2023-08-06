734f4b007c6bc90d9aadcf10eb529d9b27f774e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:11 +0000

    Add package langful/0.32/langful-0.32-py3-none-any.whl

---
+++ packages/langful/langful-0.32-py3-none-any.whl/langful/__init__.py
@@ -0,0 +1,15 @@
+"""
+# langful
+
+Help to localization
+
+# About
+
+github: https://github.com/cueavyqwp/langful
+
+pypi: https://pypi.org/project/langful
+
+issues: https://github.com/cueavyqwp/langful/issues
+"""
+
+from .lang import *
