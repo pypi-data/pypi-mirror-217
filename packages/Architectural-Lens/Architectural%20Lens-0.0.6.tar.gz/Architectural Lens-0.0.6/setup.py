58ffb57583acccca1cb3162ef9c4959f21c3e048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:38 +0000

    Add package Architectural-Lens/0.0.6/Architectural%20Lens-0.0.6.tar.gz

---
+++ packages/Architectural-Lens/Architectural%20Lens-0.0.6.tar.gz/Architectural Lens-0.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Architectural Lens",
+    version="0.0.6",
+    description="Thesis project",
+    author="Nikolai Perlt",
+    author_email="npe@itu.dk",
+    url="https://github.com/Perlten/MT-diagrams",
+    packages=find_packages(),
+    long_description="This is the long description",
+    install_requires=[
+        "plantuml",
+        "typer",
+        "astroid",
+        "six",
+        "requests",
+        "jsonschema",
+        "gitpython",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+    ],
+    entry_points={
+        "console_scripts": [
+            "archlens=src.cli_interface:main",
+        ],
+    },
+)
