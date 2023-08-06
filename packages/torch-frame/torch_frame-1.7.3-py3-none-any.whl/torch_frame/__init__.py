c0e5adb3bff9f74211f46660ca58205b7c8e8fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:08 +0000

    Add package torch-frame/1.7.3/torch_frame-1.7.3-py3-none-any.whl

---
+++ packages/torch-frame/torch_frame-1.7.3-py3-none-any.whl/torch_frame/__init__.py
@@ -0,0 +1,12 @@
+from ._get_logger import logger
+from .hooks import *
+from .lr_scheduler import LRWarmupScheduler
+from .utils import HistoryBuffer, misc, metric
+from .trainer import Trainer, MetricStorage
+from .ddp_trainer import DDPTrainer
+
+
+#  关闭opencv的多线程防止pytorch死锁
+import cv2
+cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
