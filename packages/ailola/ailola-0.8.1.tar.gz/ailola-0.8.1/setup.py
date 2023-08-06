c48eeec2fd229ffdf49eb6cc216e5b7b1ff941cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:35 +0000

    Add package ailola/0.8.1/ailola-0.8.1.tar.gz

---
+++ packages/ailola/ailola-0.8.1.tar.gz/ailola-0.8.1/setup.py
@@ -0,0 +1,32 @@
+# from setuptools import setup
+
+# setup(
+#     name='ailola',
+#     version=1.0,
+#     license='MIT',
+#     author="Elhay Efrat",
+#     author_email="elhayefrat@gmail.com"
+#     packages=['v'],
+#     install_requires=['click', 'requests' ,'openai'],
+#     entry_points={'console_scripts': ['ailola = ailola:apis']}
+# )
+
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ailola',
+    version='0.8.1',
+    license='MIT',
+    author="Elhay Efrat",
+    author_email='elhayefrat@gmail.com',
+    packages=find_packages('ailola'),
+    package_dir={'': 'ailola'},
+    url='https://github.com/lola-pola/ailola',
+    keywords='lola cli terraform ai',
+    install_requires=['click', 'requests' ,'openai'],
+    entry_points={'console_scripts': ['ailola = ailola:cli']}
+
+)
