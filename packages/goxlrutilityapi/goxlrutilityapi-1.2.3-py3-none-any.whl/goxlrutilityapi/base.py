fb1bedb4505e2a6d2bb759c0d544301473ca7c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:36 +0000

    Add package goxlrutilityapi/1.2.3/goxlrutilityapi-1.2.3-py3-none-any.whl

---
+++ packages/goxlrutilityapi/goxlrutilityapi-1.2.3-py3-none-any.whl/goxlrutilityapi/base.py
@@ -0,0 +1,14 @@
+"""GoXLR Utility API: Base"""
+from __future__ import annotations
+
+import logging
+
+
+class Base:  # pylint: disable=too-few-public-methods
+    """Base"""
+
+    def __init__(self):
+        """Initialize"""
+        name = f"{self.__module__}.{self.__class__.__name__}"
+        self._logger = logging.getLogger(name)
+        self._logger.debug("%s __init__", name)
