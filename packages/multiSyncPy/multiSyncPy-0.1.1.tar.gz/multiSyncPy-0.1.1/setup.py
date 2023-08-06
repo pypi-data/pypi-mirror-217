e7b94fef622ca499665b782ec6516fb32ede8324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:01 +0000

    Add package multiSyncPy/0.1.1/multiSyncPy-0.1.1.tar.gz

---
+++ packages/multiSyncPy/multiSyncPy-0.1.1.tar.gz/multiSyncPy-0.1.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="multiSyncPy",
+    version="0.1.1",
+    description="Functions to quantify multivariate synchrony",
+    long_description_content_type="text/markdown",
+    long_description=README,
+    url="https://github.com/cslab-hub/multiSyncPy",
+    author="Dan Hudson",
+    author_email="daniel.dominic.hudson@uni-osnabrueck.de",
+    license="GNU LGPL",
+    classifiers=[
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["multiSyncPy"],
+    ##include_package_data=True,
+    install_requires=["numpy", "scipy", "scikit-learn", "seaborn", "matplotlib"],
+)
