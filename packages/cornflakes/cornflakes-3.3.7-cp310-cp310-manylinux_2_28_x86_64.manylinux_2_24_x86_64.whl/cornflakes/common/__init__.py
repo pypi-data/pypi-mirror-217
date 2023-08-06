23e3664c6c2d414fbfdf37bf493ea8589cb83d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:50 +0000

    Add package cornflakes/3.3.7/cornflakes-3.3.7-cp310-cp310-manylinux_2_28_x86_64.manylinux_2_24_x86_64.whl

---
+++ packages/cornflakes/cornflakes-3.3.7-cp310-cp310-manylinux_2_28_x86_64.manylinux_2_24_x86_64.whl/cornflakes/common/__init__.py
@@ -0,0 +1,17 @@
+"""cornflakes common module.
+__________________________________
+"""  # noqa: RST303 D205
+from cornflakes.common._default_ca_path import default_ca_path
+from cornflakes.common._extract_var_names import extract_var_names
+from cornflakes.common._patch_module import patch_module
+from cornflakes.common._type_to_str import type_to_str
+from cornflakes.common._types import datetime_ms, unquoted_string
+
+__all__ = [
+    "default_ca_path",
+    "type_to_str",
+    "datetime_ms",
+    "extract_var_names",
+    "unquoted_string",
+    "patch_module",
+]
