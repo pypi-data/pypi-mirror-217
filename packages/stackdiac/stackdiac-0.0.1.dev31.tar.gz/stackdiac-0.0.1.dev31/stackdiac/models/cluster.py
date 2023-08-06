db8acf4d12e59fe3317ce1a197124d35bcdf614d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:35 +0000

    Add package stackdiac/0.0.1.dev31/stackdiac-0.0.1.dev31.tar.gz

---
+++ packages/stackdiac/stackdiac-0.0.1.dev31.tar.gz/stackdiac-0.0.1.dev31/stackdiac/models/cluster.py
@@ -0,0 +1,329 @@
+
+import os
+from urllib.parse import urlparse
+from pydantic import BaseModel, parse_obj_as
+import logging
+from typing import Any
+from deepmerge import always_merger
+
+import yaml, hvac
+from stackdiac.models.backend import Backend
+from stackdiac.models.spec import Spec, SpecModel
+
+from stackdiac.models.operation import Operation
+
+from .stack import Stack, StackModel, Module
+from .secret import Secret
+
+logger = logging.getLogger(__name__)
+
+class ClusterStackModel(BaseModel):
+    name: str | None = None
+    cluster_name: str | None = None
+    src: str | None = None
+    vars: dict[str, Any] = {}
+    module_vars: dict[str, Any] = {}
+    stack: StackModel | None = None
+    override: dict[str, Any] = {}
+    backend: Backend | None = None
+    operations: Any = {}
+    
+
+class ClusterStack(ClusterStackModel):
+    stack: Stack | None = None
+
+    class Config:
+        arbitrary_types_allowed = True
+        exclude = ["stack"]
+
+    def __str__(self) -> str:
+        return f"<{self.__class__.__name__}:{self.name}>"
+
+    def build(self, cluster, sd, **kwargs):
+
+        #from stackdiac.stackd import sd
+        
+        self.cluster_name = cluster.name
+
+        if self.src is None:
+            self.src = self.name
+
+        # src is url with repo in scheme
+        parsed_src = urlparse(self.src)
+        if not parsed_src.scheme:
+            parsed_src = urlparse(f"root:{self.src}") # add root repo
+        if len(self.src.split("/")) == 1:
+            parsed_src = urlparse(f"{parsed_src.scheme}:stack/{self.src}") # add stack dir
+
+        repo = sd.conf.repos[parsed_src.scheme]
+
+        if self.src.endswith(".yaml"):
+            path = os.path.join(repo.repo_dir, parsed_src.path.lstrip("/"))           
+        else:
+            path = os.path.join(repo.repo_dir, parsed_src.path.lstrip("/"), "stack.yaml")
+            
+
+        url = parsed_src.geturl()
+
+        stack_dir = os.path.dirname(path)
+
+        self.stack = Spec(path=path, 
+                        merge_from=always_merger.merge({"name": self.name}, self.override), 
+                        jinja_env=sd.get_jinja_env(stack_dir)).parse_obj_as(Stack, stackd=sd, cluster=cluster)
+
+      
+     #   logger.debug(f"{self} stack: {self.stack}")
+        self.stack.build(cluster_stack=self, cluster=cluster, sd=sd, **kwargs)
+        cluster.built_stacks[self.name] = self.stack
+
+        sd.counters.stacks += 1
+
+class ClusterModel(BaseModel):
+    name: str
+    vars: dict[str, Any] = {}
+    stacks: dict[str, ClusterStackModel] = {}    
+    backend: Backend | None = None
+    spec: SpecModel | None = None
+
+class Cluster(ClusterModel):   
+    stacks: dict[str, ClusterStack] = {}    
+    built_stacks: dict[str, Stack] = {}
+    spec: Spec | None = None
+
+
+    class Config:        
+        exceptions = True
+        exclude = {"built_stacks"}
+
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+        for sname, s in self.stacks.items():
+            # s.cluster = self
+            s.name = sname
+
+
+    def build(self, sd, stack="all", **kwargs):
+        sd.counters.clusters += 1
+        if stack == "all":
+            for s in self.stacks.values():
+                s.build(cluster=self, sd=sd, **kwargs)
+        else:
+            s = self.stacks[stack]
+            s.build(cluster=self, sd=sd, **kwargs)
+        
+
+from stackdiac.api import app as api_app
+
+
+@api_app.get("/clusters/", operation_id="get_clusters", response_model=list[ClusterModel], tags=["cluster"])
+async def _api_get_clusters() -> list[Cluster]:    
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    
+    return list(sd.clusters.values())
+
+@api_app.get("/build/{cluster_name}", operation_id="build_cluster", response_model=ClusterModel, tags=["cluster"])
+async def build_cluster(cluster_name:str) -> Cluster:
+    """
+    cluster.stacks will setup while bulding
+    """
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    cluster = sd.clusters[cluster_name]
+    cluster.build(sd=sd)
+    sd.counters.stop()
+    logger.info(f"build_cluster: {cluster_name} {sd.counters}")
+    return cluster
+
+@api_app.get("/cluster/{cluster_name}", operation_id="read_cluster", response_model=ClusterModel, tags=["cluster"])
+async def read_cluster(cluster_name:str) -> Cluster:
+    """
+    cluster.stacks will setup while bulding
+    """
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    cluster = sd.clusters[cluster_name]
+    cluster.build(sd=sd)
+    sd.counters.stop()
+    logger.info(f"build_cluster: {cluster_name} {sd.counters}")
+    return cluster
+
+@api_app.get("/stack/{cluster_name}/{stack_name}", operation_id="read_cluster_stack", response_model=ClusterStackModel, tags=["stack"])
+async def read_cluster_stack(cluster_name:str, stack_name:str) -> ClusterStack:
+    """
+    cluster.stacks will setup while bulding
+    """
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    cluster = sd.clusters[cluster_name]
+    cluster.build(sd=sd)
+    sd.counters.stop()
+    logger.info(f"build_cluster: {cluster_name} {sd.counters}")
+    try:
+        return cluster.stacks[stack_name]
+    except KeyError:
+        raise Exception(f"Stack {stack_name} not found in cluster {cluster_name}")
+    
+@api_app.get("/module/{cluster_name}/{stack_name}/{module_name}", operation_id="cluster_stack_module", response_model=Module, tags=["modules"])
+async def build_module(cluster_name:str, stack_name:str, module_name:str) -> Module:
+    """
+    cluster.stacks will setup while bulding
+    """
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    cluster = sd.clusters[cluster_name]
+    cluster.build(sd=sd)
+    sd.counters.stop()
+    logger.info(f"build_cluster: {cluster_name} {sd.counters}")
+    try:
+        return cluster.stacks[stack_name].stack.modules[module_name]
+    except KeyError:
+        raise Exception(f"Module {module_name} not found in stack {stack_name} in cluster {cluster_name}")
+    
+@api_app.post("/vars/{cluster_name}/{stack_name}/{module_name}", operation_id="write_module_vars", tags=["modules"])
+async def write_module_vars(cluster_name:str, stack_name:str, module_name:str, vars:dict) -> Module:
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    cluster = sd.clusters[cluster_name]
+    cluster.build(sd=sd)
+    sd.counters.stop()
+    logger.info(f"build_cluster: {cluster_name} {sd.counters}")
+    vars_file = cluster.stacks[stack_name].stack.modules[module_name].build_vars_file(cluster=cluster, sd=sd,
+                                                                                    module=cluster.stacks[stack_name].stack.modules[module_name],
+                                                                                    cluster_stack=cluster.stacks[stack_name])
+    if not os.path.isdir(os.path.dirname(vars_file)):
+        os.makedirs(os.path.dirname(vars_file))
+        logger.info(f"write_module_vars: {vars_file} created")
+
+    with open(vars_file, "w") as f:
+        yaml.dump(vars, f)
+
+    cluster.build(sd=sd)
+    return cluster.stacks[stack_name].stack.modules[module_name]
+
+    
+@api_app.get("/secret/{cluster_name}/{stack_name}/{module_name}", operation_id="list_module_secrets", tags=["secrets"])
+async def list_module_secrets(cluster_name:str, stack_name:str, module_name:str) -> list[Secret]:
+    """
+    module secrets list
+    """
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    cluster = sd.clusters[cluster_name]
+    cluster.build(sd=sd)
+    sd.counters.stop()
+    logger.info(f"build_cluster: {cluster_name} {sd.counters}")
+    try:
+        m = cluster.stacks[stack_name].stack.modules[module_name]
+    except KeyError:
+        raise Exception(f"Module {module_name} not found in stack {stack_name} in cluster {cluster_name}")
+    
+    try:
+        resp = sd.vault.kv.v2.list_secrets(path=m.built_vars["module_secret_path"], mount_point='kv')
+    except hvac.exceptions.InvalidPath as e:
+        logger.error(f"list_module_secrets: {e}")
+        return []
+    
+    def _get_secrets():
+        for k in resp["data"]["keys"]:
+            rr = sd.vault.kv.v2.read_secret_version(path=f"{m.built_vars['module_secret_path']}/{k}", mount_point='kv')
+            logger.debug(f"list_module_secrets: {rr}")
+            data = dict(
+                module_name=module_name,
+                name=k,
+                stack_name=stack_name,
+                cluster_name=cluster_name,                
+                **rr["data"])
+            
+            if rr["data"]["metadata"]["custom_metadata"] and rr["data"]["metadata"]["custom_metadata"].get("schema", False):
+                data["secret_type"] = rr["data"]["metadata"]["custom_metadata"]["schema"]
+                data["secret_schema"] = cluster.stacks[stack_name].stack.stack_schema['components']['schemas'][data["secret_type"]]
+   
+            yield data
+
+    return list(_get_secrets())
+
+@api_app.get("/secret/{cluster_name}/{stack_name}/{module_name}/{secret_name}", operation_id="read_module_secret", response_model=Secret, tags=["secrets"])
+async def read_module_secret(cluster_name:str, stack_name:str, module_name:str, secret_name:str) -> Secret:
+    """
+    module secrets list
+    """
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    cluster = sd.clusters[cluster_name]
+    cluster.build(sd=sd)
+    sd.counters.stop()
+    logger.info(f"build_cluster: {cluster_name} {sd.counters}")
+    try:
+        m = cluster.stacks[stack_name].stack.modules[module_name]
+    except KeyError:
+        raise Exception(f"Module {module_name} not found in stack {stack_name} in cluster {cluster_name}")
+    
+    resp = sd.vault.kv.v2.read_secret_version(path=f'{m.built_vars["module_secret_path"]}/{secret_name}', mount_point='kv')
+    
+    data = dict(
+                module_name=module_name,
+                name=secret_name,
+                stack_name=stack_name,
+                cluster_name=cluster_name,
+                **resp["data"])
+    
+    logger.info("read_module_secret: %s", resp)
+
+    if resp["data"]["metadata"]["custom_metadata"].get("schema", False):
+        data["secret_type"] = resp["data"]["metadata"]["custom_metadata"]["schema"]
+        data["secret_schema"] = cluster.stacks[stack_name].stack.stack_schema['components']['schemas'][data["secret_type"]]
+        
+    return data
+
+    
+    
+
+@api_app.post("/secret/{cluster_name}/{stack_name}/{module_name}/{secret_name}", operation_id="write_module_secret", response_model=Secret,
+              tags=["secrets"])
+async def write_module_secret(cluster_name:str, stack_name:str, module_name:str, secret_name:str, 
+                              secret_type:str, secret:dict) -> Secret:
+    """
+    module secrets list
+    """
+    from stackdiac.stackd import Stackd
+    sd = Stackd()
+    sd.configure()
+    cluster = sd.clusters[cluster_name]
+    cluster.build(sd=sd)
+    sd.counters.stop()
+    logger.info(f"build_cluster: {cluster_name} {sd.counters}")
+    try:
+        m = cluster.stacks[stack_name].stack.modules[module_name]
+    except KeyError:
+        raise Exception(f"Module {module_name} not found in stack {stack_name} in cluster {cluster_name}")
+    
+    data = secret
+    resp = sd.vault.kv.v2.create_or_update_secret(path=f'{m.built_vars["module_secret_path"]}/{secret_name}', mount_point='kv', 
+                                                  secret=secret)    
+    logger.info(f"saved secret to {m.built_vars['module_secret_path']}/{secret_name} version: {resp['data']['version']}")
+    logger.debug(f"saved secret to {m.built_vars['module_secret_path']}/{secret_name} version: {resp} data: {data} secret: {secret} <<<")
+
+    resp = sd.vault.kv.v2.read_secret_version(path=f'{m.built_vars["module_secret_path"]}/{secret_name}', mount_point='kv')    
+
+    if not resp["data"]["metadata"]["custom_metadata"] or ("schema" not in resp["data"]["metadata"]["custom_metadata"]):
+        sd.vault.kv.v2.update_metadata(path=f'{m.built_vars["module_secret_path"]}/{secret_name}', mount_point='kv', 
+                                                        custom_metadata=dict(schema=secret_type))
+        logger.info(f"saved secret metadata to {m.built_vars['module_secret_path']}/{secret_name} version: {resp['data']['version']}")
+        resp = sd.vault.kv.v2.read_secret_version(path=f'{m.built_vars["module_secret_path"]}/{secret_name}', mount_point='kv')
+
+    return dict(
+                module_name=module_name,
+                name=secret_name,
+                stack_name=stack_name,
+                cluster_name=cluster_name,
+                secret_type=secret_type,
+                **resp["data"])
---
+++ packages/stackdiac/stackdiac-0.0.1.dev31.tar.gz/stackdiac-0.0.1.dev31/stackdiac/models/stack.py
@@ -0,0 +1,393 @@
+
+from urllib.parse import urlparse
+from pydantic import BaseModel, parse_obj_as, Field
+from deepmerge import always_merger
+from copy import deepcopy
+import logging, os
+from typing import Any
+
+import yaml
+from stackdiac.models.backend import Backend
+from stackdiac.models.spec import Spec, SpecModel
+
+from stackdiac.models.operation import Operation
+from stackdiac.models.provider import Provider
+
+import hvac
+from enum import Enum
+
+logger = logging.getLogger(__name__)
+
+        
+
+
+class Variable(BaseModel):
+    """
+    terraform variable
+    """
+    name:str
+    value: Any
+    type: str = "string"
+
+class ModuleDependency(BaseModel):
+    name: str
+    path: str
+    abspath: str
+    module_name: str
+    stack_name: str
+    alias: str | None = None
+
+    @property
+    def varname(self) -> str:
+        if self.alias:
+            return self.alias
+        return f"{self.stack_name}_{self.module_name}".replace("-", "_")
+
+class ModuleSecretStatus(Enum):
+    UNKNOWN = "unknown"
+    NOT_EXISTS = "not_exists"
+    EXISTS = "exists"
+    VALID   = "valid"
+
+class ModuleSecret(BaseModel):
+    name: str | None = None
+    secret_type: str | None = None
+    secret_schema: dict | None = None
+    required: bool = False
+    status: ModuleSecretStatus = ModuleSecretStatus.UNKNOWN
+
+    def build(self, cluster, cluster_stack, stack, sd, module, status, **kwargs):       
+
+        if self.secret_schema is None and self.secret_type is not None:
+            # extracting schema from stack.schema.components.schemas
+            self.secret_schema = stack.stack_schema['components']['schemas'][self.secret_type]
+            self.status = status
+
+class ModuleSchemas(BaseModel):
+    secrets: dict[str, ModuleSecret] = {}
+    vars: str | None = None
+
+    schemas: dict[str, Any] = {}
+
+    def build(self, cluster, cluster_stack, stack, sd, module, **kwargs):        
+        if self.vars:
+            self.schemas["vars"] = stack.stack_schema['components']['schemas'][self.vars]
+
+  
+
+class Module(BaseModel):
+    name: str | None = None
+    source: str | None = None
+    src: str | None = None
+    vars: dict[str, Any] = {}
+    module_vars: dict[str, Any] = {}
+    built_vars: dict[str, Any] = {}
+    providers: list[str] = []
+    provider_overrides: dict[str, Any] = {}
+    inputs: Any = []
+    deps: list[str] = []
+    tf_state_key: str | None = None
+    tf_state_bucket: str | None = None
+    tf_state_bucket_region: str | None = None
+    tf_backend: str = "s3"
+    tf_backend_config: dict[str, Any] = {}
+    backend: Backend | None = None
+    secrets: dict[str, ModuleSecret] = {}
+    schemas: ModuleSchemas | None = None
+    
+
+    def __str__(self) -> str:
+        return f"<{self.__class__.__name__}:{self.name}>"
+
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+        if self.source:
+            self.src = self.source            
+
+        for name, secret in self.secrets.items():
+            if secret.name is None:
+                secret.name = name
+
+    @property
+    def abssrc(self) -> str:
+        from stackdiac.stackd import sd
+        assert self.src
+        return sd.resolve_path(self.src)
+        #return os.path.join(sd.root, self.src)
+
+    @property
+    def absdeps(self) -> list[str]:
+        return [ os.path.join(os.path.dirname(self.abssrc), d) for d in self.deps ]
+    
+    
+    def build_vars_file(self, sd, cluster_stack, cluster, **kwargs) -> str:
+        return os.path.join(sd.root, "vars",  cluster.name, cluster_stack.name, self.name, "vars.yaml")
+    
+    def build_module_vars(self, sd, cluster_stack, cluster, **kwargs) -> dict[str, Any]:
+        vars_file = self.build_vars_file(sd, cluster_stack, cluster)
+        if os.path.exists(vars_file):
+            return yaml.load(open(vars_file), Loader=yaml.FullLoader)
+        else:
+            return {}
+   
+
+    def build_deps(self, stack, module, cluster, deps, sd, **kwargs) -> list[ModuleDependency]:
+        if type(deps) is list:
+            _deps = { d: None for d in deps }
+        elif type(deps) is dict:
+            _deps = deps
+        else:
+            raise Exception(f"invalid deps type {type(deps)}")
+        
+        for d, alias in _deps.items():            
+            parts = [x for x in d.split("/") if x]
+            if len(parts) == 2:
+                stack_name, module_name = parts
+            elif len(parts) == 1:
+                module_name = parts[0]
+                stack_name = stack.name
+            else:
+                raise Exception(f"invalid module dep {d}")
+           
+            yield ModuleDependency(
+                name=d,
+                path=d,
+                abspath=os.path.join(sd.builddir, cluster.name, stack_name, module_name),
+                module_name=module_name,
+                stack_name=stack_name,
+                alias=alias
+            )
+
+    @property
+    def charts_root(self) -> str:
+        from stackdiac.stackd import sd
+        return os.path.join(sd.root, "charts")
+
+    def get_template(self, template_name):
+        from stackdiac.stackd import sd
+        return sd.conf.repos["core"].get_jinja_env().get_template(template_name)
+
+    def write(self, template_name, dest, **kwargs):
+        tpl = self.get_template(template_name)
+        
+        with open(dest, "w") as f:
+            f.write(tpl.render(**kwargs))
+
+        # logger.debug(f"{self} writed {dest} from {tpl}")
+
+    @property
+    def remote_state_template(self) -> str:
+        return f"remote_state.stackd.hcl.j2"
+
+    
+    def get_namespace(self, stack) -> str:
+        return f"{stack.name}-{self.name}"
+
+    
+
+    def get_prefix(self, stack) -> str:
+        return f"{stack.name}"
+
+    
+    def get_ingress_host(self, cluster, stack) -> str:
+        from stackdiac.stackd import sd
+        if self.name.startswith("in-"):
+            name = self.name[3:]
+        else:
+            name = self.name
+        return f"{stack.name}-{name}.{cluster.name}.{sd.dns_zone}"
+
+    def build_vars(self, sd, cluster, cluster_stack, stack, **kwargs):
+        
+        return dict(
+            prefix=self.get_prefix(stack),
+            cluster_name=cluster.name,
+            cluster=cluster.name,
+            env=cluster.name,
+            service=self.name,
+            tg_abspath=self.abssrc,
+            group="all", # legacy
+            environment=cluster.name,
+            ingress_host=self.get_ingress_host(cluster, stack),
+            namespace=self.get_namespace(stack),
+            charts_root=self.charts_root,
+            module_secret=f"kv/{cluster.name}/module/{stack.name}/{self.name}",
+            module_secret_path=f"{cluster.name}/module/{stack.name}/{self.name}",
+        )
+
+    @property
+    def tg_module_src(self):
+        """
+            adding // before last path component to avoid terragrunt to download the module 
+        """
+        return f"{os.path.dirname(self.abssrc)}//{os.path.basename(self.abssrc)}"
+
+    def get_build_dir(self, cluster, stack):
+        from stackdiac.stackd import sd
+        return os.path.join(sd.root, "build", cluster.name, stack.name, self.name)
+
+    def build(self, cluster, cluster_stack, stack, sd, extra_vars={}, **kwargs):
+        #from stackdiac.stackd import sd
+        path = sd.resolve_module_path(self.src)
+        dest = self.get_build_dir(cluster, stack)
+        os.makedirs(dest, exist_ok=True)
+        _vars = {
+            'build_path': dest,
+            'module_path': path,
+            'project_root': sd.root,
+        }
+        
+        varSources = [
+            self.build_vars(sd, cluster, cluster_stack, stack, **kwargs),                      
+        ]
+        
+        for v in varSources:
+            always_merger.merge(_vars, deepcopy(v))       
+        
+        _build_vars = deepcopy(_vars)
+
+        self.module_vars = self.build_module_vars(sd, cluster_stack, cluster, **kwargs)
+
+        for v in [
+                self.vars,
+                sd.conf.vars,
+                cluster.vars,
+                cluster_stack.vars,
+                cluster_stack.module_vars.get(self.name, {}),
+                self.module_vars,
+                extra_vars
+                ]:
+            always_merger.merge(_vars, deepcopy(v))
+
+        self.built_vars = deepcopy(_vars)
+
+        exclude_list = [
+            "vault_address",
+            "location",
+            # "ingress_class",
+            # "cluster_issuer",
+            "kubernetes_version",
+            "control_plane_endpoint",
+            "ingress_kind",
+            "mimir_url",
+            "ingress_port_http",
+            "ingress_port_https",
+            "ingress_kind",
+            # "nodes_network_ip_range",
+            # "subdomain",
+        ]
+        # exclude_list = []
+        # get all vars named from _build_vars.keys()
+
+
+        def get_vars_list() -> list[Variable]:
+            for k in _build_vars.keys():
+                v = _vars[k]
+                if k in exclude_list:
+                    continue
+                if isinstance(v, dict):
+                    var_type = "map"
+                elif isinstance(v, list):
+                    var_type = "list"
+                elif isinstance(v, bool):
+                    var_type = "bool"
+                elif isinstance(v, int):
+                    var_type = "number"
+                elif isinstance(v, str):
+                    var_type = "string"
+                else:
+                    logger.warn(f"unknown type for var {k}={v} (type={type(v)})")
+                    var_type = "string"
+                yield Variable(name=k, value=v, type=var_type)
+        
+        bk = self.backend or Backend()
+
+        # building secrets
+        vault_module_secrets = []
+        if self.secrets:
+            try:
+                resp = sd.vault.kv.v2.list_secrets(path=self.built_vars["module_secret_path"], mount_point='kv')
+            except hvac.exceptions.InvalidPath:
+                pass
+            else:
+                vault_module_secrets = resp["data"]["keys"]          
+
+
+        for s in self.secrets.values():
+            
+            status = ModuleSecretStatus.EXISTS if s.name in vault_module_secrets else ModuleSecretStatus.NOT_EXISTS
+            s.build(cluster, cluster_stack, stack, sd, module=self, status=status, **kwargs)
+        
+        # building schemas
+
+        if self.schemas:
+            self.schemas.build(cluster, cluster_stack, stack, sd, module=self, **kwargs)
+
+        if not self.module_vars and self.schemas and self.schemas.vars:
+            
+            for v in self.schemas.schemas["vars"]["properties"].keys():                
+                _v = self.built_vars.get(v, self.schemas.schemas["vars"]["properties"][v].get("default", None))
+            
+                if _v:
+                    self.module_vars[v] = _v
+        
+        ctx = dict(module=self, cluster=cluster, stackd=sd, vars=_vars, 
+            inputs=list(self.build_deps(stack=stack, cluster=cluster, module=self, deps=self.inputs, sd=sd, **kwargs)),
+            module_deps=[ d.abspath for d in list(self.build_deps(stack=stack, cluster=cluster, module=self, 
+                deps=self.deps, sd=sd, **kwargs)) ],
+            vars_list=list(get_vars_list()),
+            tf_backend=bk.build(sd, stack, self, cluster, cluster_stack, **kwargs),
+             **kwargs)
+        
+        self.write("terragrunt.root.j2", os.path.join(dest, "terragrunt.hcl"), **ctx)        
+        self.write("variables.tf.j2", os.path.join(dest, "_variables.tf"), **ctx)
+        
+        providers_data = { n: p.dict() for n, p in sd.providers.items() }
+        always_merger.merge(providers_data, self.provider_overrides)
+
+        versions = [ parse_obj_as(Provider, v) for k, v in providers_data.items() if k in self.providers ]
+
+        self.write("versions.tf.j2", os.path.join(dest, "_versions.tf"), **dict(versions=versions, **ctx))
+
+        self.write("vars.tfvars.json.j2", os.path.join(dest, "vars.tfvars.json"), **dict(versions=versions, **ctx))
+        self.write("vars.tfvars.json.j2", os.path.join(dest, "vars.ansible.json"), **dict(vars=dict(stackd=ctx["vars"])))
+        self.write("vars.tfvars.json.j2", os.path.join(dest, "vars.stackd.json"), **dict(vars=dict(_stackd=ctx["vars"])))
+
+        # logger.debug(f"{self} building module {self.name} in {stack.name} from {path} to {dest}")
+        sd.counters.modules += 1
+
+         
+class StackModel(BaseModel):
+    name: str | None = None
+    src: str | None = None
+    example_vars: dict[str, Any] = {}  
+    operations: dict[str, Operation] = {}
+    modules: dict[str, Module]
+    vars: dict[str, Any] = {}
+    backend: Backend | None = None
+    spec: SpecModel | None = None
+    stack_schema: Any = Field({}, alias="schema")
+
+
+class Stack(StackModel):
+    spec: Spec | None = None
+
+    class Config:
+        orm_mode = True
+        exclude = {'cluster'}
+   
+
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+               
+        for module_name, module in self.modules.items():
+            module.name = module_name
+        
+
+    def __str__(self) -> str:
+        return f"<{self.__class__.__name__}:{self.name}>"
+
+
+    def build(self, **kwargs):
+        for module in self.modules.values():
+            module.build(stack=self, **kwargs)
