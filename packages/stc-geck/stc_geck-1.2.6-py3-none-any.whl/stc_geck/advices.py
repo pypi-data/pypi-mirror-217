ca4defa41f6b94bd2bb162c709bfefe39f4f6e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:32 +0000

    Add package stc-geck/1.2.6/stc_geck-1.2.6-py3-none-any.whl

---
+++ packages/stc-geck/stc_geck-1.2.6-py3-none-any.whl/stc_geck/advices.py
@@ -0,0 +1,18 @@
+from aiosumma import SummaClient
+
+
+PR_TEMPORAL_RANKING_FORMULA = "original_score * fastsigm(abs(now - issued_at) / (86400 * 3) + 5, -1) * 1.96 * " \
+                              "fastsigm(iqpr(quantized_page_rank), 0.15)"
+
+
+async def get_documents_on_topic(summa_client: SummaClient, topic: str, documents: int = 20):
+    return await summa_client.search_documents([{
+        "index_alias": "nexus_science",
+        "query": {"boolean": {"subqueries": [
+            {"occur": "should", "query": {"match": {"value": topic, "default_fields": ["title", "abstract"], "field_boosts": {}}}},
+        ]}},
+        "collectors": [{"top_docs": {"limit": documents, "scorer": {"scorer": {
+            "eval_expr": PR_TEMPORAL_RANKING_FORMULA,
+        }}}}],
+        "is_fieldnorms_scoring_enabled": False,
+    }])
