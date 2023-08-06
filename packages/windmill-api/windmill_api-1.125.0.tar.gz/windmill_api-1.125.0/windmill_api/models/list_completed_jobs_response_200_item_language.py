a5d7a1872a536653e342d7917c71824239b9879c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:49 +0000

    Add package windmill-api/1.125.0/windmill_api-1.125.0.tar.gz

---
+++ packages/windmill-api/windmill_api-1.125.0.tar.gz/windmill_api-1.125.0/windmill_api/models/list_completed_jobs_response_200_item_language.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class ListCompletedJobsResponse200ItemLanguage(str, Enum):
+    PYTHON3 = "python3"
+    DENO = "deno"
+    GO = "go"
+    BASH = "bash"
+    POSTGRESQL = "postgresql"
+    NATIVETS = "nativets"
+
+    def __str__(self) -> str:
+        return str(self.value)
