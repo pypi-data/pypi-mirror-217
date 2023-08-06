274fa361cf97d220464e9752a8e4cefabeabc2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:09 +0000

    Add package atmoswing-vigicrues/1.1.5/atmoswing-vigicrues-1.1.5.tar.gz

---
+++ packages/atmoswing-vigicrues/atmoswing-vigicrues-1.1.5.tar.gz/atmoswing-vigicrues-1.1.5/src/atmoswing_vigicrues/__main__.py
@@ -0,0 +1,30 @@
+import argparse
+from datetime import datetime
+
+from atmoswing_vigicrues.controller import Controller
+
+
+def main(args=None) -> int:
+    parser = argparse.ArgumentParser(
+        description="Traite les prévisions et les exportations d'AtmoSwing pour "
+                    "le réseau Vigicrues.")
+    parser.add_argument(
+        '-c', '--config-file', type=str, required=False,
+        help="Fichier de configuration du présent module.")
+    parser.add_argument(
+        '-d', '--date', type=str, required=False,
+        help="Date pour laquelle émettre une prévision (YYYYMMDDHH).")
+
+    args = parser.parse_args(args)
+
+    controller = Controller(args)
+
+    if args.date:
+        date = datetime.strptime(args.date, '%Y%m%d%H')
+        return controller.run(date)
+
+    return controller.run()
+
+
+if __name__ == "__main__":
+    main()
