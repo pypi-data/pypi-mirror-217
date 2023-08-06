395c7628eb6fba7c948bd3699c6700217b2c1e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:59 +0000

    Add package fractal-server/1.3.3/fractal_server-1.3.3.tar.gz

---
+++ packages/fractal-server/fractal_server-1.3.3.tar.gz/fractal_server-1.3.3/fractal_server/common/tests/test_manifest.py
@@ -0,0 +1,54 @@
+import pytest
+from devtools import debug
+from pydantic.error_wrappers import ValidationError
+
+from schemas import ManifestV1
+from schemas import TaskManifestV1
+
+
+def test_ManifestV1():
+
+    task_without_args_schema = TaskManifestV1(
+        name="Task A",
+        executable="executable",
+        input_type="input_type",
+        output_type="output_type",
+        default_args={"arg": "val"},
+    )
+    task_with_args_schema = TaskManifestV1(
+        name="Task B",
+        executable="executable",
+        input_type="input_type",
+        output_type="output_type",
+        default_args={"arg": "val"},
+        args_schema={"something": "else"},
+    )
+
+    m = ManifestV1(
+        manifest_version="1",
+        task_list=[task_without_args_schema],
+    )
+    debug(m)
+    m = ManifestV1(
+        manifest_version="1",
+        has_args_schemas=False,
+        task_list=[task_without_args_schema],
+    )
+    debug(m)
+    m = ManifestV1(
+        manifest_version="1",
+        has_args_schemas=True,
+        task_list=[task_with_args_schema],
+    )
+    debug(m)
+
+    with pytest.raises(ValidationError) as e:
+        m = ManifestV1(
+            manifest_version="1",
+            task_list=[task_without_args_schema, task_with_args_schema],
+            has_args_schemas=True,
+        )
+    debug(e.value)
+
+    with pytest.raises(ValidationError):
+        ManifestV1(manifest_version="2", task_list=[task_with_args_schema])
