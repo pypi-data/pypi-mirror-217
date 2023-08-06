9fb3ab85c636a601e276ec68f9722d991724b6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:16 +0000

    Add package renovosolutions.aws-cdk-aws-sso/0.1.148/renovosolutions.aws-cdk-aws-sso-0.1.148.tar.gz

---
+++ packages/renovosolutions.aws-cdk-aws-sso/renovosolutions.aws-cdk-aws-sso-0.1.148.tar.gz/renovosolutions.aws-cdk-aws-sso-0.1.148/setup.py
@@ -0,0 +1,66 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "renovosolutions.aws-cdk-aws-sso",
+    "version": "0.1.148",
+    "description": "AWS CDK Construct Library for AWS SSO",
+    "license": "Apache-2.0",
+    "url": "https://github.com/RenovoSolutions/cdk-library-aws-sso.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Renovo Solutions<webmaster+cdk@renovo1.com>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/RenovoSolutions/cdk-library-aws-sso.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "sso",
+        "sso._jsii"
+    ],
+    "package_data": {
+        "sso._jsii": [
+            "cdk-library-aws-sso@0.1.148.jsii.tgz"
+        ],
+        "sso": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.7",
+    "install_requires": [
+        "aws-cdk-lib>=2.81.0, <3.0.0",
+        "constructs>=10.0.5, <11.0.0",
+        "jsii>=1.82.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
