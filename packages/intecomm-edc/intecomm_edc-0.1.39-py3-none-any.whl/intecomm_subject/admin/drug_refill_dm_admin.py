518335d1186c7e4cc0bba1878f76004e3dfd789c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:28 +0000

    Add package intecomm-edc/0.1.39/intecomm_edc-0.1.39-py3-none-any.whl

---
+++ packages/intecomm-edc/intecomm_edc-0.1.39-py3-none-any.whl/intecomm_subject/admin/drug_refill_dm_admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from edc_model_admin.mixins import TabularInlineMixin
+from edc_rx.modeladmin_mixins import DrugRefillAdminMixin, DrugSupplyInlineMixin
+from edc_sites.modeladmin_mixins import SiteModelAdminMixin
+
+from ..admin_site import intecomm_subject_admin
+from ..forms import DrugRefillDmForm, DrugSupplyDmForm
+from ..models import DrugRefillDm, DrugSupplyDm
+from .modeladmin_mixins import CrfModelAdmin
+
+
+class DrugSupplyDmInline(
+    SiteModelAdminMixin, DrugSupplyInlineMixin, TabularInlineMixin, admin.TabularInline
+):
+    model = DrugSupplyDm
+    form = DrugSupplyDmForm
+
+
+@admin.register(DrugRefillDm, site=intecomm_subject_admin)
+class DrugRefillDmAdmin(DrugRefillAdminMixin, CrfModelAdmin):
+    form = DrugRefillDmForm
+    inlines = [DrugSupplyDmInline]
---
+++ packages/intecomm-edc/intecomm_edc-0.1.39-py3-none-any.whl/intecomm_subject/reference_model_configs.py
@@ -0,0 +1,23 @@
+from edc_reference import site_reference_configs
+
+site_reference_configs.register_from_visit_schedule(
+    visit_models={"edc_appointment.appointment": "intecomm_subject.subjectvisit"}
+)
+
+configs = {
+    "intecomm_subject.clinicalreviewbaseline": [
+        "dm_dx",
+        "hiv_dx",
+        "htn_dx",
+    ],
+    "intecomm_subject.clinicalreview": [
+        "complications",
+        "dm_dx",
+        "hiv_dx",
+        "htn_dx",
+    ],
+    "intecomm_subject.medications": ["refill_hiv", "refill_dm", "refill_htn"],
+}
+
+for reference_name, fields in configs.items():
+    site_reference_configs.add_fields_to_config(name=reference_name, fields=fields)
