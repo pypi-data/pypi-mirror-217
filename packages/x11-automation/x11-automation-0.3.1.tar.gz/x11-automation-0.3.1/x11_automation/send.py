558b38cac08eb6306b971f57da28ed2c0f472d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:26 +0000

    Add package x11-automation/0.3.1/x11-automation-0.3.1.tar.gz

---
+++ packages/x11-automation/x11-automation-0.3.1.tar.gz/x11-automation-0.3.1/x11_automation/send.py
@@ -0,0 +1,18 @@
+from pyKey import pressKey, releaseKey, press, sendSequence, showKeys
+import re
+
+
+def send(keys_str: str, sec_per_key=0.05):
+    keys = []
+    if matches := re.findall(r'{(\w+)}', keys_str):
+        for match in matches:
+            if match == 'BACKSPACE':
+                match = 'BKSP'
+            keys.append(match.upper())
+    else:
+        keys = [x for x in keys_str]
+
+    for key in keys:
+        if key == ' ':
+            key = 'SPACEBAR'
+        press(key, sec=sec_per_key)
