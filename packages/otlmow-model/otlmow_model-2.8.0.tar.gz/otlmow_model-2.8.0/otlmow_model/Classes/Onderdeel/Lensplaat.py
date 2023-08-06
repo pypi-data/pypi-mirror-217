cc9e11f3273bfa1068946c7a368084f4ebc6cacb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:52 +0000

    Add package otlmow-model/2.8.0/otlmow_model-2.8.0.tar.gz

---
+++ packages/otlmow-model/otlmow_model-2.8.0.tar.gz/otlmow_model-2.8.0/otlmow_model/Classes/Onderdeel/Lensplaat.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from otlmow_model.Classes.ImplementatieElement.AIMObject import AIMObject
+from otlmow_model.GeometrieTypes.GeenGeometrie import GeenGeometrie
+
+
+# Generated with OTLClassCreator. To modify: extend, do not edit
+class Lensplaat(AIMObject, GeenGeometrie):
+    """Afsluitplaat van de camerakast."""
+
+    typeURI = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Lensplaat'
+    """De URI van het object volgens https://www.w3.org/2001/XMLSchema#anyURI."""
+
+    def __init__(self):
+        AIMObject.__init__(self)
+        GeenGeometrie.__init__(self)
---
+++ packages/otlmow-model/otlmow_model-2.8.0.tar.gz/otlmow_model-2.8.0/otlmow_model/Datatypes/KlUPSMerk.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+import random
+from otlmow_model.BaseClasses.KeuzelijstField import KeuzelijstField
+from otlmow_model.BaseClasses.KeuzelijstWaarde import KeuzelijstWaarde
+
+
+# Generated with OTLEnumerationCreator. To modify: extend, do not edit
+class KlUPSMerk(KeuzelijstField):
+    """Het merk van de UPS."""
+    naam = 'KlUPSMerk'
+    label = 'UPS merk'
+    objectUri = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#KlUPSMerk'
+    definition = 'Het merk van de UPS.'
+    status = 'ingebruik'
+    codelist = 'https://wegenenverkeer.data.vlaanderen.be/id/conceptscheme/KlUPSMerk'
+    options = {
+        'prostar': KeuzelijstWaarde(invulwaarde='prostar',
+                                    label='Prostar',
+                                    status='ingebruik',
+                                    definitie='Prostar',
+                                    objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlUPSMerk/prostar')
+    }
+
+    @classmethod
+    def create_dummy_data(cls):
+        return cls.create_dummy_data_keuzelijst(cls.options)
+
