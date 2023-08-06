b9958e29eadf0cbe48d9ad1fb54954aff2bd2e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:38 +0000

    Add package distributions-ex-num0/0.1/distributions_ex_num0-0.1.tar.gz

---
+++ packages/distributions-ex-num0/distributions_ex_num0-0.1.tar.gz/distributions_ex_num0-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_ex_num0',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = "HamzaTh573",
+      zip_safe=False)
