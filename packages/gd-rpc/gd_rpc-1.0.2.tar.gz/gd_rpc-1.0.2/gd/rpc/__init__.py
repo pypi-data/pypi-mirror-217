1021744cb4c2edcd622c354760c1a48f62fe0501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:37 +0000

    Add package gd-rpc/1.0.2/gd_rpc-1.0.2.tar.gz

---
+++ packages/gd-rpc/gd_rpc-1.0.2.tar.gz/gd_rpc-1.0.2/gd/rpc/__init__.py
@@ -0,0 +1,23 @@
+"""Geometry Dash Discord Rich Presence."""
+
+__description__ = "Geometry Dash Discord Rich Presence."
+__url__ = "https://github.com/nekitdev/gd.rpc"
+
+__title__ = "rpc"
+__author__ = "nekitdev"
+__license__ = "MIT"
+__version__ = "1.0.2"
+
+from gd.rpc.config import DEFAULT_CONFIG, Config, ConfigData, get_config, get_default_config
+from gd.rpc.main import rpc
+
+__all__ = (
+    # config
+    "DEFAULT_CONFIG",
+    "Config",
+    "ConfigData",
+    "get_config",
+    "get_default_config",
+    # main
+    "rpc",
+)
