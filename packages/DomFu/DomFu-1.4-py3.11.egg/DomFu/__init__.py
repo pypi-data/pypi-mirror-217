53449cb3ba1871c5b59dbfc0b0a4a984b1a88c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:29 +0000

    Add package DomFu/1.4/DomFu-1.4-py3.11.egg

---
+++ packages/DomFu/DomFu-1.4-py3.11.egg/DomFu/__init__.py
@@ -0,0 +1,10 @@
+from .modules.Spies.crtsh import fetchCrtSh
+from .modules.Spies.alienvault import fetchAlienv
+from .modules.Spies.certspot import fetchCertSpot
+from .modules.Spies.shodan import fetchShodan
+from .modules.Spies.chaos import fetchChaos
+from .modules.Spies.VirusTotal import fetchVirusTotal
+from .modules.Spies.webarchive import fetchWebArchive
+from .modules.Spies.subdomain import fetchAll as passive
+from .modules.Probe.probe import Probe
+
---
+++ packages/DomFu/DomFu-1.4-py3.11.egg/DomFu/modules/Spies/chaos.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''
+Copyright (C) 2020, DomFu Contributors.
+See the LICENSE.txt file for copying permission.
+'''
+
+from os import EX_PROTOCOL
+import requests
+
+
+def fetchChaos(domain, api):
+    '''
+    string -> list
+
+    This function queries Chaos to look for domain names.
+
+    Input  : fetchChaos("tropyl.com")
+    Output : ['tropyl.com', 'www.tropyl.com']
+    '''
+    try:
+        subdomains = []
+
+        headers = {'Authorization': api}
+
+        fetchURL = requests.get(
+            "https://dns.projectdiscovery.io/dns/{d}/subdomains".format(d=domain), headers=headers)
+
+        jsonResponse = fetchURL.json()
+        subdomainlst = jsonResponse['subdomains']
+
+        if subdomainlst != None:
+            for dom in subdomainlst:
+                dom_new = dom + '.' + domain
+                subdomains.append(dom_new)
+
+        if subdomains != None:
+            return(subdomains)
+    except:
+        pass
+
