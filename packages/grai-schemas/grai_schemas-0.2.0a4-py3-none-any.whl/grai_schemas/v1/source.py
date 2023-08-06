326431562aa5afd3d35ddde7f5f8346e848fe9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:10 +0000

    Add package grai-schemas/0.2.0a4/grai_schemas-0.2.0a4-py3-none-any.whl

---
+++ packages/grai-schemas/grai_schemas-0.2.0a4-py3-none-any.whl/grai_schemas/v1/source.py
@@ -0,0 +1,57 @@
+from typing import List, Literal, Optional, Union
+from uuid import UUID
+
+from grai_schemas.generics import GraiBaseModel
+from grai_schemas.v1.workspace import WorkspaceSpec
+from pydantic import validator
+
+
+class SourceSpec(GraiBaseModel):
+    id: Optional[UUID]
+    name: str
+    workspace: Union[UUID, WorkspaceSpec, None]
+
+    @property
+    def workspace_id(self) -> Optional[UUID]:
+        return (
+            self.workspace.id
+            if isinstance(self.workspace, WorkspaceSpec)
+            else self.workspace
+        )
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+
+class DataSourceMixin(GraiBaseModel):
+    data_source: SourceSpec
+
+
+class DataSourcesMixin(GraiBaseModel):
+    data_sources: List[Union[UUID, SourceSpec]]
+
+
+class SourceV1(GraiBaseModel):
+    type: Literal["Source"] = "Source"
+    version: Literal["v1"] = "v1"
+    spec: SourceSpec
+
+    def __hash__(self):
+        return hash(self.spec)
+
+    @classmethod
+    def from_spec(cls, spec_dict: dict) -> "SourceV1":
+        """
+
+        Args:
+            spec_dict (Dict):
+
+        Returns:
+
+        Raises:
+
+        """
+        return cls(version="v1", type="Source", spec=spec_dict)
+
+
+__all__ = ["SourceSpec", "SourceV1", "DataSourceMixin", "DataSourcesMixin"]
