a8e428b2836a7888f8ae24bb9874ff62d2d66136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:24 +0000

    Add package echion/0.1.0/echion-0.1.0.tar.gz

---
+++ packages/echion/echion-0.1.0.tar.gz/echion-0.1.0/echion/render.h
@@ -0,0 +1,28 @@
+// This file is part of "echion" which is released under MIT.
+//
+// Copyright (c) 2023 Gabriele N. Tornetta <phoenix1987@gmail.com>.
+
+#pragma once
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+#include <echion/stacks.h>
+#include <echion/threads.h>
+
+// ----------------------------------------------------------------------------
+static void render(FrameStack &stack, std::ostream &output)
+{
+    for (auto it = stack.rbegin(); it != stack.rend(); ++it)
+        (*it)->render(output);
+}
+
+// ----------------------------------------------------------------------------
+static void render_where(ThreadInfo *info, FrameStack &stack, std::ostream &output)
+{
+    output << "    🧵 " << info->name << ":" << std::endl;
+
+    for (auto it = stack.rbegin(); it != stack.rend(); ++it)
+        (*it)->render_where(output);
+}
