8eef827eb6bafd07b30e39ccf87a228127bcaaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:52 +0000

    Add package spacy-llm/0.4.0/spacy_llm-0.4.0-py2.py3-none-any.whl

---
+++ packages/spacy-llm/spacy_llm-0.4.0-py2.py3-none-any.whl/spacy_llm/tests/conftest.py
@@ -0,0 +1,43 @@
+import os
+from typing import Iterable
+
+import pytest
+
+from spacy_llm.registry.util import registry
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--external",
+        action="store_true",
+        default=bool(int(os.environ.get("TEST_EXTERNAL", 0))),
+        help="include tests that connects to third-party API",
+    )
+
+
+def pytest_runtest_setup(item):
+    def getopt(opt):
+        return item.config.getoption(f"--{opt}", False)
+
+    # Integration of boolean flags
+    for opt in ["external"]:
+        if opt in item.keywords and not getopt(opt):
+            pytest.skip(f"need --{opt} option to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--external"):
+        return
+
+    skip_external = pytest.mark.skip(reason="need --external option to run")
+    for item in items:
+        if "external" in item.keywords:
+            item.add_marker(skip_external)
+
+
+@registry.llm_models("test.NoOpModel.v1")
+def noop_factory(output: str = ""):
+    def noop(prompts: Iterable[str]) -> Iterable[str]:
+        return [output] * len(list(prompts))
+
+    return noop
