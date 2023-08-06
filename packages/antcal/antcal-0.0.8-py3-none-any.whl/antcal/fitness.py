6da0935bb5ae14b6196fe361ee49dfa5244b3edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:51 +0000

    Add package antcal/0.0.8/antcal-0.0.8-py3-none-any.whl

---
+++ packages/antcal/antcal-0.0.8-py3-none-any.whl/antcal/fitness.py
@@ -0,0 +1,10 @@
+"""Custom fitness functions.
+Link: https://antenna.atlanswer.com/optimization/implementation/fitness
+"""
+
+import numpy as np
+
+
+def target_scattering_parameter_point_db(s: np.ndarray, point: int, target: float):
+    """Return distance to the target at the specified point."""
+    ...
